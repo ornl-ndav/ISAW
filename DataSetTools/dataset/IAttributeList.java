efced843abeb27566dff7441f0f82f676e0a5fb9
chatter <chatter@9b3b9736-f711-0410-b1b1-9612de3a1827>
1999-11-16 15:40:00 +0000

    Crosshair cursor replaces the handcursor in the original file.

--- DataSetTools/dataset/IAttributeList.java
+++ DataSetTools/dataset/IAttributeList.java
@@ -1,22 +1,44 @@
 package  DataSetTools.dataset;
 
 
+
+
+
 import java.io.*;
 
 
+
+
+
 public interface IAttributeList 
+
 {
 
+
+
   /**
+
    *  Get a copy of the list of attributes for an object.
+
    */
+
   public AttributeList getAttributeList();
 
+
+
   /**
+
    *  Set the list of attributes for an object to be a COPY of the 
+
    *  specified list of attributes.
+
    */
+
   public void setAttributeList( AttributeList attr_list );
 
+
+
 } 
+
+
 
--- DataSetTools/dataset/OperationLog.java
+++ DataSetTools/dataset/OperationLog.java
@@ -1,73 +1,146 @@
 /*
+
  * @(#)OperationLog.java     1.0  98/08/03  Dennis Mikkelson
+
  *
+
  */
+
+
 
 package  DataSetTools.dataset;
 
+
+
 import java.util.Vector;
+
 import java.io.*;
 
+
+
 public class OperationLog implements Serializable
+
 {
+
   private Vector log;
 
+
+
   public OperationLog( )
+
   {
+
     this.log = new Vector();
+
   }
+
+
 
   public void addEntry( String str )
+
   {
+
     log.addElement( str ); 
+
   }
+
+
 
   public int numEntries( )
+
   {
+
     return log.size();
+
   }
 
+
+
   public String getEntryAt( int i )
+
   {
+
     return (String)(this.log.elementAt(i));
+
   }
+
+
+
 
 
   public String toString()           // for now just return first character
+
   {                                  // of each string in the log
+
     String  temp = new String( );
 
+
+
     for ( int i = 0; i < this.log.size(); i++ )
+
       temp = temp + ((String)this.log.elementAt( i )).charAt(0) + "\n";
 
+
+
     return temp;
+
   }
+
+
 
   public Object clone( )
+
   {
+
     OperationLog  new_log = new OperationLog();
 
+
+
     for ( int i = 0; i < this.numEntries(); i++ )
+
     {
+
       new_log.addEntry( this.getEntryAt(i) );
+
     }
 
+
+
     return new_log;
+
   }
+
+
 
   public static void main( String argv[] )
+
   {
+
     OperationLog test_log = new OperationLog();
 
+
+
     for ( int i = 0; i < 5; i++ )
+
       test_log.addEntry( i + " new entry, " );
 
+
+
     System.out.println( "test_log" );
+
     System.out.println( test_log );
 
+
+
     OperationLog new_log = (OperationLog) test_log.clone();
+
     new_log.addEntry( "Entry added to new log" );
+
     System.out.println( "new_log" );
+
     System.out.println( new_log );
+
   }
+
 }
+
