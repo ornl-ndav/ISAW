8df52bbe832e5b04256a69c9215b4ac6fe10a389
Dennis Mikkelson <mikkelsond@uwstout.edu>
2000-07-10 22:49:00 +0000

    New classes to deal with peaks/peak fitting

---
+++ DataSetTools/peak/IPeak.java
@@ -0,0 +1,199 @@
+/*
+ * @(#)IPeak.java     0.2  2000/03/06  Dennis Mikkelson
+ *
+ * ---------------------------------------------------------------------------
+ *  $Log$
+ *  Revision 1.1  2000/07/10 22:48:59  dennis
+ *  New classes to deal with peaks/peak fitting
+ *
+ *  Revision 1.5  2000/06/15 16:49:20  dennis
+ *  improved documentation
+ *
+ *  Revision 1.4  2000/06/14 19:25:18  dennis
+ *  *** empty log message ***
+ *
+ *  Revision 1.3  2000/06/13 20:39:15  dennis
+ *  added setEvaluationMode() to select between Peak, Background and
+ *  Peak plus Background
+ *
+ *  Revision 1.2  2000/05/11 16:09:42  dennis
+ *  added RCS logging
+ *
+ *
+ */
+
+package  DataSetTools.peak;
+
+import java.io.*;
+import DataSetTools.dataset.*;
+
+/**
+ * This class forms the abstract base class for sampled and model "peaks"
+ * in neutron scattering data.
+ *
+ * @see  ModelPeak 
+ * @see  HistogramDataPeak
+ * @see  Data 
+ * @see  XScale
+ */
+
+public interface IPeak
+{
+  public static final int  HISTOGRAM_DATA_PEAK  = 0;
+  public static final int  GAUSSIAN_PEAK        = 1;
+  public static final int  LORENTZIAN_PEAK      = 2;
+
+  public static final int  PEAK_ONLY            = 0;
+  public static final int  BACKGROUND_ONLY      = 1;
+  public static final int  PEAK_PLUS_BACKGROUND = 2;
+  /**
+   *  Return the integer code for the peak shape for this peak.  
+   *  For a "ModelPeak" this returns the theoretical peak shape being used.  
+   *  For a "DataPeak" this returns the peak shape that is currently being 
+   *  used to fit the data. 
+   */
+  abstract public int getShape();
+
+  /**
+   *  Get the maximum height of the peak.
+   */
+  abstract public float getAmplitude();
+
+  /**
+   *  Get the position of the peak "center".
+   */
+  abstract public float getPosition();
+
+  /**
+   *  Get the "Full Width at Half Max" of the peak.
+   */
+  abstract public float getFWHM();
+
+  /**
+   *  Return the size of the interval over which this peak is to be used.  
+   *  The size is expressed in terms of the FWHM of the peak.  In particular,
+   *  if this function returns 5 then the peak is assumed to be zero outside
+   *  of an interval of length 5*FWHM centered at the peak position.
+   */
+  abstract public float getExtent_factor();
+
+  /**
+   *  Set the size of the interval over which this peak is to be used.
+   *  The size is expressed in terms of the FWHM of the peak.  In particular, 
+   *  if 5 is specified as the factor then the peak is assumed to be zero 
+   *  outside of an interval of length 5*FWHM centered at the peak position.
+   */
+  abstract public void  setExtent_factor( float factor );
+  
+  /**
+   *  Get the slope of the linear background for this peak.
+   */
+  abstract public float getSlope();
+
+  /**
+   *  Get the y-intercept of the linear background for this peak.
+   */
+  abstract public float getIntercept();
+
+
+  /**
+   *  Choose between evaluating the peak only, background only or peak plus 
+   *  background when calling getValue, evaluating area and moments and getting
+   *  the peak data.
+   *
+   *  @param  mode   the mode to use, must be one of the values:
+   *
+   *                    PEAK_ONLY 
+   *                    BACKGROUND_ONLY 
+   *                    PEAK_PLUS_BACKGROUND 
+   */
+  abstract public void setEvaluationMode( int mode );
+
+
+  /**
+   *  Evaluate the y-value of the peak at the specified x-value.
+   *
+   *  @param  x  the point at which the peak is to be evaluated
+   *
+   *  @return the value of the peak, background or peak plus background 
+   *          at the specified point, depending on the evaluation mode.
+   *          (see method: setEvaluationMode()) 
+   */
+  abstract public double getValue( double x );
+
+  /**
+   *  Return a Data block that contains the y-values of the peak at a 
+   *  default set of x-values.
+   *
+   *  @return a reference to a Data block that contains the y-values of the
+   *  peak only, the background or the peak plus background (depending on
+   *  the evaluation mode) at a default set of x-values.
+   *  (see method: setEvaluationMode()) 
+   *
+   */
+  abstract public Data  PeakData( );
+
+  /**
+   *  Return a Data block that contains the y-values of the peak at the 
+   *  set of x-values given by the XScale interval.
+   *
+   *  @param interval  specifies the set of x-values at which the peak is to
+   *                   be evaluated.
+   *
+   *  @return a reference to a Data block that contains the y-values of the
+   *  peak only, the background or the peak plus background (depending on
+   *  the evaluation mode) at the set of x-values given by the XScale interval.
+   *  (see method: setEvaluationMode()) 
+   *
+   */
+  abstract public Data  PeakData( XScale interval );
+
+  /**
+   *  Calculate the area under the curve over the interval [a,b].
+   *
+   *  @param  a        Left endpoint of [a,b].
+   *  @param  b        Right endpoint of [a,b].
+   *
+   *  @return The area under the peak only, the background or the peak plus
+   *          background depending on the evaluation mode that has been
+   *          set.   (see method: setEvaluationMode()) 
+   *
+   */
+  abstract public float Area( float a, float b );
+
+  /**
+   *  Calculate the "nth" moment of the peak about the peak position over the 
+   *  interval [a,b].  The function  f(x)*(x-position)**n is
+   *  integrated over the specified interval, where "f()" is the peak function
+   *  and "position" is the postion of the peak.
+   *
+   *  @param  a        Left endpoint of [a,b].
+   *  @param  b        Right endpoint of [a,b].
+   *  @param  n        specifies the order of the moment to calculate. If 
+   *                   n <= 0, the area is returned.  
+   *
+   *  @return The moment of the peak only, the background or the peak plus
+   *          background depending on the evaluation mode that has been
+   *          set.   (see method: setEvaluationMode()) 
+   */
+  abstract public float Moment( float a, float b, int n ); 
+
+  /**
+   *  Calculate the "nth" moment of the peak about the specified center point
+   *  over the interval [a,b].  The function  f(x)*(x-center)**n is
+   *  integrated over the specified interval, where "f()" is the peak function
+   *  and "center" is the specified center point.
+   *
+   *  @param  a        Left endpoint of [a,b].
+   *  @param  b        Right endpoint of [a,b].
+   *  @param  center   the center point for the moment calculation. 
+   *  @param  n        specifies the order of the moment to calculate. If 
+   *                   n <= 0, the area is returned.
+   *  @return The moment of the peak only, the background or the peak plus
+   *          background depending on the evaluation mode that has been
+   *          set. (see method: setEvaluationMode()) 
+   *
+   */
+  abstract public float Moment( float a, float b, float center, int n ); 
+
+}
