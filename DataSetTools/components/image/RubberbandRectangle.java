7ba08a9f95386605cf3825e16450ce2d56a1c356
Dennis Mikkelson <mikkelsond@uwstout.edu>
2000-07-10 22:11:53 +0000

    7/10/2000 version, many changes and improvements

--- DataSetTools/components/image/RubberbandRectangle.java
+++ DataSetTools/components/image/RubberbandRectangle.java
@@ -1,6 +1,20 @@
+/*
+ * @(#) RubberbandRectangle.java  1.0    1998/07/29   Dennis Mikkelson
+ *
+ * ---------------------------------------------------------------------------   *  $Log$
+ * ---------------------------------------------------------------------------   *  Revision 1.2  2000/07/10 22:11:53  dennis
+ * ---------------------------------------------------------------------------   *  7/10/2000 version, many changes and improvements
+ * ---------------------------------------------------------------------------   *
+ * ---------------------------------------------------------------------------   *  Revision 1.3  2000/05/11 16:53:19  dennis
+ * ---------------------------------------------------------------------------   *  Added RCS logging
+ * ---------------------------------------------------------------------------   *
+ *
+ */
+
 package DataSetTools.components.image;
 
 import javax.swing.*;
+import java.io.*;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -10,9 +24,10 @@ import java.awt.Rectangle;
  * @version 1.00, 12/27/95
  * @author  David Geary
  * @see     Rubberband
- * @see     gjt.test.RubberbandTest
  */
-public class RubberbandRectangle extends Rubberband {
+public class RubberbandRectangle extends    Rubberband 
+                                 implements Serializable
+{
     public RubberbandRectangle(JPanel component) {
         super(component);
     }
