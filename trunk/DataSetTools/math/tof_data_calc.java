6d8e03f784a11fb23c234fe4c4589633ebbf84b5
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2000-07-19 21:54:50 +0000

    Added getEfficiencyFactor() to calculate detector efficiency and flight path correction as a function of neutron speed

--- trunk/DataSetTools/math/tof_data_calc.java
+++ trunk/DataSetTools/math/tof_data_calc.java
@@ -64,4 +64,79 @@ public static final float  MONITOR_PEAK_EXTENT_FACTOR = 8.5f;
   }
 
 
+
+/**
+ *  Calculate detector efficiency factor and correction to flight path for
+ *  a detector, based on the neutron final speed.  This code was adapted from
+ *  Chun Loong's FORTRAN version by Dongfeng Chen.
+ *
+ *  @param  finalSpeed    The speed of the detected neutron in meters per 
+ *                        micro-second.
+ *  @param  detectorType  Integer code for the detector type__________________
+ *
+ *  @return an array with two values, the detector efficiency factor at the
+ *          specified final speed and a flight path correction to be added 
+ *          to the flight path for the specified final speed.
+ */
+ public static float[] getEfficiencyFactor(float finalSpeed, 
+                                           int   detectorType      )
+ {
+    finalSpeed *= 1000000;   // Originally this function used finalSpeed in
+                             // meters per second.
+
+                       // SIGMA(detectorType) is macroscopic absorption 
+                       // cross section at 2200 m/sec
+
+    float[] SIGMA = { 0.80017f, 0.80017f, 1.0669f, 1.0669f, 0.80017f };
+
+    float R    = 1.27f*(1.0f-0.063f);
+    float DR   = R/100.0f;
+    float ABS  = 0.0f;
+
+    float FPCORR = 0.0f;
+
+    float Y     = 0.0f;
+    float X     = 0.0f;
+    float SIGX  = 0.0f;
+    float TRANS = 0.0f;
+
+    for (int i=0; i<100; i++)
+    {
+      Y      = DR * (i+1-0.5f);
+      X      = (float)Math.sqrt( R * R - Y * Y );
+      SIGX   = X * SIGMA[ detectorType ] * 2200.0f / finalSpeed;
+      TRANS  = (float)Math.exp( -2.0f*SIGX );
+      ABS    = ABS + ( 1.0f-TRANS );
+      FPCORR = FPCORR - X *( 1.0f-1.0f/SIGX + (1.0f+1.0f/SIGX) * TRANS );
+     }
+
+     float [] result = new float[2];
+
+     result[0] = ABS / 100.0f;           // the detector efficiency EFF
+     result[1] = FPCORR / ABS / 100.0f;  // the flight path correction FPCORR
+
+     return result;
+    }
+
+/* -------------------------------------------------------------------------
+ *
+ *  main program for test purposes only
+ *
+ */
+  public static void main( String[] args )
+  {
+    float final_speed;
+    float result[];
+    for ( int i = 0; i < 50; i++ )
+    {
+      final_speed = 200 + i * 200;
+      final_speed /= 1000000;
+      System.out.print("speed(m/us) = " + final_speed + "  " );
+      System.out.print("E(meV) = " + 
+                        tof_calc.EnergyFromVelocity(final_speed));
+      result = getEfficiencyFactor( final_speed, 1 );
+      System.out.println(" EFF = " + result[0] + "  FPCORR = " + result[1] );
+    }
+  }
+
 }
