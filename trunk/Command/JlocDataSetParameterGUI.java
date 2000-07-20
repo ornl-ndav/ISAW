a95f45cc64f0044a6aaf72c106040580d5c6be6a
chatter <chatter@9b3b9736-f711-0410-b1b1-9612de3a1827>
2000-07-20 15:53:44 +0000

    Initial commit:   Creates a Pane to let users select a Data Set from a list(constructor) of data sets

---
+++ trunk/Command/JlocDataSetParameterGUI.java
@@ -0,0 +1,79 @@
+/*
+ * @(#)JlocDataSetParameterGUI.java     1.0  99/09/02  Alok Chatterjee
+ *
+ * 1.0  99/09/02  Added the comments and made this a part of package IsawGUI
+ * 
+ */
+ 
+package Command;
+
+import javax.swing.*;
+//import javax.swing.*;
+//import DataSetTools.*;
+import DataSetTools.dataset.*;
+import DataSetTools.operator.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.tree.*;
+import javax.swing.JTree.*;
+import Command.*;
+import IsawGUI.*;
+
+/**
+ * The main class for ISAW. It is the GUI that ties together the DataSetTools, IPNS, 
+ * ChopTools and graph packages.
+ *
+ * @version 1.0  
+ */
+
+public class JlocDataSetParameterGUI extends JParameterGUI
+{
+    private JPanel segment;
+    private JTextArea dsText;
+    private JComboBox combobox;
+    public JlocDataSetParameterGUI(Parameter parameter , DataSet ds[])
+    { 
+       super(parameter);
+       
+        combobox = new JComboBox();
+    
+        //combobox.addItem(ds);
+        
+       
+  
+        if( ds == null)
+	  {
+           System.out.println("Global data set is null");
+          }          
+        else if( ds != null)
+        for( int i=0; i< ds.length; i++)
+	  {              
+               combobox.addItem(ds[i]);
+                     
+          }
+        
+        segment = new JPanel();
+        segment.setLayout(new GridLayout(1,2));
+       
+        segment.add(new JLabel(parameter.getName()));
+        segment.add(combobox);
+        
+       
+    }
+
+    public JPanel getGUISegment()
+    {
+        return segment;
+        
+    }
+
+    public Parameter getParameter()
+    { Parameter P;
+       
+       DataSet ds = (DataSet)combobox.getSelectedItem();
+       P = getParameter();
+        P.setValue(ds);
+        return P;
+    }
+
+}
