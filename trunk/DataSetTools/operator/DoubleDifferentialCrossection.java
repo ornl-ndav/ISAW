e9d4dd235c3496f91c7e17900d30e23a2d7e7311
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2000-07-25 16:39:37 +0000

    Added monitor 1 peak area as a parameter and finished the calculation, including compensation for the scattered energy increment and the flux

--- trunk/DataSetTools/operator/DoubleDifferentialCrossection.java
+++ trunk/DataSetTools/operator/DoubleDifferentialCrossection.java
@@ -2,6 +2,10 @@
  * @(#)DoubleDifferentialCrossection.java   0.1  2000/07/25   Dennis Mikkelson
  *             
  *  $Log$
+ *  Revision 1.3  2000/07/25 16:39:37  dennis
+ *  Added monitor 1 peak area as a parameter and finished the calculation,
+ *  including compensation for the scattered energy increment and the flux
+ *
  *  Revision 1.2  2000/07/24 21:14:06  dennis
  *  Initial version, partially implemented
  *
@@ -17,11 +21,12 @@ import  java.io.*;
 import  java.util.Vector;
 import  DataSetTools.dataset.*;
 import  DataSetTools.util.*;
+import  DataSetTools.math.*;
 
 /**
   *  Compute the double differential crossection for a time-of-flight 
-  *  spectrometer DataSet based on a sample run, background run and the
-  *  number of atoms in the sample.
+  *  spectrometer DataSet based on a sample with background subtracted,
+  *  the area of the peak in monitor 1 and the number of atoms in the sample.
   *
   *  @see DataSetOperator
   *  @see Operator
@@ -52,6 +57,7 @@ public class DoubleDifferentialCrossection extends    DataSetOperator
    *
    *  @param  ds               The sample DataSet for which the double 
    *                           differential crossection is to be calculated 
+   *  @param  peak_area        The area of the peak in monitor 1.
    *  @param  atoms            The number of "scattering units" in the sample
    *                           exposed to the beam times 10 ** -24.
    *  @param  make_new_ds Flag that determines whether a new DataSet is
@@ -60,17 +66,20 @@ public class DoubleDifferentialCrossection extends    DataSetOperator
    */
 
   public DoubleDifferentialCrossection( DataSet    ds,
+                                        float      peak_area,
                                         float      atoms,
                                         boolean    make_new_ds )
   {
     this();                         // do the default constructor, then set
                                     // the parameter value(s) by altering a
                                     // reference to each of the parameters
-
     Parameter parameter = getParameter( 0 );
-    parameter.setValue( new Float(atoms) );
+    parameter.setValue( new Float(peak_area) );
 
     parameter = getParameter( 1 );
+    parameter.setValue( new Float(atoms) );
+
+    parameter = getParameter( 2 );
     parameter.setValue( new Boolean( make_new_ds ) );
 
     setDataSet( ds );               // record reference to the DataSet that
@@ -95,8 +104,11 @@ public class DoubleDifferentialCrossection extends    DataSetOperator
   {
     parameters = new Vector();  // must do this to clear any old parameters
 
-    Parameter parameter = new Parameter( "Atoms in sample (times 10**-24)",
-                                new Float( 1.0 ) ); 
+    Parameter parameter = new Parameter("Monitor 1 Peak Area",
+                                         new Float(100000) );
+    addParameter( parameter );
+
+    parameter = new Parameter("Atoms in sample (times 10**-24)",new Float(1.0));
     addParameter( parameter );
 
     parameter = new Parameter( "Create new DataSet?", new Boolean(false) );
@@ -111,8 +123,9 @@ public class DoubleDifferentialCrossection extends    DataSetOperator
                                                    // get the current data set
     DataSet ds  = getDataSet();
                                                     // get the parameters
-    float   atoms      = ((Float)(getParameter(0).getValue()) ).floatValue();
-    boolean make_new_ds=((Boolean)getParameter(1).getValue()).booleanValue();
+    float   peak_area  = ((Float)(getParameter(0).getValue()) ).floatValue();
+    float   atoms      = ((Float)(getParameter(1).getValue()) ).floatValue();
+    boolean make_new_ds=((Boolean)getParameter(2).getValue()).booleanValue();
 
     if ( atoms <= 0 )
       return new ErrorString("ERROR: Number of atoms must be greater than 0");
@@ -126,27 +139,86 @@ public class DoubleDifferentialCrossection extends    DataSetOperator
     new_ds.addLog_entry( "Calculated Double Differential Cross-section" );
 
     AttributeList attr_list;
-    Float Float_val;
-    float solid_angle,
-          scale_factor;
-    int   num_data = ds.getNum_entries();
+    Float   Float_val;
+    Integer Int_val;
+    float   solid_angle,
+            scale_factor;
+
+    DetectorPosition position;
+    float spherical_coords[];
+
+    float x_vals[],
+          y_vals[],
+          corr[],
+          tof,
+          delta_tof,
+          def,
+          eff,
+          flux;
+    float energy_in,
+          velocity_in,
+          mon_1_area = 0.00516f;
+    int   num_data,
+          num_pulses;
     Data  data,
-         new_data;
-    for ( int i = 0; i < num_data; i++ )
+          new_data;
+
+    num_data = ds.getNum_entries();
+    for ( int index = 0; index < num_data; index++ )
     {
-      data = ds.getData_entry( i );
+      data = ds.getData_entry( index );
                                                // get the needed attributes
-      attr_list = data.getAttributeList();
-      Float_val = (Float)attr_list.getAttributeValue(Attribute.SOLID_ANGLE);
+      attr_list   = data.getAttributeList();
+
+      Float_val   = (Float)attr_list.getAttributeValue(Attribute.SOLID_ANGLE);
       solid_angle = Float_val.floatValue();
 
-      scale_factor = 1.0f / (solid_angle * atoms);
+      Float_val   = (Float)attr_list.getAttributeValue(Attribute.ENERGY_IN);
+      energy_in   = Float_val.floatValue();
+
+      Int_val     = (Integer)
+                    attr_list.getAttributeValue(Attribute.NUMBER_OF_PULSES);
+      num_pulses  = Int_val.intValue();
+
+      System.out.println("ID = " + data.getGroup_ID()+
+                        " SA = " + Format.real( solid_angle, 12, 6 ));
+      position = (DetectorPosition)
+                  attr_list.getAttributeValue(Attribute.DETECTOR_POS);
+      spherical_coords = position.getSphericalCoords();
+
+                                        // need velocity in m/sec not m/us
+      velocity_in = tof_calc.VelocityFromEnergy( energy_in ) * 1000000;
+      flux = peak_area * velocity_in /
+             ( mon_1_area * (num_pulses/30.0f) * 0.0022f );
+            
+                                        // compensate for solid angle subtended
+                                        // the number of atoms in the, sample
+                                        // and the incident beam flux 
+      scale_factor = 1000.0f / (solid_angle * atoms * flux);
+
       new_data = data.multiply( scale_factor );
+                                            
+                                        // compensate for detector efficiency
+                                        // as a function of neutron velocity   
+      y_vals = new_data.getY_values();
+      x_vals = new_data.getX_scale().getXs();
+
+      
+
+      for ( int i = 0; i < y_vals.length; i++ )
+      {
+        tof = (x_vals[i]+x_vals[i+1])/2;
+        delta_tof = x_vals[i+1] - x_vals[i];
+        corr = tof_data_calc.getEfficiencyFactor( spherical_coords[0]/tof, 1 );
+        eff = corr[0];
+        def = 2*tof_calc.Energy(spherical_coords[0]+corr[1], tof)*delta_tof/tof;
+        y_vals[i] /= (def*eff);
+      }
 
       if ( make_new_ds )
         new_ds.addData_entry( new_data );
       else
-        new_ds.replaceData_entry( new_data, i );
+        new_ds.replaceData_entry( new_data, index );
     }
 
     if ( make_new_ds )
