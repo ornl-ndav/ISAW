5128824ab7fbfe431cf2c587db4178715d50512e
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2000-10-03 22:21:16 +0000

    New operator for HRMECS

---
+++ trunk/DataSetTools/operator/SpectrometerImaginaryGeneralizedSusceptibility.java
@@ -0,0 +1,273 @@
+/*
+ * @(#)SpectrometerImaginaryGeneralizedSusceptibility.java   
+ *     1.1  2000/08/09   Dongfeng Chen Dennis Mikkelson
+ *             
+ *  $Log$
+ *  Revision 1.1  2000/10/03 22:21:14  dennis
+ *  New operator for HRMECS
+ *
+ *
+ *  Revision 1.1  2000/08/09 17:53:00  Dongfeng dennis
+ *  Initial version of Imaginary Part of Generalized Susceptibility
+ *
+ */
+
+package DataSetTools.operator;
+
+import  java.io.*;
+import  java.util.Vector;
+import  DataSetTools.dataset.*;
+import  DataSetTools.util.*;
+import  DataSetTools.math.*;
+
+/**
+  *  Compute the Generalized Energy Distribution Function for a direct 
+  *  geometry spectrometer based on the result of applying the scattering 
+  *  function operator.  
+  *
+  *  @see SpectrometerScatteringFunction
+  *  @see DoubleDifferentialCrossection 
+  *  @see DataSetOperator
+  *  @see Operator
+  */
+
+public class SpectrometerImaginaryGeneralizedSusceptibility 
+             extends    DataSetOperator 
+             implements Serializable
+{
+  /* ------------------------ DEFAULT CONSTRUCTOR -------------------------- */
+  /**
+   * Construct an operator with a default parameter list.  If this
+   * constructor is used, the operator must be subsequently added to the
+   * list of operators of a particular DataSet.  Also, meaningful values for
+   * the parameters should be set ( using a GUI ) before calling getResult()
+   * to apply the operator to the DataSet this operator was added to.
+   */
+
+  public SpectrometerImaginaryGeneralizedSusceptibility( )
+  {
+    super( "Imaginary Part of Generalized Susceptibility" );
+  }
+
+  /* ---------------------- FULL CONSTRUCTOR ---------------------------- */
+  /**
+   *  Construct an operator to calculate the Scattering Function
+   *  for a spectrometer DataSet.  It is assumed that the 
+   *  DoubleDifferentialCrossection operator has already been applied.
+   *
+   *  @param  ds               The sample DataSet for which the scattering 
+   *                           function is to be calculated 
+   *  @param  temperature      The sample temperature
+   *  @param  min_E            The minimum energy loss value to be binned
+   *  @param  max_E            The maximum energy loss value to be binned
+   *  @param  num_E            The number of "bins" to be used between min_E and
+   *                           max_E 
+   *  @param  make_new_ds Flag that determines whether a new DataSet is
+   *                           constructed, or the Data blocks of the original 
+   *                           DataSet are just altered.
+   */
+
+  public SpectrometerImaginaryGeneralizedSusceptibility( 
+                                         DataSet    ds,
+                                         float      temperature, 
+                                         float      min_X,
+                                         float      max_X,
+                                         int        num_X,
+                                         boolean    make_new_ds )
+  {
+    this();                         // do the default constructor, then set
+                                    // the parameter value(s) by altering a
+                                    // reference to each of the parameters
+    Parameter parameter = getParameter( 0 );
+    parameter.setValue( new Float(temperature) );
+   
+    parameter = getParameter( 1 );
+    parameter.setValue( new Float( min_X ) );
+
+    parameter = getParameter( 2 );
+    parameter.setValue( new Float( max_X ) );
+
+    parameter = getParameter( 3 );
+    parameter.setValue( new Integer( num_X ) );
+
+    parameter = getParameter( 4 );
+    parameter.setValue( new Boolean( make_new_ds ) );
+
+    setDataSet( ds );               // record reference to the DataSet that
+                                    // this operator should operate on
+  }
+
+  /* ---------------------------- getCommand ------------------------------- */
+  /**
+   * Returns the abbreviated command string for this operator.
+   */
+   public String getCommand()
+   {
+     return "ImChi";
+   }
+
+
+ /* -------------------------- setDefaultParmeters ------------------------- */
+ /**
+  *  Set the parameters to default values.
+  */
+  public void setDefaultParameters()
+  {
+    parameters = new Vector();  // must do this to clear any old parameters
+
+    Parameter parameter = new Parameter("Sample temperature (K)",
+                                         new Float(5.0) );
+    addParameter( parameter );
+
+    
+    parameter = new Parameter( "Min Energy Loss Value", new Float(-1000) );
+    addParameter( parameter );
+
+    parameter = new Parameter("Max Energy Loss Value", new Float(1000) );
+    addParameter( parameter );
+
+    parameter = new Parameter( "Num of Step of Energy Loss", new Integer( 200 ) );
+    addParameter( parameter );
+    
+    parameter = new Parameter( "Create new DataSet?", new Boolean(false) );
+    addParameter( parameter );
+  }
+
+
+  /* ---------------------------- getResult ------------------------------- */
+
+  public Object getResult()
+  {       
+    System.out.println("Start ImChi_fn_ds now!");
+    
+    final float XKCON   = 0.086165f; // conversion factor 
+
+                                     // get the current data set
+    DataSet ds     = getDataSet();
+                                                    // get the parameters
+    float   temperature = ((Float)(getParameter(0).getValue()) ).floatValue();
+    float   min_X       = ((Float)(getParameter(1).getValue()) ).floatValue();
+    float   max_X       = ((Float)(getParameter(2).getValue()) ).floatValue();
+    int     num_X       = ((Integer)(getParameter(3).getValue()) ).intValue();
+    boolean make_new_ds =((Boolean)getParameter(4).getValue()).booleanValue();
+    
+    // SpectrometerTofToEnergyLoss will create a new DataSet, so we don't need
+    // to clone it first.
+    Operator op = new SpectrometerTofToEnergyLoss( ds,
+                                                   min_X,
+                                                   max_X,
+                                                   0    );
+    DataSet new_ds =(DataSet)op.getResult();
+      
+    // ConvertHistogramToFunction for the new energy loss DataSet
+
+    System.out.println("\n\nConvertHistogramToFunction start...");
+    op = new ConvertHistogramToFunction( new_ds, false, false );
+    op.getResult();
+      
+    // ResampleDataSet for the new energy loss DataSet
+    op = new ResampleDataSet( new_ds, min_X, max_X, num_X, false );
+    op.getResult();
+      
+    //viewmanager = new ViewManager(new_ds, IViewManager.IMAGE);
+
+    if ( temperature <= 0)
+      return new ErrorString(
+                "ERROR: temperature must be greater than 0");
+
+    new_ds.addLog_entry("Calculated Imaginary Part of Generalized Susceptibility");
+
+    Float   Float_val;
+
+
+    float x_vals[],
+          y_vals[],
+          new_y_vals[],
+          new_errors[],
+          energy_transfer;
+    float xkt,
+          ebykt,
+          boseinv;
+    int   num_data;
+    Data  data,
+          conversion_data,
+          new_data;
+    num_data = new_ds.getNum_entries();
+    for ( int index = 0; index < num_data; index++ )
+    {
+      data = new_ds.getData_entry( index );
+
+      y_vals = data.getY_values();
+      x_vals = data.getX_scale().getXs();
+
+      int num_y = y_vals.length;
+      new_y_vals = new float[ num_y ];
+      new_errors = new float[ num_y ];
+
+      for ( int i = 0; i < (y_vals.length-1); i++ )
+      {
+        if ( x_vals.length > y_vals.length )  // histogram
+          energy_transfer = (x_vals[i]+x_vals[i+1])/2;
+        else                                  // function
+          energy_transfer = x_vals[i];
+
+        
+        xkt=XKCON*temperature;
+        ebykt=energy_transfer/xkt;
+        boseinv=0.0f;//Variable have to be initialized first.
+        if ( ebykt >=0) boseinv=1.0f - (float)Math.exp(-ebykt);//1/[n(E)+1] for E>0
+        else if( ebykt <0) boseinv=(float)Math.exp(-ebykt)-1.0f;//1/n(E) for E<0
+       // if(boseinv>1000.0f)boseinv=1000.0f; //add by Dongfeng Chen for low temperature
+        
+        new_y_vals[i] =boseinv;
+         /*        
+        if(index ==( 0))
+        System.out.println("new_y_vals[i]="+ new_y_vals[i]+"\n"+
+                           "energy_transfer="+ energy_transfer+"\n"+
+                           "temperature=" + temperature+"\n"+
+                           "XKCON=" + XKCON+"\n"+
+                           "xkt=" + xkt+"\n"+
+                           "ebykt=" + ebykt+"\n"+
+                           "boseinv=" + boseinv+"\n");                    
+           //*/                
+      }
+
+      conversion_data = new Data( data.getX_scale(),
+                                  new_y_vals,
+                                  new_errors,
+                                  data.getGroup_ID() );
+    
+      //now multiply the spectrum by the conversion_data;
+      new_data = data.multiply( conversion_data );
+
+      new_ds.replaceData_entry( new_data, index );
+    }
+
+    if ( make_new_ds )
+      return new_ds;
+    else
+    {
+      ds.copy( new_ds );
+      ds.notifyIObservers( IObserver.DATA_CHANGED );
+      return new String( "Calculated Imaginary Generalized Susceptibility" );
+    }
+  }  
+
+  /* ------------------------------ clone ------------------------------- */
+  /**
+   * Get a copy of the current Operator.  The list of parameters 
+   * and the reference to the DataSet to which it applies is copied.
+   */
+  public Object clone()
+  {
+    SpectrometerImaginaryGeneralizedSusceptibility new_op = 
+                  new SpectrometerImaginaryGeneralizedSusceptibility( );
+                                                 // copy the data set associated
+                                                 // with this operator
+    new_op.setDataSet( this.getDataSet() );
+    new_op.CopyParametersFrom( this );
+
+    return new_op;
+  }
+}
+
