0b3e8414a51c02e2384bfb3c2bdbeab34fc74664
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2000-10-10 20:24:02 +0000

    Added constant for PULSE_HEIGHT_DATASET

--- trunk/DataSetTools/retriever/Retriever.java
+++ trunk/DataSetTools/retriever/Retriever.java
@@ -3,6 +3,9 @@
  *
  * ---------------------------------------------------------------------------
  *  $Log$
+ *  Revision 1.3  2000/10/10 20:24:02  dennis
+ *  Added constant for PULSE_HEIGHT_DATASET
+ *
  *  Revision 1.2  2000/07/10 22:49:45  dennis
  *  July 10, 2000 version...many changes
  *
@@ -26,9 +29,10 @@ import  java.io.*;
 
 public abstract class Retriever implements Serializable
 {
-    public static final int  INVALID_DATA_SET   = 0;
-    public static final int  MONITOR_DATA_SET   = 1;
-    public static final int  HISTOGRAM_DATA_SET = 2;
+    public static final int  INVALID_DATA_SET      = 0;
+    public static final int  MONITOR_DATA_SET      = 1;
+    public static final int  HISTOGRAM_DATA_SET    = 2;
+    public static final int  PULSE_HEIGHT_DATA_SET = 3;
 
     /** The dataSource from which the retriever retrieves data */
     protected String data_source_name = null;
