7a34f59b5d2e3cdfcc851a2b9bfd3bcaa4ba380b
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-01-31 14:22:03 +0000

    Added javadoc comment.

--- trunk/NetComm/TCPCommExitClass.java
+++ trunk/NetComm/TCPCommExitClass.java
@@ -1,12 +1,15 @@
 /*
  * @(#)TCPCommExitClass.java
  *
- *  Class to send through a TCPComm object to terminat the TCP connection.
+ *  Class to send through a TCPComm object to terminate the TCP connection.
  *
  *  Programmer: Ruth Mikkelson,
  *              Dennis Mikkelson
  *
  *  $Log$
+ *  Revision 1.2  2001/01/31 14:22:03  dennis
+ *  Added javadoc comment.
+ *
  *  Revision 1.1  2001/01/30 23:27:31  dennis
  *  Initial version, network communications for ISAW.
  *
@@ -16,6 +19,9 @@ package NetComm;
 
 import java.io.*;
 
+/**
+ *  Class to send through a TCPComm object to terminate the TCP connection.
+ */
 public class TCPCommExitClass implements Serializable
 {
 }
