357ab443748d27b85c8ac335d50e8ec723186761
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-23 19:44:29 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/NetComm/TCPServiceInit.java
+++ trunk/NetComm/TCPServiceInit.java
@@ -1,13 +1,41 @@
 /*
- * @(#)TCPServiceInit.java
+ * File:  TCPServiceInit.java
  *
- *  Thread used by server to listen for clients requesting a TCP connection. 
+ * Copyright (C) 2001, Ruth Mikkelson
+ *                     Alok Chatterjee,
+ *                     Dennis Mikkelson
  *
- *  Programmers: Ruth Mikkelson,
- *               Alok Chatterjee,
- *               Dennis Mikkelson
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.4  2001/04/23 19:44:22  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.3  2001/02/15 21:25:28  dennis
  *  Changed the time out period to "0" which means an
  *  infinite timeout period
@@ -17,7 +45,6 @@
  *
  *  Revision 1.1  2001/01/30 23:27:33  dennis
  *  Initial version, network communications for ISAW.
- *
  *
  */
 package NetComm;
