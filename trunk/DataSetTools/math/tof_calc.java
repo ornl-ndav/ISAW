accb5432ca098910d0e46a71ab291825cdbeac3a
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-25 21:00:32 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/math/tof_calc.java
+++ trunk/DataSetTools/math/tof_calc.java
@@ -1,11 +1,39 @@
 /*
- * @(#)tof_calc.java 
+ * File:  tof_calc.java 
  *
- *  Programmer: Dennis Mikkelson
+ * Copyright (C) 1999, Dennis Mikkelson
  *
- *  Ported to Java from tof_vis_calc.c
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  * 
  *  $Log$
+ *  Revision 1.11  2001/04/25 20:56:54  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.10  2001/01/29 21:05:58  dennis
  *  Now uses CVS revision numbers.
  *
@@ -24,7 +52,7 @@
  *  units used for parameters.
  *
  *  Revision 1.2  2000/07/10 22:25:15  dennis
- *  July 10, 2000 version... many changes
+ *  Now Using CVS 
  *
  *  Revision 1.5  2000/07/06 21:19:59  dennis
  *  added some constants for Planck's constant and added VelocityOfEnergy()
@@ -39,7 +67,7 @@ package DataSetTools.math;
 
 /**
  *  This contains basic conversions for time-of-flight neutron scattering
- *  experiments.
+ *  experiments. ( Ported to Java from tof_vis_calc.c )
  */
 public final class tof_calc
 {
--- trunk/DataSetTools/math/tof_data_calc.java
+++ trunk/DataSetTools/math/tof_data_calc.java
@@ -1,11 +1,39 @@
 /*
- * @(#)tof_data_calc.java    
+ * File:  tof_data_calc.java    
  *
- *  Programmer: Dennis Mikkelson
+ * Copyright (C) 1999, Dennis Mikkelson
  *
- *  Basic time-of-flight calculations on DataSets and Data blocks
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.7  2001/04/25 20:56:57  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.6  2001/01/29 21:06:00  dennis
  *  Now uses CVS revision numbers.
  *
@@ -27,6 +55,9 @@ import DataSetTools.peak.*;
 import DataSetTools.dataset.*;
 import DataSetTools.util.*;
 
+/**
+ *  Basic time-of-flight calculations on DataSets and Data blocks
+ */
 public final class tof_data_calc
 {
 
