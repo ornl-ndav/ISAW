a56713a840f16bf3dc2c98d98a966b1e7a80b73f
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-25 19:26:09 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/gsastools/gsas_filemaker.java
+++ trunk/DataSetTools/gsastools/gsas_filemaker.java
@@ -1,5 +1,38 @@
 /*
- * @(#)gsas_filemaker.java  1.0 99/06/11 Dongfeng Chen
+ * File:  gsas_filemaker.java
+ *
+ * Copyright (C) 1999, Dongfeng Chen 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee <AChatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           Argonne, IL 60439-4845
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.2  2001/04/25 19:26:07  dennis
+ *  Added copyright and GPL info at the start of the file.
  *
  */
 package DataSetTools.gsastools;
@@ -14,8 +47,7 @@ import java.text.*;
 
 
 /**
- * Show image views diffractometer Data Set and transfer the 
- * histogram to GSAS file format.
+ * Transfer diffractometer Data Set histogram to GSAS file format.
  * @version 1.0
  */
 
