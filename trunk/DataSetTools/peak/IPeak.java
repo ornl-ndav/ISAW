91717b8b3813bbacdc69d9e6d4afb8c347b960a2
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-25 21:32:25 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/peak/IPeak.java
+++ trunk/DataSetTools/peak/IPeak.java
@@ -1,8 +1,39 @@
 /*
- * @(#)IPeak.java     0.2  2000/03/06  Dennis Mikkelson
+ * File:  IPeak.java
  *
- * ---------------------------------------------------------------------------
+ * Copyright (C) 2000, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
  *  $Log$
+ *  Revision 1.3  2001/04/25 21:32:23  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.2  2000/07/17 15:21:13  dennis
  *  Added convenience routines to compute area and moments over the default
  *  extent of the peak.
@@ -13,16 +44,12 @@
  *  Revision 1.5  2000/06/15 16:49:20  dennis
  *  improved documentation
  *
- *  Revision 1.4  2000/06/14 19:25:18  dennis
- *  *** empty log message ***
- *
  *  Revision 1.3  2000/06/13 20:39:15  dennis
  *  added setEvaluationMode() to select between Peak, Background and
  *  Peak plus Background
  *
  *  Revision 1.2  2000/05/11 16:09:42  dennis
  *  added RCS logging
- *
  *
  */
 
