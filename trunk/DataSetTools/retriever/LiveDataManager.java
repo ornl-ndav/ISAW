6782e8346f0966dc7c515b58703b11904cd1c84a
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-25 21:57:57 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/retriever/LiveDataManager.java
+++ trunk/DataSetTools/retriever/LiveDataManager.java
@@ -1,9 +1,39 @@
 /*
- * @(#)LiveDataManager.java      
+ * File:  LiveDataManager.java      
  *
- * Programmer: Dennis Mikkelson
+ * Copyright (C) 2001, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.5  2001/04/25 21:57:47  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.4  2001/03/01 23:22:23  dennis
  *  Now uses a private method SetUpLocalCopies() to initialize its
  *  local copies of the DataSets and associated info.  This will make
@@ -21,7 +51,6 @@
  *  Revision 1.1  2001/02/15 23:25:23  dennis
  *  Class to control periodic updates to live data sets
  *  by periodically getting new data from a LiveDataServer.
- *
  *
  */
 package DataSetTools.retriever;
--- trunk/DataSetTools/retriever/Retriever.java
+++ trunk/DataSetTools/retriever/Retriever.java
@@ -1,9 +1,39 @@
 /*
- * @(#)Retriever.java
+ * File:  Retriever.java
  *
- *  Programmer: Dennis Mikkelson
+ * Copyright (C) 1999, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.5  2001/04/25 21:57:55  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.4  2001/01/29 21:12:05  dennis
  *  Now uses CVS revision numbers.
  *
@@ -11,7 +41,7 @@
  *  Added constant for PULSE_HEIGHT_DATASET
  *
  *  Revision 1.2  2000/07/10 22:49:45  dennis
- *  July 10, 2000 version...many changes
+ *  Now Using CVS 
  *
  *  Revision 1.3  2000/05/11 16:19:12  dennis
  *  added RCS logging
