d58801ba98f74fb5b2b0e9d15bf14a3a71cc57d8
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-25 21:32:25 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/peak/GaussianPeak.java
+++ trunk/DataSetTools/peak/GaussianPeak.java
@@ -1,8 +1,39 @@
 /*
- * @(#)GaussianPeak.java     0.1  2000/03/06  Dennis Mikkelson
+ * File:  GaussianPeak.java
  *
- * ---------------------------------------------------------------------------
+ * Copyright (C) 2000, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
  *  $Log$
+ *  Revision 1.2  2001/04/25 21:32:13  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.1  2000/07/10 22:48:57  dennis
  *  New classes to deal with peaks/peak fitting
  *
@@ -15,8 +46,6 @@
  *
  *  Revision 1.2  2000/05/11 16:09:42  dennis
  *  added RCS logging
- *
- *
  */
 
 package  DataSetTools.peak;
