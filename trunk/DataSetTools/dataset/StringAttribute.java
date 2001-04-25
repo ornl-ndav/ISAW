8f56572e2a49d0a0d240e28fec781cef2170eaf0
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-25 19:04:15 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/dataset/StringAttribute.java
+++ trunk/DataSetTools/dataset/StringAttribute.java
@@ -1,13 +1,41 @@
 /*
- * @(#)StringAttribute.java    1.01 99/06/02  Dennis Mikkelson
+ * File:  StringAttribute.java
  *
- *  99/06/02, 1.01, Added methods to set the attribute's name and value.
- *                  Added method "compare" to compare objects.
- *                  Removed method "greater_than" to compare objects.
- * ---------------------------------------------------------------------------
+ * Copyright (C) 1999, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
  *  $Log$
+ *  Revision 1.4  2001/04/25 19:04:07  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.3  2000/07/10 22:24:05  dennis
- *  July 10, 2000 version... many changes
+ *  Now Using CVS 
  *
  *  Revision 1.5  2000/05/12 15:50:13  dennis
  *  removed DOS TEXT  ^M
@@ -15,7 +43,9 @@
  *  Revision 1.4  2000/05/11 16:00:45  dennis
  *  Added RCS logging
  *
- *
+ *  99/06/02, 1.01, Added methods to set the attribute's name and value.
+ *                  Added method "compare" to compare objects.
+ *                  Removed method "greater_than" to compare objects.
  */
 
 package  DataSetTools.dataset;
