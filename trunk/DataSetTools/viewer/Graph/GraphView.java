a3e4d45015c018458bdac5eaaf1378461d9077e4
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-04-26 14:31:32 +0000

    Added copyright and GPL info at the start of the file.

--- trunk/DataSetTools/viewer/Graph/GraphView.java
+++ trunk/DataSetTools/viewer/Graph/GraphView.java
@@ -1,9 +1,39 @@
 /**
- * @(#)GraphView.java  
+ * File:  GraphView.java  
  *
- * Programmer: Dennis Mikkelson
+ * Copyright (C) 1999, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.8  2001/04/26 14:23:51  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.7  2001/04/02 20:43:12  dennis
  *  num_bins_ui is now properly initilized to max X steps - 1.
  *
@@ -32,7 +62,7 @@
  *  Now calls Data.ResampleUniformly() instead of Data.ReBin()
  *
  *  Revision 1.1  2000/07/10 23:02:50  dennis
- *  July 10, 2000 version... many changes
+ *  Now Using CVS 
  *
  *  Revision 1.20  2000/06/12 20:10:03  dennis
  *  now implements Serializable
--- trunk/DataSetTools/viewer/ViewManager.java
+++ trunk/DataSetTools/viewer/ViewManager.java
@@ -1,9 +1,39 @@
 /*
- * @(#)ViewManager.java
+ * File:  ViewManager.java
  *
- * Programmer:  Dennis Mikkelson 
+ * Copyright (C) 1999, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.4  2001/04/26 14:21:40  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.3  2001/01/29 21:28:04  dennis
  *  Print error message if the DataSet is null in the
  *  constructor.
@@ -14,7 +44,7 @@
  *  Also refined some reasons for doing an update.
  *
  *  Revision 1.1  2000/07/10 22:59:16  dennis
- *  July 10, 2000 version... many changes
+ *  Now Using CVS 
  *
  *  Revision 1.23  2000/06/14 22:18:46  dennis
  *  changed order of parameters to SumCurrentlySelected() since the operator
@@ -36,23 +66,21 @@
  *  Revision 1.18  2000/05/11 15:19:52  dennis
  *  Added RCS logging.
  *
- *  Modified:
- *   1.1 2000/03/30  Dennis Mikkelson
- *                   Changed it to pass a second "tempDataSet" to the viewers,
- *                   so that axis conversions can be done and hidden Data
- *                   blocks can be omitted.
+ *  1.14 2000/05/10  Sum selected groups and delete selected groups are not
+ *                   implemented using operators.
+ *  1.13 2000/04/28  Temporarily removed Group and Hide operations.  Added
+ *                   option to save the tempDataSet to the tree as a new
+ *                   DataSet
+ *  1.12 2000/04/11  Added edit menu options to Delete, Hide, Group the
+ *                   un-selected items, as well as the selected items.
  *  1.11 2000/04/04  Dennis Mikkelson
  *                   Added translation arrays between indices in the original
  *                   DataSet and tempDataSet. "Pointed At" messages are now
  *                   transferred from one to the other.
- *  1.12 2000/04/11  Added edit menu options to Delete, Hide, Group the
- *                   un-selected items, as well as the selected items.
- *  1.13 2000/04/28  Temporarily removed Group and Hide operations.  Added
- *                   option to save the tempDataSet to the tree as a new
- *                   DataSet
- *  1.14 2000/05/10  Sum selected groups and delete selected groups are not
- *                   implemented using operators.
- *
+ *   1.1 2000/03/30  Dennis Mikkelson
+ *                   Changed it to pass a second "tempDataSet" to the viewers,
+ *                   so that axis conversions can be done and hidden Data
+ *                   blocks can be omitted.
  */
  
 package DataSetTools.viewer;
