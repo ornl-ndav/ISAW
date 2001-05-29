f666dc919c14df113fa5333e884477476f96b491
Dennis Mikkelson <mikkelsond@uwstout.edu>
2001-05-29 15:28:20 +0000

    Major revision, now supports scripts as operators and separates the ComandPane GUI.

---
+++ Command/OperatorHandler.java
@@ -0,0 +1,8 @@
+package Command;
+import DataSetTools.operator.*;
+public interface OperatorHandler
+   {
+     public int getNum_operators();
+     public  Operator getOperator(int index);
+     
+    }
