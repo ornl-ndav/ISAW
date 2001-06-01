554d31b6ff2025925943f27fd39a73955b7a2a64
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-06-01 21:14:13 +0000

    Added Documentation for javadocs etc.

--- trunk/Command/DSgetArray.java
+++ trunk/Command/DSgetArray.java
@@ -1,3 +1,43 @@
+/*
+ * File:  DSgetArray.java 
+ *             
+ * Copyright (C) 2001, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.3  2001/06/01 21:14:13  rmikk
+ * Added Documentation for javadocs etc.
+ *
+ *
+ *  5-25-2001  Created
+ *
+ */
 package Command;
 import DataSetTools.dataset.*;
 import DataSetTools.components.ParametersGUI.*;
@@ -6,6 +46,9 @@ import IsawGUI.*;
 import java.util.*;
 import javax.swing.tree.*;
 import javax.swing.*;
+
+/** Implements DataSetListHandler when the Data Set list is in a JTreeUI
+ */
 public class DSgetArray implements DataSetListHandler
 {DataSet DS[];
 
@@ -14,7 +57,11 @@ public class DSgetArray implements DataSetListHandler
 
    {return DS;}
 
+/**
+*@param jtui  the JTreeUI with the list of data sets
+*@see  #IsawGUI.JTreeUI
 
+*/
 public DSgetArray( JTreeUI jtui)
   { if( jtui == null)
      { DS = null;
