d1b316a333b722c07e722f37b9c61a1ea0eb8b39
chatter <chatter@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-06-06 14:32:39 +0000

    Added the gpl license

--- trunk/IsawGUI/Isaw.java
+++ trunk/IsawGUI/Isaw.java
@@ -1,7 +1,39 @@
-
-
 /*
- * @(#)Isaw.java     0.7  99/09/02  Alok Chatterjee
+ * File:  Isaw.java
+ *
+ * Copyright (C) 1999, Alok Chatterjee
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee <achatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 S. Cass Avenue, Bldg 360
+ *           Argonne, IL 60440
+ *           USA
+ *
+ * 
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.13  2001/06/06 14:32:39  chatter
+ *  Added the gpl license
+ *
  *
  * 0.7 2000/06/01 Added comments in Javadoc format. Also changed the loader
  *			to make use of the loader in the Util class in IsawGUI package.
