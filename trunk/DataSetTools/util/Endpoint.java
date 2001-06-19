fd8d35917d2855fa858c65914c01a6a5bcb783d3
neffk <neffk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-06-19 20:36:29 +0000

    added .isBefore() and .isAfter() in order to deal with open or closed intervals.

--- trunk/DataSetTools/util/Endpoint.java
+++ trunk/DataSetTools/util/Endpoint.java
@@ -4,6 +4,7 @@ import DataSetTools.dataset.Attribute;
 
 public class Endpoint
 {
+  final int SEPERATOR = ':';
   private boolean closed;
   private Attribute attr;
 
@@ -30,43 +31,47 @@ public class Endpoint
    * test if this object comes before the given Endpoint.  this method
    * considers whether the interval is open or closed.
    */
-  public boolean before( Attribute a )
+  public boolean isBefore( Endpoint e )
   {
     if( closed )
-      if( attr.compare(a) >= 0 )
+      if( attr.compare(e.getAttr()) <= 0 )
         return true;
-      else if( attr.compare(a) < 0 )
+      else if( attr.compare(e.getAttr()) > 0 )
         return false;
     
     if( !closed )
-      if( attr.compare(a) > 0 )
+      if( attr.compare(e.getAttr()) < 0 )
         return true;
-      else if( attr.compare(a) <= 0 )
+      else if( attr.compare(e.getAttr()) >= 0 )
         return false;
+
+    return false;  //javac is retarded
   }
 
-
-   /**
-    * test if this object comes after the given ednpoint.  this method
-    * considers whether the interval is open or closed.
-    */
-  public boolean after( Attribute a )
+  /**
+   * test if this object comes after 'e' the given ednpoint.  this method
+   * takes into account whether the interval is open or closed.  
+   * if e comes after this...
+   */
+  public boolean isAfter( Endpoint e )
   {
     if( closed )
-      if( attr.compare(a) <= 0 )
+      if( attr.compare(e.getAttr()) >= 0 )
         return true;
-      else if( attr.compare(a) > 0 )
+      else if( attr.compare(e.getAttr()) < 0 )
         return false;
   
     if( !closed )
-      if( attr.compare(a) < 0 )
+      if( attr.compare(e.getAttr()) > 0 )
         return true;
-      else if( attr.compare(a) >= 0 )
+      else if( attr.compare(e.getAttr()) <= 0 )
         return false;
+
+    return false;  //javac is retarded
   }
 
   
-  public boolean closed()
+  public boolean isClosed()
   {
     if( closed )
      return true;
@@ -75,11 +80,21 @@ public class Endpoint
   }
 
 
+  public void setClosed( boolean c )
+  {
+    closed = c;
+  }
+
+
   public String toString()
   {
-    if( closed )
-      return new String(  "[" + attr.getName() + "]"  );
-    else
-      return new String(  "(" + attr.getName() + ")"  );
+    return new String( attr.getName() + "=" + attr.getValue() );
   }
+
+ 
+  public Object clone()
+  {
+    return new Endpoint( attr, closed );
+  }
+
 }
