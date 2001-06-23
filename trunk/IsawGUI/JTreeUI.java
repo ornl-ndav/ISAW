0b8fdd28b6249c6cc6292d26fc94b812fbaccfe9
neffk <neffk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-06-23 03:42:21 +0000

    added a mouse selection of left click, call to menu on right click

--- trunk/IsawGUI/JTreeUI.java
+++ trunk/IsawGUI/JTreeUI.java
@@ -1,6 +1,11 @@
 /*
  * @(#)JTreeUI.java     1.0  99/09/02  Alok Chatterjee
  *
+ * $Log$
+ * Revision 1.7  2001/06/23 03:42:21  neffk
+ * added a mouse selection of left click, call to menu on right click
+ *
+ * ---------- 
  * 1.0  99/09/02  Added the comments and made this a part of package IsawGUI
  * 
  */
@@ -55,41 +60,260 @@ public class JTreeUI extends JPanel implements IObserver, Serializable
     private JTree logTree;
     DataSet current_ds = null;
 
-    public  JTreeUI(JPropertiesUI jpui, JCommandUI jcui, CommandPane cp) 
+
+  /**
+   *
+   */
+  public JTreeUI( JPropertiesUI jpui, JCommandUI jcui, CommandPane cp ) 
+  {
+    this.jpui = jpui;
+    this.jcui = jcui;  
+    this.cp = cp;
+    setLayout(new GridLayout(1,1));
+
+    //
+    // set up the JTree object that runfiles and
+    // data sets will be shown in.
+    //
+    root = new DefaultMutableTreeNode("Session");
+    DefaultMutableTreeNode level1 = new DefaultMutableTreeNode( "Modified DataSet(s)");
+    model = new DefaultTreeModel(root);
+    model.insertNodeInto(level1, root, 0);
+    tree = new JTree(model);
+    tree.setShowsRootHandles(true);
+    //tree.putClientProperty("JTree.lineStyle", "Horizontal");
+    tree.putClientProperty("JTree.lineStyle", "Angled");
+
+
+
+/*--------------------------=[ start MouseAdapter ]=--------------------------*/
+
+    MouseListener ml = new MouseAdapter() 
     {
-		this.jpui = jpui;
-		this.jcui = jcui;  
-            this.cp = cp;
-            setLayout(new GridLayout(1,1));
-            root = new DefaultMutableTreeNode("Session");
-            DefaultMutableTreeNode level1 = new DefaultMutableTreeNode( "Modified DataSet(s)");
-            model = new DefaultTreeModel(root);
-            model.insertNodeInto(level1, root, 0);
-            tree = new JTree(model);
-            tree.setShowsRootHandles(true);
-           // tree.putClientProperty("JTree.lineStyle", "Horizontal");
-	   	tree.putClientProperty("JTree.lineStyle", "Angled");
-            JScrollPane pane = new JScrollPane(tree);
+      public void mousePressed(MouseEvent e) 
+//      public void mouseClicked(MouseEvent e) 
+      {
+        if(  tree.getSelectionCount() > 0  ) 
+        {
+          TreePath[] selectedPath = null;
+          TreePath[] tp           = tree.getSelectionPaths();  
 
- 		root1 = new DefaultMutableTreeNode("TreeLog");
-        	model1 = new DefaultTreeModel(root1);
-		logTree = new JTree(model1);
-        	logTree.setShowsRootHandles(true);
-	   	logTree.putClientProperty("JTree.lineStyle", "Angled");
-		JScrollPane pane1 = new JScrollPane(logTree);
-		setBorder(new CompoundBorder(new EmptyBorder(4,4,4,4), new EtchedBorder (EtchedBorder.RAISED)));
+          int button1 =  e.getModifiers() & InputEvent.BUTTON1_MASK;
+          int button3 =  e.getModifiers() & InputEvent.BUTTON3_MASK;
 
-           /* DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();		
-              renderer.setLeafIcon(new ImageIcon("C://new-orange.gif"));
-              tree.setCellRenderer(renderer);
-	    */
-	     jdvui = new JDataViewUI();
- 	     jtp = new JTabbedPane();
-           jtp.addTab("Runfiles", pane);
-	    // jtp.addTab("Operations Log", pane1);
-		add(jtp);
+          if(  button3 == InputEvent.BUTTON3_MASK  )
+          {
+            actionMenu( tp );
+          }
+          else if(  button1 == InputEvent.BUTTON1_MASK  )
+          {
+            if(e.getClickCount() == 1) 
+            {
+              pointAtNode( tp );
+            }
+            else if(e.getClickCount() == 2) 
+            {
+              selectNode( tp );
+            }
+          }
+        }
+      }
 
-     }// End of Constructor
+      public void pointAtNode( TreePath[] tp )
+      {
+        System.out.println( "data tree> " + IObserver.POINTED_AT_CHANGED  );
+      }
+
+
+      /**
+       * sets newly selected Data as selected and notifies their containing
+       * DataSet.
+       */
+      public void selectNode( TreePath[] tp )
+      {
+        System.out.println( "data tree> " + IObserver.SELECTION_CHANGED  );
+
+        if(  tp.length > 0  )  //probably don't need this, but keep it for good karma
+        {
+          DefaultMutableTreeNode dmtn = null;
+
+          //
+          //we can only deal w/ selections from one DataSet at a time,
+          //so we'll arbitrarily choose the DataSet that the first
+          //selection corresponds to
+          //
+          DataSet ds = traverseUpToDataSet( tp[0] );
+
+          for( int i=0;  i<tp.length;  i++ )
+          {                                                   //only use first DataSet's Data
+            dmtn = (DefaultMutableTreeNode)(  tp[i].getLastPathComponent()  ); 
+            if(   dmtn != null  &&  ds.equals(  traverseUpToDataSet( tp[i] )  )   );
+            {
+              if(  dmtn.getUserObject() instanceof Data  )
+              {
+                Data d = (Data)dmtn.getUserObject();
+                if(   ds.getData_entry(  d.getGroup_ID()  ).isSelected() == false   )
+                  ds.getData_entry(  d.getGroup_ID()  ).setSelected( true );
+                else
+                  ds.getData_entry(  d.getGroup_ID()  ).setSelected( false );
+
+                System.out.println(  "Data: " + d.toString()  );
+              }
+              else
+              {
+                if(  dmtn.getUserObject() != null  ) 
+                  System.out.println(  "non-Data: " + dmtn.getUserObject().toString()  );
+              }
+            }
+          }
+          ds.notifyIObservers( IObserver.SELECTION_CHANGED );
+        }
+      }
+
+
+      public void actionMenu( TreePath[] tp )
+      {
+        DataSet ds = traverseUpToDataSet( tp[0] );
+        DefaultMutableTreeNode dmtn = null;
+        if(  tp.length > 0  )
+          dmtn = (DefaultMutableTreeNode)(  tp[0].getLastPathComponent()  ); 
+
+        //
+        // single Data object
+        //
+        if(   dmtn.getUserObject() instanceof Data  &&  tp.length == 1   )
+        {
+          SingleDataBlockPopupMenu( tp );
+        }
+ 
+        //
+        // multiple Data objects
+        //
+        else if(  dmtn.getUserObject() instanceof Data  )
+        {
+          for( int i=0;  i<tp.length;  i++ )
+          {
+            dmtn = (DefaultMutableTreeNode)(  tp[i].getLastPathComponent()  ); 
+
+            if(  !ds.equals( traverseUpToDataSet(tp[i]) )  ||        //only use first DataSet's Data.
+                 !(dmtn.getUserObject() instanceof Data )  )         //make sure we're dealing with a group of
+            {                                                        //consistant objects.
+              System.out.println( "non-Data or illegal multiple DataSet selection" );
+              return; 
+            }
+          }
+          MultipleDataBlockPopupMenu( tp );
+        }
+
+        //
+        // single DataSet object
+        //
+        else if(  dmtn.getUserObject() instanceof DataSet  &&  tp.length == 1  )
+        {
+          SingleDataSetPopupMenu( tp );
+        }
+ 
+        //
+        // multiple DataSet objects
+        //
+        else if(  dmtn.getUserObject() instanceof DataSet  )
+        {
+          for( int i=0;  i<tp.length;  i++ )
+          {
+            dmtn = (DefaultMutableTreeNode)(  tp[i].getLastPathComponent()  ); 
+
+            if(   ds.equals( traverseUpToDataSet( tp[i] ) )  &&    //only use first DataSet's Data.
+                  dmtn.getUserObject() instanceof DataSet   )       //make sure we're dealing with a group of
+            {                                                        //consistant objects.
+              MultipleDataSetPopupMenu( tp );
+            }
+          }
+        }
+        else
+        {
+          System.out.println( "type not appropriate for actionMenu" );
+        }
+      }
+
+      public void SingleDataBlockPopupMenu( TreePath[] tp )
+      {
+        System.out.println( "singleDataBlockPopupMenu(...)" );
+      }
+
+
+      public void MultipleDataBlockPopupMenu( TreePath[] tp )
+      {
+        System.out.println( "MultipleDataBlockPopupMenu(...)" );
+      }
+
+
+      public void SingleDataSetPopupMenu( TreePath[] tp )
+      {
+        System.out.println( "SingleDataSetPopupMenu(...)" );
+      }
+
+
+      public void MultipleDataSetPopupMenu( TreePath[] tp )
+      {
+        System.out.println( "MultipleDataSetPopupMenu(...)" );
+      }
+
+
+      /**
+       * recursivly find the DataSet that this Data block
+       * belongs to and return it the caller
+       */
+      protected DataSet traverseUpToDataSet( TreePath tp )
+      {
+        DefaultMutableTreeNode dmtn = (DefaultMutableTreeNode)(  tp.getLastPathComponent()  );
+        
+        //terminal case(s)
+        if(  tp.getParentPath() == null  )
+          return null;
+        else if(  dmtn.getUserObject() instanceof DataSet  )
+          return (DataSet)dmtn.getUserObject();  
+
+        //reCURSE!
+        else
+          return traverseUpToDataSet(  tp.getParentPath()  );
+
+//        Object o = new Object();  //jikes is having trouble with returns hidden in if's
+//        return (DataSet)o;
+      }
+    };
+
+/*---------------------------=[ end MouseAdapter ]=---------------------------*/
+
+
+
+    tree.addMouseListener(ml);
+    JScrollPane pane = new JScrollPane(tree);
+
+    root1 = new DefaultMutableTreeNode("TreeLog");
+    model1 = new DefaultTreeModel(root1);
+    logTree = new JTree(model1);
+    logTree.setShowsRootHandles(true);
+    logTree.putClientProperty("JTree.lineStyle", "Angled");
+    JScrollPane pane1 = new JScrollPane(logTree);
+    setBorder(new CompoundBorder(new EmptyBorder(4,4,4,4), new EtchedBorder (EtchedBorder.RAISED)));
+
+    /* 
+    DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();		
+    renderer.setLeafIcon(new ImageIcon("C://new-orange.gif"));
+    tree.setCellRenderer(renderer);
+    */
+
+    jdvui = new JDataViewUI();
+    jtp = new JTabbedPane();
+    jtp.addTab("Runfiles", pane);
+    //jtp.addTab("Operations Log", pane1);
+    add(jtp);
+  }
+
+
+    /**
+     *
+     */
     public void showLog(DataSet ds)
 	 {
 		if(ds == current_ds)    //Only draw the log for a different dataset.
@@ -116,26 +340,19 @@ public class JTreeUI extends JPanel implements IObserver, Serializable
 	 }	 
 
  
-        
-      public DefaultMutableTreeNode getSelectedNode()
-	  {
-	    TreePath selectedPath = null;
-	    if(tree.getSelectionCount()>0)
-        {int[] selectedRows = tree.getSelectionRows();
-        //System.out.println("Tree is entered");
-       // if(tree == null) 
-	 // 	System.out.println("Tree is null");
-       // else System.out.println("Tree is NOT null");
-      
-        selectedPath =  tree.getPathForRow(selectedRows[0]); 
-        
-        return (DefaultMutableTreeNode)selectedPath.getLastPathComponent();
-        }
-        else 
-        {
-            return null;
-        }
-	  }//getSelectedNode
+ 
+  public DefaultMutableTreeNode getSelectedNode()
+  {
+    TreePath selectedPath = null;
+    if(tree.getSelectionCount()>0)
+    {
+      int[] selectedRows = tree.getSelectionRows();
+      selectedPath =  tree.getPathForRow(selectedRows[0]); 
+      return (DefaultMutableTreeNode)selectedPath.getLastPathComponent();
+    }
+    else 
+      return null;
+  }
 	  
 
 
@@ -143,39 +360,49 @@ public class JTreeUI extends JPanel implements IObserver, Serializable
      {
         return tree;  
      }
+
      
      public TreeModel getTreeModel()
      {
         return model;  
      }
      
-     public void addDataSets(DataSet[] dss, String name)
-     {
-        DefaultMutableTreeNode level1 = new DefaultMutableTreeNode(name);
-        int index = root.getChildCount();
-        ( model).insertNodeInto(level1,root,index);
-            for (int i = 0; i<dss.length; i++)
-            { 
 
-                cp.addDataSet(dss[i]);
-                DefaultMutableTreeNode level2 = new DefaultMutableTreeNode(dss[i]);
-                index = level1.getChildCount();
-                ( model ).insertNodeInto(level2,level1,index);
-			
+  /**
+   * adds data to the tree that displays runfiles
+   * and data sets,
+   */
+  public void addDataSets(DataSet[] dss, String name)
+  {
+    DefaultMutableTreeNode level1 = new DefaultMutableTreeNode(name);
+    int index = root.getChildCount();
+    (model).insertNodeInto(level1,root,index);
+    for (int i = 0; i<dss.length; i++)
+    { 
+      cp.addDataSet(dss[i]);  //update command pane
 
-                for (int k = 0; k < dss[i].getNum_entries() ; k++)
-                {
-                     Data entry = dss[i].getData_entry(k);
+      //
+      // add DataSet object to tree
+      //
+      DefaultMutableTreeNode level2 = new DefaultMutableTreeNode(dss[i]);
+      index = level1.getChildCount();
+      (model).insertNodeInto(level2,level1,index);
 
-                     DefaultMutableTreeNode level3 = new DefaultMutableTreeNode(entry); 
-                     index = level2.getChildCount();
-                     ( model ).insertNodeInto(level3,level2,index);
-                }   
-	
-				tree.expandRow(2);
-				tree.setSelectionRow(4);
-            }   
-     }
+      //
+      // add Data objects to tree
+      //
+      for (int k = 0; k < dss[i].getNum_entries() ; k++)
+      {
+        Data entry = dss[i].getData_entry(k);
+        DefaultMutableTreeNode level3 = new DefaultMutableTreeNode(entry); 
+        index = level2.getChildCount();
+        (model).insertNodeInto(level3,level2,index);
+      }   
+
+    tree.expandRow(2);
+    tree.setSelectionRow(4);
+    }   
+  }
 
      public void addDataSet(DataSet ds)
      {
@@ -239,6 +466,12 @@ public class JTreeUI extends JPanel implements IObserver, Serializable
 		return null;
 	}
 
+
+
+	/**
+	 * examines the type of change made to 'observed' and
+	 * makes changes that are appropriate for this view
+	 */
 	public void update( Object observed, Object reason )
    	{
      		if ( !( reason instanceof String) && !( reason instanceof DataSet) )   
@@ -362,7 +595,8 @@ public class JTreeUI extends JPanel implements IObserver, Serializable
 					Data data = (Data)dataNode.getUserObject();
 					if(data.isSelected())
 				//	tree.setSelectionRow(i);
-                              System.out.println("Selected block" +i);
+                              //System.out.println("Selected block" +i);
+                              System.out.print("");
 				}
 		
 
@@ -420,8 +654,4 @@ public class JTreeUI extends JPanel implements IObserver, Serializable
       		return; 
      		}          	
    	}
-
-
-
-    
  }
