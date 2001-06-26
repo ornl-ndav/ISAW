b0a187f2142cecc773fec37cbe668a7a59428f81
Dennis Mikkelson <mikkelsond@uwstout.edu>
2001-06-26 18:37:43 +0000

    Added Copyright and GPL license. Removed un-needed imports and improved code format.

--- DataSetTools/components/ParametersGUI/JDataSetParameterGUI.java
+++ DataSetTools/components/ParametersGUI/JDataSetParameterGUI.java
@@ -1,28 +1,47 @@
 /*
- * @(#)JDataSetParameterGUI.java     1.0  99/09/02  Alok Chatterjee
+ * File: JDataSetParameterGUI.java
  *
- * 1.0  99/09/02  Added the comments and made this a part of package IsawGUI
- * 
+ * Copyright (C) 1999, Alok Chatterjee, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee achatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 S. Cass Avenue, Bldg 360
+ *           Argonne, IL 60440
+ *           USA
+ *
+ * For further information, see http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.3  2001/06/26 18:37:28  dennis
+ *  Added Copyright and GPL license.
+ *  Removed un-needed imports and improved
+ *  code format.
+ *
  */
  
 package DataSetTools.components.ParametersGUI;
 
 import javax.swing.*;
-//import javax.swing.*;
-import DataSetTools.*;
 import DataSetTools.dataset.*;
 import DataSetTools.operator.*;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.tree.*;
-import javax.swing.JTree.*;
-
-/**
- * The main class for ISAW. It is the GUI that ties together the DataSetTools, IPNS, 
- * ChopTools and graph packages.
- *
- * @version 1.0  
- */
 
 public class JDataSetParameterGUI extends JParameterGUI
 {
@@ -35,23 +54,23 @@ public class JDataSetParameterGUI extends JParameterGUI
     { 
        super(parameter);
        
-        combobox = new JComboBox();
-        //combobox.addItem(ds);
-        JLabel label = new JLabel(parameter.getName());
+       combobox = new JComboBox();
+       JLabel label = new JLabel(parameter.getName());
        label.setPreferredSize(new Dimension(150,25));
 
-        DS = new_ds;
-        if( DS == null)
-           return;
+       DS = new_ds;
+       if( DS == null)
+          return;
 
-        for( int i = 0; i < DS.length ; i++)
-          { combobox.addItem( DS[i]);
-          }
-        segment = new JPanel();
-        segment.setLayout(new FlowLayout(FlowLayout.CENTER, 70, 5)); 
-       
-        segment.add(label);
-        segment.add(combobox);
+       for( int i = 0; i < DS.length ; i++)
+       { 
+         combobox.addItem( DS[i]);
+       }
+       segment = new JPanel();
+       segment.setLayout(new FlowLayout(FlowLayout.CENTER, 70, 5)); 
+      
+       segment.add(label);
+       segment.add(combobox);
     }
 
     public JPanel getGUISegment()
@@ -61,13 +80,12 @@ public class JDataSetParameterGUI extends JParameterGUI
 
     public Parameter getParameter()
     {
-       if( DS == null )
-         {return parameter;
-           }
-       DataSet ds = (DataSet)combobox.getSelectedItem();
-        parameter.setValue(ds);
+      if( DS == null )
+      {
         return parameter;
+      }
+      DataSet ds = (DataSet)combobox.getSelectedItem();
+      parameter.setValue(ds);
+      return parameter;
     }
-
 }
-
