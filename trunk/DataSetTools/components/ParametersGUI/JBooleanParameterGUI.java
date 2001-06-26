39a576cf89a3ac65b75095bb081dae9a7e334e9e
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-06-26 18:37:43 +0000

    Added Copyright and GPL license. Removed un-needed imports and improved code format.

--- trunk/DataSetTools/components/ParametersGUI/JBooleanParameterGUI.java
+++ trunk/DataSetTools/components/ParametersGUI/JBooleanParameterGUI.java
@@ -1,132 +1,77 @@
 /*
-
- * @(#)JBooleanParameterGUI.java     1.0  99/09/02  Alok Chatterjee
-
+ * File: JBooleanParameterGUI.java
  *
-
- * 1.0  99/09/02  Added the comments and made this a part of package IsawGUI
-
- * 
-
+ * Copyright (C) 1999, Alok Chatterjee, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee achatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 S. Cass Avenue, Bldg 360
+ *           Argonne, IL 60440
+ *           USA
+ *
+ * For further information, see http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.3  2001/06/26 18:37:26  dennis
+ *  Added Copyright and GPL license.
+ *  Removed un-needed imports and improved
+ *  code format.
+ *
  */
-
  
-
 package DataSetTools.components.ParametersGUI;
 
-
-
 import javax.swing.*;
-
-//import javax.swing.*;
-
 import DataSetTools.*;
-
 import DataSetTools.operator.*;
-
 import java.awt.*;
-
-import java.util.zip.*;
-
-import java.io.Serializable;
+import java.io.*;
 
 
-
-/**
-
- * The main class for ISAW. It is the GUI that ties together the DataSetTools, IPNS, 
-
- * ChopTools and graph packages.
-
- *
-
- * @version 1.0  
-
- */
-
-
-
-public class JBooleanParameterGUI extends JParameterGUI implements Serializable
-
+public class JBooleanParameterGUI extends    JParameterGUI 
+                                  implements Serializable
 {
-
     private JPanel segment;
-
     private JCheckBox jcb;
 
-    
-
     public JBooleanParameterGUI(Parameter parameter)
-
     { 
-
        super(parameter);
-
-       
-
-       
-
        boolean value = ((Boolean)parameter.getValue()).booleanValue();
-
-       
-
-       
-
        segment = new JPanel();
-
        segment.setLayout(new GridLayout(1,2));
-
        segment.add(new JLabel(""));
-
        jcb = new JCheckBox(parameter.getName(),value);
-
        segment.add(jcb);
-
-       
-
-       
-
-    
-
-   
-
     }
-
-    
-
-    
-
-    
 
     public JPanel getGUISegment()
-
     {
-
         return segment;
-
-        
-
     }
-
-
-
-
 
     public Parameter getParameter()
-
     {
-
         boolean val = jcb.isSelected();
-
         Boolean value = new Boolean(val);
-
         parameter.setValue(value);
-
         return parameter;
-
     }
-
-
-
 }
-
--- trunk/DataSetTools/components/ParametersGUI/JObjectParameterGUI.java
+++ trunk/DataSetTools/components/ParametersGUI/JObjectParameterGUI.java
@@ -1,374 +1,238 @@
 /*
-
- * @(#)JObjectParameterGUI.java     1.0  99/09/02  Alok Chatterjee
-
+ * File:  JObjectParameterGUI.java
  *
-
- * 1.0  99/09/02  Added the comments and made this a part of package IsawGUI
-
- * 
-
+ * Copyright (C) 2000, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.3  2001/06/26 18:37:35  dennis
+ * Added Copyright and GPL license.
+ * Removed un-needed imports and improved
+ * code format.
+ *
  */
-
- 
 
 package DataSetTools.components.ParametersGUI;
 
-
-
 import javax.swing.*;
-
-//import javax.swing.*;
-
-//import DataSetTools.*;
-
 import DataSetTools.operator.*;
-
 import java.awt.*;
-
 import javax.swing.border.*;
-
-import java.io.Serializable;
-
+import java.io.*;
 import java.awt.event.*;
-
 import Command.*;
 
-/**
 
- * The main class for ISAW. It is the GUI that ties together the DataSetTools, IPNS, 
-
- * ChopTools and graph packages.
-
- *
-
- * @version 1.0  
-
- */
-
- 
-
-public class JObjectParameterGUI extends JParameterGUI implements Serializable,
-
-                                                                  ItemListener
-
-{   public static final String ER__NumberFormat_Error = "Number Format Error";
-
+public class JObjectParameterGUI extends    JParameterGUI 
+                                 implements Serializable, 
+                                            ItemListener
+{   
+    public static final String ER__NumberFormat_Error = "Number Format Error";
     private JPanel segment;
-
     private JTextField intText;
-
     private JComboBox  DataType;
-
     private JCheckBox  Array;
-
     public String serror = "";
-
-   
-
 
 
     public JObjectParameterGUI(Parameter parameter)
-
     { 
-
        super(parameter);
 
        serror = "";
-
        String value = null; //((Integer)parameter.getValue()).toString();
 
        intText = new JTextField();
-
        intText.setText("");
 
        segment = new JPanel();
-
        segment.setLayout(new FlowLayout(FlowLayout.CENTER, 70, 5)); 
 
        JPanel P = new JPanel();
-
        P.setLayout( new GridLayout( 3 , 1 ));
-
-       P.add(new JLabel(parameter.getName(), javax.swing.SwingConstants.CENTER));
-
-       P.add(new JLabel( "Select DataType" , javax.swing.SwingConstants.CENTER ) );
-
+       P.add(new JLabel(parameter.getName(),javax.swing.SwingConstants.CENTER));
+       P.add(new JLabel("Select DataType", javax.swing.SwingConstants.CENTER));
        P.add( new JLabel("Form Array ", javax.swing.SwingConstants.CENTER ) );
-
        segment.add( P );
 
        P = new JPanel();
-
        P.setLayout(new GridLayout( 3,1));
-
        P.add(intText);
 
        DataType= new JComboBox();
+       DataType.addItem("Integer");
+       DataType.addItem("Float");
+       DataType.addItem("String");
 
-         DataType.addItem("Integer");
-
-         DataType.addItem("Float");
-
-         DataType.addItem("String");
-
-         DataType.setEditable(false);
-
+       DataType.setEditable(false);
        P.add(DataType);
 
        Array= new JCheckBox("Array" , false);
-
        P.add(Array);
-
-       // P.setBorder( new LineBorder( Color.black) ); 
-
        segment.add( P );
-
        segment.setBorder( new LineBorder( Color.black) ); 
-
        DataType.addItemListener( this);
-
-   
-
     }
-
-    
-
-    
-
     
 
     public JPanel getGUISegment()
 
     {
-
         return segment;
-
-        
-
     }
 
 
-
-
-
     public Parameter getParameter()
-
-    {   int i;
-
+    {   
+        int i;
         String s = intText.getText();
-
         int dt = DataType.getSelectedIndex();
 
         if( dt < 0 )
-
             return parameter;
 
         Object U[] =Array.getSelectedObjects();
 
-        
-
         boolean Ar ;
 
         if( U== null)
-
           Ar = false;
 
         else 
-
           Ar = true;
 
         try{
-
           if( Ar)
-
-           { int c = 0;
+           {
+              int c = 0;
 
              for( i = intText.getText().indexOf(","); 
-
-                        (i >= 0) && (i < intText.getText().length()); )
-
-               {c++;
-
-                i =intText.getText().indexOf(",", i + 1);
-
-              }
+                (i >= 0) && (i < intText.getText().length()); )
+             {
+               c++;
+               i =intText.getText().indexOf(",", i + 1);
+             }
 
              Object X[];
 
              if( dt == 0)
-
-               { X = new Integer[c+1];
-
-               }
+               X = new Integer[c+1];
 
              else if( dt == 1)
-
-               { X = new Float[c+1];
-
-               }         
+               X = new Float[c+1];
 
              else if( dt == 2)
-
-               { X = new String[c+1];
-
-               }
+               X = new String[c+1];
 
              else
-
                X = null;
 
              int st = 0;
-
              String S = intText.getText() ;
-
              int en;
 
              for( i = 0; i < c+1; i++)
+             { 
+               if( i < c)
+                 en = S.indexOf("," , st );
+               else 
+                 en = S.length(); 
 
-               { if( i < c)
+               if( dt == 0)
+                 X[i] = new Integer( S.substring(st, en));
+               else if( dt == 1)
+                 X[i] = new Float( S.substring(st, en));
+               else if( dt == 2)
+                 X[i] = new String( S.substring(st, en));
 
-                   en = S.indexOf("," , st );
-
-                 else 
-
-                   en = S.length(); 
-
-                  if( dt == 0)
-
-                    X[i] = new Integer( S.substring(st, en));
-
-                  else if( dt == 1)
-
-                    X[i] = new Float( S.substring(st, en));
-
-                  else if( dt == 2)
-
-                    X[i] = new String( S.substring(st, en));
-
-                  st = en + 1;
-
-  
-
-               } 
+               st = en + 1;
+             } 
 
              parameter.setValue( X);        
-
-              
-
-            
-
              return parameter;
-
            }
 
          if(dt == 0)
-
             parameter.setValue( new Integer( intText.getText()));
 
          else if( dt == 1)
-
            parameter.setValue( new Float( intText.getText()));
 
          else if( dt == 2)
-
            parameter.setValue( intText.getText());
-
       }
 
-     catch( NumberFormatException z)
-
-        { parameter.setValue( null );
-
-          serror = "Number Format Error";
-
+       catch( NumberFormatException z)
+       { 
+         parameter.setValue( null );
+         serror = "Number Format Error";
        }
 
-      
-
-       
-
-       // parameter.setValue(value);
-
         return parameter;
-
     }
 
- public void itemStateChanged(ItemEvent e)
-
-   { if( DataType.getSelectedIndex() == 2)
-
-         Array.setEnabled( false);
-
-     else
-
-         Array.setEnabled( true);
-
+  public void itemStateChanged(ItemEvent e)
+  { if( DataType.getSelectedIndex() == 2)
+      Array.setEnabled( false);
+    else
+      Array.setEnabled( true);
    }
 
 /*   public static void main( String args[])
+    { 
+     JFrame F; JObjectParameterGUI JO;
 
-    { JFrame F; JObjectParameterGUI JO;
-
-
-
-      F = new JFrame(" JObjectParameterGUI");
-
+     F = new JFrame(" JObjectParameterGUI");
      F.setSize( 400, 200 );
-
      JO = new JObjectParameterGUI( new Parameter("Joe", null));
-
      F.getContentPane().add(JO.getGUISegment());
-
      F.show();
-
      F.validate();
-
      char c='g';
 
      while( c >= ' ' )
-
-     {try{
-
+     {
+     try
+     {
          for( c=0; c< ' ';       c = (char) System.in.read()){}
-
-        }
+     }
 
      catch(java.io.IOException s){}
-
      Parameter P= JO.getParameter();
-
      System.out.print(P.getName()+",");
 
      if(P.getValue() == null )
-
         System.out.println( "null");
-
      else
-
        System.out.println(P.getValue());
-
      }
-
-          
-
     }
-
 */
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
