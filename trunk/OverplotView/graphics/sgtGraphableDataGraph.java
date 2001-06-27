b6b1eb0bd7e9041f194a42ef4b1d61cd787547d9
neffk <neffk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-06-27 16:51:36 +0000

    this class now implements the IGraphableDataGraph interface.

--- trunk/OverplotView/graphics/sgtGraphableDataGraph.java
+++ trunk/OverplotView/graphics/sgtGraphableDataGraph.java
@@ -3,37 +3,14 @@ package OverplotView.graphics;
 /**
  * $Id$
  *
+ * graphical output panel
+ *
  * $Log$
- * Revision 1.1  2001/06/21 21:46:30  neffk
- * *** empty log message ***
+ * Revision 1.2  2001/06/27 16:51:36  neffk
+ * this class now implements the IGraphableDataGraph interface.
  *
  */
 
-import java.util.Vector;
-import OverplotView.GraphableDataGraph;
-
-public class sgtGraphableDataGraph 
-  implements GraphableDataGraph
-{
-
-  public void init( Vector gd )
-  {
-    System.out.println( "selected:" );
-    for( int i=0;  i<gd.size();  i++ )
-      System.out.println(  gd.get(i).toString()  );
-
-    System.out.println( "----------" );
-  }
-
-
-  public void redraw()
-  {
-    System.out.println( 
-      "if there were a graphic output, it'd be redrawing right now" );
-  }
-}
-
-import java.util.Vector;
 
 import gov.noaa.pmel.sgt.swing.JPlotLayout;
 import gov.noaa.pmel.sgt.swing.JClassTree;
@@ -41,20 +18,29 @@ import gov.noaa.pmel.sgt.swing.prop.LineAttributeDialog;
 import gov.noaa.pmel.sgt.JPane;
 import gov.noaa.pmel.sgt.LineCartesianRenderer;
 import gov.noaa.pmel.sgt.LineAttribute;
-
 import gov.noaa.pmel.sgt.demo.TestData;
 import gov.noaa.pmel.util.GeoDate;
 import gov.noaa.pmel.util.TimeRange;
 import gov.noaa.pmel.util.IllegalTimeValue;
-
 import gov.noaa.pmel.sgt.dm.*;
 
-import java.awt.*;
-import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Vector;
 
-public class JTimeSeries extends JApplet {
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+
+import OverplotView.GraphableData;
+import OverplotView.IGraphableDataGraph;
+
+
+public class sgtGraphableDataGraph 
+  extends JApplet
+  implements IGraphableDataGraph
+{ 
+
   JButton tree_;
   JButton space_ = null;
   JPane pane_;
@@ -64,101 +50,28 @@ public class JTimeSeries extends JApplet {
   Vector data;
   JPlotLayout ltsl;
 
-  
-
-  /** default constructor */
-  public JTimeSeries()
+  /** 
+   * default constructor 
+   */
+  public sgtGraphableDataGraph()
   {
     data = new Vector();
   }
 
 
 
-  /** takes a vector of SimpleLine objects.  GraphableDataManager has already
-   *  taken care of the messy details of offsets and such, so all we have 
-   *  to do is visualize the data.
+
+
+  /**
+   * shows editable attributes when editable items are clicked upon
    */
-  public JTimeSeries( Vector d )
-  {
-    if( data.size() == 0 )
-      data = d;
-    else
-      for( int i=0;  i<d.size();  i++ )
-        data.addElement( d.elementAt(i) );
-  }
-
-
-
-  public void addData( Collection c )
-  {
-    data.add( c );
-  }
-
-
-
-  public void addData( Vector d )
-  {
-    if( data.size() == 0 )
-      data = d;
-    else
-      for( int i=0;  i<d.size();  i++ )
-        data.addElement( d.elementAt(i) );
-  }
-
-
- 
-  /** this is the main method of this class.  it creates a graphics screen 
-   * and adds all of the current data to it. 
-   */
-  JPlotLayout makeGraph() 
-  {
-    
-    ltsl = new JPlotLayout( (SGTData)data.firstElement(), 
-                            "Time Series Demo", 
-                            null, 
-                            false );
-
-
-    for( int i=1;  i<data.size();  i++ )
-      //ltsl.addData( (SGTData)data.get(i), ""+i );
-      //ltsl.addData( (Collection)data.get(i), ""+i );
-      ltsl.addData( (SGTLine)data.get(i), ""+i );
-
-
-    ltsl.setTitles("Time Series Demo", 
-                   "using JPlotLayout", 
-                   "");
-
-    myMouse_ = new MyMouse();
-    ltsl.addMouseListener(myMouse_);
-
-    ltsl.setOverlayed( false );
-    return ltsl;
-  }
-
-
-
-  class MyAction implements java.awt.event.ActionListener {
-    public void actionPerformed(java.awt.event.ActionEvent event) {
-      System.out.println( "mouse event" );
-      /*
-      Object obj = event.getSource();
-      if(obj == space_) {
-	System.out.println("  <<Mark>>");
-      }
-      if(obj == tree_)
-	tree_actionPerformed(event);
-      */
-    }
-  }
-
   class MyMouse extends MouseAdapter {
     public void mouseReleased(MouseEvent event) {
       Object object = event.getSource();
       if(object == pane_)
 	maybeShowLineAttributeDialog(event);
     }
-
+    
     void maybeShowLineAttributeDialog(MouseEvent e) {
       if(e.isPopupTrigger() || e.getClickCount() == 2) {
 	Object obj = pane_.getObjectAt(e.getX(), e.getY());
@@ -175,4 +88,64 @@ public class JTimeSeries extends JApplet {
       }
     }
   }
+
+
+  private SGTData convert_GraphableData_to_SGTData( GraphableData d )
+  {
+
+//TODO: code this converstion function
+
+    return new SimpleLine();
+  }
+
+
+/*----------------------=[ required interface methods ]=----------------------*/
+
+
+  /**
+   * adds data to this container or replaces it.  in order to keep things
+   * simple for me, there's no way to incrementally build this graph by adding
+   * one bit of data at a time, so developers should keep track of it in their 
+   * own code. 
+   */
+  public void init( Vector gd )
+  {
+    if( data.size() == 0 )
+      data = gd;
+    else
+      for( int i=0;  i<gd.size();  i++ )
+        data.addElement( 
+          convert_GraphableData_to_SGTData( (GraphableData)gd.elementAt(i) )  );
+  }
+
+
+  /**
+   * draws the graph
+   */
+  public JComponent redraw()
+  {
+    System.out.println( 
+      "if there were a graphic output, it'd be redrawing right now" );
+
+    ltsl = new JPlotLayout( (SGTData)data.firstElement(), 
+                            "Time Series Demo", 
+                            null, 
+                            false );
+
+    for( int i=1;  i<data.size();  i++ )
+      //ltsl.addData( (SGTData)data.get(i), ""+i );
+      //ltsl.addData( (Collection)data.get(i), ""+i );
+      ltsl.addData( (SGTLine)data.get(i), ""+i );
+
+    ltsl.setTitles("Time Series Demo", 
+                   "using JPlotLayout", 
+                   "");
+
+    myMouse_ = new MyMouse();
+    ltsl.addMouseListener(myMouse_);
+
+    ltsl.setOverlayed( false );
+    return ltsl;
+  }
+
 }
