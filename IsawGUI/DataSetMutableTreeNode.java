6b88249c2fffb03d13be452cd5f06f85531a4a35
neffk <neffk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-07-18 16:38:02 +0000

    uses DataMutableTreeNodes instead of DefaultMutableTreeNodes. also, fixed setParent(...) to set the parent of this node, not its children.  (oops)

--- IsawGUI/DataSetMutableTreeNode.java
+++ IsawGUI/DataSetMutableTreeNode.java
@@ -3,6 +3,11 @@
  * $Id$
  *
  * $Log$
+ * Revision 1.2  2001/07/18 16:38:02  neffk
+ * uses DataMutableTreeNodes instead of DefaultMutableTreeNodes.
+ * also, fixed setParent(...) to set the parent of this node, not
+ * its children.  (oops)
+ *
  * Revision 1.1  2001/07/15 05:26:23  neffk
  * contains Data objects in JDataTree.
  *
@@ -15,7 +20,6 @@ import java.util.Enumeration;
 import java.util.Vector;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  * encapsulates a collection of Data objects.
@@ -24,10 +28,10 @@ public class DataSetMutableTreeNode
   implements TreeNode, MutableTreeNode
 {
 
-  private DataSet         ds         = null;
-  private MutableTreeNode parent     = null;
-  private Vector          data_nodes = null;
-  private String          name       = null;
+  private DataSet         ds          = null;
+  private MutableTreeNode parent      = null;
+  private Vector          data_nodes  = null;
+  private String          name        = null;
 
 
   public DataSetMutableTreeNode( DataSet ds )
@@ -134,22 +138,22 @@ public class DataSetMutableTreeNode
   public void removeFromParent()
   {
     for( int i=0;  i<data_nodes.size();  i++ )
-      ( (DefaultMutableTreeNode)data_nodes.get(i) ).removeFromParent();
+      ( (DataMutableTreeNode)data_nodes.get(i) ).removeFromParent();
   }
 
 
   public void setParent( MutableTreeNode node )
   {
     parent = node;
-   
-    for( int i=0;  i<data_nodes.size();  i++ )
-      ( (DefaultMutableTreeNode)data_nodes.get(i) ).setParent( parent );
   }
 
 
   /**
    * sets this object to contain nodes of the Data objects
-   * contained by in the DataSet parameter of this function.
+   * contained by in the DataSet parameter of this function.  also, this
+   * method resets all previous selections.  use this method to 
+   * make the tree reflect changes in the ordering of DataSet objects
+   * (after sorting operations, etc).
    */
   public void setUserObject( Object obj )
   {
@@ -157,7 +161,12 @@ public class DataSetMutableTreeNode
     data_nodes = new Vector();
     
     for( int i=0;  i<ds.getNum_entries();  i++ )
-      data_nodes.addElement(  new DefaultMutableTreeNode( ds.getData_entry(i) )  );
+    {
+      DataMutableTreeNode node = new DataMutableTreeNode( ds.getData_entry(i) );
+      node.setParent( this );
+
+      data_nodes.addElement( node );
+    }
   }
 
 
