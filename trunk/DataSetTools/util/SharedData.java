08a7fa286a4348827eb43ed2f7501cd1937849c6
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-07-23 19:04:20 +0000

    Utility class to intialize and/or contain data of which there should be only one copy and that will be shared by several packages.

---
+++ trunk/DataSetTools/util/SharedData.java
@@ -0,0 +1,57 @@
+/*
+ * File: SharedData.java
+ *
+ * Copyright (C) 2001, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2001/07/23 19:04:20  dennis
+ *  Utility class to intialize and/or contain data of which there
+ *  should be only one copy and that will be shared by several
+ *  packages.
+ *
+ */
+
+package DataSetTools.util;
+
+/**
+ *  Objects of this class have one instance of objects that are to be shared
+ *  by several packages.  The shared objects are instantiated one time 
+ *  as static members of the class.
+ */
+
+public class SharedData implements java.io.Serializable 
+{
+
+  public static final String VERSION    = "1.1 alpha";
+  public static final String BUILD_DATE = "2001/07/23";
+  public static final PropertiesLoader isaw_props 
+                                        = new PropertiesLoader("IsawProps.dat");
+  
+}
