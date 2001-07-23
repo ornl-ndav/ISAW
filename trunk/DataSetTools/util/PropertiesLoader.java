f1c0277b0425f7c9e3148a08cdacdd5b3e0f7d93
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-07-23 19:03:18 +0000

    Utility class to load System properties one time.

---
+++ trunk/DataSetTools/util/PropertiesLoader.java
@@ -0,0 +1,78 @@
+/*
+ * File: PropertiesLoader.java
+ *
+ * Copyright (C) 2001, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2001/07/23 19:03:18  dennis
+ *  Utility class to load System properties one time.
+ *
+ */
+package DataSetTools.util;
+
+import java.util.*;
+import java.io.*;
+
+
+/**
+ *  Constructing an object of this class will load the system properties
+ *  from the specified properties file.  If the properties file is edited,
+ *  the properties file may be re-read by calling the reload() method.
+ */
+
+public class PropertiesLoader implements java.io.Serializable 
+{
+  private String f_name = "";
+
+  public PropertiesLoader( String file_name )
+  {
+    f_name = file_name;
+    reload();
+  }
+
+  public void reload()
+  {
+    String full_name = System.getProperty( "user.home" ) + "/" + f_name;
+    try
+    {
+      FileInputStream input = new FileInputStream( full_name );
+      Properties isawProp = new Properties( System.getProperties() );
+      isawProp.load( input );
+      System.setProperties( isawProp );
+      input.close();
+    }
+    catch ( IOException e )
+    {
+      System.out.println("Properties file: " + f_name + " NOT FOUND" );
+      return;
+    }  
+  }
+  
+}
