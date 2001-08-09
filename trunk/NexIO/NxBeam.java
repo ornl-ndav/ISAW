305883b7ea9c5378ce5a1aae5b921e2568c962f0
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-08-09 16:44:02 +0000

    Added Documentation

--- trunk/NexIO/NxBeam.java
+++ trunk/NexIO/NxBeam.java
@@ -1,3 +1,40 @@
+/*
+ * File:  NxBean.java 
+ *             
+ * Copyright (C) 2001, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.2  2001/08/09 16:44:02  rmikk
+ * Added Documentation
+ *
+ */
 package NexIO;
 
 import DataSetTools.dataset.*;
@@ -7,22 +44,22 @@ public class NxBeam
 {
     String errormessage;
     public NxBeam()
-    {errormessage ="";}
+    {errormessage = "";}
     public String getErrorMessage()
      { return errormessage;
      }
-    public boolean processDS( NxNode node,  DataSet DS)
-    {errormessage= "Improper inputs to NxBeam";
+    public boolean processDS(  NxNode node,  DataSet DS)
+    {errormessage = "Improper inputs to NxBeam";
     if( node == null)
 	return true;
     if( DS == null)
        return true;
-    if( !node.getNodeClass().equals("NXbeam"))
+    if( !node.getNodeClass().equals( "NXbeam"))
        return true;
-    errormessage ="";
+    errormessage = "";
      NxData_Gen ng = new NxData_Gen();
     NXData_util nu = new NXData_util();
-    NxNode n1= node.getChildNode( "incident_energy");
+    NxNode n1 = node.getChildNode( "incident_energy");
     if( n1 != null)
       {Object O = n1.getNodeValue();
        float x[];
@@ -32,7 +69,7 @@ public class NxBeam
          if( x.length > 0)
 	     f = x[0];
          else
-            f= Float.NaN;
+            f = Float.NaN;
        else f = Float.NaN;
       if( f == Float.NaN)
        {Float F = ng.cnvertoFloat( O );
@@ -51,7 +88,7 @@ public class NxBeam
       }      
 
 //  energy out
-  n1= node.getChildNode( "final_energy");
+  n1 = node.getChildNode( "final_energy");
     if( n1 != null)
       {Object O = n1.getNodeValue();
        float x[];
@@ -61,7 +98,7 @@ public class NxBeam
          if( x.length > 0)
 	     f = x[0];
          else
-            f= Float.NaN;
+            f = Float.NaN;
        else f = Float.NaN;
       if( f == Float.NaN)
        {Float F = ng.cnvertoFloat( O );
