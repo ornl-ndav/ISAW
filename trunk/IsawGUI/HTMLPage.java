f6e6010cf34966111e3e68acbfaa7b4a3ba2a966
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2001-11-12 21:36:54 +0000

    1. Fixed the "BACK" in The help page with the list of all      installed commands.
      2. Added GPL and some java-doc's.

--- trunk/IsawGUI/HTMLPage.java
+++ trunk/IsawGUI/HTMLPage.java
@@ -1,3 +1,44 @@
+/*
+ * File: HTMLPage.java 
+ *             
+ * Copyright (C) 1999-2001, Alok Chatterjee, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Alok Chatterjee achatterjee@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 S. Cass Avenue, Bldg 360
+ *           Argonne, IL 60440
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.6  2001/11/12 21:36:54  dennis
+ *   1. Fixed the "BACK" in The help page with the list of all
+ *      installed commands.
+ *
+ *   2. Added GPL and some java-doc's.
+ *
+*/
 //import javax.swing.*;
 //import javax.swing.event.*;
 package IsawGUI;
@@ -10,9 +51,26 @@ import java.io.IOException;
 import java.net.*;
 import Command.*;
 import DataSetTools.operator.*;
+/** Creates a pop up window that can display rudimentary HTML pages or pages that are
+*   customized to help on the operators.
+*/
 public class HTMLPage extends JFrame {
 	private JEditorPane editorPane = new JEditorPane();
         String OperatorPane = null;
+
+/** Constructor and creator of the HTML viewer.
+*
+*@param  url  The url of the original page
+*
+*NOTE: This HTML Page has been customized as follows <OL Type = "A">
+ *<LI> If the href contains XX$netscape, the browser will appear pointing to the java docs for
+ *     the data set operators
+ *<LI> If the href contains XX$Panel1, Text will appear listing all the installed Generic 
+ *     operators with links for each of the operators containg further information
+ *<LI> if the href is XX$Panel2 with reference ref, text will appear giving further information
+ *     on the ref's operator.  This information includes the full class name or filename if
+ *     it is a script, the command, the title, and the argument data types.     
+*/
 	public HTMLPage(String url)
 	{
 		Container contentPane = getContentPane();
@@ -21,7 +79,8 @@ public class HTMLPage extends JFrame {
 		try { 
 			editorPane.setPage(url);
 		}
-		catch(IOException ex) { ex.printStackTrace(); }
+		catch(IOException ex) { ex.printStackTrace(); 
+                                         return; }
 
 		contentPane.add(new JScrollPane(editorPane), 
 						BorderLayout.CENTER);
@@ -54,7 +113,9 @@ public class HTMLPage extends JFrame {
                                            { 
                                              SetText( editorPane , U.getRef() );
                                            }
-					else 
+					else if( U.getFile().indexOf("XX$Panel3")>=0)
+                                            SetTextBack( editorPane );
+                                        else
                                              editorPane.setPage(e.getURL());
 				}
 			catch(IOException ex) { ex.printStackTrace(); }
@@ -97,7 +158,7 @@ public class HTMLPage extends JFrame {
     }
   private void setUpOpPanel()
     {Script_Class_List_Handler SH = new Script_Class_List_Handler();
-     String Textt="<HTML><BODY><A href=\"Commands.html\">BACK</a><P>";
+     String Textt="<HTML><BODY><A href=\"XX$Panel3\">BACK</a><P>";
      Textt +=" <Center><H1>Current Operators</H1></Center>";
      int n = SH.getNum_operators();
      int nrows = n / 4 ;
@@ -135,9 +196,37 @@ public class HTMLPage extends JFrame {
      JP.setText(OperatorPane);
      
     }
+ private void SetTextBack( JEditorPane JP)
+    {String P ="<HTML><BODY><A href=\"CommandPane.html\"> CONTENTS </a><P>";
+
+     P+="<h3><Center> Commands</center></H3>";
+
+      P+="<OL Type=\"A\">";
+      P+="<LI> Arithmetic,Logic and Assignment Operations; <A href=\"ComDes.html\">+,-, *, /, & ,";
+      P+=" ^(power) and = </a>, AND, Or, Not<P>";
+      P+="<LI> Subroutines:";
+    P+="<ul>";
+   P+="<A href=\"ComDes.html#Load\">Load</a>   ,    <A href=\"ComDes.html#Display\">Display</a> ,";
+        P+=" <A href=\"ComDes.html#Send\">Send</a> ,  <A href=\"ComDes.html#Save\">Save</a><P>";
+    P+="</ul>";
+     P+="<LI> Possible Data Set Operations.  Not all are available for a specific data set";
+   
+    P+="<ul> <li><A href = \"XX$netscape\">Browse documentation for operators written in Java</a>";
+    P+="<li> <A href =\"XX$Panel1\">List of available commands with argument data types</a>";
+   P+="       <BR><Center> Wait the first time </Center>";
+   P+="</ul>";
+
+   P+="</ol>";
+
+   P+="</body></html>";
+
+   JP.setText(P);
+     }
 	public static void main(String args[]) {
 		GJApp.launch(new HTMLPage("http://www.pns.anl.gov/gppd/index.htm"), 
 					"JEditorPane",300,300,450,300);
+
+                
 	}
 }
 class GJApp extends WindowAdapter {
