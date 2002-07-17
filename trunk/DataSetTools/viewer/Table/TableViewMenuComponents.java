c0c5a416ace05a817425a135699e0b70f5664149
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2002-07-17 19:11:39 +0000

    Added GPL Fixed up the table views menu choices

--- trunk/DataSetTools/viewer/Table/TableViewMenuComponents.java
+++ trunk/DataSetTools/viewer/Table/TableViewMenuComponents.java
@@ -1,11 +1,51 @@
 
-
+/*
+ * File:  TableViewMenuComponents.java
+ *
+ * Copyright (C) 2000, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.2  2002/07/17 19:11:39  rmikk
+ *  Added GPL
+ *  Fixed up the table views menu choices
+ *
+ */
 package DataSetTools.viewer.Table;
 
 import javax.swing.*;
 import java.awt.event.*;
 import DataSetTools.dataset.*;
 import DataSetTools.viewer.*;
+/** Can be used to set Menu components and retrieve their corresponding
+*   operations
+*/
 public class TableViewMenuComponents  
   { boolean error, index;
     JCheckBoxMenuItem jmErr=null; 
@@ -16,25 +56,29 @@ public class TableViewMenuComponents
       index = false;
   
      }
-  public JCheckBoxMenuItem  getErrorItem()
+  private JCheckBoxMenuItem  getErrorItem()
      {jmErr = new JCheckBoxMenuItem("Show Errors");
       jmErr.addActionListener( new MyActionListener(0));
       return jmErr;
       }
-  public JCheckBoxMenuItem  getIndexItem()
+ private JCheckBoxMenuItem  getIndexItem()
      {jmInd = new JCheckBoxMenuItem("Show Indicies");
       jmInd.addActionListener( new MyActionListener(1));
       return jmInd;
       }
-  public boolean getErrors()
+  private boolean getErrors()
     {return error;}
 
-  public boolean getIndex()
+ private boolean getIndex()
     {return index;}
-
+  /** Returns the number of menu items
+  */
   public static int getNMenuItems()
     { return 3;
     }
+
+  /** Returns the menu Name associated with the menu items
+  */
    public static String getNameMenuItem( int i)
     { if( i < 0)
          return null;
@@ -47,6 +91,8 @@ public class TableViewMenuComponents
       else return null;
     }
 
+ /** Adds associated Menu items to the JMenu Tables  with the given actionlistener
+ */
  public void addMenuItems( JMenu Tables , ActionListener view_menu_handler)
    { JMenuItem button;
     for( int i = 0; i < getNMenuItems(); i++) 
@@ -54,26 +100,34 @@ public class TableViewMenuComponents
       button.addActionListener( view_menu_handler );
       Tables.add( button);
       }
-    
+  /*  
     Tables.addSeparator();
     Tables.add( getErrorItem());
     Tables.add( getIndexItem());
+    jmErr.setState( error);
+    jmInd.setState( index);
+  */
 
    }
+
+ /** Returns the TableView associated with the view_type that corresponds to the
+ * Name associated with the Menu items
+ */
  public TableView getDataSetViewer( String view_type, DataSet DS, ViewerState state)
    {
-    table_view tv= new table_view(0);
+    /*table_view tv= new table_view(0);
     String state1 = "X values;Y values;";
     if( error || (view_type.indexOf("err")>=0))
       state1 += "Error values;";
     if( index || (view_type.indexOf("indx") >=0))
       state1 += "XY index;";
-   
+    state.set_String( ViewerState.TABLE_DATA, state1);
     DataSet[] DSS = new DataSet[1];
     DSS[0] = DS;
     tv.setDataSets( DSS );
     tv.restoreState( state1);
     DefaultListModel LM = tv.getListModel();
+    */
    // for( int j = 0; j<LM.size();j++)
    //   System.out.println("LM+"+LM.elementAt(j));
     if( DS.getSelectedIndices().length<1)
@@ -81,16 +135,16 @@ public class TableViewMenuComponents
         return null;
        }
     if( view_type.indexOf("Gr,Time vs Field")==0)
-      return new TableView( DS, state,tv.getGenTableModel( DS,LM,"HGT,F",DS.getSelectedIndices() ));
+      return new TableView( DS, state,"HGT,F");//tv.getGenTableModel( DS,LM,"HGT,F",DS.getSelectedIndices() ));
     if( view_type.indexOf("Time vs Gr,Field")==0)
-       return new TableView( DS, state,tv.getGenTableModel( DS,LM,"HT,FG",DS.getSelectedIndices() ));
+       return new TableView( DS, state,"HT,GF");//tv.getGenTableModel( DS,LM,"HT,FG",DS.getSelectedIndices() ));
     if(view_type.indexOf("Time,Row vs Col")==0)
-       return new TableView( DS, state,tv.getGenTableModel( DS,LM,"HTI,JF",DS.getSelectedIndices() ));
+       return new TableView( DS, state,"HTI,JF");//tv.getGenTableModel( DS,LM,"HTI,JF",DS.getSelectedIndices() ));
     
     return null;
 
    }
- class MyActionListener implements ActionListener
+ private class MyActionListener implements ActionListener
     {int action;
      public MyActionListener( int action)
        { this.action = action;}
