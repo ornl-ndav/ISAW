aa368ae6279a2d8556bdb39f0f209a4db7738b5f
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-01-13 17:33:44 +0000

    Added getDocumentation() method, main test code and javadocs for getResult().  (Chris Bouzek)

--- trunk/DataSetTools/operator/DataSet/Special/MonitorPeakArea.java
+++ trunk/DataSetTools/operator/DataSet/Special/MonitorPeakArea.java
@@ -1,5 +1,5 @@
 /*
- * File:  MonitorPeakArea.java  
+ * File:  MonitorPeakArea.java
  *
  * Copyright (C) 2000, Dennis Mikkelson
  *
@@ -28,8 +28,12 @@
  * For further information, see <http://www.pns.anl.gov/ISAW/>
  *
  * Modified:
- *             
+ *
  * $Log$
+ * Revision 1.4  2003/01/13 17:33:44  dennis
+ * Added getDocumentation() method, main test code and javadocs for
+ * getResult().  (Chris Bouzek)
+ *
  * Revision 1.3  2002/11/27 23:19:20  pfpeterson
  * standardized header
  *
@@ -51,19 +55,20 @@ import  DataSetTools.math.*;
 import  DataSetTools.peak.*;
 import  DataSetTools.operator.Parameter;
 import  DataSetTools.parameter.*;
+import  DataSetTools.retriever.*;
 
 /**
   *  This operator calculates the area under a peak in a specified monitor
-  *  using a specified extent factor. The Group ID of the monitor to be 
-  *  integrated is specified by the parameter "Group ID".  The interval 
-  *  [a,b] over which the integration is determined based on the "extent 
-  *  factor".  Specifically, the peak is used over the interval of length
-  *  (extent_factor * FWHM) centered on the peak.
-  *  This operator just produces a numerical result that is displayed 
+  *  using a specified extent factor. The Group ID of the monitor to be
+  *  integrated is specified by the parameter "Group ID".  The interval
+  *  [a,b] over which the integration is determined based on the "extent
+  *  factor".  Specifically, the peak is integrated over the interval of
+  *  length (extent_factor * FWHM) centered on the peak.
+  *  This operator just produces a numerical result that is displayed
   *  in the operator dialog box.
   */
 
-public class MonitorPeakArea  extends    DS_Special 
+public class MonitorPeakArea  extends    DS_Special
                               implements Serializable
 {
   /* ------------------------ DEFAULT CONSTRUCTOR -------------------------- */
@@ -83,20 +88,20 @@ public class MonitorPeakArea  extends    DS_Special
   /* ---------------------- FULL CONSTRUCTOR ---------------------------- */
   /**
    *  Construct an operator to find the area under a monitor peak for
-   *  the specified monitor DataSet using the specified group_id and 
-   *  extent factor. 
+   *  the specified monitor DataSet using the specified group_id and
+   *  extent factor.
    *
    *  @param  ds              The monitor DataSet from which a peak area is to
-   *                          be found. 
+   *                          be found.
    *  @param  group_id        The group_id of the monitor Data block that is
    *                          to be integrated.
    *  @param  extent_factor   The peak is integrated over an interval of
-   *                          length extent_factor*FWHM centered on the peak. 
+   *                          length extent_factor*FWHM centered on the peak.
    */
 
   public MonitorPeakArea( DataSet      ds,
                           int          group_id,
-                          float        extent_factor ) 
+                          float        extent_factor )
   {
     this();                         // do the default constructor, then set
                                     // the parameter value(s) by altering a
@@ -114,7 +119,8 @@ public class MonitorPeakArea  extends    DS_Special
 
   /* ---------------------------- getCommand ------------------------------- */
   /**
-   * @return	the command name to be used with script processor: in this case, PeakA
+   * @return the command name to be used with script processor: 
+   *         in this case, PeakA
    */
    public String getCommand()
    {
@@ -122,7 +128,7 @@ public class MonitorPeakArea  extends    DS_Special
    }
 
 
- /* -------------------------- setDefaultParmeters ------------------------- */
+ /* -------------------------- setDefaultParameters ------------------------- */
  /**
   *  Set the parameters to default values.
   */
@@ -138,23 +144,57 @@ public class MonitorPeakArea  extends    DS_Special
 
   }
 
+  /* ---------------------- getDocumentation --------------------------- */
+  /**
+   *  Returns the documentation for this method as a String.  The format
+   *  follows standard JavaDoc conventions.
+   */
+  public String getDocumentation()
+  {
+    StringBuffer s = new StringBuffer("");
+    s.append("@overview This operator calculates the area under a peak in ");
+    s.append("a specified monitor using a specified extent factor.\n");
+    s.append("@assumptions The specified group ID corresponds to a valid ");
+    s.append("data entry.\n");
+    s.append("@algorithm First this operator acquires the data entry ");
+    s.append("corresponding to the group_id parameter.\n");
+    s.append("Then it uses the extent_factor parameter to determine over ");
+    s.append("which interval to integrate.\n");
+    s.append("Finally it calculates the area by integration.\n");
+    s.append("@param ds The monitor DataSet from which a peak area is to ");
+    s.append("be found.\n");
+    s.append("@param group_id The group_id of the monitor Data block that ");
+    s.append("is to be integrated.\n");
+    s.append("@param extent_factor The peak is integrated over an interval ");
+    s.append("of length extent_factor*FWHM centered on the peak.\n");
+    s.append("@return Float object which represents the area under the ");
+    s.append("specified peak.\n");
+    s.append("@error Returns an error if the specified group ID does not ");
+    s.append("correspond to a valid data entry.\n");
+    return s.toString();
+  }
 
   /* ---------------------------- getResult ------------------------------- */
-
+  /**
+   *  Calculates the area under a peak based on a group ID and an extent
+   *  factor.
+   *
+   *  @return Float object which represents the area under the specified peak.
+   */
   public Object getResult()
   {                                  // get the parameters
 
     int group_id        = ( (Integer)(getParameter(0).getValue()) ).intValue();
     float extent_factor = ( (Float)(getParameter(1).getValue()) ).floatValue();
 
-                                     // get the current data set and do the 
+                                     // get the current data set and do the
                                      // operation
     DataSet ds = this.getDataSet();
 
     Data data = ds.getData_entry_with_id( group_id );
     if ( data == null )
     {
-      ErrorString message = new ErrorString( 
+      ErrorString message = new ErrorString(
                            "ERROR: no data entry with the group_ID "+group_id );
       System.out.println( message );
       return message;
@@ -164,14 +204,14 @@ public class MonitorPeakArea  extends    DS_Special
       HistogramDataPeak peak = new HistogramDataPeak( data, extent_factor );
       peak.setEvaluationMode( IPeak.PEAK_ONLY );
       float result = peak.Area();
-      return new Float( result );  
+      return new Float( result );
     }
-  }  
+  }
 
   /* ------------------------------ clone ------------------------------- */
   /**
-   * Get a copy of the current MonitorPeakArea Operator.  The list of 
-   * parameters and the reference to the DataSet to which it applies are 
+   * Get a copy of the current MonitorPeakArea Operator.  The list of
+   * parameters and the reference to the DataSet to which it applies are
    * also copied.
    */
   public Object clone()
@@ -191,13 +231,45 @@ public class MonitorPeakArea  extends    DS_Special
    */
   public static void main( String[] args )
   {
-    MonitorPeakArea op = new MonitorPeakArea();
+    DataSet ds1 = null;
+    int groupID;
+    float extentFactor;
+    StringBuffer m = new StringBuffer();
+    MonitorPeakArea op;
 
-    String list[] = op.getCategoryList();
-    System.out.println( "Categories are: " );
-    for ( int i = 0; i < list.length; i++ )
-      System.out.println( list[i] );
+    groupID = 26;
+    extentFactor = (float)10.0;
+
+    String file_name = "/home/groups/SCD_PROJECT/SampleRuns/hrcs2447.run ";
+                       //"D:\\ISAW\\SampleRuns\\hrcs2447.run";
+
+    try
+    {
+      RunfileRetriever rr = new RunfileRetriever( file_name );
+      ds1 = rr.getDataSet(1);
+    }
+    catch(Exception e)
+    {
+      System.err.println("Error opening file");
+    }
+
+    op = new MonitorPeakArea(ds1, groupID, extentFactor);
+
+    Object result = op.getResult();
+    m.append("\nThe results of calling getResult() for ");
+    m.append("FocusIncidentSpectrum for file ");
+    m.append(file_name);
+    m.append(" on a group ID of ");
+    m.append(groupID);
+    m.append(" and an extent factor of ");
+    m.append(extentFactor);
+    m.append(" are:\n\n");
+    m.append(result.toString());
+    m.append("\n\nThe results of calling getDocumentation() for ");
+    m.append("FocusIncidentSpectrum are:\n\n");
+    m.append(op.getDocumentation());
+
+    System.out.print(m.toString());
   }
-
 
 }
