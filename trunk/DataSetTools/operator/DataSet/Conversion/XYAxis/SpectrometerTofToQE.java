1f5a85039f601f30947460f176976ff04c19841b
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-01-14 19:18:52 +0000

    Added getDocumentation(), basic main test program and java docs for getResult().  (Chris Bouzek)

--- trunk/DataSetTools/operator/DataSet/Conversion/XYAxis/SpectrometerTofToQE.java
+++ trunk/DataSetTools/operator/DataSet/Conversion/XYAxis/SpectrometerTofToQE.java
@@ -1,6 +1,6 @@
 /*
- * File:  SpectrometerTofToQE.java 
- *             
+ * File:  SpectrometerTofToQE.java
+ *
  * Copyright (C) 2000, Dennis Mikkelson
  *
  * This program is free software; you can redistribute it and/or
@@ -30,6 +30,10 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.5  2003/01/14 19:18:52  dennis
+ * Added getDocumentation(), basic main test program and java docs
+ * for getResult().  (Chris Bouzek)
+ *
  * Revision 1.4  2002/11/27 23:17:15  pfpeterson
  * standardized header
  *
@@ -53,20 +57,22 @@ import  DataSetTools.math.*;
 import  DataSetTools.util.*;
 import  DataSetTools.operator.Parameter;
 import  DataSetTools.parameter.*;
+import  DataSetTools.viewer.*;
+import  DataSetTools.retriever.*;
 
 /**
   *  Convert an energy spectrum from a Spectrometer into a DataSet containing
-  *  the rows of a QE image. The Data blocks will first be rebinned into 
-  *  energy bins with the same bin boundaries.  Next, for each energy-bin, 
-  *  the data values from all detectors at that energy-bin, will be merged 
-  *  into a set of equal Q bins, using the estimated Q coverage of each 
-  *  detector.  This will produce a two-dimensional set of values which will 
+  *  the rows of a QE image. The Data blocks will first be rebinned into
+  *  energy bins with the same bin boundaries.  Next, for each energy-bin,
+  *  the data values from all detectors at that energy-bin, will be merged
+  *  into a set of equal Q bins, using the estimated Q coverage of each
+  *  detector.  This will produce a two-dimensional set of values which will
   *  be cut into rows to produce a new DataSet.
   *
   *  @see XYAxisConversionOp
   */
 
-public class SpectrometerTofToQE extends    XYAxisConversionOp 
+public class SpectrometerTofToQE extends    XYAxisConversionOp
                                  implements Serializable
 {
   /* ------------------------ DEFAULT CONSTRUCTOR -------------------------- */
@@ -78,7 +84,7 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
    * to apply the operator to the DataSet this operator was added to.
    */
 
-  public SpectrometerTofToQE( ) 
+  public SpectrometerTofToQE( )
   {
     super( "Convert to QE plot" );
   }
@@ -91,11 +97,11 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
    *
    *  @param  ds          The DataSet to which the operation is applied
    *  @param  min_Q       The minimum Q to include
-   *  @param  max_Q       The maximum Q to include 
+   *  @param  max_Q       The maximum Q to include
    *  @param  n_Q_bins    The number of "bins" to be used between min_Q and
    *                      max_Q.
    *  @param  min_E       The minimum E to include
-   *  @param  max_E       The maximum E to include 
+   *  @param  max_E       The maximum E to include
    *  @param  n_E_bins    The number of "bins" to be used between min_E and
    *                      max_E.
    */
@@ -114,19 +120,19 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
 
     IParameter parameter = getParameter(0);
     parameter.setValue( new Float(min_Q) );
-    
-    parameter = getParameter(1); 
+
+    parameter = getParameter(1);
     parameter.setValue( new Float(max_Q));
-    
+
     parameter = getParameter(2);
     parameter.setValue( new Integer(n_Q_bins) );
 
     parameter = getParameter(3);
     parameter.setValue( new Float(min_E) );
-    
-    parameter = getParameter(4); 
+
+    parameter = getParameter(4);
     parameter.setValue( new Float(max_E));
-    
+
     parameter = getParameter(5);
     parameter.setValue( new Integer(n_E_bins) );
 
@@ -137,7 +143,8 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
 
   /* ---------------------------- getCommand ------------------------------- */
   /**
-   * @return the command name to be used with script processor: in this case, ToQE
+   * @return the command name to be used with script processor: 
+   *         in this case, ToQE
    */
    public String getCommand()
    {
@@ -145,7 +152,7 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
    }
 
 
- /* -------------------------- setDefaultParmeters ------------------------- */
+ /* -------------------------- setDefaultParameters ------------------------- */
  /**
   *  Set the parameters to default values.
   */
@@ -172,9 +179,62 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
     addParameter( parameter );
   }
 
+  /* ---------------------- getDocumentation --------------------------- */
+  /**
+   *  Returns the documentation for this method as a String.  The format
+   *  follows standard JavaDoc conventions.
+   */
+  public String getDocumentation()
+  {
+    StringBuffer s = new StringBuffer("");
+    s.append("@overview This operator converts an energy spectrum from ");
+    s.append("a Spectrometer into a DataSet containing the rows of a ");
+    s.append("QE image.  The X-Axis is in units of Q values; the Y-axis ");
+    s.append("is in units of energy.\n");
+    s.append("@assumptions The DataSet must contain spectra with an ");
+    s.append("attribute giving the detector position.  In addition, ");
+    s.append("it is assumed that the XScale for the spectra represents ");
+    s.append("the time-of-flight from the sample to the detector.\n");
+    s.append("Furthermore, a valid range of Q values must be specified, ");
+    s.append("and a valid delta_2theta attribute must be available from ");
+    s.append("the DataSet.\n");
+    s.append("@algorithm The Data blocks will first be rebinned into ");
+    s.append("energy bins with the same bin boundaries.\n  Next, for ");
+    s.append("each energy-bin, the data values from all detectors at ");
+    s.append("that energy-bin will be merged into a set of equal Q ");
+    s.append("bins, using the estimated Q coverage of each detector.\n  ");
+    s.append("This will produce a two-dimensional set of values which ");
+    s.append("will be cut into rows to produce a new DataSet.  ");
+    s.append("The new DataSet also has a message appended to its log ");
+    s.append("that a conversion to a S(Q,E) plot was done.\n");
+    s.append("@param ds The DataSet to which the operation is applied.\n");
+    s.append("@param min_Q The minimum Q value to include.\n");
+    s.append("@param max_Q The maximum Q value to include.\n");
+    s.append("@param n_Q_bins The number of \"bins\" to be used between ");
+    s.append("min_Q and max_Q.\n");
+    s.append("@param min_E The minimum energy value to include.\n");
+    s.append("@param max_E The maximum energy value to include.\n");
+    s.append("@param n_E_bins The number of \"bins\" to be used between ");
+    s.append("min_E and max_E.\n");
+    s.append("@return A new DataSet which is the result of converting the ");
+    s.append("input DataSet's X-axis units to Q values and its Y-axis units ");
+    s.append("to energy.\n");
+    s.append("@error Returns an error if no valid Q range is specified.\n");
+    s.append("@error Returns an error if no detector position attribute is ");
+    s.append("available.\n");
+    s.append("@error Returns an error if no delta_2theta attribute is ");
+    s.append("available.\n");
+    return s.toString();
+  }
 
-  /* ---------------------------- getResult ------------------------------- */
-
+ /* ---------------------------- getResult ------------------------------- */
+ /**
+  *  Converts the input DataSet to a DataSet which is a S(Q,E) plot of the
+  *  original DataSet.
+  *
+  *  @return DataSet whose X-axis units have been converted to Q values and
+  *  whose Y-axis units have been converted to energy.
+  */
   public Object getResult()
   {
                                      // get the current data set
@@ -221,7 +281,7 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
       max_Q  = temp;
     }
 
-    if ( n_Q_bins <= 0 )                // calculate a default 
+    if ( n_Q_bins <= 0 )                // calculate a default
       n_Q_bins = 100;                   // number of E bins.
 
     if ( min_Q >= max_Q )               // no valid range specified, so err out
@@ -235,11 +295,11 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
     // Now, map the counts from each time channel in each spectrum to a
     // point in QE space.  The values at the grid of points in QE space
     // will be averaged.
-  
+
                                                    // set up arrays for S(Q,E)
-                                                   // and counters.        
-    float QE_vals[][]   = new float[n_Q_bins][n_E_bins] ; 
-    int   n_QE_vals[][] = new int[n_Q_bins][n_E_bins] ; 
+                                                   // and counters.
+    float QE_vals[][]   = new float[n_Q_bins][n_E_bins] ;
+    int   n_QE_vals[][] = new int[n_Q_bins][n_E_bins] ;
     float old_QE_val;
     int   count;
 
@@ -257,11 +317,11 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
     float            e_in;
     Data             data;
     DetectorPosition position = null;
-    float            scattering_angle; 
+    float            scattering_angle;
     Float            delta_theta_obj = null;
     float            delta_theta;
     Float            energy_in_obj = null;
-    float            q1, 
+    float            q1,
                      q2,
                      q_val;
     float            e_val,
@@ -274,17 +334,17 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
     float            y_vals[];
 
     //
-    // Now step along each time-of-flight spectrum, calculate the Q,E 
+    // Now step along each time-of-flight spectrum, calculate the Q,E
     // coordinates for each bin and map the events in the bin to a point in
     // the Q,E plane.
     //
     int num_data = ds.getNum_entries();
-    for ( int i = 0; i < num_data; i++ )  
+    for ( int i = 0; i < num_data; i++ )
     {
       data             = ds.getData_entry(i);
-                                              // first, get the position, 
-                                              // delta 2theta and initial 
-                                              // energy attributes for the 
+                                              // first, get the position,
+                                              // delta 2theta and initial
+                                              // energy attributes for the
                                               // current spectrum
       attr_list = data.getAttributeList();
       position = (DetectorPosition)
@@ -296,7 +356,7 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
         return message;
       }
       spherical_coords = position.getSphericalCoords();
-     
+
       delta_theta_obj = (Float)
                         attr_list.getAttributeValue( Attribute.DELTA_2THETA );
       if ( delta_theta_obj == null )
@@ -312,21 +372,21 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
       scattering_angle = position.getScatteringAngle() * (float)(180.0/Math.PI);
       delta_theta = delta_theta_obj.floatValue();
 
-      min_ang = (float)(Math.PI/180.0) * (scattering_angle - delta_theta/2); 
-      max_ang = (float)(Math.PI/180.0) * (scattering_angle + delta_theta/2); 
+      min_ang = (float)(Math.PI/180.0) * (scattering_angle - delta_theta/2);
+      max_ang = (float)(Math.PI/180.0) * (scattering_angle + delta_theta/2);
 
                                              // Now get the x and y values from
                                              // the spectrum and map them to
                                              // the Q,E plane
       tof_vals = data.getX_scale().getXs();
       y_vals   = data.getY_values();
-      for ( int col = 0; col < y_vals.length; col++ )    
+      for ( int col = 0; col < y_vals.length; col++ )
       {
                                              // calculate the energy at the
                                              // bin center
         center_tof = (tof_vals[col] + tof_vals[col+1]) / 2.0f;
         e_val = tof_calc.Energy( spherical_coords[0], center_tof );
-          
+
                                              // calculate the q values at the
                                              // left and right edges of the
                                              // detector
@@ -339,13 +399,13 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
           q2 = temp;
         }                               // use the average Q value
         q_val = ( q1 + q2 ) / 2.0f;
-                                        // divide by the counts by delta Q, 
+                                        // divide by the counts by delta Q,
                                         // to get intensity  ( we assume that
                                         // the values have already been divided
                                         // by delta E and delta Omega as in the
                                         // DSDODE operator
-        y_val = y_vals[col] / (q2 - q1); 
-                                        // now map the y(Q,E) value to the 
+        y_val = y_vals[col] / (q2 - q1);
+                                        // now map the y(Q,E) value to the
                                         // array of values and average it with
                                         // any previous values
 
@@ -358,7 +418,7 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
           old_QE_val = QE_vals[q_index][e_index];
           count      = n_QE_vals[q_index][e_index];
           QE_vals[q_index][e_index] = (old_QE_val * count + y_val)/( count + 1);
-          n_QE_vals[q_index][e_index] = count + 1;   
+          n_QE_vals[q_index][e_index] = count + 1;
         }
 
       }
@@ -389,15 +449,15 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
       Attribute q_attr = new FloatAttribute( Attribute.Q_VALUE, q_val );
       new_data.setAttribute( q_attr );
 
-      new_ds.addData_entry( new_data );      
+      new_ds.addData_entry( new_data );
     }
 
     return new_ds;
-  }  
+  }
 
   /* ------------------------------ clone ------------------------------- */
   /**
-   * Get a copy of the current TofToChannel Operator.  The list 
+   * Get a copy of the current TofToChannel Operator.  The list
    * of parameters and the reference to the DataSet to which it applies are
    * also copied.
    */
@@ -418,12 +478,33 @@ public class SpectrometerTofToQE extends    XYAxisConversionOp
    */
   public static void main( String[] args )
   {
-    SpectrometerTofToQE op = new SpectrometerTofToQE();
+    float minQ, maxQ, minE, maxE;
+    int Ebins, Qbins;
 
-    String list[] = op.getCategoryList();
-    System.out.println( "Categories are: " );
-    for ( int i = 0; i < list.length; i++ )
-      System.out.println( list[i] );
+    minE = (float)4.0;
+    maxE = (float)250;
+    Ebins = 500;
+    minQ = (float)0.225;
+    maxQ = (float)3.2;
+    Qbins = 500;
+
+    String file_name = "/home/groups/SCD_PROJECT/SampleRuns/hrcs2447.run ";
+                       //"D:\\ISAW\\SampleRuns\\hrcs2447.run ";
+    try
+    {
+       RunfileRetriever rr = new RunfileRetriever( file_name );
+       DataSet ds1 = rr.getDataSet(1);
+       ViewManager viewer = new ViewManager(ds1, IViewManager.IMAGE);
+       SpectrometerTofToQE op =
+             new SpectrometerTofToQE(ds1, minQ, maxQ, Qbins, minE, maxE, Ebins);
+       DataSet new_ds = (DataSet)op.getResult();
+       ViewManager new_viewer = new ViewManager(new_ds, IViewManager.IMAGE);
+       System.out.println(op.getDocumentation());
+    }
+    catch(Exception e)
+    {
+       e.printStackTrace();
+    }
   }
 
 
