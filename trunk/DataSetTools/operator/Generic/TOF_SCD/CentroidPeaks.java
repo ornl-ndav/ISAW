eda195483b196a60c0cd7f82d3872517e8dac36f
pfpeterson <pfpeterson@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-01-30 21:08:56 +0000

    Huge reduction in amount of code due to new Util class.

--- trunk/DataSetTools/operator/Generic/TOF_SCD/CentroidPeaks.java
+++ trunk/DataSetTools/operator/Generic/TOF_SCD/CentroidPeaks.java
@@ -29,6 +29,9 @@
  * For further information, see <http://www.pns.anl.gov/ISAW/>
  *
  * $Log$
+ * Revision 1.9  2003/01/30 21:08:56  pfpeterson
+ * Huge reduction in amount of code due to new Util class.
+ *
  * Revision 1.8  2003/01/15 20:54:25  dennis
  * Changed to use SegmentInfo, SegInfoListAttribute, etc.
  *
@@ -105,199 +108,26 @@ public class CentroidPeaks extends GenericTOF_SCD implements HiddenOperator{
   }
   
   /** 
-   *  Executes this operator using the values of the current
-   *  parameters.
+   * Executes this operator using the values of the current
+   * parameters.
    *
-   *  @return If successful, this operator prints out a list of
-   *  x,y,time bins and intensities.
+   * @return If successful, this returns a vector of peaks
    */
   public Object getResult(){
     DataSet data_set = (DataSet)(getParameter(0).getValue());
     Vector  peaks    = (Vector) (getParameter(1).getValue());
     Vector  cpeaks   = new Vector();
     
-    float[] times=data_set.getData_entry(0).getX_scale().getXs();
+    XScale times=data_set.getData_entry(0).getX_scale();
+    int[][] ids=Util.createIdMap(data_set);
     
-    Peak peak=new Peak();
+    Peak peak=null;
     for( int i=0 ; i<peaks.size() ; i++ ){
-      peak=(Peak)peaks.elementAt(i);
-      float[][][] surround=makeSurround(data_set,peak);
-      //System.out.print("t["+(int)peak.z()+" -> ");
-      peak=centroid(peak,surround);
-      //System.out.print(peak.z()+"]="+peak.t()+" -> ");
-      peak.times(times[(int)peak.z()],times[(int)peak.z()+1]);
-      //System.out.println(peak.t());
+      peak=Util.centroid((Peak)peaks.elementAt(i),data_set,ids);
+      peak.time(times);
     }
     
     return peaks;
-  }
-  
-  /**
-   * This method takes a peak and finds the surrounding area needed
-   * for centroiding it.
-   *
-   * @return a 7x7x3 array of counts centered around the peak
-   */
-  private float[][][] makeSurround( DataSet data, Peak peak){
-    float[][][] surround=new float[7][7][3];
-    int column = (int)peak.x();
-    int row    = (int)peak.y();
-    int time   = (int)peak.z();
-    
-    // initialize the array
-    for( int i=0 ; i<surround.length ; i++ ){
-      for( int j=0 ; j<surround[0].length ; j++ ){
-        for( int k=0 ; k<surround[0][0].length ; k++ ){
-          surround[i][j][k]=0f;
-        }
-      }
-    }
-    
-    // fill the array with the appropriate values
-    int x;
-    int y;
-    Data spectrum;
-    SegInfoListAttribute segI;
-    SegmentInfo det;
-    float[] intens;
-    for( int i=0 ; i<data.getNum_entries() ; i++ ){
-      spectrum=data.getData_entry(i);
-      segI=(SegInfoListAttribute)
-        spectrum.getAttribute(Attribute.SEGMENT_INFO_LIST);
-      det=((SegmentInfo[])segI.getValue())[0];
-      x=det.getColumn();
-      y=det.getRow();
-      intens=spectrum.getCopyOfY_values();
-      if( x>=column-3 && x<=column+3 && y>=row-3 && y<=row+3 ){
-        surround[x-column+3][y-row+3][0]=getIntens(intens,time-1);
-        surround[x-column+3][y-row+3][1]=getIntens(intens,time  );
-        surround[x-column+3][y-row+3][2]=getIntens(intens,time+1);
-      }
-    }
-    // lets add a bug to set the background to something bad and
-    // be in agreement with the original software
-    for( int i=0 ; i<data.getNum_entries() ; i++ ){
-      spectrum=data.getData_entry(i);
-      segI=(SegInfoListAttribute)
-        spectrum.getAttribute(Attribute.SEGMENT_INFO_LIST);
-      det=((SegmentInfo[])segI.getValue())[0];
-      x=det.getColumn();
-      y=det.getRow();
-      intens=spectrum.getCopyOfY_values();
-      if( y==1 ){
-        if(x>0 && x<=7){
-          surround[x-1][y-1][0]=getIntens(intens,time-1);
-          surround[x-1][y-1][1]=getIntens(intens,time  );
-          surround[x-1][y-1][2]=getIntens(intens,time+1);
-        }
-      }
-    }
-    for( int i2=0 ; i2<7 ; i2++ ){
-      surround[i2][6][0]=surround[i2][0][0];
-      surround[i2][6][1]=surround[i2][0][1];
-      surround[i2][6][2]=surround[i2][0][2];
-    }
-    for( int i2=1 ; i2<6 ; i2++ ){
-      surround[0][i2][0]=surround[0][0][0];
-      surround[0][i2][1]=surround[0][0][1];
-      surround[0][i2][2]=surround[0][0][2];
-      surround[6][i2][0]=surround[6][0][0];
-      surround[6][i2][1]=surround[6][0][1];
-      surround[6][i2][2]=surround[6][0][2];
-    }
-    // end of the intentional bug
-    
-    return surround;
-  }
-  
-  /**
-   * Method to get the intensity of a given time channel that checks
-   * the array boundaries.
-   */
-  private float getIntens( float[] intensity, int time_bin ){
-    if(time_bin<0)
-      return 0f;
-    else if(time_bin>=intensity.length)
-      return 0f;
-    else
-      return intensity[time_bin];
-  }
-  
-  /**
-   * This does the actual centroiding. 
-   */
-  private Peak centroid(Peak peak, float[][][] surround){
-    
-    double asum  = 0.;
-    double xsum  = 0.;
-    double ysum  = 0.;
-    double zsum  = 0.;
-    double back  = 0.;
-    double count = 0.;
-    
-    float x,y,z;
-    int reflag=peak.reflag();
-    reflag=(reflag/100)*100+reflag%10;
-    
-    if( peak.nearedge()<=4.0f ){ //too close to edge
-      peak.reflag(reflag+20);
-      return peak;
-    }
-    
-    for( int k=0 ; k<3 ; k++ ){
-      // determine the background for this time slice
-      back=0.;
-      for( int j=0 ; j<7 ; j++ ){
-        for( int i=0 ; i<7 ; i++ ){
-          if( i==0 || i==6 ){ // left and right borders
-            back=back+(double)surround[i][j][k];
-          }else{
-            if( j==0 || j==6 ){ // top and bottom borders
-              back=back+(double)surround[i][j][k];
-            }
-          }
-        }
-      }
-      back=back/24f; // normalize the background by number of points
-      // find the sums for the centroid
-      for( int j=2 ; j<5 ; j++ ){
-        for( int i=2 ; i<5 ; i++ ){
-          count=(double)surround[i][j][k]-back;
-          xsum=xsum+count*((double)i+1.);
-          ysum=ysum+count*((double)j+1.);
-          zsum=zsum+count*(peak.z()+(double)k);
-          asum=asum+count;
-        }
-      }
-    }
-    
-    // total count must be greater than zero for this to make sense
-    if(asum<=0){
-      peak.reflag(reflag+30);
-      return peak;
-    }
-    // centroid the peaks
-    x=(float)(xsum/asum)+(float)peak.x()-4f;
-    y=(float)(ysum/asum)+(float)peak.y()-4f;
-    z=(float)(zsum/asum)-1f; // -1 is to convert to java counting
-    
-    // find out how far the peaks were moved
-    float dx=Math.abs(x-(float)peak.x());
-    float dy=Math.abs(y-(float)peak.y());
-    float dz=Math.abs(z-(float)peak.z());
-    
-    if( dx>1.0 || dy>1.0 || dz>1.0 ){
-      // don't shift positions if it is moving more than one bin
-      peak.reflag(reflag+30);
-      return peak;
-    }else{
-      // update the peak
-      peak.pixel(x,y,z);
-    }
-    
-    // return the updated peak
-    peak.reflag(reflag+10);
-    return peak;
   }
   
   /** 
@@ -324,8 +154,7 @@ public class CentroidPeaks extends GenericTOF_SCD implements HiddenOperator{
     Vector peaked=(Vector)fo.getResult();
     Peak peak=new Peak();
     for( int i=0 ; i<peaked.size() ; i++ ){
-      peak=(Peak)peaked.elementAt(i);
-      System.out.println(peak);
+      System.out.println(peaked.elementAt(i));
     }
     System.out.println("done with FindPeaks");
     
@@ -333,8 +162,7 @@ public class CentroidPeaks extends GenericTOF_SCD implements HiddenOperator{
     co = new CentroidPeaks( rds, peaked );
     peaked=(Vector)co.getResult();
     for( int i=0 ; i<peaked.size() ; i++ ){
-      peak=(Peak)peaked.elementAt(i);
-      System.out.println(peak);
+      System.out.println(peaked.elementAt(i));
     }
     System.out.println("done with CentroidPeaks");
     System.exit(0);
