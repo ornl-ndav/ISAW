d722b9dd9816f28c9d8fa4e67fd3fdf81c4dd397
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-02-03 18:15:22 +0000

    Added getDocumentation() method and java docs on getResult(). (Shannon Hintzman).

--- trunk/DataSetTools/operator/Generic/Batch/GetDate.java
+++ trunk/DataSetTools/operator/Generic/Batch/GetDate.java
@@ -29,10 +29,12 @@
  * For further information, see <http://www.pns.anl.gov/ISAW/>
  *
  * $Log$
+ * Revision 1.3  2003/02/03 18:15:22  dennis
+ * Added getDocumentation() method and java docs on getResult().
+ * (Shannon Hintzman).
+ *
  * Revision 1.2  2002/11/27 23:20:52  pfpeterson
  * standardized header
- *
- *
  */
 package DataSetTools.operator.Generic.Batch;
 
@@ -43,55 +45,98 @@ import java.util.*;
  * This operator returns a string representing the current date and
  * time.
  */
-public class GetDate extends GenericBatch{
-    private static final String     TITLE                 = "Date";
+ 
+public class GetDate extends GenericBatch
+{
+  private static final String     TITLE                 = "Date";
+  /**
+   *  Creates operator with title "Date" and a default list of
+   *  parameters (no parameters).
+   */  
+  public GetDate(){/*DataSetTools/operator/Generic/Batch*/
 
-    /**
-     *  Creates operator with title "Date" and a default list of
-     *  parameters (no parameters).
-     */  
-    public GetDate(){
 	super( TITLE );
-    }
+  }
 
-    /** 
-     * Get the name of this operator to use in scripts
-     * 
-     * @return "GetDate", the command used to invoke this
-     * operator in Scripts
-     */
-    public String getCommand(){
-        return "GetDate";
-    }
+  /** 
+   * Get the name of this operator to use in scripts
+   * 
+   * @return "GetDate", the command used to invoke this
+   * operator in Scripts
+   */
+  /* ---------------------------- getDocumentation -------------------------- */
+ 
+  public String getDocumentation()
+  {
+    StringBuffer Res = new StringBuffer();
+    
+    Res.append("@overview This operator returns a string representing the ");
+    Res.append("current date and time, which will be printed in the ");
+    Res.append("StatusPane.");
+ 
+    Res.append("@algorithm Executes this operator using the values of the ");
+    Res.append("current parameters to get the current date and time to be ");
+    Res.append("displayed in the StatusPane.");
+    
+    Res.append("@return Returns a string of the current date and time.");
+    
+    return Res.toString();
+  }
+  
+  /* ---------------------------- getCommand ------------------------------- */
+  /**
+   * @return the command name to be used with script processor: 
+   *         in this case GetDate
+   */    
+  public String getCommand()
+  {
+      return "GetDate";
+  }
 
-    /** 
-     * Sets default values for the parameters. This must match the
-     * data types of the parameters.
-     */
-    public void setDefaultParameters(){
+  /* ------------------------- setDefaultParmeters ------------------------- */
+  /** 
+   * Sets default values for the parameters. This must match the
+   * data types of the parameters.
+   */
+  public void setDefaultParameters(){
         parameters = new Vector();
-    }
+  }
     
-    /** 
-     *  Executes this operator using the values of the current
-     *  parameters. This will print the current date and time in the
-     *  StatusPane as well as return the string to the caller.
-     *
-     *  @return If successful, this operator gives back a string of
-     *  the current date and time.
-     */
-    public Object getResult(){
-        Date date=new Date(System.currentTimeMillis());
-        DataSetTools.util.SharedData.addmsg(date.toString());
-        return date.toString();
-    }
+  /* ---------------------------- getResult ------------------------------- */
+  /** 
+   *  Executes this operator using the values of the current
+   *  parameters. This will print the current date and time in the
+   *  StatusPane as well as return the string to the caller.
+   *
+   *  @return If successful, this operator gives back DataSetTools/operator/
+   *          Generic/Batch a string of the current date and time.
+   */
+  public Object getResult(){
+      Date date=new Date(System.currentTimeMillis());
+      DataSetTools.util.SharedData.addmsg(date.toString());
+      return date.toString();
+  }
     
-    /** 
-     *  Creates a clone of this operator.
-     */
-    public Object clone(){
-        GetDate op = new GetDate();
-        op.CopyParametersFrom( this );
-        return op;
-    }
+  /* ---------------------------- clone ---------------------------------- */
+  /** 
+   *  Creates a clone of this operator.
+   */
+  public Object clone(){
+      GetDate op = new GetDate();
+      op.CopyParametersFrom( this );
+      return op;
+  }
+    
+  /* --------------------------- main ----------------------------------- */
+  /**
+   *  Main program for testing purposes
+   */
+  public static void main( String[] args )
+  {
+    GetDate op = new GetDate();
+     
+    System.out.println(op.getDocumentation() + "\n");
+    System.out.println(op.getResult());
+  }
 }
+
