a425e451f1242601d2fb64d021cec45d3d9811ba
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-02-17 19:19:22 +0000

    Added getDocumentation() method. (Chris Bouzek)

--- trunk/Operators/TOF_DG_Spectrometer/ImaginaryGeneralizedSusceptibility.java
+++ trunk/Operators/TOF_DG_Spectrometer/ImaginaryGeneralizedSusceptibility.java
@@ -31,6 +31,9 @@
  * Modified:
  * 
  *  $Log$
+ *  Revision 1.7  2003/02/17 19:19:22  dennis
+ *  Added getDocumentation() method. (Chris Bouzek)
+ *
  *  Revision 1.6  2002/12/11 22:31:31  pfpeterson
  *  Removed the '_2' from getCommand() and its javadocs.
  *
@@ -65,6 +68,8 @@ import  DataSetTools.math.*;
 import  DataSetTools.operator.*;
 import  DataSetTools.operator.Generic.TOF_DG_Spectrometer.*;
 import  DataSetTools.parameter.*;
+import  DataSetTools.retriever.*;
+import  DataSetTools.viewer.*;
 
 /**
   *  Compute the Imaginary Generalized Susceptibility Function for a direct 
@@ -95,12 +100,13 @@ public class ImaginaryGeneralizedSusceptibility
 
   /* ---------------------- FULL CONSTRUCTOR ---------------------------- */
   /**
-   *  Construct an operator to calculate the Scattering Function
-   *  for a spectrometer DataSet.  It is assumed that the 
-   *  DoubleDifferentialCrossection operator has already been applied.
+   *  Construct an operator to calculate the Imaginary Generalized Susceptibility
+   *  Function for a spectrometer DataSet.  It is assumed that the 
+   *  Scattering Function operator has already been applied.
    *
-   *  @param  ds               The sample DataSet for which the scattering 
-   *                           function is to be calculated 
+   *  @param  ds               The sample DataSet for which the imaginary  
+   *                           generalized susceptibility function is to be 
+   *                           calculated 
    *  @param  temperature      The sample temperature
    *  @param  make_new_ds Flag that determines whether a new DataSet is
    *                           constructed, or the Data blocks of the original 
@@ -136,7 +142,7 @@ public class ImaginaryGeneralizedSusceptibility
    }
 
 
- /* -------------------------- setDefaultParmeters ------------------------- */
+ /* -------------------------- setDefaultParameters ------------------------- */
  /**
   *  Set the parameters to default values.
   */
@@ -154,8 +160,58 @@ public class ImaginaryGeneralizedSusceptibility
     addParameter( parameter );
   }
 
+  /* ---------------------- getDocumentation --------------------------- */
+  /**
+   *  Returns the documentation for this method as a String.  The format
+   *  follows standard JavaDoc conventions.
+   */
+   public String getDocumentation()
+   {
+     StringBuffer s = new StringBuffer("");
+     s.append("@overview This operator computes the imaginary generalized ");
+     s.append("susceptibility function for a direct geometry spectrometer ");
+     s.append("based on the result of applying the scattering function ");
+     s.append("operator.\n");
+     s.append("@assumptions It is assumed that the ScatteringFunction ");
+     s.append("operator has been applied.\n");
+     s.append("@algorithm For each data entry in the DataSet, this operator ");
+     s.append("first uses the initial energy and the data's x-values to ");
+     s.append("calculate the energy transfer.\n");
+     s.append("Next it uses the conversion factor XKCON, the sample ");
+     s.append("temperature, and the energy transfer to calculate ");
+     s.append("conversion values.\n");
+     s.append("These conversion values, along with conversion error data ");
+     s.append("based on the data's y-values, are used by Data's getInstance ");
+     s.append("method to calculate conversion data.\n");
+     s.append("Finally it multiplies the spectrum by the conversion data ");
+     s.append("and appends a log to the DataSet indicating that the ");
+     s.append("imaginary part of generalized susceptibility was ");
+     s.append("calculated.\n");
+     s.append("@param ds The sample DataSet for which the imaginary ");
+     s.append("generalized susceptibility function is to be calculated.\n");
+     s.append("@param temperature The sample temperature.\n");
+     s.append("@param make_new_ds Flag that determines whether a new ");
+     s.append("DataSet is constructed, or the Data blocks of the original ");
+     s.append("DataSet are just altered.\n");
+     s.append("@return If make_new_ds is true, returns the new DataSet to ");
+     s.append("which the imaginary generalized susceptibility function has ");
+     s.append("been applied.  Otherwise, it returns a String indicating ");
+     s.append("that the imaginary generalized susceptibility function was ");
+     s.append("applied.\n");
+     s.append("@error An error message is returned if temperature ");
+     s.append("is not be greater than 0.\n");
+     return s.toString();
+    }
 
   /* ---------------------------- getResult ------------------------------- */
+  /** 
+    * Computes the imaginary generalized susceptibility function.
+    *
+    * @return If make_new_ds is true, returns the new DataSet to which the 
+    * imaginary generalized susceptibility function has been applied.  
+    * Otherwise, it returns a String indicating that the imaginary generalized 
+    * susceptibility function was applied.
+    */
 
   public Object getResult()
   {       
@@ -165,8 +221,8 @@ public class ImaginaryGeneralizedSusceptibility
 
                                                     // get the parameters
     DataSet ds          = (DataSet)(getParameter(0).getValue());
-    float   temperature = ((Float)(getParameter(0).getValue()) ).floatValue();
-    boolean make_new_ds =((Boolean)getParameter(4).getValue()).booleanValue();
+    float   temperature = ((Float)(getParameter(1).getValue()) ).floatValue();
+    boolean make_new_ds =((Boolean)getParameter(2).getValue()).booleanValue();
     
     DataSet new_ds;
     if ( make_new_ds )
@@ -211,7 +267,7 @@ public class ImaginaryGeneralizedSusceptibility
 
       for ( int i = 0; i < (y_vals.length-1); i++ )
       {
-        if ( x_vals.length > y_vals.length )  // histogram
+        if ( data.isHistogram() )             // histogram
           energy_transfer = (x_vals[i]+x_vals[i+1])/2;
         else                                  // function
           energy_transfer = x_vals[i];
@@ -274,13 +330,63 @@ public class ImaginaryGeneralizedSusceptibility
     return new_op;
   }
 
-  /* ------------------------------- main ---------------------------------- */
-  /**
-   *  Main program for testing purposes.
-   */
-   public static void main( String args[] )
-   {
-     System.out.println( "SpectrometerGeneralizedEnergyDistributionFunction" );
-   }
+ /* ------------------------------- main ---------------------------------- */
+ /**
+  *  Main program for testing purposes.
+  */
+  public static void main( String args[] )
+  {
+    System.out.println("Test of ImaginaryGeneralizedSusceptibilityFunction" );
+   
+    String    run_name = "/home/groups/SCD_PROJECT/SampleRuns/hrcs2447.run";
+    //String    run_name = "d:\\SCD_PROJECT\\SampleRuns\\hrcs2447.run";
+    Retriever rr       = new RunfileRetriever( run_name );
+    DataSet   ds       = rr.getDataSet(1);
+
+    Operator op = new DoubleDifferentialCrossection( ds, null, false,
+                                                     10000, 1, true );
+
+    Object ddif_ds = op.getResult();
+    if ( ddif_ds == null )
+      System.out.println("Error in calculating DSDODE... returned null");
+    else
+    {
+      System.out.println("DSDODE returned:" + ddif_ds );
+      if ( ddif_ds instanceof DataSet )
+      {
+        ViewManager vm1 = new ViewManager((DataSet)ddif_ds,IViewManager.IMAGE);
+      }
+    }
+
+   op = new ScatteringFunction( (DataSet)ddif_ds, 1, true );
+   Object scat_ds = op.getResult();
+   if ( scat_ds == null )
+      System.out.println("Error in calculating SCAT... returned null");
+    else
+    {
+      System.out.println("SCAT returned:" + scat_ds );
+      if ( scat_ds instanceof DataSet )
+      {
+        ViewManager vm3 = new ViewManager((DataSet)scat_ds,IViewManager.IMAGE);
+      }
+    }
+    
+   op = new ImaginaryGeneralizedSusceptibility( (DataSet)scat_ds, 30.0f, true );
+   Object imchi_ds = op.getResult();
+   if ( imchi_ds == null )
+      System.out.println("Error in calculating IMCHI... returned null");
+    else
+    {
+      System.out.println("IMCHI returned:" + imchi_ds );
+      if ( imchi_ds instanceof DataSet )
+      {
+        ViewManager vm4 = new ViewManager((DataSet)imchi_ds,IViewManager.IMAGE);
+      }
+    }
+    
+    System.out.println("Documentation: " + op.getDocumentation());
+
+    System.out.println("End of test of ImaginaryGeneralizedSusceptibilityFunction");
+  }
 
 }
