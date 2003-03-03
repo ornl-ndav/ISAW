4abd2d0bdb5cb36fdbd82c59a135ae0d4b3c1ba1
John Hammonds <jphammonds@anl.gov>
2003-03-03 20:12:54 +0000

    Documentation cleanup.  Convert to new Parameter types.

--- Operators/LoadScriptArray.java
+++ Operators/LoadScriptArray.java
@@ -29,6 +29,9 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.3  2003/03/03 20:12:54  hammonds
+ *  Documentation cleanup.  Convert to new Parameter types.
+ *
  *  Revision 1.2  2003/02/26 23:02:14  hammonds
  *  Comment change only.
  *
@@ -43,9 +46,11 @@ package Operators;
 import  java.io.*;
 import  java.util.*;
 import  DataSetTools.operator.*;
+import  DataSetTools.parameter.LoadFilePG;
+import  DataSetTools.parameter.IntegerPG;
 import  DataSetTools.util.*;
 import  DataSetTools.operator.Generic.Batch.*;
-import  IPNS.Runfile.*;
+//import  IPNS.Runfile.*;
 /**
  *   This class supports reading of Strings, floats, ints etc. from an
  *   ordinary text file.  In addition to methods to read each of the 
@@ -60,27 +65,26 @@ public class LoadScriptArray extends GenericBatch implements Serializable
 	
   /* -------------------------- Constructor -------------------------- */
   /**
-   *  Construct a SetupReader to read from the specified file.  The
-   *  constructor will throw an exception if the file can't be opened.  The
+   *  Construct a LoadScriptArray operator to read from the specified file.  
+   *  The constructor will throw an exception if the file can't be opened.  The
    *  other methods of this class should not be used if the file can't be
    *  opened.
    *
    *  @param file_name  The fully qualified file name.
    *  @param num_head The number of header lines that should be
    *  skipped while reading in the file
-   *  @param num_data The number of data lines to read in. If set to
-   *  zero all lines until the end of file are read.
+   *  @param num_data The number of data lines to read in.
+   *  @param num_col The number of data columns to read in
+   */
 
-  */
-
-  public LoadScriptArray( String file_name,Integer num_head, Integer num_data )
+  public LoadScriptArray( String file_name,Integer num_head, Integer num_data,
+			  Integer num_col )
   {  
     this();
-    Vector parameters = new Vector();
-    addParameter(new Parameter("Filename", null) );
-    addParameter( new Parameter("# of Header lines", null) );
-    addParameter( new Parameter("# of Data lines", null) );
-    addParameter( new Parameter("# of Columns", null) );
+    getParameter(0).setValue(file_name);
+    getParameter(1).setValue(num_head);
+    getParameter(2).setValue(num_data);
+    getParameter(3).setValue(num_col);
   }
 
 
@@ -95,7 +99,7 @@ public class LoadScriptArray extends GenericBatch implements Serializable
 
   /** 
    * Get the name of this operator, used in scripts
-   * @return "SetupReader", the command used to invoke this operator
+   * @return "LoadScriptArray", the command used to invoke this operator
    * in Scripts
    */
   public String getCommand()
@@ -109,10 +113,14 @@ public class LoadScriptArray extends GenericBatch implements Serializable
   public void setDefaultParameters()
   {
     parameters = new Vector();
-    addParameter( new Parameter("Filename", null));
-    addParameter( new Parameter("# header lines",null) );
-    addParameter( new Parameter("# data lines",null) );
-    addParameter( new Parameter("# of cols",null) );
+    Parameter inFile=new Parameter("Input File", new LoadFileString(""));
+    addParameter( inFile );
+    IntegerPG head_in = new IntegerPG(" *# of header lines", 0);
+    addParameter( head_in );
+    IntegerPG data_in = new IntegerPG("# of data lines", 0);
+    addParameter( data_in );
+    IntegerPG col_in = new IntegerPG("# of columns",0 );
+    addParameter( col_in );
   }
 
   /** 
@@ -122,10 +130,13 @@ public class LoadScriptArray extends GenericBatch implements Serializable
 
   public Object getResult()
   {
-    String  file_name = getParameter(0).getValue().toString();
-    int     num_head = ((Integer)getParameter(1).getValue()).intValue();
-    int     num_data = ((Integer)getParameter(2).getValue()).intValue();
-    int     max_col = ((Integer)getParameter(3).getValue()).intValue();
+    String  file_name = ((LoadFilePG)getParameter(0)).getStringValue();
+    int num_head = 
+      ((Integer)((IntegerPG)getParameter(1)).getValue()).intValue();
+    int num_data = 
+      ((Integer)((IntegerPG)getParameter(2)).getValue()).intValue();
+    int max_col = 
+      ((Integer)((IntegerPG)getParameter(3)).getValue()).intValue();
 
     TextFileReader f = null;
     String line;
@@ -203,6 +214,7 @@ public class LoadScriptArray extends GenericBatch implements Serializable
     catch ( IOException e )
       {
         //throw e;
+	return new ErrorString("LoadScriptArray: ERROR on file read");
       }
 
       
@@ -244,11 +256,45 @@ public class LoadScriptArray extends GenericBatch implements Serializable
     s.append("@param num_col The number of columns in the array");
     s.append("@return If successful, this returns a vector of vectors " );
     s.append(" which contain the data from the file ");
-    //    s.append("If unsuccessful, an error string is returned.");
-    //    s.append("@error If execution of the operator generates any exception, ");
-    //s.append("an error string is returned.");
     return s.toString();
   }  
+ /* ------------------------------- main ----------------------------------- */
+ /** 
+  * Test program to verify that this will complile and run ok.  
+  *
+  */
+  public static void main( String args[] )
+  {
+
+      String filename = new String();;
+      Integer num_head = new Integer (0);
+      Integer num_data = new Integer (0);
+      Integer num_col = new Integer (0);
+    try {
+
+      filename = args[0];
+      num_head = new Integer (args[1]);
+      num_data = new Integer (args[2]);
+      num_col = new Integer (args[3]);
+      System.out.println(filename +" "+  num_head+" "+ num_data+" "+  num_col );
+    }
+    catch ( Exception ex) {
+      System.out.println( "ERROR*******");
+      System.out.print( "Command requires four arguments: filename, num_head, "
+ );
+      System.out.println( "num_data, num_col" );
+      ex.printStackTrace();
+      System.exit(0);
+   }
+      Operator op  = new LoadScriptArray(filename, num_head, num_data, num_col );
+      Object   obj = op.getResult();
+                                                 // display any message string
+                                                 // that might be returned    
+                                                 // if the operator produced a
+      System.out.println("Operator returned: " + obj );
+      
+      System.exit(0);
+  }
 
 
 }
