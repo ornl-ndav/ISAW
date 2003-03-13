db673abaa7cf30d045aebd8111480a726f751351
pfpeterson <pfpeterson@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-03-13 15:31:20 +0000

    The next generation of parameter viewing. This allows any parameter to be viewed. (Chris Bouzek).

--- trunk/DataSetTools/wizard/Wizard.java
+++ trunk/DataSetTools/wizard/Wizard.java
@@ -30,6 +30,10 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.13  2003/03/13 15:31:20  pfpeterson
+ * The next generation of parameter viewing. This allows any parameter
+ * to be viewed. (Chris Bouzek).
+ *
  * Revision 1.12  2003/03/11 19:51:20  pfpeterson
  * First implementation of the 'View DataSet' button (Chris Bouzek).
  *
@@ -96,6 +100,8 @@ import javax.swing.*;
 import DataSetTools.operator.*;
 import DataSetTools.util.*;
 import DataSetTools.parameter.*;
+import DataSetTools.viewer.*;
+import DataSetTools.dataset.DataSet;
 
 /**
  *  The Wizard class provides the top level control for a sequence of
@@ -118,7 +124,7 @@ public class Wizard implements Serializable{
     // size of the window
     private static final int FRAME_WIDTH   = 650;
     private static final int FRAME_HEIGHT  = 500;
-    
+
     // string constants for the menus and buttons
     private static final String EXIT_COMMAND        = "Exit";
     private static final String FIRST_COMMAND       = "First";
@@ -135,13 +141,14 @@ public class Wizard implements Serializable{
     private static final String LOAD_FORM_COMMAND   = "Load Current Form";
     private static final String SAVE_WIZARD_COMMAND = "Save Wizard";
     private static final String LOAD_WIZARD_COMMAND = "Load Wizard";
-    private static final String VIEW_COMMAND        = "View DataSet(s)";
+    private static final String VIEW_DATASET        = "View DataSet";
+    private static final String VIEW_MENU           = "View";
 
     // default help and about messages
     private String help_message  = "Help not available for Wizard";
     private String about_message = "Default Help About Message";
     private boolean standalone   = true;
-    
+
     // the status pane
     //public static TextArea status_display = new TextArea();
 
@@ -162,10 +169,12 @@ public class Wizard implements Serializable{
     private JButton      last_button;
     private JLabel       form_label;
     private JProgressBar progress;
-    
+    private JMenu        view_menu;
+    private CommandHandler command_handler;
+
     /**
      * The legacy constructor
-     * 
+     *
      * @param title Name displayed at top of window.
      */
     public Wizard( String title ){
@@ -188,22 +197,24 @@ public class Wizard implements Serializable{
         form_panel  = new JPanel();
         form_label  = new JLabel(" ",SwingConstants.CENTER);
         progress    = new JProgressBar();
+        command_handler = new CommandHandler(this);
     }
-    
-    
+
+
     protected void makeGUI(){
         GridBagConstraints gbc=new GridBagConstraints();
         gbc.fill      = GridBagConstraints.HORIZONTAL;
         gbc.weightx   = 1.0;
         gbc.anchor    = GridBagConstraints.WEST;
         gbc.gridwidth = GridBagConstraints.REMAINDER;
-        
+
         JPanel   work_area      = new JPanel( new GridBagLayout() );
         JPanel   button_panel   = new JPanel( new GridBagLayout() );
         JPanel   progress_panel = new JPanel( new GridBagLayout() );
         JMenuBar menu_bar       = new JMenuBar();
         JMenu    file_menu      = new JMenu("File");
         JMenu    help_menu      = new JMenu("Help");
+                 view_menu      = new JMenu(VIEW_MENU);
 
         frame.setJMenuBar( menu_bar );
         frame.addWindowListener( new CloseWizardWindow() );
@@ -211,11 +222,11 @@ public class Wizard implements Serializable{
         {
             int screenheight=(int)
                 Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-            frame.setBounds( (screenheight*4/3-FRAME_WIDTH)/2, 
+            frame.setBounds( (screenheight*4/3-FRAME_WIDTH)/2,
                              (screenheight-FRAME_HEIGHT)*3/10,
                              FRAME_WIDTH, FRAME_HEIGHT );
         }
-        
+
         JMenuItem help_about  = new JMenuItem( HELP_ABOUT_COMMAND );
         JMenuItem wizard_help = new JMenuItem( WIZARD_HELP_COMMAND );
         JMenuItem form_help   = new JMenuItem( FORM_HELP_COMMAND );
@@ -223,16 +234,16 @@ public class Wizard implements Serializable{
         help_menu.addSeparator();
         help_menu.add( wizard_help );
         help_menu.add( form_help );
-        
+
         JScrollPane form_scrollpane=new JScrollPane(form_panel,
                                  JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                                  JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        
-        JMenuItem save_form   = new JMenuItem( SAVE_FORM_COMMAND ); 
-        JMenuItem load_form   = new JMenuItem( LOAD_FORM_COMMAND ); 
-        JMenuItem save_wizard = new JMenuItem( SAVE_WIZARD_COMMAND ); 
-        JMenuItem load_wizard = new JMenuItem( LOAD_WIZARD_COMMAND ); 
-        JMenuItem exit_item   = new JMenuItem( EXIT_COMMAND ); 
+
+        JMenuItem save_form   = new JMenuItem( SAVE_FORM_COMMAND );
+        JMenuItem load_form   = new JMenuItem( LOAD_FORM_COMMAND );
+        JMenuItem save_wizard = new JMenuItem( SAVE_WIZARD_COMMAND );
+        JMenuItem load_wizard = new JMenuItem( LOAD_WIZARD_COMMAND );
+        JMenuItem exit_item   = new JMenuItem( EXIT_COMMAND );
         save_form.setEnabled(false);
         load_form.setEnabled(false);
         save_wizard.setEnabled(false);
@@ -244,9 +255,16 @@ public class Wizard implements Serializable{
         file_menu.add( load_wizard );
         file_menu.addSeparator();
         file_menu.add( exit_item );
+
+        //view menu
+        JMenuItem view_ds   = new JMenuItem( VIEW_DATASET );
+        view_ds.setEnabled(true);
+        view_menu.add( view_ds );
+
         menu_bar.add(file_menu);
+        menu_bar.add(view_menu);
         menu_bar.add(help_menu);
-        
+
         // add the title to the panel
         work_area.add( form_label,gbc );
 
@@ -255,7 +273,7 @@ public class Wizard implements Serializable{
         gbc.fill=GridBagConstraints.BOTH;
         form_panel.setLayout( new GridLayout(1,1) );
         work_area.add(form_scrollpane,gbc);
-        
+
         // add the progress bar to the panel
         JButton clear_button = new JButton( CLEAR_COMMAND );
         exec_all_button=new JButton(EXEC_ALL_COMMAND);
@@ -280,7 +298,7 @@ public class Wizard implements Serializable{
             gbc.anchor=GridBagConstraints.EAST;
             progress_panel.add( exec_all_button, gbc );
         }
-        
+
         // add the navigation buttons to the panel
         gbc.weightx=1.0;
         gbc.anchor=GridBagConstraints.WEST;
@@ -292,10 +310,6 @@ public class Wizard implements Serializable{
         next_button          = new JButton( NEXT_COMMAND  );
         last_button          = new JButton( LAST_COMMAND  );
         JButton exec_button  = new JButton( EXEC_COMMAND  );
-
-        JButton view_button = new JButton( VIEW_COMMAND);
-        view_button.setEnabled(true);
-        button_panel.add(view_button);
 
         back_button.setEnabled(false);
         next_button.setEnabled(false);
@@ -326,7 +340,7 @@ public class Wizard implements Serializable{
             gbc.gridwidth=GridBagConstraints.REMAINDER;
             button_panel.add( exec_button,  gbc );
         }
-            
+
 
         // add the status to the panel
         gbc.fill=GridBagConstraints.BOTH;
@@ -335,7 +349,7 @@ public class Wizard implements Serializable{
         if( standalone)
           work_area.add(SharedData.getStatusPane(), gbc);
 
-        CommandHandler command_handler = new CommandHandler(this);
+        //CommandHandler command_handler = new CommandHandler(this);
         save_form      .addActionListener( command_handler );
         load_form      .addActionListener( command_handler );
         save_wizard    .addActionListener( command_handler );
@@ -351,7 +365,8 @@ public class Wizard implements Serializable{
         wizard_help    .addActionListener( command_handler );
         form_help      .addActionListener( command_handler );
         exit_item      .addActionListener( command_handler );
-        view_button    .addActionListener( command_handler );
+        view_ds        .addActionListener( command_handler );
+        view_menu      .addActionListener( command_handler );
     }
 
     protected void showGUI(){
@@ -362,24 +377,24 @@ public class Wizard implements Serializable{
      *  This method adds a new parameter to the master list of
      *  parameters maintained by the Wizard.
      *
-     *  @param  name   A descriptive name for this parameter.  
+     *  @param  name   A descriptive name for this parameter.
      *  @param  param  The parameter to be added to the master list
      */
     public void setParameter( String name, IParameterGUI param ){
         master_list.put( name, param );
     }
-    
-    
+
+
     /**
      *  This method gets a parameter from the master list of
      *  parameters maintained by the Wizard.
      *
-     *  @param  name   the name of the paramter to be retrieved from the 
+     *  @param  name   the name of the paramter to be retrieved from the
      *                 master list.
-     *  @return param  The parameter from the master list that is identified 
+     *  @return param  The parameter from the master list that is identified
      *                 by the given name.  If there is no such parameter, this
-     *                 returns null. 
-     */ 
+     *                 returns null.
+     */
     public IParameterGUI getParameter( String name ){
         Object obj = master_list.get( name );
         if ( obj == null || !(obj instanceof IParameterGUI )){
@@ -387,30 +402,30 @@ public class Wizard implements Serializable{
                                   +name+"\n");
             return null;
         }else{
-            return (IParameterGUI)obj; 
+            return (IParameterGUI)obj;
         }
     }
 
     /**
      *  Add another form to the list of forms maintained by this
      *  wizard.
-     * 
+     *
      *  @param f  The form to be added to the list.
      */
     public void add( Form f ){
         forms.addElement( f );
         progress.setMaximum(forms.size());
     }
-    
+
     /**
      *  Get the form that is currently displayed by the wizard.
-     * 
+     *
      *  @return  The currently displayed form.
-     */ 
+     */
     public Form getCurrentForm(){
         return getForm(getCurrentFormNumber());
     }
-    
+
     /**
      * Get the number of the form currently being shown.
      */
@@ -430,8 +445,8 @@ public class Wizard implements Serializable{
             return null;
         }
     }
-    
-    
+
+
     /**
      *  Show the form at the specified position in the list of forms.
      *  If the index is invalid, an error message will be displayed in
@@ -450,21 +465,21 @@ public class Wizard implements Serializable{
                                   + index + "\n");
             return;
         }
-        
+
         Form f = getCurrentForm();          // get rid of any current form
         if ( f != null ){
             //form_panel.remove(f.getPanel());
             f.hide();
         }
-        
+
         form_panel.removeAll();
-        
+
         f = getForm(index);           // show the specified form
         form_panel.add( f.getPanel() );
         f.show();
         form_panel.validate();
         form_num = index;
-        
+
         // enable/disable the navigation buttons
         if( index>=forms.size()-1 ){
             next_button.setEnabled(false);
@@ -486,16 +501,16 @@ public class Wizard implements Serializable{
         }else{
             first_button.setEnabled(true);
         }
-        
+
         if(forms.size()==1){
             form_label.setText(f.getTitle());
         }else{
             form_label.setText("Form "+(index+1)+": "+f.getTitle());
         }
-        
+
     }
-    
-    
+
+
     /**
      *  Save the state of the wizard then exit the wizard application.
      */
@@ -506,8 +521,8 @@ public class Wizard implements Serializable{
         if(standalone) System.exit(0); // this should only be done if this is
                                        // running as a stand alone application
     }
-    
-    
+
+
     /**
      *  Save the state of the wizard to a file
      */
@@ -517,12 +532,12 @@ public class Wizard implements Serializable{
 
     /**
      *  Load the state of the wizard from a file
-     */ 
+     */
     public boolean load(){
         DataSetTools.util.SharedData.addmsg("Wizard State load() Not Implemented\n");
         return false;
     }
-    
+
     /**
      *  Set the help message that will be displayed when the user
      *  requests help with this wizard.
@@ -533,7 +548,7 @@ public class Wizard implements Serializable{
     public void setHelpMessage( String help_message ){
         this.help_message = help_message;
     }
-    
+
     /**
      *  Get the help message for this wizard.
      *
@@ -542,18 +557,18 @@ public class Wizard implements Serializable{
     public String getHelpMessage(){
         return help_message;
     }
-    
+
     /**
      *  Set the message that will be displayed when the user chooses
      *  the help about option.
      *
-     *  @param about_message  String giving the message to use for the 
+     *  @param about_message  String giving the message to use for the
      *                        "Help About" option.
      */
     public void setAboutMessage( String about_message ){
         this.about_message = about_message;
     }
-    
+
     /**
      *  Get the help about message for this wizard.
      *
@@ -592,7 +607,7 @@ public class Wizard implements Serializable{
     private class CloseWizardWindow extends WindowAdapter{
         public void windowClosing( WindowEvent event ){
             close();
-        }   
+        }
     }
 
     /**
@@ -604,7 +619,7 @@ public class Wizard implements Serializable{
       for( int i=0 ; i<=end ; i++ ){
         progress.setValue(i);
         f=getForm(i);
-        if(!f.done()){ 
+        if(!f.done()){
           if(DEBUG) System.out.print("EXECUTING "+i);
           //invalidate(i);
           boolean worked=f.execute();
@@ -617,10 +632,87 @@ public class Wizard implements Serializable{
         }
         progress.setValue(end+1);
       }
-        
+
       // invalidate subsequent forms
       invalidate(end+1);
     }
+
+    /**
+     *
+     * Creates the view menu and listeners for all of the currently
+     * validated parameters in the wizard.  Actually, what it does
+     * at this point is creates a view menu and listeners for
+     * every parameter, validated or not.
+     */
+    private void populateViewMenu(int end)
+    {
+      JMenuItem jmi;
+      //get the keys
+      Enumeration e = master_list.keys();
+      while( e.hasMoreElements() ){
+        jmi = new JMenuItem(e.nextElement().toString());
+        view_menu.add(jmi);
+        jmi.addActionListener( command_handler );
+      }
+
+    }
+
+    /**
+     *
+     * Shows the parameters according to a preferred viewing state.
+     * For example, a DataSet will be shown in a ViewManager, but a
+     * number (such as a Float) is simply displayed in a message
+     * box.
+     */
+    protected void showParameterViewer(String param_name)
+    {
+      //see if the parameter actually exists
+      Object obj;
+      Vector v;
+      ViewManager vm;
+      JPopupMenu jpm;
+
+      obj = master_list.get(param_name);
+      if( obj != null && obj instanceof IParameterGUI )
+      {
+        obj = ((IParameterGUI)obj).getValue();
+
+        //is it a DataSet?
+        if( obj instanceof DataSet )
+          vm = new ViewManager((DataSet)obj,IViewManager.IMAGE);
+
+        //is it a Vector?
+        else if( obj instanceof Vector )
+        {
+          jpm = new JPopupMenu();
+          v = (Vector)obj;
+
+          for( int i = 0; i < v.size(); i++ )
+          {
+            obj = v.elementAt(i);
+            jpm.add(new JMenuItem(obj.toString()));
+
+            //is the element a DataSet?
+            if( obj instanceof DataSet )
+              vm = new ViewManager((DataSet)obj,IViewManager.IMAGE);
+
+            else
+            {
+              //do something else
+            }
+
+          }
+          //jpm.show(null, 100, 100);
+        }
+        else
+        {
+          //handle some other IParameterGUI types
+          JOptionPane.showMessageDialog(null, obj.toString());
+        }
+
+      }
+    }
+
 
     /* ---------------- Internal Event Handler Classes --------------------- */
     /**
@@ -634,7 +726,7 @@ public class Wizard implements Serializable{
         }
         public void actionPerformed( ActionEvent event ){
             String command = event.getActionCommand();
-            
+
             if ( command.equals( FIRST_COMMAND) ){
                 form_num=0;
                 show(form_num);
@@ -660,7 +752,7 @@ public class Wizard implements Serializable{
             }else if ( command.equals( EXEC_ALL_COMMAND) ){
                 exec_forms(forms.size()-1);
             }else if ( command.equals( EXEC_COMMAND ) ){
-                exec_forms(form_num); 
+                exec_forms(form_num);
             }else if ( command.equals( HELP_ABOUT_COMMAND ) ){
                 ShowHelpMessage( about_message, "About: "+wizard.title );
             }else if ( command.equals( WIZARD_HELP_COMMAND ) ){
@@ -681,32 +773,47 @@ public class Wizard implements Serializable{
                 Form f = getCurrentForm();
                 if ( f != null )
                     f.load();
+            //this is a band-aid until I can figure out how to
+            //make a top level menu item listen to mouse
+            //clicks
+            }else if ( command.equals( VIEW_DATASET ) ){
+                populateViewMenu(form_num);
+            //this is what should really call the
+            //populateViewMenu()
+            }else if ( command.equals( VIEW_MENU ) ){
+                populateViewMenu(form_num);
             }else if ( command.equals( EXIT_COMMAND ) ){
                 close();
+            }else{
+              //obviously, more sophistication here would be nice,
+              //but the method below will not crash if the
+              //command does not match up with an actual
+              //wizard parameter
+                showParameterViewer(command);
             }
-        } 
-        
+        }
+
     }
-    
+
     /**
      *  Main program for preliminary testing only.
      */
     public static void main( String args[] ){
         System.out.println("Wizard Main");
-        
-        Wizard w = new Wizard( "Wizard Test" ); 
-        
-        w.setParameter( "Height", 
+
+        Wizard w = new Wizard( "Wizard Test" );
+
+        w.setParameter( "Height",
                   new FloatPG( "Height_Name", new Float(5.0), false ));
-        w.setParameter( "Name", 
+        w.setParameter( "Name",
                   new StringPG( "Name_Name","Dennis", false ));
-        
+
         IParameter p = w.getParameter( "Height" );
         System.out.println("P1=" + p.getName() + ", " + p.getValue() );
-        
+
         p = w.getParameter( "Name" );
         System.out.println("P2=" + p.getName() + ", " + p.getValue() );
-        
+
         w.show(0);
     }
 }
