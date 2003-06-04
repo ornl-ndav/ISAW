08c9ad8675e9cd9aa4bf6bb5c5250ece32dffe20
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-04 14:18:19 +0000

    Added test of least-squares errors, timing comparison between current implementation and implementation based on Jama and some different test cases.

--- trunk/DataSetTools/trial/BestFitTest.java
+++ trunk/DataSetTools/trial/BestFitTest.java
@@ -30,6 +30,11 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.2  2003/06/04 14:18:19  dennis
+ * Added test of least-squares errors, timing comparison
+ * between current implementation and implementation
+ * based on Jama and some different test cases.
+ *
  * Revision 1.1  2003/03/19 21:38:43  dennis
  * Basic functionality test and example of use of the
  * LinearAlgebra.BestFitMatrix(M,q,r) method.
@@ -37,23 +42,24 @@
  */
 import java.io.*;
 import DataSetTools.math.*;
+import DataSetTools.util.*;
 
 public class BestFitTest
 {
   public static void main( String args[] )
   {
     double q[][] = { { 1.1, 0, 0 },               // list of "q" vectors
-                     { 1.9, 0, 0 },
-                     { 2.9, 0, 0 },
-                     { 4.1, 0, 0 },
-                     {  .75, 0,  .75 },
+                     { 2, 0, 0 },
+                     { 3, 0, 0 },
+                     { 4, 0, 0 },
+                     {  .75, 0.1,  .75 },
                      { 1.50, 0, 1.50 },
                      { 2.25, 0, 2.25 },
                      { 3.00, 0, 3.00 },
-                     { 0, 1, 1.5 },
+                     { 0, 1, 1.6 },
                      { 0, 2, 3.0 },
                      { 0, 3, 4.5 },
-                     { 0, 4, 6.0 } };
+                     { 0.1, 4, 6.0 } };
 
     double r[][] = { { 1, 0, 0 },
                      { 2, 0, 0 },
@@ -69,8 +75,37 @@ public class BestFitTest
                      { 0, 0, 4 } };
   
     double M[][] = new double[3][3];
+                                                   // save copies of originals
+    float r_orig[][] = new float[r.length][3];
+    for ( int row = 0; row < r.length; row++ )
+      for ( int col = 0; col < 3; col++ )
+        r_orig[row][col] = (float)r[row][col];
 
-    double errors = LinearAlgebra.BestFitMatrix( M, q, r ); 
+    float q_orig[][] = new float[q.length][3];
+    for ( int row = 0; row < q.length; row++ )
+      for ( int col = 0; col < 3; col++ )
+        q_orig[row][col] = (float)q[row][col];
+
+
+    ElapsedTime timer = new ElapsedTime();
+    timer.reset();
+    double errors = 0;
+    for ( int count = 0; count < 100000; count++ )   // solve the system repeatedly to test time needed
+    {
+       for ( int row = 0; row < r.length; row++ )
+         for ( int col = 0; col < 3; col++ )
+           r[row][col] = r_orig[row][col];
+
+       for ( int row = 0; row < q.length; row++ )
+         for ( int col = 0; col < 3; col++ )
+           q[row][col] = q_orig[row][col];
+                                                  // find the best fit matrix
+      //errors = LinearAlgebra.BestFitMatrix2( M, q, r ); 
+      errors = LinearAlgebra.BestFitMatrix( M, q, r ); 
+    }
+    System.out.println("Time required to solve 100000 systems is: " + timer.elapsed() );
+//    System.out.println("Using BestFitMatrix 3   ****");
+    System.out.println("Using BestFitMatrix   ****");
 
     System.out.println("Residual errors are: " + errors );
     System.out.println("Matrix M is : " );
@@ -80,5 +115,32 @@ public class BestFitTest
         System.out.print( " " + M[row][col] + "    " );
       System.out.println();
     }
+
+                                                 // calculate the actual errors
+                                                 // between Mq and r.
+    Tran3D matrix = new Tran3D();                // identity by default;
+    float m[][] = matrix.get();                  // get reference to the matrix;
+    for ( int row = 0; row < 3; row++ )
+      for ( int col = 0; col < 3; col++ )
+        m[row][col] = (float)M[row][col];
+
+
+    float length;
+    float sum_sq = 0;                            // map each original vector
+    for ( int i = 0; i < q_orig.length; i++ )    // and accumulate the errors
+    {
+      Vector3D  vec_q = new Vector3D( q_orig[i] );
+      Vector3D  m_q   = new Vector3D();
+      matrix.apply_to( vec_q, m_q );
+      Vector3D  vec_r = new Vector3D( r_orig[i] );
+      vec_r.subtract( m_q );
+      length = vec_r.length();
+      sum_sq += length * length; 
+
+      System.out.println("q = " + vec_q + ",  m_q = " + m_q );
+    }
+
+    System.out.println( "Actual errors = " + Math.sqrt( sum_sq ) );
+
   }
 }
