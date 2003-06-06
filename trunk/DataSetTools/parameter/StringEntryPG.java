428312216aa651c02490cf32d0f014158c58e1cc
pfpeterson <pfpeterson@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-06 18:50:13 +0000

    Added to CVS.

---
+++ trunk/DataSetTools/parameter/StringEntryPG.java
@@ -0,0 +1,141 @@
+/*
+ * File:  StringEntryPG.java 
+ *
+ * Copyright (C) 2002, Peter F. Peterson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Peter F. Peterson <pfpeterson@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 South Cass Avenue, Bldg 360
+ *           Argonne, IL 60439-4845, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2003/06/06 18:48:49  pfpeterson
+ *  Added to CVS.
+ *
+ */
+
+package DataSetTools.parameter;
+import javax.swing.*;
+import java.util.Vector;
+import java.lang.String;
+import java.beans.*;
+import DataSetTools.components.ParametersGUI.*;
+import DataSetTools.util.StringFilterer;
+
+/**
+ * This is a superclass to take care of many of the common details of
+ * StringEntryPGs.
+ */
+abstract public class StringEntryPG extends ParameterGUI{
+  protected static final int            DEF_COLS = 20;
+  protected              StringFilterer FILTER   = null;
+
+  // ********** Constructors **********
+  public StringEntryPG(String name, Object value){
+    this(name,value,false);
+    this.setDrawValid(false);
+  }
+
+  public StringEntryPG(String name, Object value, boolean valid){
+    this.setName(name);
+    this.setValue(value);
+    this.setEnabled(true);
+    this.setValid(valid);
+    this.setDrawValid(true);
+    this.type="UNKNOWN";
+    this.initialized=false;
+    this.ignore_prop_change=false;
+  }
+
+  // ********** IParameter requirements **********
+
+  /**
+   * Returns the value of the parameter. While this is a generic
+   * object specific parameters will return appropriate
+   * objects. There can also be a 'fast access' method which returns
+   * a specific object (such as String or DataSet) without casting.
+   */
+  public Object getValue(){
+    Object value=null;
+    if(this.initialized){
+      value=((JTextField)this.entrywidget).getText();
+    }else{
+      value=this.value;
+    }
+    return value;
+  }
+
+  /**
+   * Sets the value of the parameter.
+   */
+  protected void setEntryValue(Object value){
+    if(this.initialized){
+      if(value==null){
+        ((JTextField)this.entrywidget).setText("");
+      }else{
+        if(value instanceof String){
+          ((JTextField)this.entrywidget).setText((String)value);
+        }else{
+          ((JTextField)this.entrywidget).setText(value.toString());
+        }
+      }
+    }else{
+      return;
+    }
+    this.setValid(true);
+  }
+
+  // ********** IParameterGUI requirements **********
+  /**
+   * Allows for initialization of the GUI after instantiation.
+   */
+  public void init(Vector init_values){
+    if(this.initialized) return; // don't initialize more than once
+    if(init_values!=null){
+      if(init_values.size()==1){
+        // the init_values is what to set as the value of the parameter
+        this.setValue(init_values.elementAt(0));
+      }else{
+        // something is not right, should throw an exception
+      }
+    }
+
+    entrywidget=new StringEntry(this.value.toString(),DEF_COLS,FILTER);
+    entrywidget.addPropertyChangeListener(IParameter.VALUE, this);
+    this.setEnabled(this.getEnabled());
+    super.initGUI();
+  }
+
+  /**
+   * Set the enabled state of the EntryWidget. This produces a more
+   * pleasant effect than the default setEnabled of the widget.
+   */
+  public void setEnabled(boolean enabled){
+    this.enabled=enabled;
+    if(this.getEntryWidget()!=null){
+      ((JTextField)this.entrywidget).setEditable(enabled);
+    }
+  }
+}
