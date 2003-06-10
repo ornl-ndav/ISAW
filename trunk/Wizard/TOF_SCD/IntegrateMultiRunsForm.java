16d8c36dbe906af4d5dd26eb6c0354954160b251
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-10 19:55:27 +0000

    Moved Operator creation into a private method to avoid excessive Object recreation. Added parameter to specify SCD calibration file line. Updated documentation.

--- trunk/Wizard/TOF_SCD/IntegrateMultiRunsForm.java
+++ trunk/Wizard/TOF_SCD/IntegrateMultiRunsForm.java
@@ -28,6 +28,12 @@
  * number DMR-0218882.
  *
  * $Log$
+ * Revision 1.7  2003/06/10 19:55:27  bouzekc
+ * Moved Operator creation into a private method to avoid
+ * excessive Object recreation.
+ * Added parameter to specify SCD calibration file line.
+ * Updated documentation.
+ *
  * Revision 1.6  2003/06/09 21:53:23  bouzekc
  * Updated documentation.
  * Added constructor to set HAS_CONSTANTS to reduce
@@ -64,6 +70,9 @@ public class IntegrateMultiRunsForm extends Form
   private Vector choices;
   protected static int RUN_NUMBER_WIDTH = 5;
   protected final String SCDName = "SCD";
+  private LoadOneHistogramDS loadHist;
+  private Integrate integrate;
+  private LoadSCDCalib loadSCD;
 
   /* ----------------------- DEFAULT CONSTRUCTOR ------------------------- */
   /**
@@ -100,12 +109,14 @@ public class IntegrateMultiRunsForm extends Form
    *  @param calibfile          SCD calibration file.
    *  @param time_slice_range   The time-slice range
    *  @param increase_amt       Amount to increase slice size by.
+   *  @param line2use           SCD calibration file line to use.
+   *  @param append             Append to file (yes/no).
    */
   
   public IntegrateMultiRunsForm(String rawpath, String outpath, String runnums,
                                 String expname, int ctype, String calibfile,
                                 String time_slice_range, 
-                                int increase_amt)
+                                int increase_amt, int line2use, boolean append)
   {
     this();
     getParameter(0).setValue(rawpath);
@@ -116,6 +127,8 @@ public class IntegrateMultiRunsForm extends Form
     getParameter(5).setValue(calibfile);
     getParameter(6).setValue(time_slice_range);
     getParameter(7).setValue(new Integer(increase_amt));
+    getParameter(8).setValue(new Integer(line2use));
+    getParameter(9).setValue(new Boolean(append));
   }
 
   /**
@@ -149,14 +162,19 @@ public class IntegrateMultiRunsForm extends Form
     addParameter(new IntegerPG("Amount to Increase Slice Size By", 
                                new Integer(1), false));
     //8
+    addParameter(new IntegerPG( "SCD Calibration File Line to Use", 
+                                new Integer(-1), false));
+    //9
+    addParameter(new BooleanPG( "Append to File?", Boolean.FALSE, false));
+    //10
     addParameter(new LoadFilePG( "Integrated Peaks File ", "", false));
 
     //don't monkey around with the run numbers and such if this form
     //relies on previously calculated values
     if(HAS_CONSTANTS)
-      setParamTypes(new int[]{0,1,2,3,5}, new int[]{4,6,7}, new int[]{8});
+      setParamTypes(new int[]{0,1,2,3,5}, new int[]{4,6,7,8,9}, new int[]{10});
     else
-      setParamTypes(null, new int[]{0,1,2,3,4,5,6,7}, new int[]{8});
+      setParamTypes(null, new int[]{0,1,2,3,4,5,6,7,8,9}, new int[]{10});
   }
 
 
@@ -189,6 +207,9 @@ public class IntegrateMultiRunsForm extends Form
     s.append("@param calibfile SCD calibration file.\n");
     s.append("@param time_slice_range The time-slice range.\n");
     s.append("@param increase_amt Amount to increase slice size by.\n");
+    s.append("@param line2use SCD calibration file line to use.\n");
+    s.append("@param append True/false indicating whether to append to the ");
+    s.append(".integrate file.\n");
     s.append("@return A Boolean indicating success or failure of the Form's ");
     s.append("execution.\n");
     s.append("@error Invalid raw data path.\n");
@@ -219,10 +240,10 @@ public class IntegrateMultiRunsForm extends Form
     SharedData.addmsg("Executing...\n");
     IParameterGUI param;
     Object obj;
-    String outputDir, centerType, matrixName, calibFile, expName, rawDir;
+    String outputDir, matrixName, calibFile, expName, rawDir, centerType;
     String integName, sliceRange, loadName, runNum;
     boolean append, first;
-    int timeSliceDelta, histNum;
+    int timeSliceDelta, SCDline;
     DataSet histDS;
     int[] runsArray;
 
@@ -310,14 +331,35 @@ public class IntegrateMultiRunsForm extends Form
       return errorOut(param,
         "ERROR: you must enter a valid integer to increment the slice size by.");
 
+    //get line number for SCD calibration file
+    param = (IParameterGUI)super.getParameter( 8 );
+    obj = param.getValue();
+    if( obj != null && obj instanceof Integer )
+    {
+        SCDline = ((Integer)obj).intValue();
+        param.setValid(true);
+    }
+    else
+      return errorOut(param,
+         "ERROR: you must enter a valid line number to use.\n");
+
+    //get append to file value
+    param = (IParameterGUI)super.getParameter( 9 );
+    //this one doesn't need to be checked for validity
+    param.setValid(true);
+    append = ((BooleanPG)param).getbooleanValue();
+
     //the name for the saved *.integrate file
     integName = outputDir + "/" + expName + ".integrate";
     integName = StringUtil.setFileSeparator(integName);
 
     //first time through the file
     first = true;
-    //appending to file?
-    append = false;
+
+    //to avoid excessive object creation, we'll create all of the 
+    //Operators here, then just set their parameters in the loop
+    createIntegrateOperators(calibFile, SCDline, integName, sliceRange,
+                             timeSliceDelta, append, centerType);
 
     for(int i = 0; i < runsArray.length; i++)
     {
@@ -328,19 +370,15 @@ public class IntegrateMultiRunsForm extends Form
                .Format
                .integerPadWithZero(runsArray[i], RUN_NUMBER_WIDTH);
 
-      loadName = rawDir + "/" + SCDName + runNum + ".RUN";
-      loadName = StringUtil.setFileSeparator(loadName);
+      loadName = StringUtil.setFileSeparator(
+                   rawDir + "/" + SCDName + runNum + ".RUN");
 
       SharedData.addmsg("Loading " + loadName + ".");
 
-      //get the histogram from runfile retriever.
-      histNum = 1;
-
-      /*If you want to be able to use a group mask,
-        change the "" below to a String variable.
-        I've been told this is not used. -CMB*/
-      obj = new LoadOneHistogramDS(loadName, histNum, "").getResult();
-
+      //load the histogram
+      
+      loadHist.getParameter(0).setValue(loadName);
+      obj = loadHist.getResult();
       //make sure it is a DataSet
       if(obj instanceof DataSet)
         histDS = (DataSet)obj;
@@ -349,8 +387,10 @@ public class IntegrateMultiRunsForm extends Form
 
       SharedData.addmsg("Integrating peaks for " + loadName);
 
-      //load the SCD calibration data
-      obj = new LoadSCDCalib(histDS, calibFile,1,"").getResult();
+      //load calibration data 
+
+      loadSCD.setDataSet(histDS);
+      obj = loadSCD.getResult();
       if(obj instanceof ErrorString)
         return errorOut("LoadSCDCalib failed: " + obj.toString());
 
@@ -360,11 +400,15 @@ public class IntegrateMultiRunsForm extends Form
                      outputDir + "/ls" + expName + runNum + ".mat");
 
       SharedData.addmsg("Integrating run number " + runNum + ".");
-
-      obj = new Integrate(histDS, integName, 
+      
+      integrate.getParameter(0).setValue(histDS);
+      integrate.getParameter(2).setValue(matrixName);
+      integrate.getParameter(7).setValue(new Boolean(append));
+      obj = integrate.getResult();
+      /*obj = new Integrate(histDS, integName, 
                          matrixName,
                          sliceRange, timeSliceDelta, 1,
-                         append).getResult();
+                         append).getResult();*/
       if(obj instanceof ErrorString)
         return errorOut("Integrate failed: " + obj.toString());
 
@@ -379,7 +423,7 @@ public class IntegrateMultiRunsForm extends Form
     SharedData.addmsg("Peaks are listed in " + integName);
 
     //set the integrate file name for the result
-    param = (IParameterGUI)getParameter(9);
+    param = (IParameterGUI)getParameter(10);
     param.setValue(integName.toString());
     param.setValid(true);
 
@@ -399,5 +443,57 @@ public class IntegrateMultiRunsForm extends Form
     choices.add("[f]ace centered");         // 4
     choices.add("[i] body centered");       // 5
     choices.add("[r]hombohedral centered"); // 6
+  }
+
+  /**
+   *  Creates the Operators necessary for this Form and sets their
+   *  constant values.
+   *
+   *  @param  calibFile              SCD calibration file.
+   *
+   *  @param  SCDline                The line to use from the SCD calib file.
+   *
+   *  @param  integName              The name of the .integrate file.
+   *
+   *  @param  sliceRange             The time slice range.
+   *
+   *  @param  timeSliceDelta         Amount to increase slice size by.
+   *
+   *  @param  append                 Whether to append to peaks file.
+   *
+   *  @param  centerType             Centering type.
+   *
+   */
+  private void createIntegrateOperators(String calibFile, int SCDline,
+                                        String integName, String sliceRange,
+                                        int timeSliceDelta, boolean append,
+                                        String centerType)
+  {
+    loadHist = new LoadOneHistogramDS();
+    integrate = new Integrate(); 
+    loadSCD = new LoadSCDCalib(); 
+
+    //LoadOneHistogramDS
+
+    //get the histogram.  We want to retrieve the first one.
+    loadHist.getParameter(1).setValue(new Integer(1));
+    /*If you want to be able to use a group mask,
+      change the "" below to a String variable.
+      I've been told this is not used. -CMB*/
+    loadHist.getParameter(2).setValue("");
+
+    //Integrate
+    
+    integrate.getParameter(1).setValue(integName);
+    integrate.getParameter(3).setValue(centerType);
+    integrate.getParameter(4).setValue(sliceRange);
+    integrate.getParameter(5).setValue(new Integer(timeSliceDelta));
+    integrate.getParameter(6).setValue(new Integer(1));
+
+    //LoadSCDCalib
+
+    loadSCD.getParameter(0).setValue(calibFile);
+    loadSCD.getParameter(1).setValue(new Integer(SCDline));
+    loadSCD.getParameter(2).setValue("");
   }
 }
