66102e1e4ae1413afa977c67fb2f2749840ac876
pfpeterson <pfpeterson@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-13 14:58:34 +0000

    Added to CVS.

---
+++ trunk/DataSetTools/operator/OperatorFactory.java
@@ -0,0 +1,44 @@
+/*
+ * File:  OperatorFactory.java
+ *
+ * Copyright (C) 2003, Peter Peterson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Peter F. Peterson <pfpeterson@anl.gov>
+ *           Intense Pulsed Neutron Source Division
+ *           Argonne National Laboratory
+ *           9700 South Cass Avenue, Bldg 360
+ *           Argonne, IL 60439-4845, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * $Log$
+ * Revision 1.1  2003/06/13 14:56:31  pfpeterson
+ * Added to CVS.
+ *
+ */
+package DataSetTools.operator;
+
+public interface OperatorFactory{
+  /**
+   * Generate an operator instance from the given source. In principle
+   * this can be a filename or a classname (package resolve).
+   */
+  public Operator getInstance(String source);
+}
