99b9121dff84791601ddb4868f426f2b890a9a31
pfpeterson <pfpeterson@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-16 19:07:38 +0000

    Removed old code and updated to work with new getCategoryList() code in base operator class.

--- trunk/DataSetTools/operator/DataSet/EditList/DS_EditList.java
+++ trunk/DataSetTools/operator/DataSet/EditList/DS_EditList.java
@@ -30,6 +30,10 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.3  2003/06/16 19:03:04  pfpeterson
+ *  Removed old code and updated to work with new getCategoryList() code
+ *  in base operator class.
+ *
  *  Revision 1.2  2002/11/27 23:17:40  pfpeterson
  *  standardized header
  *
@@ -56,23 +60,11 @@ abstract public class DS_EditList extends    DataSetOperator
                                   implements Serializable
 {
 
+   private static String[] categoryList=null;
    protected DS_EditList( String title )
    {
       super( title );
    } 
-
-  /* -------------------------- getCategory -------------------------------- */
-  /**
-   * Get the category of this operator
-   *
-   * @return  A String specifying the category of this operator.  This is
-   *          actually the category of the abstract base class from which
-   *          the current operator is directly derived.
-   */
-  public String getCategory()
-  {
-    return EDIT_LIST;
-  }
 
   /* ------------------------ getCategoryList ------------------------------ */
   /**
@@ -89,11 +81,9 @@ abstract public class DS_EditList extends    DataSetOperator
    */
   public String[] getCategoryList()
   {
-    String partial_list[] = super.getCategoryList();  // get list of ancestor
-                                                      // categories and put 
-                                                      // them in a new larger
-                                                      // list.
-    return AppendCategory( EDIT_LIST, partial_list );
-  }
+    if(categoryList==null)
+      categoryList=createCategoryList();
 
+    return categoryList;
+  }
 } 
--- trunk/DataSetTools/operator/Generic/Special/GenericSpecial.java
+++ trunk/DataSetTools/operator/Generic/Special/GenericSpecial.java
@@ -30,6 +30,10 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.3  2003/06/16 19:07:06  pfpeterson
+ *  Removed old code and updated to work with new getCategoryList() code
+ *  in base operator class.
+ *
  *  Revision 1.2  2002/11/27 23:21:43  pfpeterson
  *  standardized header
  *
@@ -53,24 +57,12 @@ import DataSetTools.operator.Generic.GenericOperator;
 abstract public class GenericSpecial extends    GenericOperator 
                                      implements Serializable
 {
+  private static String[] categoryList=null;
 
    protected GenericSpecial( String title )
    {
       super( title );
    } 
-
-  /* -------------------------- getCategory -------------------------------- */
-  /**
-   * Get the category of this operator
-   *
-   * @return  A String specifying the category of this operator.  This is
-   *          actually the category of the abstract base class from which
-   *          the current operator is directly derived.
-   */
-  public String getCategory()
-  {
-    return SPECIAL;
-  }
 
   /* ------------------------ getCategoryList ------------------------------ */
   /**
@@ -87,10 +79,9 @@ abstract public class GenericSpecial extends    GenericOperator
    */
   public String[] getCategoryList()
   {
-    String partial_list[] = super.getCategoryList();  // get list of ancestor
-                                                      // categories and put 
-                                                      // them in a new larger
-                                                      // list.
-    return AppendCategory( SPECIAL, partial_list );
+    if(categoryList==null)
+      categoryList=createCategoryList();
+
+    return categoryList;
   }
 } 
