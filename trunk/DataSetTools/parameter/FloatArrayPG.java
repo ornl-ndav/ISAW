be297c766ebfa862901f9c93c62b5e0b06a6c9d9
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-23 15:23:45 +0000

    Reformatted for consistent indenting.

--- trunk/DataSetTools/parameter/FloatArrayPG.java
+++ trunk/DataSetTools/parameter/FloatArrayPG.java
@@ -5,37 +5,28 @@ import javax.swing.*;
 import java.awt.*;
 import DataSetTools.util.PGActionListener;
 
-public class FloatArrayPG extends VectorPG
-  {
+public class FloatArrayPG extends VectorPG{
 
-   public FloatArrayPG( String Prompt, Object value)
-      { super( new FloatPG("Enter Float",0.0f),"Enter Float List");
-        setValue( value);
-       }
+  public FloatArrayPG( String Prompt, Object value){ 
+    super( new FloatPG("Enter Float",0.0f),"Enter Float List");
+    setValue( value);
+  }
 
-      public static void main( String args[] )
-      {
-         JFrame jf = new JFrame("Test");
-         jf.getContentPane().setLayout( new GridLayout( 1,2));
-         FloatArrayPG IaPg = new FloatArrayPG( "Enter Int list", null);
-         IaPg.init();
-         jf.getContentPane().add(IaPg.getGUIPanel());
-         JButton  jb = new JButton("Result");
-         jf.getContentPane().add(jb);
-         jb.addActionListener( new PGActionListener( IaPg));
-         jf.setSize( 500,100);
-         jf.invalidate();
-         jf.show();
-
-
-
-
-      }    
-public Object clone()
-  {
+  public static void main( String args[] ){
+    JFrame jf = new JFrame("Test");
+    jf.getContentPane().setLayout( new GridLayout( 1,2));
+    FloatArrayPG IaPg = new FloatArrayPG( "Enter Int list", null);
+    IaPg.init();
+    jf.getContentPane().add(IaPg.getGUIPanel());
+    JButton  jb = new JButton("Result");
+    jf.getContentPane().add(jb);
+    jb.addActionListener( new PGActionListener( IaPg));
+    jf.setSize( 500,100);
+    jf.invalidate();
+    jf.show();
+  }    
+  public Object clone(){
     FloatArrayPG faap = new FloatArrayPG( getName(), getValue());
     return (Object)faap;
-
   }       
-
-   }
+}
