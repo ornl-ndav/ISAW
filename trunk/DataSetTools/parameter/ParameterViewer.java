5dc356951f5e22933eede1f58e825adda8155382
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-06-26 16:46:46 +0000

    Reformatted for readability.

--- trunk/DataSetTools/parameter/ParameterViewer.java
+++ trunk/DataSetTools/parameter/ParameterViewer.java
@@ -1,4 +1,4 @@
- /*
+/*
  * File:  ParameterViewer.java
  *
  * Copyright (C) 2003, Christopher M. Bouzek
@@ -32,6 +32,9 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.5  2003/06/26 16:46:46  bouzekc
+ * Reformatted for readability.
+ *
  * Revision 1.4  2003/06/10 21:57:30  bouzekc
  * Now allows viewing of arrays of ASCII files using the
  * multiple view menu.  Split off code for viewing ASCII
@@ -49,111 +52,106 @@
  * Added to CVS. (Chris Bouzek)
  *
  */
- 
-package DataSetTools.parameter; 
- 
-import java.util.*; 
+package DataSetTools.parameter;
+
+import DataSetTools.dataset.DataSet;
+
+import DataSetTools.operator.Generic.Special.ViewASCII;
+
+import DataSetTools.parameter.*;
+
+import DataSetTools.util.*;
+
+import DataSetTools.viewer.*;
+
 import java.awt.*;
 import java.awt.event.*;
+
+import java.util.*;
+
 import javax.swing.*;
-import DataSetTools.parameter.*;
-import DataSetTools.viewer.*;
-import DataSetTools.dataset.DataSet;
-import DataSetTools.operator.Generic.Special.ViewASCII;
-import DataSetTools.util.*;
- 
+
+
 /**
-  *  Class for viewing IParameterGUIs.  Originally designed for the 
-  *  Wizards, so that DataSets and other parameters could be viewed 
-  *  after operations were performed.
-  */
-public class ParameterViewer implements ActionListener
-{
+ *  Class for viewing IParameterGUIs.  Originally designed for the
+ *  Wizards, so that DataSets and other parameters could be viewed
+ *  after operations were performed.
+ */
+public class ParameterViewer implements ActionListener {
   private IParameterGUI param;
   private JFrame holder;
   private JList selector;
   private Container c;
-  private JButton view_b, cancel_b;
+  private JButton view_b;
+  private JButton cancel_b;
   private JScrollPane jsp;
   private Vector items;
   private JPanel button_p;
   private String[] item_names;
-  
-  private final String VIEW = "View";
+  private final String VIEW   = "View";
   private final String CANCEL = "Cancel";
-  
-  public ParameterViewer(IParameterGUI ipg)
-  {
+
+  public ParameterViewer( IParameterGUI ipg ) {
     param = ipg;
   }
- 
- 
+
   /**
    *
    * Shows the parameters according to a preferred viewing state.
    * For example, a DataSet will be shown in a ViewManager, but a
    * number (such as a Float) is simply displayed in a message
-   * box.  For parameters which can be selected from a list, 
-   * such as an ArrayPG of DataSets, only one can be selected at a 
+   * box.  For parameters which can be selected from a list,
+   * such as an ArrayPG of DataSets, only one can be selected at a
    * time.
    */
-  public void showParameterViewer() 
-  {
+  public void showParameterViewer(  ) {
     Object obj;
     Vector v;
 
-    if( param != null )
-    {
-      obj = param.getValue();
+    if( param != null ) {
+      obj = param.getValue(  );
 
       //is it a DataSet?
-      if( obj instanceof DataSet )
-        new ViewManager((DataSet)obj,IViewManager.IMAGE);
-
+      if( obj instanceof DataSet ) {
+        new ViewManager( ( DataSet )obj, IViewManager.IMAGE );
+      }
       //is it a Vector?
-      else if( obj instanceof Vector )
-      {    
-        v = (Vector)obj;
+      else if( obj instanceof Vector ) {
+        v = ( Vector )obj;
 
-        if(!v.isEmpty())
-        {
-          obj = v.elementAt(0);
-          if( v.size() == 1 )
-          {
-            if( obj instanceof DataSet )
-              new ViewManager((DataSet)obj,IViewManager.IMAGE);
-            else if(obj instanceof String)
-              tryToDisplayASCII((String)obj);
-          }  
-          else
-          {
+        if( !v.isEmpty(  ) ) {
+          obj = v.elementAt( 0 );
+
+          if( v.size(  ) == 1 ) {
+            if( obj instanceof DataSet ) {
+              new ViewManager( ( DataSet )obj, IViewManager.IMAGE );
+            } else if( obj instanceof String ) {
+              tryToDisplayASCII( ( String )obj );
+            }
+          } else {
             //display a list so the user can choose the DataSet to view
             //basic setup for selection list
-            int num_items = v.size();
-            item_names = new String[num_items]; 
-            items = new Vector(num_items);  
+            int num_items = v.size(  );
 
-            for( int i = 0; i < num_items; i++ )
-            {
-              obj = v.elementAt(i);
-              item_names[i] = obj.toString();
-              items.add(obj);
+            item_names   = new String[num_items];
+            items        = new Vector( num_items );
+
+            for( int i = 0; i < num_items; i++ ) {
+              obj             = v.elementAt( i );
+              item_names[i]   = obj.toString(  );
+              items.add( obj );
             }
-           
-            this.makeSelectionList();
+
+            this.makeSelectionList(  );
           }
         }
-        
+      } else if( obj instanceof String ) {  //see if it is a file
+        tryToDisplayASCII( ( String )obj );
       }
-      else if(obj instanceof String)  //see if it is a file
-        tryToDisplayASCII((String)obj);
-
-      else
-      {
+      else {
         //handle some other IParameterGUI types
-        JOptionPane.showMessageDialog(null, obj.toString());
+        JOptionPane.showMessageDialog( null, obj.toString(  ) );
       }
-
     }
   }
 
@@ -162,92 +160,88 @@ public class ParameterViewer implements ActionListener
    *  ASCII file.  It simply drops the String (i.e. does not display
    *  it) if it is not a file.
    */
- private void tryToDisplayASCII(String fileName)
- {
-   String tempName;
-   Object obj;
-   //try to determine if it is a viewable file
+  private void tryToDisplayASCII( String fileName ) {
+    String tempName;
+    Object obj;
 
-   //is this a file we can read?
-   if(fileName.indexOf('.') > 0)
-   {
-     tempName = fileName.toLowerCase();
-          
-     //ASCII files
-     //we don't want any windoze executables, or 
-     //any big ISAW data files
-     if( !( (tempName.indexOf(".sdds") >= 0)  || 
-            (tempName.indexOf(".exe" ) >= 0)  ||
-            (tempName.indexOf(".hdf" ) >= 0)  ||
-            (tempName.indexOf(".run" ) >= 0)  ||
-            (tempName.indexOf(".nxs" ) >= 0) )
-       )
-     {
-       //look at the peaks file
-       obj = new ViewASCII(fileName).getResult();  
-       if(obj instanceof ErrorString)
-         SharedData.addmsg(obj.toString());
-     }
-   } 
- }
-   
+    //try to determine if it is a viewable file
+    //is this a file we can read?
+    if( fileName.indexOf( '.' ) > 0 ) {
+      tempName = fileName.toLowerCase(  );
+
+      //ASCII files
+      //we don't want any windoze executables, or 
+      //any big ISAW data files
+      if( 
+        !( ( tempName.indexOf( ".sdds" ) >= 0 ) ||
+          ( tempName.indexOf( ".exe" ) >= 0 ) ||
+          ( tempName.indexOf( ".hdf" ) >= 0 ) ||
+          ( tempName.indexOf( ".run" ) >= 0 ) ||
+          ( tempName.indexOf( ".nxs" ) >= 0 ) ) ) {
+        //look at the peaks file
+        obj = new ViewASCII( fileName ).getResult(  );
+
+        if( obj instanceof ErrorString ) {
+          SharedData.addmsg( obj.toString(  ) );
+        }
+      }
+    }
+  }
+
   /**
    *  Note that this relies mainly on what is in the String
    *  array item_names in order to create the list
    */
-  private void makeSelectionList()
-  {        
-    if( item_names != null )
-    {
+  private void makeSelectionList(  ) {
+    if( item_names != null ) {
       //create the selection list 
-      holder = new JFrame();  
-      holder.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-      c = holder.getContentPane();       
-      holder.setSize(640,480);
-      selector = new JList(item_names); 
-      selector.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-      selector.setPreferredSize(new Dimension(320,240));
-      jsp = new JScrollPane(selector);
-      jsp.setPreferredSize(new Dimension(320,240));
-          
-      view_b = new JButton(VIEW);
-      cancel_b = new JButton(CANCEL);
-      view_b.addActionListener(this);
-      cancel_b.addActionListener(this);
-      view_b.setPreferredSize(new Dimension(80,40));
-      cancel_b.setPreferredSize(new Dimension(80,40));
-         
-      c.setLayout(new BorderLayout());
-      c.add(jsp, BorderLayout.CENTER);
-      button_p = new JPanel(new GridLayout());
-      button_p.add(view_b);
-      button_p.add(cancel_b);
-      c.add(button_p, BorderLayout.SOUTH);
-         
-      holder.show();
+      holder = new JFrame(  );
+      holder.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+      c = holder.getContentPane(  );
+      holder.setSize( 640, 480 );
+      selector = new JList( item_names );
+      selector.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
+      selector.setPreferredSize( new Dimension( 320, 240 ) );
+      jsp = new JScrollPane( selector );
+      jsp.setPreferredSize( new Dimension( 320, 240 ) );
+
+      view_b     = new JButton( VIEW );
+      cancel_b   = new JButton( CANCEL );
+      view_b.addActionListener( this );
+      cancel_b.addActionListener( this );
+      view_b.setPreferredSize( new Dimension( 80, 40 ) );
+      cancel_b.setPreferredSize( new Dimension( 80, 40 ) );
+
+      c.setLayout( new BorderLayout(  ) );
+      c.add( jsp, BorderLayout.CENTER );
+      button_p = new JPanel( new GridLayout(  ) );
+      button_p.add( view_b );
+      button_p.add( cancel_b );
+      c.add( button_p, BorderLayout.SOUTH );
+
+      holder.show(  );
     }
   }
-   
-  public void actionPerformed(ActionEvent e)
-  {
+
+  public void actionPerformed( ActionEvent e ) {
     int index;
     Object obj;
-   
-    if( e.getActionCommand().equals(CANCEL) )
-      holder.dispose();
-    else if( e.getActionCommand().equals(VIEW) )
-    {
-      index = selector.getSelectedIndex();
-              
-      if( index >= 0 && items != null && items.size() > 0)
-      {
-        obj = items.elementAt(index);
+
+    if( e.getActionCommand(  ).equals( CANCEL ) ) {
+      holder.dispose(  );
+    } else if( e.getActionCommand(  ).equals( VIEW ) ) {
+      index = selector.getSelectedIndex(  );
+
+      if( ( index >= 0 ) && ( items != null ) && ( items.size(  ) > 0 ) ) {
+        obj = items.elementAt( index );
+
         //DataSet?
-        if( obj instanceof DataSet )
-          new ViewManager((DataSet)obj,IViewManager.IMAGE);
-        else if(obj instanceof String)
-          tryToDisplayASCII((String)obj);
+        if( obj instanceof DataSet ) {
+          new ViewManager( ( DataSet )obj, IViewManager.IMAGE );
+        } else if( obj instanceof String ) {
+          tryToDisplayASCII( ( String )obj );
+        }
       }
-    }  
+    }
   }
 }
