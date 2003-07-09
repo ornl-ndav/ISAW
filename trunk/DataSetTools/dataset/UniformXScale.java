0e922b607503ba6280aaa567f267ae1846a7a5bc
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-07-09 14:57:15 +0000

    Updated java docs for getI() method.

--- trunk/DataSetTools/dataset/UniformXScale.java
+++ trunk/DataSetTools/dataset/UniformXScale.java
@@ -30,6 +30,9 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.12  2003/07/09 14:57:14  dennis
+ *  Updated java docs for getI() method.
+ *
  *  Revision 1.11  2003/02/24 13:33:54  dennis
  *  Added method restrict() to restrict an XScale to the intersection
  *  of the XScale and a ClosedInterval.
@@ -161,7 +164,8 @@ public class UniformXScale extends XScale implements Serializable
 
 
   /**
-   *  Get the position of the specified x-value in this XScale.
+   *  Get the position (or index of the LUB) of the specified x-value in
+   *  this XScale.
    *
    *  @param  x    The x value to find in the "list" of x values represented
    *               by this x scale.
@@ -172,6 +176,8 @@ public class UniformXScale extends XScale implements Serializable
    *          the index of the first x that is greater than or equal to the 
    *          specified x.  If the specified x value is above the end of 
    *          the x scale, the number of points in the x scale is returned.  
+   *          This is the index of the Least Upper Bound (chosen from the
+   *          list of values in the XScale) for the specified x_value.
    */
   public int getI( float x )
   {
