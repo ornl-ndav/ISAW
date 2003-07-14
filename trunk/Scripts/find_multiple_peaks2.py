4757007a093f906ab76a2bf053ec093488bc3128
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-07-14 20:37:41 +0000

    Added parameters and code to trim out border pixels.  Now returns the name of the peaks file.

--- trunk/Scripts/find_multiple_peaks2.py
+++ trunk/Scripts/find_multiple_peaks2.py
@@ -30,6 +30,10 @@
 # Modified:
 #
 # $Log$
+# Revision 1.2  2003/07/14 20:37:41  bouzekc
+# Added parameters and code to trim out border pixels.  Now
+# returns the name of the peaks file.
+#
 # Revision 1.1  2003/07/08 23:42:44  bouzekc
 # Added to CVS (Chris Bouzek).
 #
@@ -38,23 +42,37 @@ class find_multiple_peaks2(GenericOperator):
     def setDefaultParameters(self):
         self.addParameter(DataDirPG("Raw Data Path",None))
         self.addParameter(DataDirPG("Output Data Path",None))
-        self.addParameter(ArrayPG("Run Numbers",None))
+        self.addParameter(IntArrayPG("Run Numbers",None))
         self.addParameter(StringPG("Experiment Name",""))
         self.addParameter(IntegerPG("Number of Peaks",50))
         self.addParameter(IntegerPG("Miniumum Peak Intensity",3))
         self.addParameter(BooleanPG("Append",1))
         self.addParameter(LoadFilePG("SCD Calibration File",None))
+        self.addParameter(IntegerPG("SCD Calibration File Line",-1))
+        self.addParameter(IntArrayPG("Pixel Rows and Columns to Keep","0:100"))
 
     def getResult(self):
         in_path=self.getParameter(0).value
         out_path=self.getParameter(1).value
-        run_numbers=self.getParameter(2).value
+        run_numbers=self.getParameter(2).getArrayValue()
         expname=self.getParameter(3).value
         num_peaks=self.getParameter(4).value
         min_int=self.getParameter(5).value
         append=self.getParameter(6).value
         calibfile=self.getParameter(7).value
+        calibLine=self.getParameter(8).value
+        #get the detector border range
+        border = self.getParameter(9).getArrayValue()
 
+        if border != None:
+            lowerLimit = border[0]  #lower limit of range
+            #upper limit of range
+            upperLimit = border[len(border) - 1]
+        else:  
+            #shouldn't happen, but default to 0:MAX_VALUE
+            lowerLimit = 0
+            upperLimit = Integer.MAX_VALUE
+    
         inst="SCD"
         SharedData.addmsg("Instrument="+inst)
         SharedData.addmsg("Calibration File="+calibfile)
@@ -75,6 +93,17 @@ class find_multiple_peaks2(GenericOperator):
             #find peaks
             findPeaks=FindPeaks(ds[dsnum],monct,num_peaks,min_int) # FindPeaks
             peaks=findPeaks.getResult()
+            #trim out edge peaks (defined by the border parameter)
+            peaksArray = peaks.toArray( )
+            i = 0
+            for k in peaksArray:
+                peak = peaksArray[i]
+                #see if the peak pixels are within the user defined array.  We are
+                #assuming a SQUARE detector, so we'll reject it if the x or y position
+                #is not within our range
+                if peak.x() > upperLimit or peak.x() < lowerLimit or peak.y() > upperLimit or peak.y() < lowerLimit:
+                    peaksVec.remove( k )
+                i = i + 1
             centroidPeaks=CentroidPeaks(ds[dsnum],peaks)  # CentroidPeaks
             peaks=centroidPeaks.getResult()
             # write out the results
@@ -85,7 +114,7 @@ class find_multiple_peaks2(GenericOperator):
             if(first):
                 first=0
                 append=Boolean(1)
-        echo=EchoObject("--- find_multiple_peaks is done ---") # Echo
+        echo=EchoObject("--- Find Multiple Peaks is done ---") # Echo
         echo.getResult()
         # show the peaks file
         viewASCII=ViewASCII(out_path+expname+".peaks") # ViewASCII
@@ -93,6 +122,7 @@ class find_multiple_peaks2(GenericOperator):
         SharedData.addmsg("Peaks are listed in %s%s.peaks"%(out_path,expname))
         # close the dialog automatically (broken)
         ExitDialog().getResult()
+        return out_path+expname+".peaks"
 
     def __init__(self):
         Operator.__init__(self,"Find Multiple Peaks")
