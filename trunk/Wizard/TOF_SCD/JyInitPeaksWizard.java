84fabfe631f31b040f34c2c9f51b1c9fb389a89d
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-07-14 20:36:46 +0000

    Added to CVS.

---
+++ trunk/Wizard/TOF_SCD/JyInitPeaksWizard.java
@@ -0,0 +1,153 @@
+/*
+ * File:  JyInitPeaksWizard.java
+ *
+ * Copyright (C) 2003, Chris M. Bouzek
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ *           Chris Bouzek <coldfusion78@yahoo.com>
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0218882.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * $Log$
+ * Revision 1.1  2003/07/14 20:36:46  bouzekc
+ * Added to CVS.
+ *
+ *
+ */
+package Wizard.TOF_SCD;
+
+import DataSetTools.operator.*;
+
+import DataSetTools.parameter.*;
+
+import DataSetTools.util.*;
+
+import DataSetTools.wizard.*;
+
+import Operators.TOF_SCD.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.io.*;
+
+import java.util.*;
+
+import javax.swing.*;
+
+
+/**
+ * This class constructs a Wizard used for initially finding peaks.  In this
+ * Wizard, BlindJ is used for creating a matrix file.  This Wizard uses Jython
+ * Script Forms and Operator Forms, as opposed to the InitialPeaksWizard,
+ * which uses OperatorForms and custom crafted Forms.
+ */
+public class JyInitPeaksWizard extends Wizard {
+  //~ Static fields/initializers ***********************************************
+
+  private static final String LOADFILETYPE = "LoadFile";
+
+  //~ Constructors *************************************************************
+
+  /**
+   * Default constructor.  Sets standalone in Wizard to true.
+   */
+  public JyInitPeaksWizard(  ) {
+    this( true );
+  }
+
+  /**
+   * Constructor for setting the standalone variable in Wizard.
+   *
+   * @param standalone Boolean indicating whether the Wizard stands alone
+   *        (true) or is contained in something else (false).
+   */
+  public JyInitPeaksWizard( boolean standalone ) {
+    super( "Initial SCD Peaks Wizard", standalone );
+    this.createAllForms(  );
+
+    StringBuffer s = new StringBuffer(  );
+
+    s.append( "This Wizard is designed to be used as an initial\n" );
+    s.append( "tool for finding peaks from SCD run files.  It\n" );
+    s.append( "applies BlindJ, IndexJ, ScalarJ, and LsqrsJ\n" );
+    s.append( "to the output .peaks file from the first Form.\n" );
+    this.setHelpMessage( s.toString(  ) );
+  }
+
+  //~ Methods ******************************************************************
+
+  /**
+   * Method for running the Initial Peaks wizard as standalone.
+   */
+  public static void main( String[] args ) {
+    JyInitPeaksWizard w = new JyInitPeaksWizard( true );
+
+    //specified a --nogui switch but forgot to give a filename
+    if( args.length == 1 ) {
+      System.out.println( 
+        "USAGE: java Wizard.TOF_SCD.InitialPeaksWizard " +
+        "[--nogui] <Wizard Save File>" );
+    } else if( args.length == 2 ) {
+      w.executeNoGUI( args[1] );
+    } else {
+      w.showForm( 0 );
+    }
+  }
+
+  /**
+   * Adds and coordinates the necessary Forms for this Wizard.
+   */
+  private void createAllForms(  ) {
+    int[][] fpi = {
+      { 10, 0, 0, -1, 0, 0 },  //peaks file 
+      { -1, 2, 1, 0, -1, -1 }
+    };  //matrix
+
+    JyScriptForm peaksform = new JyScriptForm( 
+        "find_multiple_peaks2.py", LOADFILETYPE, "Peaks file" );
+
+    //the return types of all of these Operator Forms is LoadFilePG,
+    //hence the "LoadFile"
+    OperatorForm blindjform = new OperatorForm( 
+        new BlindJ(  ), LOADFILETYPE, "BlindJ log file", new int[]{ 0 } );
+    OperatorForm indexjform = new OperatorForm( 
+        new IndexJ(  ), LOADFILETYPE, "IndexJ log file", new int[]{ 0, 1 } );
+    OperatorForm scalarjform = new OperatorForm( 
+        new ScalarJ(  ), LOADFILETYPE, "ScalarJ log file", new int[]{ 0 } );
+    OperatorForm lsqrsjform = new OperatorForm( 
+        new LsqrsJ(  ), LOADFILETYPE, "LsqrsJ log file", new int[]{ 0 } );
+    OperatorForm indexjform2 = new OperatorForm( 
+        new IndexJ(  ), LOADFILETYPE, "IndexJ log file", new int[]{ 0 } );
+
+    this.addForm( peaksform );
+    this.addForm( blindjform );
+    this.addForm( indexjform );
+    this.addForm( scalarjform );
+    this.addForm( lsqrsjform );
+    this.addForm( indexjform2 );
+
+    super.linkFormParameters( fpi );
+  }
+}
