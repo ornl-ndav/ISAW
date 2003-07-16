2dc9cfb869584ba85134e5f573fda70212e26410
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-07-16 22:36:54 +0000

    Added seralVersionUID, so that small changes can be made to this class without destroying compatibility.  This will break communication capability with previous versions of the Live/FileDataServers, but will allow longer term compatibility with future releases.

--- trunk/DataSetTools/dataset/CompressedDataSet.java
+++ trunk/DataSetTools/dataset/CompressedDataSet.java
@@ -30,6 +30,12 @@
  * Modified:
  * 
  *  $Log$
+ *  Revision 1.3  2003/07/16 22:36:54  dennis
+ *  Added seralVersionUID, so that small changes can be made to this
+ *  class without destroying compatibility.  This will break communication
+ *  capability with previous versions of the Live/FileDataServers, but
+ *  will allow longer term compatibility with future releases.
+ *
  *  Revision 1.2  2003/07/09 21:24:31  dennis
  *  Added test main program.
  *
@@ -51,6 +57,26 @@ import DataSetTools.retriever.*;
 
 public class CompressedDataSet implements Serializable
 {
+  // NOTE: any field that is static or transient is NOT serialized.
+  //
+  // CHANGE THE "serialVersionUID" IF THE SERIALIZATION IS INCOMPATIBLE WITH
+  // PREVIOUS VERSIONS, IN WAYS THAT CAN NOT BE FIXED BY THE readObject()
+  // METHOD.  SEE "IsawSerialVersion" COMMENTS BELOW.  CHANGING THIS CAUSES
+  // JAVA TO REFUSE TO READ DIFFERENT VERSIONS.
+  //
+  public  static final long serialVersionUID = 1L;
+
+  // NOTE: The following fields are serialized.  If new fields are added that
+  //       are not static, reasonable default values should be assigned in the
+  //       readObject() method for compatibility with old servers, until the
+  //       servers can be updated.
+
+  private int IsawSerialVersion = 1;         // CHANGE THIS WHEN ADDING OR
+                                             // REMOVING FIELDS, IF
+                                             // readObject() CAN FIX ANY
+                                             // COMPATIBILITY PROBLEMS
+
+
   private byte   ds_bytes[] = null;         // contains the compressed DataSet
   private String ds_title   = "UNKNOWN";
 
@@ -130,6 +156,38 @@ public class CompressedDataSet implements Serializable
     return ds_bytes;
   }
 
+/* -----------------------------------------------------------------------
+ *
+ *  PRIVATE METHODS
+ *
+ */
+
+/* ---------------------------- readObject ------------------------------- */
+/**
+ *  The readObject method is called when objects are read from a serialized
+ *  ojbect stream, such as a file or network stream.  The non-transient and
+ *  non-static fields that are common to the serialized class and the
+ *  current class are read by the defaultReadObject() method.  The current
+ *  readObject() method MUST include code to fill out any transient fields
+ *  and new fields that are required in the current version but are not
+ *  present in the serialized version being read.
+ */
+
+  private void readObject( ObjectInputStream s ) throws IOException,
+                                                        ClassNotFoundException
+  {
+    s.defaultReadObject();               // read basic information
+
+    if ( IsawSerialVersion != 1 )
+    {
+      System.out.println("Warning:Data IsawSerialVersion != 1");
+                                         // put code here to "fix" the problem
+                                         // with different versions if possible.
+    }
+
+                                         // set meaningful values for transient
+                                         // fields, if any, here.
+  }
 
 
   /* ----------------------------- main --------------------------------- */
@@ -165,7 +223,6 @@ public class CompressedDataSet implements Serializable
 
     CompressedDataSet comp_ds2 = new CompressedDataSet( ds );
     System.out.println("Reduced, compressed DataSet size = " + comp_ds2.size());
-
   }
 
 } 
