2a677f57554a90f522f9f6b4d03cce0538a97fa3
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2003-10-11 19:24:33 +0000

    Removed declaration of "ParamUsesString" as the superclass declares it already.  Removed clone() definition as the superclass implements it using reflection.

--- trunk/DataSetTools/parameter/IntegerPG.java
+++ trunk/DataSetTools/parameter/IntegerPG.java
@@ -31,11 +31,14 @@
  * Modified:
  *
  *  $Log$
- *  Revision 1.13  2003/10/08 22:39:09  dennis
- *  Reverting to previous version, that was in ISAW 1.5.1 beta 8.
- *  The most recent checkin (10/07/03) also removed the clone() method
- *  and was not consistent with the version in CVS.  ISAW crashed on
- *  startup with null pointer exception.
+ *  Revision 1.14  2003/10/11 19:24:33  bouzekc
+ *  Removed declaration of "ParamUsesString" as the superclass declares it
+ *  already.  Removed clone() definition as the superclass implements it
+ *  using reflection.
+ *
+ *  Revision 1.12  2003/10/07 18:38:51  bouzekc
+ *  Removed declaration of "implements ParamUsesString" as the
+ *  StringEntryPG superclass now declares it.
  *
  *  Revision 1.11  2003/09/13 23:36:34  bouzekc
  *  Added call to validateSelf() in setValue().
@@ -89,7 +92,7 @@ import DataSetTools.util.*;
  * This is a superclass to take care of many of the common details of
  * IntegerPGs.
  */
-public class IntegerPG extends StringEntryPG implements ParamUsesString{
+public class IntegerPG extends StringEntryPG {
     private static final String TYPE="Integer";
 
     // ********** Constructors **********
@@ -221,14 +224,4 @@ public class IntegerPG extends StringEntryPG implements ParamUsesString{
         fpg.initGUI(null);
         fpg.showGUIPanel();
     }*/
-
-    /**
-     * Definition of the clone method.
-     */
-    public Object clone(){
-        IntegerPG pg=new IntegerPG(this.name,this.value,this.valid);
-        pg.setDrawValid(this.getDrawValid());
-        pg.initialized=false;
-        return pg;
-    }
 }
