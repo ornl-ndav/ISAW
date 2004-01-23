b47c087ca8905696a9222821d2dda11407d73353
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-01-23 15:33:49 +0000

    Reformatted for clarity.

--- trunk/DataSetTools/components/ParametersGUI/StringField.java
+++ trunk/DataSetTools/components/ParametersGUI/StringField.java
@@ -32,6 +32,9 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.5  2004/01/23 15:33:49  bouzekc
+ *  Reformatted for clarity.
+ *
  *  Revision 1.4  2003/08/14 19:11:49  bouzekc
  *  Made inner Toolkit transient.
  *
@@ -49,110 +52,164 @@
  *
  *
  */
- 
 package DataSetTools.components.ParametersGUI;
 
-import javax.swing.*; 
-import javax.swing.text.*; 
-import java.awt.Toolkit;
-import java.util.Locale;
-import java.beans.*;
 import DataSetTools.parameter.*;
 
+import java.awt.Toolkit;
+
+import java.beans.*;
+
+import java.util.Locale;
+
+import javax.swing.*;
+import javax.swing.text.*;
+
+
 /**
- * This class is intended to be used as a replacement for JTextField
- * whan a integer value is to be entered. The major difference is an
- * overridden insertString method which beeps when something that
- * isn't found in an integer is entered.
+ * This class is intended to be used as a replacement for JTextField whan a
+ * integer value is to be entered. The major difference is an overridden
+ * insertString method which beeps when something that isn't found in an
+ * integer is entered.
  */
 public class StringField extends JTextField {
-    private transient Toolkit toolkit;
-    private PropertyChangeSupport propBind=new PropertyChangeSupport(this);
+  //~ Instance fields **********************************************************
+
+  private transient Toolkit toolkit;
+  private PropertyChangeSupport propBind = new PropertyChangeSupport( this );
+
+  //~ Constructors *************************************************************
+
+  /**
+   * Constructs an StringField with the appropriate number of columns and the
+   * default value of zero.
+   */
+  public StringField( int columns ) {
+    this( "", columns );
+  }
+
+  /**
+   * Constructs an StringField with a specified default value and number of
+   * columns.
+   */
+  public StringField( String value, int columns ) {
+    super( value, columns );
+    toolkit = Toolkit.getDefaultToolkit(  );
+  }
+
+  //~ Methods ******************************************************************
+
+  /**
+   * DOCUMENT ME!
+   *
+   * @param prop DOCUMENT ME!
+   * @param pcl DOCUMENT ME!
+   */
+  public void addPropertyChangeListener( 
+    String prop, PropertyChangeListener pcl ) {
+    super.addPropertyChangeListener( prop, pcl );
+
+    if( propBind != null ) {
+      propBind.addPropertyChangeListener( prop, pcl );
+    }
+  }
+
+  /**
+   * DOCUMENT ME!
+   *
+   * @param pcl DOCUMENT ME!
+   */
+  public void addPropertyChangeListener( PropertyChangeListener pcl ) {
+    super.addPropertyChangeListener( pcl );
+
+    if( propBind != null ) {
+      propBind.addPropertyChangeListener( pcl );
+    }
+  }
+
+  /**
+   * DOCUMENT ME!
+   *
+   * @param pcl DOCUMENT ME!
+   */
+  public void removePropertyChangeListener( PropertyChangeListener pcl ) {
+    super.removePropertyChangeListener( pcl );
+
+    if( propBind != null ) {
+      propBind.removePropertyChangeListener( pcl );
+    }
+  }
+
+  /**
+   * A hook to override the insertString method.
+   */
+  protected Document createDefaultModel(  ) {
+    return new StringDocument( this );
+  }
+
+  /**
+   * Internal method to confirm that the text can be added.
+   */
+  private boolean isOkay( int offs, String inString, String curString ) {
+    return true;
+  }
+
+  //~ Inner Classes ************************************************************
+
+  /**
+   * Internal class to do all of the formatting checks.
+   */
+  protected class StringDocument extends PlainDocument {
+    //~ Instance fields ********************************************************
+
+    private StringField textBox;
+
+    //~ Constructors ***********************************************************
 
     /**
-     * Constructs an StringField with the appropriate number of
-     * columns and the default value of zero.
+     * Creates a new StringDocument object.
+     *
+     * @param T DOCUMENT ME!
      */
-    public StringField(int columns){
-        this("",columns);
+    public StringDocument( StringField T ) {
+      super(  );
+      textBox = T;
     }
+
+    //~ Methods ****************************************************************
 
     /**
-     * Constructs an StringField with a specified default value and
-     * number of columns.
+     * Overrides the default insertString method. Insert if okay, beep if not.
      */
-    public StringField(String value, int columns) {
-        super(value,columns);
-        toolkit = Toolkit.getDefaultToolkit();
-    }
+    public void insertString( int offs, String str, AttributeSet a )
+      throws BadLocationException {
+      String oldText = textBox.getText(  );
 
-    /** 
-     * A hook to override the insertString method.
-     */
-    protected Document createDefaultModel() {
-        return new StringDocument(this);
-    }
+      if( textBox.isOkay( offs, str, textBox.getText(  ) ) ) {
+        super.insertString( offs, str, a );
 
-    public void addPropertyChangeListener(String prop,
-                                          PropertyChangeListener pcl){
-        super.addPropertyChangeListener(prop,pcl);
-        if(propBind!=null) propBind.addPropertyChangeListener(prop,pcl);
-    }
-    public void addPropertyChangeListener(PropertyChangeListener pcl){
-        super.addPropertyChangeListener(pcl);
-        if(propBind!=null) propBind.addPropertyChangeListener(pcl);
-    }
-    public void removePropertyChangeListener(PropertyChangeListener pcl){
-        super.removePropertyChangeListener(pcl);
-        if(propBind!=null) propBind.removePropertyChangeListener(pcl);
-    }
-
-    /**
-     * Internal method to confirm that the text can be added.
-     */
-    private boolean isOkay(int offs, String inString, String curString){
-        return true;
-    }
-
-    /**
-     * Internal class to do all of the formatting checks.
-     */
-    protected class StringDocument extends PlainDocument {
-        private StringField textBox;
-        public StringDocument(StringField T){
-            super();
-            textBox=T;
+        if( propBind != null ) {
+          propBind.firePropertyChange( 
+            IParameter.VALUE, oldText, textBox.getText(  ) );
         }
-
-        /**
-         * Overrides the default insertString method. Insert if okay,
-         * beep if not.
-         */
-        public void insertString(int offs, String str, AttributeSet a) 
-            throws BadLocationException {
-            
-            String oldText=textBox.getText();
-            if(textBox.isOkay(offs,str,textBox.getText())){
-                super.insertString(offs,str,a);
-                if(propBind!=null)
-                    propBind.firePropertyChange(IParameter.VALUE,
-                                                oldText,textBox.getText());
-            }else{
-                toolkit.beep();
-            }
-        }
-
-        /** 
-         * Overrides the default remove method.
-         */
-        public void remove(int offs, int len) throws BadLocationException{
-            String oldText=textBox.getText();
-            super.remove(offs,len);
-            //System.out.println("value:"+oldText+"->"+textBox.getText());
-            if(propBind!=null)
-                propBind.firePropertyChange(IParameter.VALUE,
-                                            oldText,textBox.getText());
-        }
-
+      } else {
+        toolkit.beep(  );
+      }
     }
+
+    /**
+     * Overrides the default remove method.
+     */
+    public void remove( int offs, int len ) throws BadLocationException {
+      String oldText = textBox.getText(  );
+
+      super.remove( offs, len );
+
+      //System.out.println("value:"+oldText+"->"+textBox.getText());
+      if( propBind != null ) {
+        propBind.firePropertyChange( 
+          IParameter.VALUE, oldText, textBox.getText(  ) );
+      }
+    }
+  }
 }
