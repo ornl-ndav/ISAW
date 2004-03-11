4f934c036c0a356cabef25e793f5477a167bd106
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-03-11 18:21:32 +0000

    Documented file using javadoc statements. Removed the main method.

--- trunk/devTools/Hawk/classDescriptor/gui/panel/search/AdvancedOptionsJPanel.java
+++ trunk/devTools/Hawk/classDescriptor/gui/panel/search/AdvancedOptionsJPanel.java
@@ -32,6 +32,10 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.2  2004/03/11 18:21:32  bouzekc
+ * Documented file using javadoc statements.
+ * Removed the main method.
+ *
  * Revision 1.1  2004/02/07 05:15:49  bouzekc
  * Added to CVS.  Changed package name.  Uses RobustFileFilter
  * rather than ExampleFileFilter.  Added copyright header for
@@ -40,35 +44,51 @@
  */
 package devTools.Hawk.classDescriptor.gui.panel.search;
 
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
 /**
- * @author kramer
- *
  *This class is a special BasicOptionsJPanel which has the correct options when searching for a group of terms (such as parameters).
  *This includes an extra JComboBox and JSpinner combination that allows the user to specify where each term should be
- *(either anywhere, as last element, or at a specific element number).
+ *(either anywhere, as last element, or at a specific element number) in the Vector.
+ *@author Dominic Kramer
  */
 public class AdvancedOptionsJPanel extends BasicOptionsJPanel implements ActionListener
 {
+	/**
+	 * Integer specifying that the String has to be anywhere in the Vector.
+	 */
 	public static int ANYWHERE = 0;
+	/**
+	 * Integer specifying that the String has to be at the last element ing the Vector.
+	 */
 	public static int AT_LAST_ELEMENT = 1;
+	/**
+	 * Integer specifying the String has to be at a specific element.
+	 */
 	public static int AT_ELEMENT = 2;
+	/** The label on the panel. */
 	protected JLabel locationLabel;
+	/** The combo box specifying where the String should be in the Vector. */
 	protected JComboBox locationBox;
+	/** The JSpinner associated with the panel. */
 	protected JSpinner numberSpinner;
+	/** The model describing the elements in the list. */
 	protected SpinnerNumberModel model;
+	/** The frame onto which this panel is added to. */
 	protected JFrame frame;
 	
+	/**
+	 * Create a new AdvancedOptionsJPanel.
+	 * @param textTitle The title of the panel.
+	 * @param FRAME The frame onto which this panel is placed.
+	 */
 	public AdvancedOptionsJPanel(String textTitle, JFrame FRAME)
 	{
 		super(textTitle);
@@ -93,43 +113,74 @@ public class AdvancedOptionsJPanel extends BasicOptionsJPanel implements ActionL
 		add(numberSpinner);
 	}
 	
+	/**
+	 * Get the frame associated with this panel.
+	 */
 	public JFrame getJFrame()
 	{
 		return frame;
 	}
 	
+	/**
+	 * Set the frame associated with this panel.
+	 * @param FRAME The frame to associate with this panel.
+	 */
 	public void setJFrame(JFrame FRAME)
 	{
 		frame = FRAME;
 	}
-	
+
+	/**
+	 * Set the JComboBox specifying where the String is supposed to be.
+	 * @param num Either ANYWHERE, AT_LAST_ELEMENT, or AT_ELEMENT.
+	 */
 	public void setLocationBoxSelectedBox(int num)
 	{
 		if (num==ANYWHERE || num==AT_LAST_ELEMENT || num==AT_ELEMENT)
 			locationBox.setSelectedIndex(num);
 	}
 	
+	/**
+	 * Get the text of locationBox.
+	 * @return Either "Anywhere", "At last element", or "At element".
+	 */
 	public String getLocationBoxSelectedText()
 	{
 		return (String)locationBox.getItemAt(locationBox.getSelectedIndex());
 	}
 	
+	/**
+	 * Get the value of locationBox.  This method is more useful for checking 
+	 * conditions.
+	 * @return Either ANYWHERE, AT_LAST_ELEMENT, or AT_ELEMENT.
+	 */
 	public int getLocationBoxSelectedValue()
 	{
 		return locationBox.getSelectedIndex();
 	}
 	
+	/**
+	 * Set the JSpinner's value.
+	 * @param num The value to set.
+	 */
 	public void setNumberSpinnerValue(int num)
 	{
 		if (num>0)
 			model.setValue(new Integer(num));
 	}
-
+	
+	/**
+	 * Get the JSpinner's value.
+	 * @return The JSpinner's value.
+	 */
 	public int getNumberSpinnerValue()
 	{
 		return ((Integer)model.getValue()).intValue();
 	}
 	
+	/**
+	 * Handles action events.
+	 */	
 	public void actionPerformed(ActionEvent event)
 	{
 		if (event.getActionCommand().equals("location.box"))
@@ -143,18 +194,5 @@ public class AdvancedOptionsJPanel extends BasicOptionsJPanel implements ActionL
 		}
 		
 		super.actionPerformed(event);
-	}
-
-	//this is to test how the JPanel looks
-	public static void main(String args[])
-	{
-		JFrame frame = new JFrame();
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new BorderLayout());
-		AdvancedOptionsJPanel panel = new AdvancedOptionsJPanel("Class Name:", frame);
-		mainPanel.add(panel, BorderLayout.CENTER);
-		frame.getContentPane().add(mainPanel);
-		frame.setVisible(true);
-		frame.pack();
 	}
 }
