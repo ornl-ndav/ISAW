fca55493db33d5d5cd9fbd2b398e1cd07052abc7
bouzekc <bouzekc@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-03-11 18:30:31 +0000

    Documented file using javadoc statements. Added support for colored line numbers.

--- trunk/devTools/Hawk/classDescriptor/gui/internalFrame/ColorfulTextGUI.java
+++ trunk/devTools/Hawk/classDescriptor/gui/internalFrame/ColorfulTextGUI.java
@@ -32,6 +32,10 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.2  2004/03/11 18:30:31  bouzekc
+ * Documented file using javadoc statements.
+ * Added support for colored line numbers.
+ *
  * Revision 1.1  2004/02/07 05:09:14  bouzekc
  * Added to CVS.  Changed package name.  Uses RobustFileFilter
  * rather than ExampleFileFilter.  Added copyright header for
@@ -41,6 +45,7 @@
 package devTools.Hawk.classDescriptor.gui.internalFrame;
 
 import java.awt.Color;
+import java.text.DecimalFormat;
 
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
@@ -53,44 +58,76 @@ import devTools.Hawk.classDescriptor.tools.SourceCodeToken;
 import devTools.Hawk.classDescriptor.tools.SourceCodeTokenizer;
 import devTools.Hawk.classDescriptor.tools.SystemsManager;
 
-
 /**
- * @author kramer
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ * This is the superclass to the classes SourceCodeGUI and ShortenedSourceGUI because 
+ * it handles some general methods for writing colored text to a window.
+ * @author Dominic Kramer
  */
 public abstract class ColorfulTextGUI extends DesktopInternalFrame
 {
+	/**
+	 * Create a new ColorfulTextGUI object.
+	 * @param desk The HawkDesktop associated with this window.
+	 */
 	public ColorfulTextGUI(HawkDesktop desk)
 	{
 		super(desk);
 	}
 	
+	/**
+	 * Gets a copy of this window.
+	 */
 	public abstract DesktopInternalFrame getCopy();
 	
+	/**
+	 * This method takes the string supplied and uses a SourceCodeTokenizer 
+	 * to break the string into source code, comment, and javadocs sections.  Each 
+	 * section is added to the StyledDocument with the correct color depending if the 
+	 * section is source code, javadocs statement, or comment.  This method calls the 
+	 * addProcessedText(String, StyledDocument) to handle coloring source code sections.
+	 * @param str A String representing part or all of a source code file.
+	 * @param doc The document to add the colorized version of the string to.
+	 */
 	public void styleDocument(String str, StyledDocument doc)
 	{
 		SourceCodeTokenizer tokenizer = new SourceCodeTokenizer(str);
+		int maxLineNum = tokenizer.getNumberOfLinesInString();
+		int numOfDigits = 0;
+		while (maxLineNum%(Math.pow(10,numOfDigits)) != maxLineNum)
+			numOfDigits++;
+		
+		String formatStr = "";
+		for (int i=0; i<numOfDigits; i++)
+			formatStr += "0";
+		DecimalFormat numberModifier = new DecimalFormat(formatStr);
 		SourceCodeToken token = tokenizer.nextToken();
 		while (token != null)
 		{
-			System.out.println("token.getString()="+token.getString());
-			System.out.println("token.getType()="+token.getType());
-			
 			try
 			{
 				if (token.isSourceCode())
 				{
-					addProcessedText(token.getString(), doc);
+					for (int i=0; i<token.getStringVec().size(); i++)
+					{
+						doc.insertString(doc.getLength(),""+numberModifier.format(token.getLineNumberAt(i))+"  ",doc.getStyle("lineNumber"));
+						addProcessedText(token.getStringAt(i)+"\n", doc);
+					}
 				}
 				else if (token.isComment())
 				{
-					doc.insertString(doc.getLength(),token.getString(),doc.getStyle("comment"));
+					for (int i=0; i<token.getStringVec().size(); i++)
+					{
+						doc.insertString(doc.getLength(),""+numberModifier.format(token.getLineNumberAt(i))+"  ",doc.getStyle("lineNumber"));
+						doc.insertString(doc.getLength(),token.getStringAt(i)+"\n",doc.getStyle("comment"));
+					}
 				}
 				else if (token.isJavadocs())
 				{
-					doc.insertString(doc.getLength(),token.getString(),doc.getStyle("javadocs"));
+					for (int i=0; i<token.getStringVec().size(); i++)
+					{
+						doc.insertString(doc.getLength(),""+numberModifier.format(token.getLineNumberAt(i))+"  ",doc.getStyle("lineNumber"));
+						doc.insertString(doc.getLength(),token.getStringAt(i)+"\n",doc.getStyle("javadocs"));
+					}
 				}
 			}
 			catch (BadLocationException e)
@@ -100,7 +137,15 @@ public abstract class ColorfulTextGUI extends DesktopInternalFrame
 			token = tokenizer.nextToken();
 		}
 	}
-
+	
+	/**
+	 * This method adds the string to the document  coloring words as needed.  
+	 * Any keywords found in the string are colored blue and the rest of the words 
+	 * are colored black.  Thus this method handles coloring only source code sections 
+	 * of code (not javadocs statements or comments).
+	 * @param str The string to add to the document.
+	 * @param doc The document to add the colorized string to.
+	 */
 	public void addProcessedText(String str, StyledDocument doc)
 	{
 		String token = "";
@@ -113,10 +158,13 @@ public abstract class ColorfulTextGUI extends DesktopInternalFrame
 								
 				if (isAnInvisibleString(token))
 				{
+					/*
 					if (token.equals("\t"))
 						doc.insertString(doc.getLength(), "  ", null);
 					else
-						doc.insertString(doc.getLength(), token, null);
+					*/
+							doc.insertString(doc.getLength(), token, null);
+					
 				}
 				else
 				{
@@ -146,6 +194,12 @@ public abstract class ColorfulTextGUI extends DesktopInternalFrame
 		}
 	}
 	
+	/**
+	 * Returns true if str is equal to a space, end of line character, tab character, or 
+	 * return character.
+	 * @param str The String to analyze.
+	 * @return True for "invisible" characters (\n, \t, \r, or a space).
+	 */
 	private boolean isAnInvisibleString(String str)
 	{
 		if (str.equals(" ") || str.equals("\n") || str.equals("\t") || str.equals("\r"))
@@ -154,10 +208,20 @@ public abstract class ColorfulTextGUI extends DesktopInternalFrame
 			return false;
 	}
 	
+	/**
+	 * This adds styles to the document.  The style name and color pairs are as follows:
+	 * <br> Name            Color
+	 * <br> keyword        Blue
+	 * <br> comment       Green
+	 * <br> javadocs        Red
+	 * @param doc
+	 */
 	public void addStylesToDocument(StyledDocument doc)
 	{
 		//this is the default style which is the root of the style hierarchy
 		Style def = StyleContext.getDefaultStyleContext().getStyle(StyleContext.DEFAULT_STYLE);
+		Style lineNumberStyle = doc.addStyle("lineNumber",def);
+		StyleConstants.setForeground(lineNumberStyle,Color.GRAY);
 		Style keyword = doc.addStyle("keyword",def);
 		StyleConstants.setForeground(keyword,Color.BLUE);
 		Style comment = doc.addStyle("comment",keyword);
@@ -166,6 +230,16 @@ public abstract class ColorfulTextGUI extends DesktopInternalFrame
 		StyleConstants.setForeground(javadocs,Color.RED);
 	}
 	
+	/**
+	 * This returns true if the string supplied is a keyword.  Here are the following keywords:  
+	 * abstract, boolean, break, byte, case, catch, char, class, const, continue, default, do, double, 
+	 * else, extends, final, finally, float, for, future, generic, goto, if, implements, imports, inner, 
+	 * instanceof, int, interface, long, native, new, null, operator, outer, package, private, protected, 
+	 * public, rest, return, short, static, super, switch, synchronized, this, throw, throws, transient, 
+	 * try, var, void, volatile, while
+	 * @param str The string to analyze.
+	 * @return True if str is a keyword and false otherwise.
+	 */
 	protected boolean isAKeyword(String str)
 	{
 		if (str.equals("abstract") || str.equals("boolean") || str.equals("break") || str.equals("byte") || str.equals("case") || str.equals("catch") || str.equals("char") || str.equals("class") || str.equals("const") ||
