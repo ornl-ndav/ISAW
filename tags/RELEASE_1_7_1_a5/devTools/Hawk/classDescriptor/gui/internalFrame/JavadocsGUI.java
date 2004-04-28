5737cca38e1e28328f95e238430fb41022409884
nobody <nobody@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-04-28 21:22:56 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_7_1_a5'.

---
+++ tags/RELEASE_1_7_1_a5/devTools/Hawk/classDescriptor/gui/internalFrame/JavadocsGUI.java
@@ -0,0 +1,160 @@
+/*
+ * File:  JavadocsGUI.java
+ *
+ * Copyright (C) 2004 Dominic Kramer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Dominic Kramer <kramerd@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA and by
+ * the National Science Foundation under grant number DMR-0218882.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.3  2004/03/12 19:46:16  bouzekc
+ * Changes since 03/10.
+ *
+ * Revision 1.1  2004/02/07 05:09:15  bouzekc
+ * Added to CVS.  Changed package name.  Uses RobustFileFilter
+ * rather than ExampleFileFilter.  Added copyright header for
+ * Dominic.
+ * 
+ */
+package devTools.Hawk.classDescriptor.gui.internalFrame;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JEditorPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import devTools.Hawk.classDescriptor.gui.frame.HawkDesktop;
+import devTools.Hawk.classDescriptor.modeledObjects.Interface;
+
+/**
+ * This is a special type of JInternalFrame that displays the javadocs file for a class 
+ * or interface.
+ * @author Dominic Kramer
+ */
+public class JavadocsGUI extends DesktopInternalFrame implements ActionListener
+{
+	/**
+	 * The pane to add the javadocs file to.
+	 */
+	protected JEditorPane htmlPane;
+	/**
+	 * The Interface whose javadocs file is to be displays.
+	 */
+	protected Interface selectedInterface;
+	
+	/**
+	 * Create a new JavadocsGUI.
+	 * @param INT The Interface whose javadocs file is to be displayed.
+	 * @param title The title of the window.
+	 * @param desk The HawkDesktop that this window is on.
+	 */
+	public JavadocsGUI(Interface INT, String title, HawkDesktop desk)
+	{
+		super(desk);
+		
+		//now to instantiate selectedInterface
+			selectedInterface = INT;
+		//now to set some of the characteristics of the window
+			setTitle(title);
+			setLocation(0,0);
+			setSize(175,400);
+			setClosable(true);
+			setIconifiable(true);
+			setMaximizable(true);
+			setResizable(true);
+		
+		//now to make the main areas of the frame
+			Container pane = getContentPane();
+			JPanel mainPanel = new JPanel();
+			mainPanel.setLayout(new BorderLayout());
+			
+		//now to create the area for placing the javadocs in html format
+			htmlPane = new JEditorPane("text/html", INT.getJavadocAsString());
+			htmlPane.setEditable(false);
+							
+		//now to create the JScrollPane to put the JEditorPane on
+			JScrollPane scrollPane = new JScrollPane(htmlPane);
+			
+		//now to add the components to the main panel
+			mainPanel.add(scrollPane, BorderLayout.CENTER);
+						
+		pane.add(mainPanel);
+		
+		//Now to make the JMenuBar
+			JMenuBar javadocsMenuBar = new JMenuBar();
+				JMenu fileMenu = new JMenu("File");
+					JMenuItem closeItem = new JMenuItem("Close");
+					closeItem.setActionCommand("Close");
+					closeItem.addActionListener(this);
+				fileMenu.add(closeItem);
+			javadocsMenuBar.add(fileMenu);
+			javadocsMenuBar.add(InternalFrameUtilities.constructViewMenu(this,true,true,false,true));					
+//			refreshMoveAndCopyMenu();
+//			windowMenu.addMenuListener(new WindowMenuListener(this,menuBar,windowMenu));
+			javadocsMenuBar.add(windowMenu);
+		menuBar = javadocsMenuBar;
+		setJMenuBar(javadocsMenuBar);
+		
+		resizeAndRelocate();
+	}
+	
+	/**
+	 * Gets a copy of this window.
+	 * @return A copy of this window.
+	 */	
+	public DesktopInternalFrame getCopy()
+	{
+		return new JavadocsGUI(selectedInterface,getTitle(),desktop);
+	}
+	
+	/**
+	 * Handles ActionEvents.
+	 */
+	public void actionPerformed(ActionEvent event)
+	{
+		if (event.getActionCommand().equals("Close"))
+		{
+			dispose();
+		}
+		else
+		{
+//			JavadocsGUI copy = (JavadocsGUI)getCopy();
+//			copy.setVisible(true);
+//			processWindowChange(event,copy,this);
+			super.actionPerformed(event);
+			InternalFrameUtilities.processActionEventFromViewMenu(event,selectedInterface,desktop);
+		}
+	}
+}
---
+++ tags/RELEASE_1_7_1_a5/devTools/Hawk/classDescriptor/threads/FindClassThread.java
@@ -0,0 +1,127 @@
+/*
+ * File:  FindClassThread.java
+ *
+ * Copyright (C) 2004 Dominic Kramer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Dominic Kramer <kramerd@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA and by
+ * the National Science Foundation under grant number DMR-0218882.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.3  2004/03/12 19:46:19  bouzekc
+ * Changes since 03/10.
+ *
+ * Revision 1.1  2004/02/07 05:10:26  bouzekc
+ * Added to CVS.  Changed package name.  Uses RobustFileFilter
+ * rather than ExampleFileFilter.  Added copyright header for
+ * Dominic.
+ *
+ */
+package devTools.Hawk.classDescriptor.threads;
+
+import java.util.Vector;
+
+import devTools.Hawk.classDescriptor.gui.frame.CreateNewProjectGUI;
+import devTools.Hawk.classDescriptor.gui.frame.ProgressGUI;
+import devTools.Hawk.classDescriptor.tools.FileReflector;
+
+/**
+ * This class is used with the class CreateNewProjectGUI.  It opens a JFileChooser and asks the user 
+ * to select a .class or .jar file, or a directory.  It then locates .class and .jar files from a directory if chosen 
+ * and adds the .class and .jar files found to the CreateNewProjectGUI object's JList of .class and .jar files 
+ * found.  This class locates these files in a separate thread so the gui doesn't seem to freeze.
+ * @author Dominic Kramer
+ */
+public class FindClassThread extends Thread
+{
+	/**
+	 * The CreateNewProjectGUI associated with this thread.
+	 */
+	protected CreateNewProjectGUI gui;
+	/**
+	 * This displays the progress of the search.
+	 */	
+	protected ProgressGUI progress;
+	
+	/**
+	 * This creates a new thread to locate .class and .jar files.
+	 * @param GUI The CreateNewProjectGUI associated with this thread.
+	 */
+	public FindClassThread(CreateNewProjectGUI GUI)
+	{
+		gui = GUI;
+		
+		progress = new ProgressGUI(0,100,"Finding .jar and .class Files");
+		progress.setText("Locating .class and .jar files . . . .\n");
+		progress.setIndeterminante(true);
+	}
+	
+	/**
+	 * This method is called when the start() method is used to start the search.
+	 */
+	public void run()
+	{
+		Vector fileVec = new Vector();
+		
+		fileVec = FileReflector.getVectorOfInterfacesGUI(fileVec,progress);
+		
+		progress.setIndeterminante(false);
+		progress.setValue(0);
+		progress.setMaximum(fileVec.size());
+		progress.setProgressBarString(null);
+		progress.appendMessage("Adding names to list . . . .\n");
+		
+		//gui.getModel().removeAllElements();
+		gui.getCreateProjectButton().setEnabled(false);
+		gui.getCreateProjectButton().setToolTipText("The list is still being populated.");
+		int i=0;
+		while (i<fileVec.size() && !progress.isCancelled())
+		{
+			try
+			{
+				gui.getModel().addElement((String)fileVec.elementAt(i));
+				progress.setValue(i+1);
+			}
+			catch(NullPointerException e)
+			{
+				System.out.println("A NullPointerException was thrown in actionPerformed(ActionEvent) in CreateNewProjectGUI");
+				System.err.println(e);
+			}
+			
+			i++;
+		}
+		
+		if (!progress.isCancelled())
+		{
+			progress.appendMessage("Done");
+			progress.isCompleted();
+			progress.dispose();
+		}
+		gui.getCreateProjectButton().setEnabled(true);
+		gui.getCreateProjectButton().setToolTipText("Load the files in the list to make the project");
+	}
+}
