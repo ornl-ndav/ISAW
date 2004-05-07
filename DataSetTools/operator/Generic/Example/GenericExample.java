6b332adc3626472c37cad5a1321882d7de9c8ba1
Dennis Mikkelson <mikkelsond@uwstout.edu>
2004-05-07 16:11:57 +0000

    Base class for examples of Generic operators.

---
+++ DataSetTools/operator/Generic/Example/GenericExample.java
@@ -0,0 +1,80 @@
+/*
+ * File:  GenericExample.java 
+ *
+ * Copyright (C) 2004, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2004/05/07 16:11:57  dennis
+ *  Base class for examples of Generic operators.
+ *
+ */
+
+package DataSetTools.operator.Generic.Example;
+
+import java.io.*;
+import DataSetTools.operator.Generic.GenericOperator;
+
+/**
+ *   Base class for generic operators that are simple examples of
+ *   Generic operators. 
+ *
+ * @see GenericOperator
+ */
+
+abstract public class GenericExample extends    GenericOperator 
+                                     implements Serializable
+{
+  private static String[] categoryList=null;
+
+   protected GenericExample( String title )
+   {
+      super( title );
+   } 
+
+  /* ------------------------ getCategoryList ------------------------------ */
+  /**
+   * Get an array of strings listing the operator category names of base
+   * classes for this operator.  The first entry in the array is the string:
+   *
+   *      Operator.OPERATOR
+   *
+   * The last entry is the category of the last abstract base class that is
+   * is a base class for the current operator.
+   * 
+   * @return  A list of Strings specifying the category names of the abstract
+   * base classes from which this operator is derived.
+   */
+  public String[] getCategoryList()
+  {
+    if(categoryList==null)
+      categoryList=createCategoryList();
+
+    return categoryList;
+  }
+} 
