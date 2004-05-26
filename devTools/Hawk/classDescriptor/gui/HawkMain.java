fae9d045a50f97e1932a80b3e411a5469ddad247
kramer <kramer@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-05-26 19:19:51 +0000

    *** empty log message ***

--- devTools/Hawk/classDescriptor/gui/HawkMain.java
+++ devTools/Hawk/classDescriptor/gui/HawkMain.java
@@ -32,6 +32,9 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.4  2004/05/26 19:19:51  kramer
+ * *** empty log message ***
+ *
  * Revision 1.3  2004/03/12 19:46:14  bouzekc
  * Changes since 03/10.
  *
@@ -55,7 +58,7 @@ import devTools.Hawk.classDescriptor.tools.SystemsManager;
  *
  */
 public class HawkMain
-{
+{	
 	/**
 	 * This constructor is private to signify that HawkMain doesn't need to be instantiated.
 	 */
@@ -67,7 +70,7 @@ public class HawkMain
 	 * @param args The array of Strings passed as arguments to the main method.
 	 */
 	public static void main(String args[])
-	{
+	{		
 		//now to check for any command line arguments
 			CommandLineManager commandManager = new CommandLineManager(args);
 			int result = commandManager.parseCommandLine();
