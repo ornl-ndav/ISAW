672ff6443adf494c31c74f77237fda58f661176d
kramer <kramer@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-05-27 13:40:39 +0000

    Added method   public Component[] determineWaitingComponents() Added toolbars to the gui that allows the user to search and filter the list of classes displayed.

--- trunk/devTools/Hawk/classDescriptor/gui/internalFrame/AlphabeticalListGUI.java
+++ trunk/devTools/Hawk/classDescriptor/gui/internalFrame/AlphabeticalListGUI.java
@@ -32,6 +32,12 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.4  2004/05/27 13:40:39  kramer
+ * Added method
+ *   public Component[] determineWaitingComponents()
+ * Added toolbars to the gui that allows the user to search and filter the list
+ * of classes displayed.
+ *
  * Revision 1.3  2004/03/12 19:46:16  bouzekc
  * Changes since 03/10.
  *
@@ -42,6 +48,8 @@
  *
  */
 package devTools.Hawk.classDescriptor.gui.internalFrame;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -49,9 +57,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 import devTools.Hawk.classDescriptor.gui.frame.HawkDesktop;
 import devTools.Hawk.classDescriptor.gui.panel.AlphabeticalListJPanel;
+import devTools.Hawk.classDescriptor.modeledObjects.Interface;
 import devTools.Hawk.classDescriptor.modeledObjects.Project;
 
 /**
@@ -76,7 +86,7 @@ public class AlphabeticalListGUI extends DesktopInternalFrame implements ActionL
 	 */
 	public AlphabeticalListGUI(Project pro, String title, boolean shortJava, boolean shortOther, HawkDesktop desk)
 	{
-		super(desk);
+		super(desk,desk.getSelectedDesktop(),new Interface(),false,false,false,false);
 		
 		//Now to set some characteristics about the main window
 			setTitle(title);
@@ -87,8 +97,22 @@ public class AlphabeticalListGUI extends DesktopInternalFrame implements ActionL
 			setMaximizable(true);
 			setResizable(true);
 			
+			JPanel mainPanel = new JPanel();
+			mainPanel.setLayout(new BorderLayout());
+			
 			listPanel = new AlphabeticalListJPanel(pro,shortJava,shortOther,desktop,this);
-			listPanel.setLayout(new GridLayout(1,1));
+			
+			JPanel topPanel = new JPanel(new GridLayout(2,0));
+			topPanel.add(listPanel.createToolBar());
+			topPanel.add(listPanel.getSearchPanel());
+			
+			JPanel labelPanel = new JPanel(new GridLayout(1,1));
+			labelPanel.add(listPanel.getLabel());
+			
+			mainPanel.add(topPanel, BorderLayout.NORTH);
+			mainPanel.add(listPanel, BorderLayout.CENTER);
+			mainPanel.add(labelPanel, BorderLayout.SOUTH);
+			
 			JMenuBar menuBar = new JMenuBar();
 			JMenu fileMenu = new JMenu("File");
 				JMenuItem closeItem = listPanel.getCloseMenuItem();
@@ -99,12 +123,9 @@ public class AlphabeticalListGUI extends DesktopInternalFrame implements ActionL
 			menuBar.add(listPanel.getViewMenu());
 			menuBar.add(listPanel.getPropertiesMenu(shortJava,shortOther));			
 			menuBar.add(windowMenu);
-
+			
 			setJMenuBar(menuBar);
-//			JPanel mainPanel = new JPanel();
-//			mainPanel.setLayout(new BorderLayout());
-//			mainPanel.add(listPanel,BorderLayout.CENTER);
-			getContentPane().add(listPanel);
+			getContentPane().add(mainPanel);
 			
 			pack();
 	}
@@ -113,7 +134,7 @@ public class AlphabeticalListGUI extends DesktopInternalFrame implements ActionL
 	 * Gets a copy of this window.
 	 * @return A copy of this window.
 	 */
-	public DesktopInternalFrame getCopy()
+	public AttachableDetachableFrame getCopy()
 	{
 		return new AlphabeticalListGUI(listPanel.getProject(), getTitle(), listPanel.getShortenJavaCheckBox().isSelected(), listPanel.getShortenOtherCheckBox().isSelected(),desktop);
 	}
@@ -132,5 +153,14 @@ public class AlphabeticalListGUI extends DesktopInternalFrame implements ActionL
 			listPanel.actionPerformed(event);
 			super.actionPerformed(event);
 		}
+	}
+	
+	/**
+	 * The Components in the array returned from this method are the Components that should have the 
+	 * mouse use the waiting animation when an operation is in progress.
+	 */
+	public Component[] determineWaitingComponents()
+	{
+		return listPanel.determineWaitingComponents();
 	}
 }
