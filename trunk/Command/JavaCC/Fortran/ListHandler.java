8375a2825a8cb73ae4ace7f3b8f6f5906a4b9eac
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2004-06-04 22:01:54 +0000

    Added Documentation and GPL

--- trunk/Command/JavaCC/Fortran/ListHandler.java
+++ trunk/Command/JavaCC/Fortran/ListHandler.java
@@ -1,6 +1,54 @@
+/*
+ * File:  ListHandler.java 
+ *             
+ * Copyright (C) 2004, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ * This work was supported by the National Science Foundation under
+ * grant number DMR-0218882
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.2  2004/06/04 22:01:54  rmikk
+ * Added Documentation and GPL
+ *
+ */
+
 package Command.JavaCC.Fortran;
 import java.util.*;
 
+
+/**
+ *  This class forms a linked list of items that have a name and value
+ * 
+ * @author MikkelsonR
+ * 
+
+ */
 public class ListHandler{
 
    LList List = null;
@@ -9,6 +57,12 @@ public class ListHandler{
       List = null;
    }
 
+   /**
+    *  Adds the given name, value pair to the list(ordered by name)
+    * @param value  The value to be added
+    * @param name    The name to be added
+    * @return     true if successful otherwise false
+    */
    public boolean add( Object value, String name){
       LList newItem = new LList( value,name);
       n++;
@@ -19,6 +73,12 @@ public class ListHandler{
       return true;
 
    }
+   
+   /**
+    * Returns the object(value) associated with the given name
+    * @param name   The name
+    * @return   The value associated with the name
+    */
    public Object get( String name){
      if( List == null)
        return null;
@@ -30,11 +90,20 @@ public class ListHandler{
 
    }
   
-  
+  /**
+   *   Returns the number of elements in this list
+   * @return  The number of elements in this list
+   */
   public int size(){
     return n;
   }
 
+  /**
+   *   Returns the name of the ith element( sorted by name) in this
+   *   list
+   * @param i   the position alphabetically in the list of the name   
+   * @return   The name associated with that position
+   */
   public String getName( int i){
     if( (i < 0) || ( i >= n))
        return null;
@@ -43,6 +112,11 @@ public class ListHandler{
 
   } 
 
+  /**
+   *  Returns the value of the ith member, when arranged alphabetically, in the list
+   * @param i   The position of the name in the list when arranged alphabetically
+   * @return    The value associated with that position
+   */
   public Object getValue( int i){
     if( (i < 0) || ( i >= n))
        return null;
@@ -50,6 +124,11 @@ public class ListHandler{
     return List.get(i).value;
 
   } 
+  
+ /**
+  *  Utility for the main test program
+  * @return  The next word entered by System.in
+  */ 
  public static String readlin(){
      char c=0;
      String S="";
@@ -64,6 +143,11 @@ public class ListHandler{
      }catch(Exception ss){ c=0;}
      return S;
  } 
+ 
+ /**
+  *  Test program for this module. It is menu driven
+  * @param args  Not used
+  */
  public static void main( String args[]){
    ListHandler LH= new ListHandler();
    char c=0;
