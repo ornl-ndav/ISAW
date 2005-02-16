e82c628ae0508d96188fd9210e1467021af71c00
kramer <kramer@9b3b9736-f711-0410-b1b1-9612de3a1827>
2005-02-16 02:02:55 +0000

    Added javadoc documentation.

--- trunk/NexIO/Nxlog.java
+++ trunk/NexIO/Nxlog.java
@@ -30,7 +30,11 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.3  2005/02/16 02:02:55  kramer
+ * Added javadoc documentation.
+ *
  * Revision 1.2  2005/02/03 06:41:40  kramer
+ *
  * Now this class can read the Nxlog section of a NeXus file if it is
  * written using the standard as it is on 02/03/2005.
  *
@@ -52,16 +56,36 @@ import DataSetTools.dataset.StringAttribute;
 import DataSetTools.dataset.VariableXScale;
 import NexIO.NexApi.NexNode;
 
-
+/**
+ * This class is used to read an NxLog node from a NeXus file and create a 
+ * {@link DataSetTools.dataset.DataSet DataSet} from the data read.
+ * @author Dominic Kramer
+ */
 public class Nxlog
 {
+   /** The name of the time node in the file. */
    public static final String TIME_PARAM_NAME = "time";
+      /** The name of the time node's start time parameter in the file. */
       public static final String START_ATTRIBUTE_NAME = "start";
+      /** The name of the time node's units parameter in the file. */
       public static final String UNITS_ATTRIBUTE_NAME = "units";
+   /**
+    * The name of the node in the file that contains the NxLog node's 
+    * values.
+    */
    public static final String VALUE_PARAM_NAME = "value";
+   /**
+    * The name of the node in the file that contains the NxLog node's 
+    * raw values.
+    */
    public static final String RAW_VALUE_PARAM_NAME = "raw_value";
-   public static final String DURATION_PARAM_NAME = "duration";
+   /** The name of the description node in the file. */
    public static final String DESC_PARAM_NAME = "description";
+   /**
+    * The name of the node in the file that contains the duration of 
+    * an experiment.
+    */
+   public static final String DURATION_PARAM_NAME = "duration";
    
    //the following are not used to access data from the NxLog node.
    //Therefore, memory should not be used to hold these constants if they 
@@ -71,21 +95,43 @@ public class Nxlog
       //public static final String AVERAGE_VAL_ERR_PARAM_NAME = 
       //                               "average_value_error";
       //public static final String MIN_VAL_PARAM_NAME = "minimum_value";
-      //public static final String MAX_VAL_PARAM_NAME = "maximum_value";
+      //public static final String MAX_VAL_PARAM_NAME = "maximum_value";   
    
-   
+   /**
+    * Holds the message describing any errors that 
+    * occur when the file is read.
+    */
    private String errormessage;
 
+   /**
+    * Creates a new Nxlog object.  If the method 
+    * {@link #getErrorMessage() getErrorMessage()} is called on the 
+    * object it will return a blank string.
+    */
    public Nxlog()
    {
      errormessage = "";
    }
 
+   /**
+    * Returns this class's currently recorded error message.  If an error 
+    * occurs when the method 
+    * {@link #processDS(NxNode, DataSet) processDS(NxNode, DataSet)} is 
+    * invoked, this method can be invoked to learn what the error is.
+    * @return The string holding any error that occured while the file was 
+    *         being read.  A blank String is returned if no error has occured.
+    */
    public String getErrorMessage()
    {
      return errormessage;
    }
    
+   /**
+    * Sets the error message recorded by this class.
+    * @param message The new error message.
+    * @return true if the message was successfully set and false if 
+    *         it wasn't.
+    */
    private boolean setErrorMessage(String message)
    {
       errormessage = message;
@@ -93,15 +139,15 @@ public class Nxlog
    }
    
    /**
-    * 
-    * @param nxlogNode
-    * @param ds
+    * Reads an NxLog node and fills a {@link DataSet DataSet} with the 
+    * data is has read.
+    * @param nxlogNode The NxLog node to read.
+    * @param ds        The {@link DataSet DataSet} where the data read is 
+    *                  stored.
     * @return The error status:  true if an error occured and false otherwise.
     */
    public boolean processDS(  NxNode nxlogNode,  DataSet ds)
    {
-      System.out.println("Inside Nxlog.processDS(....)");
-      
      errormessage = "Improper inputs to Nxlog";
      if( nxlogNode == null)
        return true;
@@ -111,116 +157,113 @@ public class Nxlog
        return true;
      errormessage = "";
      
-     
-     //now to get the time information
+     //first look for the 'units' attribute.  If this value cannot be 
+     //determined, it is impossible to create a DataSet.  To get the 'units' 
+     //attribute the 'time' node needs to be located.
      NxNode timeNode = nxlogNode.getChildNode(TIME_PARAM_NAME);
-     if (timeNode == null)
-        return setErrorMessage("Cannot find time node found in NxLog node.");
+        if (timeNode == null)
+           return setErrorMessage("Cannot find time node found in NxLog node.");
      
-     //used to convert Objects read from the node into Strings
-     NxData_Gen stringGenerator = new NxData_Gen();
-     
-     Object timeStartOb = timeNode.getAttrValue(START_ATTRIBUTE_NAME);
-     if (timeStartOb == null)
-        return setErrorMessage("Time node's \"start\" attribute cannot " +
-                                  "be found.");     
-     String timeStartValue = stringGenerator.cnvertoString(timeStartOb);
-     if (timeStartValue == null)
-        return setErrorMessage("The value of the time node's \"start\" " +
-                                  "attribute cannot be determined.");
-     
-     //now to get the times written in the log file as well as well as the 
-     //start time and the units for the time
-     Object times = timeNode.getNodeValue();
-     if (times == null)
-        return setErrorMessage("The time node in the NxLog node could not " +
-            "be found.");
-     float[] timeArr = NXData_util.Arrayfloatconvert(times);
-     if (timeArr == null)
-        return setErrorMessage("The value of the time node in the NxLog " +
-            "node could not be found.");     
-     
-     Object timeUnitsOb = timeNode.getAttrValue(UNITS_ATTRIBUTE_NAME);
-     if (timeUnitsOb == null)
-        return setErrorMessage("Time node's \"units\" attribute cannot " +
+        //used to convert Objects read from the node into Strings
+        NxData_Gen stringGenerator = new NxData_Gen();
+      
+        //now to get the 'units' attribute
+        Object timeUnitsOb = timeNode.getAttrValue(UNITS_ATTRIBUTE_NAME);
+        if (timeUnitsOb == null)
+           return setErrorMessage("Time node's \"units\" attribute cannot " +
                                   "be found.");
-     String timeUnitsValue = stringGenerator.cnvertoString(timeUnitsOb);
-     if (timeStartValue == null)
-        return setErrorMessage("The value of the time node's \"units\" " +
+        String timeUnitsValue = stringGenerator.cnvertoString(timeUnitsOb);
+        if (timeUnitsValue == null) //fail if the units cannot be found
+           return setErrorMessage("The value of the time node's \"units\" " +
                                   "attribute cannot be determined.");
-     
-     //now to convert the start time from a String into a more useful 
-     //Object and set the start date and time
-     Date timeStartDate = NxNodeUtils.parseISO8601(timeStartValue);
-     if (timeStartDate == null)
-        return setErrorMessage("Parsing of the start time into its " +
-                                  "corresponding date and/or time has failed.");
-     String[] startDateTime = getDateAndTimeStrings(timeStartDate);
-        String startDateStr = startDateTime[0];
-        String startTimeStr = startDateTime[1];
-     ds.setAttribute(new StringAttribute(Attribute.START_DATE,startDateStr));
-     ds.setAttribute(new StringAttribute(Attribute.START_TIME,startTimeStr));
-     
-     
-     //now to get the duration so that the end date and time can be set
-     NxNode durationNode = nxlogNode.getChildNode(DURATION_PARAM_NAME);
-     if (durationNode == null)
-        return setErrorMessage("Cannot find the duration node in NxLog node.");
-     Object durationOb = durationNode.getNodeValue();
-     if (durationOb == null)
-        return setErrorMessage("Cannot determine duration node's value in " +
-                               "NxLog node.");
-     NxData_Gen dataGenerator = new NxData_Gen();
-     Float durationFloatValue = dataGenerator.cnvertoFloat(durationOb);
-     if (durationFloatValue == null)
-        return setErrorMessage("Cannot resolve the duration node's value's " +
-                               "float value.");
-     //TODO This assumes that the duration from the file is in seconds
-     long durationMilliseconds = (long)(durationFloatValue.floatValue()*1000);
-     long startDateMilliseconds = timeStartDate.getTime();
-     Date timeEndDate = new Date(startDateMilliseconds+durationMilliseconds);
-     String[] endDateTime = getDateAndTimeStrings(timeEndDate);
-        String endDateStr = endDateTime[0];
-        String endTimeStr = endDateTime[1];
-     ds.setAttribute(new StringAttribute(Attribute.END_DATE,endDateStr));
-     ds.setAttribute(new StringAttribute(Attribute.END_TIME,endTimeStr));
-     
+        
+        //now to read the start time.  From this point on, a 'null' value is 
+        //used to signal other code that the value couldn't be determined
+        Object timeStartOb = timeNode.getAttrValue(START_ATTRIBUTE_NAME);
+        String timeStartValue = null;
+        if (timeStartOb != null)
+           timeStartValue = stringGenerator.cnvertoString(timeStartOb);
+        Date timeStartDate = null; //this will be used later if its defined 
+                                   //i.e. non-null
+        if (timeStartValue != null)
+        {
+           //now to convert the start time from a String into a more useful 
+           //Object and set the start date and time
+           timeStartDate = NxNodeUtils.parseISO8601(timeStartValue);
+           if (timeStartDate != null)
+           {
+              String[] startDateTime = getDateAndTimeStrings(timeStartDate);
+                 String startDateStr = startDateTime[0];
+                 String startTimeStr = startDateTime[1];
+              ds.setAttribute(
+                    new StringAttribute(Attribute.START_DATE,startDateStr));
+              ds.setAttribute(
+                    new StringAttribute(Attribute.START_TIME,startTimeStr));
+           }
+        }
+        
+        //now to get the times written in the log file
+        float[] timeArr = null;
+        Object times = timeNode.getNodeValue();
+        if (times != null)
+           timeArr = NXData_util.Arrayfloatconvert(times);
+        
+        //now to set the DataSet's end time
+        if (timeArr != null)
+        {
+           //the last value in the time array is used to determine 
+           //the duration of the experiment
+           float lastTime = timeArr[timeArr.length-1];
+           long numMilliseconds = getNumMilliseconds(lastTime,timeUnitsValue);
+           if (timeStartDate != null && numMilliseconds != -1)
+           {
+              Date timeEndDate = 
+                 new Date(timeStartDate.getTime()+numMilliseconds);
+              String[] endDateTime = getDateAndTimeStrings(timeEndDate);
+                 String endDateStr = endDateTime[0];
+                 String endTimeStr = endDateTime[1];
+              ds.setAttribute(
+                    new StringAttribute(Attribute.END_DATE,endDateStr));
+              ds.setAttribute(
+                    new StringAttribute(Attribute.END_TIME,endTimeStr));
+           }
+        }
      
      //now to make the Data objects for the DataSet
      //first to get the data from the node
      NxNode valueArrNode = nxlogNode.getChildNode(VALUE_PARAM_NAME);
+     if (valueArrNode != null)
+     {
+        Object valueArrOb = valueArrNode.getNodeValue();
+        if (valueArrOb != null)
+        {
+           float[] valueArr = NXData_util.Arrayfloatconvert(valueArrOb);
+           if (valueArr != null && timeArr != null)
+           {
+              //now to make the Data object for the regular 
+              //values read from the node
+              VariableXScale valueXScale = new VariableXScale(timeArr);
+              ds.addData_entry(Data.getInstance(valueXScale,valueArr,0));
+           }
+        }
+     }
+     
+     //now to get the raw data from the node
      NxNode rawValueArrNode = nxlogNode.getChildNode(RAW_VALUE_PARAM_NAME);
-     
-     if (valueArrNode == null || rawValueArrNode == null)
-        return setErrorMessage("Cannot find data nodes in NxLog node.");
-     
-     Object valueArrOb = valueArrNode.getNodeValue();
-     Object rawValueArrOb = rawValueArrNode.getNodeValue();
-     if (valueArrOb == null || rawValueArrOb == null)
-        return setErrorMessage("Cannot determine the data nodes' values " +
-                                  "in NxLog node.");
-     
-     float[] valueArr = NXData_util.Arrayfloatconvert(valueArrOb);
-     float[] rawValueArr = NXData_util.Arrayfloatconvert(rawValueArrOb);
-     
-     if (valueArr == null || rawValueArr == null)
-        return setErrorMessage("Cannot determine the data nodes' values' " +
-                                  "arrays in NxLog node.");
-     
-     //these are used to make the XScale objects used in the Data objects
-     //and were needed when UniformXScale objects were used
-     //now they shouldn't be needed
-     //float startTimeSeconds = timeStartDate.getTime()/1000f;
-     //float endTimeSeconds = timeEndDate.getTime()/1000f;
-     
-     //now to make the Data object for the regular values read from the node
-     VariableXScale valueXScale = new VariableXScale(timeArr);
-     ds.addData_entry(Data.getInstance(valueXScale,valueArr,0));
-     
-     //now to make the Data object for the raw values read from the node
-     VariableXScale rawValueXScale = new VariableXScale(timeArr);
-     ds.addData_entry(Data.getInstance(rawValueXScale,rawValueArr,1));
-     
+     if (rawValueArrNode != null)
+     {
+        Object rawValueArrOb = rawValueArrNode.getNodeValue();
+        if (rawValueArrOb != null)
+        {
+           float[] rawValueArr = NXData_util.Arrayfloatconvert(rawValueArrOb);
+           if (rawValueArr != null && timeArr != null)
+           {
+              //now to make the Data object for the raw values read from the node
+              VariableXScale rawValueXScale = new VariableXScale(timeArr);
+              ds.addData_entry(Data.getInstance(rawValueXScale,rawValueArr,1));
+           }
+        }
+     }
      
      //now to read the description from the NxLog node
      //this information is not absolutely necessary and thus 
@@ -228,7 +271,7 @@ public class Nxlog
      Object descriptionOb = nxlogNode.getChildNode(DESC_PARAM_NAME);
      if (descriptionOb != null)
      {
-        String descriptionStr = dataGenerator.cnvertoString(descriptionOb);
+        String descriptionStr = (new NxData_Gen()).cnvertoString(descriptionOb);
         if (descriptionStr != null)
            ds.setAttribute(new StringAttribute(Attribute.LABEL,descriptionStr));
      }
@@ -236,6 +279,22 @@ public class Nxlog
      return false;
   }//processDS
   
+  /**
+   * Converts a Date object into a date string and a time string.
+   * @param date The date to analyze.
+   * @return A two element String array.  
+   *         <ul>
+   *            <li>
+   *               The first element is a String representing the date's 
+   *               day, month, and year in the format yy-mm-dd.
+   *            </li>
+   *            <li>
+   *               The second element is a String representing the 
+   *               date's hours, minutes, and seconds in the format 
+   *               hh-mm-ss.
+   *            </li>
+   *         </ul>
+   */
   private static String[] getDateAndTimeStrings(Date date)
   {
      GregorianCalendar calendar = new GregorianCalendar();
@@ -255,7 +314,40 @@ public class Nxlog
         result[1] = timeStr;
      return result;
   }
-   
+  
+  //TODO Determine if there are any more possible units that could be read 
+  //     from a NeXus file.
+  /**
+   * Converts a time from its recorded units into milliseconds.
+   * @param timeValue 
+   * @param units The string representing the units that the time 
+   *              <code>timeValue</code> is recorded in.
+   *              <br>
+   *              Possible values are:
+   *              <ul>
+   *                 <li>second</li>
+   *                 <li>microseconds</li>
+   *              </ul>
+   * @return The number of milliseconds equivalent of the 
+   *         time <code>timeValue</code> or -1 if <code>units</code> 
+   *         is not recognized.
+   */
+  private long getNumMilliseconds(float timeValue, String units)
+  {
+     if (units == null)
+        return -1;
+     else if (units.equals("second"))
+        return (long)(timeValue*1000);
+     else if (units.equals("microseconds"))
+        return (long)(timeValue*10);
+     else
+        return -1;
+  }
+  
+  /**
+   * Testbed.
+   * @param args args[0] should hold the name of the NeXus file to read.
+   */
   public static void main(String[] args)
   {
      // Not tested
