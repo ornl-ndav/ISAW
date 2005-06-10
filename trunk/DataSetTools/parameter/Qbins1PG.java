816d1b1939028bad30d2a13b8073fc6521b5d722
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2005-06-10 15:30:49 +0000

    Can now have natural initial values of start, end, numx

--- trunk/DataSetTools/parameter/Qbins1PG.java
+++ trunk/DataSetTools/parameter/Qbins1PG.java
@@ -31,6 +31,9 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.10  2005/06/10 15:30:49  rmikk
+ * Can now have natural initial values of start, end, numx
+ *
  * Revision 1.9  2004/05/11 18:23:54  bouzekc
  * Added/updated javadocs and reformatted for consistency.
  *
@@ -63,6 +66,7 @@
  */
 package DataSetTools.parameter;
 
+import Command.JavaCC.ParameterGUIParser;
 import DataSetTools.components.ParametersGUI.EntryWidget;
 import DataSetTools.components.ParametersGUI.StringEntry;
 
@@ -94,17 +98,23 @@ public class Qbins1PG extends ParameterGUI implements Concatenator {
   private StringEntry end;
   private StringEntry steps;
   private JRadioButton dQ;
-
+  private float startv=Float.NaN, //Saves for the StringEntry values until
+          endv = Float.NaN;       //GUI elements are initialized
+  private  int   stepsv = -1;
+  private  boolean dQ_v = true;
   //~ Constructors *************************************************************
 
   /**
    * Creates a new Qbins1PG object.
    *
    * @param Prompt The name of this PG.
-   * @param val The initial value of this PG.
+   * @param val The initial value of this PG Either the initial Vector value
+   *           or a String form for a list representing start, end, number
+   *           of points, and whether to use constant dQ 
    */
   public Qbins1PG( String Prompt, Object val ) {
     super( Prompt, val );
+    initTextBoxes(val);
     this.setType( "Qbins1" );
   }
 
@@ -112,22 +122,63 @@ public class Qbins1PG extends ParameterGUI implements Concatenator {
    * Creates a new Qbins1PG object.
    *
    * @param Prompt The name of this PG.
-   * @param val The initial value of this PG.
+   * @param val The initial start,end,steps and dQ choice of this PG.
    * @param valid Whether this PG should be considered initially valid.
    */
   public Qbins1PG( String Prompt, Object val, boolean valid ) {
-    super( Prompt, val, valid );
+    super( Prompt, null, valid );
+    initTextBoxes( val);
     this.setType( "Qbins1" );
   }
 
+  // Checks val1.  It takes care of the case if val1 is a string
+  private boolean initTextBoxes(Object val1){
+    if( val1 instanceof String){
+       Vector val;
+       try{
+          val = ParameterGUIParser.parseText((String) val1);
+       }catch( Throwable ss){
+         return false;
+       }
+       
+       try{
+         startv= ((Number)((val).elementAt(0))).floatValue();
+
+         endv= ((Number)((val).elementAt(1))).floatValue();
+         stepsv= ((Number)((val).elementAt(2))).intValue();
+         if( ((Vector)val).size()>3){
+           String s4=val.elementAt(3).toString();
+           dQ_v= (new Boolean(s4)).booleanValue();
+         }
+         setValue( startv,endv,stepsv,dQ_v);
+         if( getInitialized()){
+           start.setText(""+startv);
+           end.setText(""+endv);
+           steps.setText(""+stepsv);
+           if(dQ_v)
+             dQ.setSelected(true);
+           else
+             dQ.setSelected( false);
+           
+         }
+         return true;
+       }catch(Exception ss){
+       }
+    }
+   return false; 
+  }
   //~ Methods ******************************************************************
 
   /**
-   * Sets the value of this QBinsPG.  This really wants a Vector.
-   *
+   * Sets the value of this QBinsPG.  If value is a string it is assumed
+   * that it represents the start, end, size of Vector result and whether
+   * to use const dQ, otherwise it is the bin boundaries.
+   * The saved and returned value is always a Vector of bins
    * @param V The new value.
    */
   public void setValue( Object V ) {
+    if(initTextBoxes(V))
+       return;
     if( V instanceof Vector ) {
       super.setValue( V );
     } else {
@@ -146,16 +197,16 @@ public class Qbins1PG extends ParameterGUI implements Concatenator {
    */
   public Object getValue(  ) {
     if( !this.getInitialized(  ) ) {
-      return new Vector(  );
+      return super.getValue();
     }
-
+          
     float s     = ( new Float( start.getText(  ) ) ).floatValue(  );
     float e     = ( new Float( end.getText(  ) ) ).floatValue(  );
-    int n       = ( new Integer( steps.getText(  ) ) ).intValue(  );
+    int n       =( new Integer( steps.getText(  ) ) ).intValue(  );
     Vector temp = new Vector(  );
     String R;
 
-    if( dQ.isSelected(  ) ) {
+    if( dQ.isSelected() ) {
       R = "dQ";
     } else {
       R = "dQ/Q";
@@ -196,10 +247,40 @@ public class Qbins1PG extends ParameterGUI implements Concatenator {
         s = s + stepSize;
       }
     }
-
+    super.setValue( temp);
     return temp;
   }
 
+
+  // Calculates and sets the values corresponding to a given start, end...
+  private void setValue( float start, float end, int nsteps, boolean dQ){
+
+    
+
+    boolean mult =!dQ;
+    float e=end, s=start, n=nsteps;
+   
+    float stepSize;
+
+    if( mult ) {
+      stepSize = ( float )Math.pow( e / s, 1.0 / n );
+    } else {
+      stepSize = ( e - s ) / n;
+    }
+    Vector temp = new Vector();
+    for( int i = 0; i <= n; i++ ) {
+      temp.add( new Float( s ) );
+
+      if( mult ) {
+        s = s * stepSize;
+      } else {
+        s = s + stepSize;
+      }
+    }
+
+    setValue(temp);
+
+  }
   /**
    * Used to clear out the PG.  This resets the Vector to an empty one.
    */
@@ -215,12 +296,25 @@ public class Qbins1PG extends ParameterGUI implements Concatenator {
   public void initGUI( Vector V ) {
     this.setEntryWidget( new EntryWidget(  ) );
     this.getEntryWidget(  ).setLayout( new GridLayout( 2, 3 ) );
-    start   = new StringEntry( ".0035", 7, new FloatFilter(  ) );
-    end     = new StringEntry( "1.04", 7, new FloatFilter(  ) );
-    steps   = new StringEntry( "117", 5, new IntegerFilter(  ) );
-    dQ      = new JRadioButton( "dQ" );
+    String S=".0035";
+    String E = "1.04";
+    String N="117";
+    String DQ ="dQ";
+    if( !Float.isNaN(startv) &&!Float.isNaN(endv)){
+        S = ""+startv;
+        E = ""+endv;
+    }
+    if( stepsv>=0)
+      N=""+stepsv;
+    if(!dQ_v)
+      DQ="dQ/Q";
+      
+    start   = new StringEntry( S, 7, new FloatFilter(  ) );
+    end     = new StringEntry( E, 7, new FloatFilter(  ) );
+    steps   = new StringEntry( N, 5, new IntegerFilter(  ) );
+    dQ      = new JRadioButton( DQ );
 
-    JRadioButton dQQ = new JRadioButton( "dQ/Q" );
+    JRadioButton dQQ = new JRadioButton( DQ );
 
     dQQ.setSelected( true );
 
@@ -258,6 +352,20 @@ public class Qbins1PG extends ParameterGUI implements Concatenator {
     }
   }
 
+  /**
+   *  Copies the start, end, nbinBoundaries and wether to use dQ
+   *  to the super cloned value.  These are not part of the regular
+   *  value.
+   */
+  public Object clone(){
+      Object O = super.clone();
+      Qbins1PG pg = (Qbins1PG)O;
+      pg.startv=startv;
+      pg.endv=endv;
+      pg.stepsv = stepsv;
+      pg.dQ_v=dQ_v;
+      return pg;
+  }
   //~ Inner Classes ************************************************************
 
   /**
