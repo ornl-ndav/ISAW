c961c08b927d658f1be9f35b26e5fd7c8c55de82
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2005-12-15 20:52:56 +0000

    Added tag for CVS logging so that future modifications can be tracked.

--- trunk/Operators/TOF_Diffractometer/GLADScatter.java
+++ trunk/Operators/TOF_Diffractometer/GLADScatter.java
@@ -28,6 +28,10 @@
  *
  * For further information, see <http://www.pns.anl.gov/ISAW/>
  * 
+ * $Log$
+ * Revision 1.4  2005/12/15 20:52:56  dennis
+ * Added tag for CVS logging so that future modifications can be tracked.
+ *
  */
  
 package Operators.TOF_Diffractometer;
