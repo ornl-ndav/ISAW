2f3d4c99c1d90f0912df18c03d9bd3c24dcfacd3
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2006-01-05 22:24:43 +0000

    Uses the scripts from the scratch directory

--- trunk/Wizard/TOF_SCD/DailyPeaksWizard_new.java
+++ trunk/Wizard/TOF_SCD/DailyPeaksWizard_new.java
@@ -43,7 +43,8 @@ import DataSetTools.wizard.*;
 import DataSetTools.parameter.*;
 public class DailyPeaksWizard_new extends Wizard 
 {
-     int[][] ParamTable= {     {0,-1,-1,-1,0}
+     int[][] ParamTable= {   
+     {0,-1,-1,-1,0}
      ,{1,-1,4,4,1}
      ,{2,-1,2,5,2}
      ,{3,-1,1,9,3}
@@ -70,14 +71,19 @@ public class DailyPeaksWizard_new extends Wizard
    public DailyPeaksWizard_new(boolean standalone ){
         super("Daily Peaks Wizard",standalone);
      
+     String path = System.getProperty("ISAW_HOME");
+     path = path.replace('\\','/');
+     if( !path.endsWith("/"))
+        path +="/";
+     path += "Wizard/TOF_SCD/Scripts_new/";
 
-   addForm( new ScriptForm("TOF_SCD/find_multiple_peaks1.iss", new PlaceHolderPG("Peaks",null),new int[0]));
-   addForm( new ScriptForm("TOF_SCD/JIndxSave1.iss", new StringPG("Result1",null),new int[0]));
-    addForm( new ScriptForm(  "TOF_SCD/LSqrs.iss",new ArrayPG( "Result",null ),ConstList[2]));
-   addForm( new ScriptForm("TOF_SCD/JIndxSave2.iss"));
-   addForm( new ScriptForm("TOF_SCD/integrate_multiple_runs.iss"));
-      linkFormParameters( ParamTable );
-   String S="" ;
+     addForm( new ScriptForm(path+"find_multiple_peaks1.iss", new PlaceHolderPG("Peaks",null),new int[0]));
+     addForm( new ScriptForm(path+"JIndxSave1.iss", new StringPG("Result1",null),new int[0]));
+     addForm( new ScriptForm(  path+"LSqrs.iss",new ArrayPG( "Result",null ),ConstList[2]));
+     addForm( new ScriptForm(path+"JIndxSave2.iss"));
+     addForm( new ScriptForm(path+"integrate_multiple_runs.iss"));
+     linkFormParameters( ParamTable );
+    String S="" ;
     S+="This wizard does the following is the replacement for the";
     S+="initial Daily Peaks Wizard that assumes that the detector(s)";
     S+="are perpendicular to the beam plane and centered at the";
@@ -88,6 +94,7 @@ public class DailyPeaksWizard_new extends Wizard
    }
 
    public static void main( String[] args){
+      
       DailyPeaksWizard_new Wiz= new DailyPeaksWizard_new(true);
     Wiz.wizardLoader( args );
    }
