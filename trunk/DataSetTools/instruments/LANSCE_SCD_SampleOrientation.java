7c64ee229bc04762735dcf8c1b93c81ee08cc2b7
dennis <dennis@9b3b9736-f711-0410-b1b1-9612de3a1827>
2006-01-16 02:00:14 +0000

    Added static default omega offset of 91.5 degrees for LANSCE goniometer.  This offset will be automatically applied to all instances of this type of sample orientation object. Added methods to get and set the omega offset angle.

--- trunk/DataSetTools/instruments/LANSCE_SCD_SampleOrientation.java
+++ trunk/DataSetTools/instruments/LANSCE_SCD_SampleOrientation.java
@@ -30,6 +30,12 @@
  * Modified:
  * 
  *  $Log$
+ *  Revision 1.2  2006/01/16 02:00:14  dennis
+ *  Added static default omega offset of 91.5 degrees for LANSCE
+ *  goniometer.  This offset will be automatically applied to all
+ *  instances of this type of sample orientation object.
+ *  Added methods to get and set the omega offset angle.
+ *
  *  Revision 1.1  2005/08/10 14:43:53  dennis
  *  Initial version of SampleOrientation class for the LANSCE SCD.
  *  This is intended to encapsulate the change of coordinates from
@@ -47,42 +53,43 @@ import DataSetTools.math.*;
 /**
  *  This class stores the phi, chi and omega values and computes the 
  *  corresponding rotation matrices using the conventions employed by the
- *  SCD instrument at LANSCE.  NOTE: The goniometer at the LANSCE SCD
- *  is defined in terms of coordinate axes similar to MCSTAS and NeXus,
- *  while internally, the ISAW coordinate system uses the conventions
- *  at IPNS.  Specifically, for the LANSCE SCD, the positive z-axis 
- *  points in the direction that the beam is traveling, the x-axis is
- *  horizontal and the y-axis points upward, in such a way as to form a 
- *  right handed coordinate system.  For the IPNS SCD, the positive x-axis
- *  is in the direction that the beam is traveling, the z-axis points upward
- *  and the y-axis is horizontal, in such a way as to form a right
- *  handed coordinate system.   
+ *  SCD instrument at LANSCE.  NOTE: The zero point of the Omega axis of 
+ *  the LANSCE goniometer is offset by slightly more than 90 degrees from
+ *  our "standard" convention in ISAW.  This class will accept the chi, phi
+ *  and omega values as used on the LANSCE SCD goniometer, and will 
+ *  provide the shift, internally, so that the methods to get the 
+ *  goniometer transformations can be used polymorphically.  
  *
- *  NOTE: Since this is intended to be used for the LANSCE SCD, the 
- *  documentation for the methods in this subclass is written in terms
- *  of the LANSCE SCD/NeXus coordinates. 
+ *  A default offset of 91.5 degrees is provided.  There is also a 
+ *  method to specify this offset, if the instrument is recalibrated.
  */  
 
 public class LANSCE_SCD_SampleOrientation extends SampleOrientation
 {
+  private static float omega_offset = 91.5f;    // default offset
+
   /**
    *  Construct a SampleOrientation object with the values specified in
    *  degrees following the sign conventions for the SCD instrument at LANSCE.
    *
    *  @param  phi    This is the angle (in degrees) to rotate by first 
-   *                 about the positive y-axis, in NeXus coordinates. 
+   *                 about the positive y-axis.  The positive y-axis points
+   *                 up.
    *  @param  chi    This is the angle (in degrees) to rotate by second 
-   *                 about the positive z-axis, in NeXus coordinates.
+   *                 about the positive x-axis.  The positive x-axis points 
+   *                 in the direction the beam of neutrons travel.
    *                 As the goniometer is currently set up at the LANSCE SCD,
-   *                 chi is fixed at -135 degrees.
+   *                 chi is fixed at 120 degrees.
    *  @param  omega  This is the angle (in degrees) to rotate by third 
-   *                 about the positive y-axis.
-   *              
+   *                 about the positive y-axis.  The values read from the
+   *                 goniometer are offset by approximately 91.5 degrees
+   *                 from the zero position for omega at IPNS.
    */
   public LANSCE_SCD_SampleOrientation( float phi, float chi, float omega  )
   {
     super( phi, chi, omega );
   }
+
 
   /**
    *  Get the rotation matrix representing the rotation of the sample in
@@ -94,8 +101,9 @@ public class LANSCE_SCD_SampleOrientation extends SampleOrientation
    */
   public Tran3D getGoniometerRotation()
   {
-    return tof_calc.makeEulerRotation( phi, chi, omega );
+    return tof_calc.makeEulerRotation( phi, chi, omega + omega_offset );
   }
+
 
   /**
    *  Get the inverse of the rotation matrix representing the rotation 
@@ -111,7 +119,31 @@ public class LANSCE_SCD_SampleOrientation extends SampleOrientation
    */
   public Tran3D getGoniometerRotationInverse()
   {
-    return tof_calc.makeEulerRotationInverse( phi, chi, omega );
+    return tof_calc.makeEulerRotationInverse(phi, chi, omega + omega_offset);
+  }
+
+
+  /** 
+   *  Get the current value of the offset applied to the omega angle by ALL
+   *  instances of this class.
+   *
+   *  @return the current value of the offset applied to omega.  
+   */
+  public double getOmegaOffset()
+  {
+    return omega_offset;
+  }
+
+
+  /** 
+   *  Set a new value for the offset that is applied to the omega angle by ALL
+   *  instances of this class.
+   *
+   *  @param  offset  The new value for the offset to be applied to omega.  
+   */
+  public void setOmegaOffset( double offset )
+  {
+    omega_offset = offset;
   }
 
 
@@ -122,7 +154,7 @@ public class LANSCE_SCD_SampleOrientation extends SampleOrientation
   public static void main( String args[] )
   {
     float phi   =   90;
-    float chi   = -135;
+    float chi   =  120;
     float omega =  290;
 
     LANSCE_SCD_SampleOrientation test = 
