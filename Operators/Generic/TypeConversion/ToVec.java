ea718d63edf8e06d42b96848a7386260e01799f7
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2006-05-19 16:07:29 +0000

    Operator generated from the static method, Command.ScriptUtl.ToVec. It attempts to convert Objects to Vectors by converting arrays and sub-arrays to vectors.

---
+++ Operators/Generic/TypeConversion/ToVec.java
@@ -0,0 +1,141 @@
+/* 
+ * File: ToVec.java
+ *  
+ * Copyright (C) 2006     Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact :  Ruth Mikkelson<Mikkelsonr@uwstout.edu>
+ *            Department of Mathematics, Statistics and Computer Science
+ *            University of Wisconsin-Stout
+ *            Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ * This work was supported by the National Science Foundation under
+ * grant number DMR-0218882
+ *
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.1  2006/05/19 16:07:29  rmikk
+ * Operator generated from the static method, Command.ScriptUtl.ToVec. It
+ * attempts to convert Objects to Vectors by converting arrays and sub-arrays
+ * to vectors.
+ *
+ *
+ */
+
+package Operators.Generic.TypeConversion;
+import DataSetTools.operator.*;
+import DataSetTools.operator.Generic.*;
+import DataSetTools.parameter.*;
+
+import gov.anl.ipns.Util.SpecialStrings.*;
+
+import Command.*;
+
+/**
+ * This class has been dynamically created using the Method2OperatorWizard
+ * and usually should not be edited.
+ */
+public class ToVec extends GenericOperator{
+
+
+   /**
+	* Constructor for the operator.  Calls the super class constructor.
+    */
+   public ToVec(){
+     super("ToVec");
+     }
+
+
+   /**
+    * Gives the user the command for the operator.
+    *
+	* @return  The command for the operator, a String.
+    */
+   public String getCommand(){
+      return "ToVec";
+   }
+
+
+   /**
+    * Sets the default parameters for the operator.
+    */
+   public void setDefaultParameters(){
+      clearParametersVector();
+      addParameter( new PlaceHolderPG("Enter Value",new Object()));
+   }
+
+
+   /**
+    * Writes a string for the documentation of the operator provided by
+    * the user.
+    *
+	* @return  The documentation for the operator.
+    */
+   public String getDocumentation(){
+      StringBuffer S = new StringBuffer();
+      S.append("@overview    "); 
+      S.append("Converts object to a Vector, changing arrays to Vectors");
+      S.append("@algorithm    "); 
+      S.append("Recurse through all elements of the Vector or array changing them to Vectors as needed");
+      S.append("@assumptions    "); 
+      S.append("val is not null and is an array or a Vector");
+      S.append("@param   ");
+      S.append("The value that is to have arrays converted to Vectors");
+      S.append("@error ");
+      S.append("");
+      return S.toString();
+   }
+
+
+   /**
+    * Returns a string array with the category the operator is in.
+    *
+    * @return  An array containing the category the operator is in.
+    */
+   public String[] getCategoryList(){
+            return new String[]{
+                     "Macros",
+                     "Utils",
+                     "Convert"
+                     };
+   }
+
+
+   /**
+    * Returns the result of the operator, otherwise and ErrorString.
+    *
+    * @return  The result of the operator, or an ErrorString.
+    */
+   public Object getResult(){
+      try{
+
+         java.lang.Object val = (java.lang.Object)(getParameter(0).getValue());
+         java.lang.Object Xres=Command.ScriptUtil.ToVec(val )
+;
+         return Xres;
+       }catch( Throwable XXX){
+         return new ErrorString( XXX.toString()+":"
+             +ScriptUtil.GetExceptionStackInfo(XXX,true,1)[0]);
+      }
+   }
+
+}
+
+
