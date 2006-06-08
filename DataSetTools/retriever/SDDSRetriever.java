8ffef1ed5c1bc32aaea25e3114efa339e002404a
Dennis Mikkelson <mikkelsond@uwstout.edu>
2006-06-08 22:21:04 +0000

    Now sets the Data block labels from the SDDS column names for each Data block constructed. Now sets the DataSet title as the name of the first Data block in the DataSet, followed by the units used when forming that DataSet. Added javadocs and did some code cleanup and reformatting.

--- DataSetTools/retriever/SDDSRetriever.java
+++ DataSetTools/retriever/SDDSRetriever.java
@@ -31,6 +31,14 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.8  2006/06/08 22:21:04  dennis
+ * Now sets the Data block labels from the SDDS column names for
+ * each Data block constructed.
+ * Now sets the DataSet title as the name of the first Data block in
+ * the DataSet, followed by the units used when forming that DataSet.
+ * Added javadocs and did some code cleanup and reformatting.
+ *
+ *
  * Revision 1.7  2004/01/24 20:36:22  bouzekc
  * Removed unused variables and imports.
  *
@@ -57,8 +65,7 @@
  * Retriever for SDDS files. Need to instantiate this class with the sdds 
  * file name to load it into ISAW.
  *
- *
-*/
+ */
 
 package DataSetTools.retriever ; 
 import DataSetTools.dataset.* ;
@@ -68,6 +75,11 @@ import javax.swing.filechooser.FileFilter;
 import DataSetTools.operator.DataSet.Information.XAxis.*;
 import java.util.*;
 
+/**
+ *  The SDDS Retriever will load an SDDS Log file into a collection of
+ *  DataSets, with each DataSet containing SDDS Columns with the same 
+ *  units. 
+ */
 public class SDDSRetriever extends Retriever
 {
    int i , m;
@@ -82,149 +94,202 @@ public class SDDSRetriever extends Retriever
    int x_col=0;
    FileFilter FF;
 
-   public SDDSRetriever(  String fileName )
+  /* --------------------------- constructor ---------------------------- */
+  /**
+   *  Construct an SDDSRetriever object, by opening the specified SDDS log 
+   *  file and reading the columns of SDDS data into DataSets, organized by
+   *  the units of the SDDS data. 
+   *
+   *  @param fileName  The name of the SDDS log file to load.
+   */
+   public SDDSRetriever( String fileName )
    {
-      super(  fileName ) ;
-      num_data_sets    = 0;
-	sdds = new SDDSFile(fileName);
-     if (!sdds.readFile()){return;}
+     super( fileName ) ;
+     num_data_sets = 0;
+     sdds = new SDDSFile( fileName );
+     if ( !sdds.readFile() )
+       return;
 
-       String[] columnNames=null, parameterNames=null;
-	 Object[][] columnValues=null;
-	 Object[][] parameterValues=null;
-	 int i, j, numberOfColumns=0, numberOfParameters=0;
-	 int rows;int type =0;
+     String[] columnNames    = null, 
+              parameterNames = null;
+     Object[][] columnValues = null;
+     Object[][] parameterValues = null;
+     int i, 
+         j, 
+         numberOfColumns = 0, 
+         numberOfParameters = 0;
+     int rows;
+     int type =0;
 
-       String[] col_units = sdds.getAllColumnUnits();
-     	 rows = sdds.getRowCount(page);
-       columnNames = sdds.getColumnNames();
-       int cols = Array.getLength(columnNames);
+     String[] col_units = sdds.getAllColumnUnits();
+     rows = sdds.getRowCount(page);
+     columnNames = sdds.getColumnNames();
+     int cols = Array.getLength(columnNames);
 
-   	 double xData[] = new double[rows];
-       double xx[] = new double[rows];
-       float x[] = new float[rows];
+     double xData[] = new double[rows];
+     double xx[] = new double[rows];
+     float x[] = new float[rows];
 
-       float y[][]= new float[cols][rows];
-	 parameterNames = sdds.getParameterNames();
+     float y[][]= new float[cols][rows];
+     parameterNames = sdds.getParameterNames();
 
-      DS = new DataSet[cols];
+     DS = new DataSet[cols];
 
-	 if (parameterNames != null) 
-       {
-	    numberOfParameters = Array.getLength(parameterNames);
-	    parameterValues = new Object[numberOfParameters][1];
-	    for (i=0;i<numberOfParameters;i++) 
-		parameterValues[i][0] = sdds.getParameterValue(i,page,false);   	    
-	  }
-	 
-	 if (columnNames != null) 
-	 {
-	    numberOfColumns = Array.getLength(columnNames);         
-	    columnValues = new Object[numberOfColumns][rows];
-	     for (i=0;i<numberOfColumns;i++)
-             columnValues[i] = sdds.getColumnValues(i,page,false);
-	  }
+     if (parameterNames != null) 
+     {
+       numberOfParameters = Array.getLength(parameterNames);
+       parameterValues = new Object[numberOfParameters][1];
+       for (i=0;i<numberOfParameters;i++) 
+         parameterValues[i][0] = sdds.getParameterValue(i,page,false);         
+     }
+   
+     if (columnNames != null) 
+     {
+       numberOfColumns = Array.getLength(columnNames);         
+       columnValues = new Object[numberOfColumns][rows];
+       for (i=0;i<numberOfColumns;i++)
+         columnValues[i] = sdds.getColumnValues(i,page,false);
+     }
 
-       for (j=0;j<numberOfColumns;j++) 
-	 {
-         if (columnNames[j].equals("Time"))
+     for (j=0;j<numberOfColumns;j++) 
+     {
+       if (columnNames[j].equals("Time"))
          for (m=0;m < rows ;m++) 
-   	   {
-	     x_col=j;
-	     xData[m] = ((Double)columnValues[x_col][m]).doubleValue();
+         {
+           x_col=j;
+           xData[m] = ((Double)columnValues[x_col][m]).doubleValue();
            xx[m] = xData[m]-xData[0];
            x[m] =   (float)xx[m];       
        //  System.out.println("Column values are :" +m +" , " +x[m]);
-          }
-        }
+         }
+     }
 
-       for (j=0;j<numberOfColumns;j++) 
-	 {
-         type = sdds.getColumnType(j);
-	   y[j] = (float[])( SDDSUtil.castArrayAsFloat
-					(sdds.getColumnValues(j,page,false), type) );
-      }
+     for (j=0;j<numberOfColumns;j++) 
+     {
+       type = sdds.getColumnType(j);
+       y[j] = (float[])( 
+           SDDSUtil.castArrayAsFloat(sdds.getColumnValues(j,page,false), type));
+     }
 
+     int n_ds = 0;     // n_ds counts the distinct datasets being built
+         j    = 0;     // j steps across the available data blocks
 
-	int n_ds = 0;     // n_ds counts the distinct datasets being built
-    	j    = 0;     // j steps across the available data blocks
+     boolean done = false;
+     Hashtable ht = new Hashtable();
+  
+     while ( j < cols && !done )     // While there are more DataSets to build,
+     {                               // store index of DataSets with same
+                                     // units in hashtable, with units as key.
+       if (col_units[j] == null)
+         col_units[j] = new String("");
 
-	boolean done = false;
-      Hashtable  ht = new Hashtable();
-	
-    while ( j < cols && !done )           // While there are more DataSets to build
-	{
-  	  if (col_units[j] == null)
-     	    col_units[j] = new String("");
+       String  y_units = col_units[j]; 
+       String  x_units = col_units[x_col];    
 
-  	  String y_units = col_units[j]; 
-  	  String x_units = col_units[x_col];    
-        Object O= ht.get(col_units[j]);
-        DataSet DSS=null;
-        if(O==null)
- 	    {DataSetFactory ds_factory = new DataSetFactory( columnNames[j], 
-                                         x_units,"Time",
-                                        y_units, columnNames[j]);
+       DataSet DSS = null;
+       Object  O   = ht.get(col_units[j]);     // Look for DataSet with same
+                                               // units as the current column
 
+       if( O == null )                        // if no such DataSet, make a
+       {                                      // a new DataSet with right units
+         String ds_title = columnNames[j] + " " + y_units;
+         /*
+         if ( ds_title.length() == 0 )
+           ds_title = "UN-LABELED";
+         */
+         DataSetFactory ds_factory = 
+                        new DataSetFactory( ds_title, 
+                                            x_units,"Time",
+                                            y_units, columnNames[j] );
 
-  	     DS[n_ds] = ds_factory.getDataSet();
-           DS[n_ds].addOperator( new XDateTime() ); 
-           ht.put(col_units[j], new Integer( n_ds));
-           DSS =DS[n_ds];
-           n_ds++;
-           
-          }
-       else
-          {int kk = ((Integer)O).intValue();
-           DSS = DS[kk];
-                                               // While there are more Data 
-           }                                           // blocks with the same units
+         DS[n_ds] = ds_factory.getDataSet();
+         DS[n_ds].addOperator( new XDateTime() ); 
+         ht.put( col_units[j], new Integer(n_ds) );
+         DSS = DS[n_ds];
+         n_ds++;
+       }
+       else                                   // we have an existing DataSet
+       {                                      // with the right units so use it
+         int kk = ((Integer)O).intValue();
+         DSS = DS[kk];
+       }  
+                                            // now that we found the DataSet
+                                            // for this SDDS column, make
+                                            // a new Data block for the 
+                                            // column and add it to the DataSet
+       Data d  = Data.getInstance( new VariableXScale(x), y[j], j );
+       d.setAttribute( new StringAttribute(columnNames[j], col_units[j]));
+       d.setLabel( columnNames[j] );
 
- 	   Data d  = Data.getInstance( new VariableXScale(x), y[j], j );
-    	   d.setAttribute( new StringAttribute(columnNames[j], col_units[j]));
+       for ( i=0; i < numberOfParameters; i++ )
+         d.setAttribute( new StringAttribute(parameterNames[i],
+                                             parameterValues[i][0].toString()));
+       DSS.addData_entry( d );
+       j++;
 
-    	   for ( i=0; i < numberOfParameters; i++ )
-          d.setAttribute( new StringAttribute(parameterNames[i],
-                                          parameterValues[i][0].toString()));
+       if ( j >= cols )                      // all Data blocks used so done
+         done = true;
 
-    	   DSS.addData_entry( d );
-         j++;
-        
-                
+     } //end of outer while
 
-
-
-  	 if (j >= cols )                                     // all Data blocks used so  
-     	  done = true;
-    }
      num_data_sets= ht.size();
-	 //end of outer while
-       
-      
-   }
+  } 
 
-	public DataSet getDataSet( int data_set_num ) 
-    	{ 
-        ds = DS[data_set_num];
-        if(ds==null)
-           System.out.println("getDS res=null for"+data_set_num);
-        return ds ;
-      }               
+
+  /* -------------------------- getDataSet ---------------------------- */
+  /**
+   * Get the specified DataSet that was loaded from the SDDS log file.
+   *
+   * @param data_set_num  The number of the DataSet from the SDDS file. 
+   *
+   * @return The specified DataSet, if it exists, or null if no such
+   *         DataSet exists.
+   */
+  public DataSet getDataSet( int data_set_num ) 
+  { 
+    ds = DS[data_set_num];
+    if ( ds == null )
+      System.out.println("getDS res=null for"+data_set_num);
+    return ds ;
+  }               
   
-     public   int getType( int data_set_num ) 
-     { 
-       int type = 2;
-       return type ;
-     }           
+
+  /* ---------------------------- getType ------------------------------ */
+  /**
+   *  Get the type code of a particular DataSet in this SDDS file. 
+   *  Currently, this just returns type code Retriever.HISTOGRAM_DATA_SET.
+   *  It may be necessary to add new type codes to support SDDS properly.
+   *
+   *  @param  data_set_num  The number of the DataSet in this runfile whose
+   *                        type code is needed.  data_set_num must be between
+   *                        0 and numDataSets()-1
+   *
+   *  @return the type code for the specified DataSet.
+   */
+  public int getType( int data_set_num ) 
+  { 
+    if ( data_set_num < num_data_sets && data_set_num >= 0 )
+      return HISTOGRAM_DATA_SET;
+    else
+      return INVALID_DATA_SET;
+  }           
+
   
-    public   int  numDataSets() 
-      {
-    	  if ( sdds == null )
-        return 0;
+  /* ------------------------ numDataSets -------------------------- */
+  /**
+   * Get the number of distinct DataSets that were loaded from the SDDS
+   * file.
+   *
+   *  @return The number of distinct DataSets from the SDDS file.  
+   */
+  public int numDataSets() 
+  {
+    if ( sdds == null )
+      return 0;
  
-        return num_data_sets;
-      }         
+    return num_data_sets;
+  }         
 
 
-}		
+}    
 
