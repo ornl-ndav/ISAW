c3fe834bda359a78bc587886d342d822221aeb9b
Dennis Mikkelson <mikkelsond@uwstout.edu>
2007-07-04 19:13:58 +0000

    Minor formatting improvements.

--- DataSetTools/operator/Generic/TOF_SAD/Reduce_KCL.java
+++ DataSetTools/operator/Generic/TOF_SAD/Reduce_KCL.java
@@ -30,6 +30,9 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.39  2007/07/04 19:13:57  dennis
+ * Minor formatting improvements.
+ *
  * Revision 1.38  2007/07/04 17:10:36  rmikk
  * changed setMonitorInd to getMonitorInd
  *
@@ -38,7 +41,9 @@
  *
  * Revision 1.36  2005/11/23 17:22:39  hammonds
  * Make small changes to take out edit only differences between Reduce_KCL and Reduce_LPSD.
- * The only variation from this is to change the variable tofs in Reduce_LPSD to xvals in order to more closely match the syntax in Reduce_KCL therby reducing differences in the files.
+ * The only variation from this is to change the variable tofs in Reduce_LPSD
+ * to xvals in order to more closely match the syntax in Reduce_KCL therby 
+ * reducing differences in the files.
  *
  * Revision 1.35  2005/05/11 22:51:56  dennis
  * Minor improvement to documentation.
@@ -550,33 +555,37 @@ public class Reduce_KCL  extends GenericTOF_SAD{
     
       Res.append("@param TransS The sample Transmission data set");
       Res.append("@param TransB The background Transmission data set");
-      Res.append("@param  Eff   The Efficiency data set");
+      Res.append("@param Eff    The Efficiency data set");
       Res.append("@param Sens   The sensitivity data set");
       Res.append("@param qu     The q bins if 1d or qxmin, qxmax, " );
       Res.append("              qymin, qymax, if 2d.");
-      Res.append("@param RUNSds0   the monitor  for the sample");
 
-      Res.append("@param RUNSds1  the Histogram  for the sample");
+      Res.append("@param RUNSds0 the monitor for the sample");
+      Res.append("@param RUNSds1 the Histogram for the sample");
+
       Res.append("@param RUNBds0 the  monitor for the Background");
       Res.append("@param RUNBds1 the  Histogramfor the Background");
-      Res.append("@param RUNCds0 null or the monitor for the  Cadmium run");
-      Res.append("@param RUNCds1 null or the  Histogram for the  Cadmium run");
-      Res.append("@param BETADN The delayed neutron fraction");
-      Res.append("@param SCALE  The scale factor to be applied to all data");
-      Res.append("@param  THICK The sample thickness in m");
-      Res.append("@param  XOFF  The Xoffset of beam from the center in meters");
-      Res.append("@param  YOFF  The Yoffset of beam from center in meters");
-      Res.append("@param NQxBins  The number of Qx bins if 2D,otherwise use");
+
+      Res.append("@param RUNCds0 null or the monitor for the Cadmium run");
+      Res.append("@param RUNCds1 null or the Histogram for the Cadmium run");
+
+      Res.append("@param BETADN  The delayed neutron fraction");
+      Res.append("@param SCALE   The scale factor to be applied to all data");
+      Res.append("@param THICK   The sample thickness in m");
+      Res.append("@param XOFF    The Xoffset of beam from the center in meters");
+      Res.append("@param YOFF    The Yoffset of beam from center in meters");
+      Res.append("@param NQxBins The number of Qx bins if 2D,otherwise use");
       Res.append(" a neg number");
-      Res.append("@param NQyBins  The number of Qx bins if 2D,otherwise use a");
+      Res.append("@param NQyBins The number of Qx bins if 2D,otherwise use a");
       Res.append(" neg number"); 
-      Res.append("@param useTransB  Use the background Transmission run"); 
+      Res.append("@param useTransB      Use the background Transmission run"); 
       Res.append("@param upStreamMonID  Upstream monitor ID or -1");
+
       Res.append("@return The vector of three DataSets, the Sample,");
       Res.append(" Background, and Samp-Back S(Q) or S(Qx,Qy)for 2D,");
 
       Res.append(" if no errors are encountered, otherwise an");
-      Res.append("  ErrorString will be returned.");
+      Res.append(" ErrorString will be returned.");
 
       return Res.toString();
 }
