15372ba4054a263fc6ef6e5c5fc55e09ca77c5fc
rmikk <rmikk@9b3b9736-f711-0410-b1b1-9612de3a1827>
2007-07-11 17:51:15 +0000

    Added documentation for this class

--- trunk/NexIO/DataSetInfo.java
+++ trunk/NexIO/DataSetInfo.java
@@ -1,10 +1,27 @@
 package NexIO;
 
 
+  /**
+   * This class is used to store information on the data sets in the NeXus file
+   * so that the number of data sets and default group and detector ID's are
+   * accurate 
+   * 
+   * @author Ruth
+   *
+   */
   public class DataSetInfo{
-    NxNode NxentryNode, NxdataNode;
-    int startGroupID;
-    int endGroupID;
+     
+    public NxNode NxentryNode, 
+                  NxdataNode;
+    
+    public int startGroupID;
+    public int endGroupID;
+    
+    public int startDetectorID;
+    public int endDetectorID;
+    public int nelts;
+    public int ndetectors;
+    
     String label;   //or field name other than time for NXlog
     /**
     *   NxdataNode is null for monitors.  All monitors are merged
@@ -12,10 +29,17 @@ package NexIO;
     */
     public DataSetInfo( NxNode NxentryNode,NxNode NxdataNode,
                    int startGroupID,int EndGroupID, String label){
+       
        this.NxentryNode = NxentryNode;
        this.NxdataNode = NxdataNode ;
+       
        this.startGroupID = startGroupID;
        this.endGroupID = EndGroupID ;
+       
+       this.startDetectorID =-1;
+       this.endDetectorID = -1;
        this.label = label;
+       nelts = -1;
+       ndetectors = -1;
     }
 }//DataSetInfo
