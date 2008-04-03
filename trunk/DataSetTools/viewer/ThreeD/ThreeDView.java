263c4b4ffe450996c147ce4640623769f57d3665
moea <moea@9b3b9736-f711-0410-b1b1-9612de3a1827>
2008-04-03 00:06:40 +0000

    Undid previous revision 19201 to avoid breaking project. Having SVN issues; cannot commit code that this depends on.

--- trunk/DataSetTools/viewer/ThreeD/ThreeDView.java
+++ trunk/DataSetTools/viewer/ThreeD/ThreeDView.java
@@ -1111,9 +1111,10 @@ private void init()
   threeD_panel = new ThreeD_JPanel();
   threeD_panel.setBackground( new Color( 90, 90, 90 ) );
   
-  //setting coord info source to null because it doesn't really apply to 
-  // a 3d environment
-  threeD_panel.setCoordInfoSource(null);
+  threeD_panel.setWCToolTipVisible(false); //Set world-coordinate tooltip to 
+                                           //false.  This is because 
+                                           //CoordJPanel's world-coordinates 
+                                           //don't apply in 3D.
 
   control_panel = new Box( BoxLayout.Y_AXIS );
 
