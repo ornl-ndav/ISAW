991556369fe54359fee94778173f77f5fd70d467
Dennis Mikkelson <mikkelsond@uwstout.edu>
2008-06-14 01:42:55 +0000

    Class to hold static final Strings that are standardized names for facilities and instruments.

---
+++ DataSetTools/instruments/FacilityInstrumentNames.java
@@ -0,0 +1,68 @@
+/*
+ * File:  FacilityInstrumentNames.java
+ *
+ * Copyright (C) 2008, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * For further information, see <http://ftp.sns.gov/ISAW/>
+ *
+ * Last Modified:
+ *
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ *
+ */
+package DataSetTools.instruments;
+
+/**
+ *  This class defines String constants with names for various facilities
+ *  and instruments.
+ */
+
+public class FacilityInstrumentNames 
+{
+  public static final String SNS    = "SNS";
+
+  public static final String IPNS   = "IPNS";
+
+  public static final String LANSCE = "LANSCE";
+
+  public static final String SCD0   = "SCD0";
+
+  public static final String SXD    = "SXD";
+ 
+  public static final String SNAP   = "SNAP";
+
+  public static final String TOPAZ  = "TOPAZ";
+ 
+  public static final String MANDI  = "MANDI";
+
+  /**
+   *  This class just contains static methods, so don't construct an 
+   *  instance of this class.
+   */
+  private FacilityInstrumentNames()
+  {
+    // private constructor so this class can't be instantiated.
+  }
+
+}
