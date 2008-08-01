aec1c4fdb30bebdcdb7e97a7d3b2de1486a420fe
eu7 <eu7@9b3b9736-f711-0410-b1b1-9612de3a1827>
2008-08-01 01:43:44 +0000

    Added method to merge IEventList3D objects into a new FloatArrayEventList3D object. refs #13

--- trunk/EventTools/EventList/FloatArrayEventList3D.java
+++ trunk/EventTools/EventList/FloatArrayEventList3D.java
@@ -34,6 +34,7 @@
 
 package EventTools.EventList;
 
+import java.util.*;
 import EventTools.Histogram.IEventBinner;
 import EventTools.Histogram.UniformEventBinner;
 
@@ -63,10 +64,10 @@ public class FloatArrayEventList3D implements IEventList3D
    * @param y_vals  Array of y-coordinates for the events.
    * @param z_vals  Array of z-coordinates for the events.
    */
-  public FloatArrayEventList3D( int[]         codes,
-                                float[]       x_vals,
-                                float[]       y_vals,
-                                float[]       z_vals    ) 
+  public FloatArrayEventList3D( int[]   codes,
+                                float[] x_vals,
+                                float[] y_vals,
+                                float[] z_vals  ) 
   {
     if ( codes == null || x_vals == null || y_vals == null || z_vals == null )
       throw new IllegalArgumentException( "array null" );
@@ -206,5 +207,47 @@ public class FloatArrayEventList3D implements IEventList3D
     return new UniformEventBinner( min, max, 1);
   }
  
+
+  /**
+   *  Merge all of the event lists in the Vector into a new 
+   *  event list.
+   *
+   *  @param lists  Vector of IEventList3D objects
+   *  
+   *  @return a new FloatArrayEventList3D containing all of the events
+   *          in the lists of events.
+   */
+  public static FloatArrayEventList3D merge( Vector<IEventList3D> lists )
+  {
+     IEventList3D list;
+     int total_size = 0;
+     for ( int i = 0; i < lists.size(); i++ )
+     {
+       list = lists.elementAt(i);
+       total_size += list.getNumEntries();
+     }
+
+     float[] all_x     = new float[total_size];
+     float[] all_y     = new float[total_size];
+     float[] all_z     = new float[total_size];
+     int[]   all_codes = new int[total_size];
+     int index = 0;
+     for ( int i = 0; i < lists.size(); i++ )
+     {
+       list = lists.elementAt(i);
+       int length = list.getNumEntries();
+       if ( length > 0 )
+         for ( int k = 0; k < length; k++ )
+         {
+           all_x[index] = (float)list.getEventX(k);
+           all_y[index] = (float)list.getEventY(k);
+           all_z[index] = (float)list.getEventZ(k);
+           all_codes[index] = list.getEventCode(k);
+           index++;        
+         }
+     }
+
+     return new FloatArrayEventList3D( all_codes, all_x, all_y, all_z );
+  }
 
 }
