b98033e395c46d6a432e192b9fbaf1bcd4645d1e
ehx <ehx@9b3b9736-f711-0410-b1b1-9612de3a1827>
2008-09-26 14:26:12 +0000

    Converted to an operator. Added documentation

--- trunk/Scripts/SNS/SaveNexCache.py
+++ trunk/Scripts/SNS/SaveNexCache.py
@@ -1,18 +1,54 @@
 #           Save NeXus Cache information
 #
 # Saves the cache of classnames needed to quickly load in NeXus
-#   files with a lot of NXdata or fields under an NXentry
+#   files with a lot of NXdata or fields under an NXentry and/or NxInstrument
+# @param filename   The name of a NeXus file with standard template
+#@param  CacheFile  The name of the file where the names are cached or the 
+#                      default location and name if blank
+# @return  "OK" or error string.  The cache file is set up if successful
 
-#NOTE: replace filename ="C:/ISAW/SampleRuns/SNS/ARCS/ARCS_65.nxs" by an
-#    actual filename on your system
 
-from IPNSSrc import blind
-from Operators.TOF_SCD import IndexJ
+
+
+from gov.anl.ipns.Util.SpecialStrings  import *
 from DataSetTools.retriever import NexusRetriever
+from DataSetTools.operator.Generic.Save import *
 
-filename = "C:/ISAW/SampleRuns/SNS/ARCS/ARCS_65.nxs"
-retriever = NexusRetriever(filename)
-retriever.SaveSetUpInfo( None)
-#replace None by the name of the file to store this information
+class SaveNexCache ( GenericSave):
+    
+    
+    
+   def setDefaultParameters(self):
+       self.super__clearParametersVector()
+       X=System.getProperty("Data_Directory", None)
+       self.addParameter( LoadFilePG("NeXus File name", X ) )
+       X = System.getProperty("user.home", None)       
+       self.addParameter( SaveFilePG("Cache File name or blank for default", X ) )
+                     
+       
+   def getCommand(self):
+       return "SaveNeXusCache"
+   
+   def getResult(self):
+      filename = self.getParameter(0).getValue()
+      toFilename = self.getParameter(1).getValue()
+      retriever = NexusRetriever(filename)
+      retriever.SaveSetUpInfo( toFilename)
+      #replace None by the name of the file to store this information
 
-print "finished"
+      print "finished"
+      return "OK"
+
+   def getDocumentation( self):
+      S = StringBuffer("         Create NeXus Cache\n")
+      S.append("@overview Saves the cache of classnames needed to quickly load in NeXus ")
+      S.append("files with a lot of NXdata or fields under an NXentry and/or NxInstrument\n")
+      S.append(" @param filename   The name of a NeXus file with standard template\n")
+      S.append("@param  CacheFile  The name of the file where the names are cached or the ") 
+      S.append("                      default location and name if blank\n")
+      S.append(" @return  \"OK\" or error string.  The cache file is set up if successful\n")
+      return S.toString()     
+   
+       # Constructor
+   def __init__( self ):
+       Operator.__init__( self, "Create NeXus Cache" )
