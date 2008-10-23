7501007bfad50ee8b06840dd115221c8d8ee78fa
ehx <ehx@9b3b9736-f711-0410-b1b1-9612de3a1827>
2008-10-23 14:47:45 +0000

    Checks the data type for the parameter before throwing casting.  Also, the GetUB Matrix method now returns an errorString instead of null.  This addresses ticket #23

--- trunk/DataSetTools/operator/Generic/TOF_SCD/WriteMatrix.java
+++ trunk/DataSetTools/operator/Generic/TOF_SCD/WriteMatrix.java
@@ -131,8 +131,10 @@ public class WriteMatrix extends GenericOperator implements HiddenOperator{
       try{
 
          java.lang.String Filename = getParameter(0).getValue().toString();
-         if( getParameter(1).getValue() == null)
+         if( getParameter(1).getValue() == null )
             return new ErrorString(" UB matrix is null");
+         if( !(getParameter(1).getValue() instanceof float[][]))
+            return  new ErrorString(" UB matrix not correct data type");
          float[][] UB = (float[][])(getParameter(1).getValue());
          gov.anl.ipns.Util.SpecialStrings.ErrorString Xres=DataSetTools.operator.Generic.TOF_SCD.Util.WriteMatrix(Filename,UB );
 
