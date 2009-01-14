2f7422575b48d393e99276b0c2356368449baab7
Dennis Mikkelson <mikkelsond@uwstout.edu>
2009-01-14 15:20:34 +0000

    Changed version number to 1.9.0

--- IsawGUI/Isaw.java
+++ IsawGUI/Isaw.java
@@ -1007,7 +1007,7 @@ public class Isaw
         String val="";
 
         if(SharedData.VERSION.equals("Unknown_Version"))
-          version="1.9.0_b5";
+          version="1.9.0";
         else
           version=SharedData.VERSION;
 
