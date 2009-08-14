c9bd0a143e5a9e97520ccf97484abccd5a9d780c
ehx <ehx@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-08-14 15:34:56 +0000

    Title bar for the 3D event viewer now shows the filename.

--- trunk/EventTools/ShowEventsApp/MainApp.java
+++ trunk/EventTools/ShowEventsApp/MainApp.java
@@ -4,16 +4,17 @@ package EventTools.ShowEventsApp;
 import javax.swing.*;
 import MessageTools.*;
 
+import EventTools.ShowEventsApp.Command.LoadEventsCmd;
 import EventTools.ShowEventsApp.DataHandlers.*;
 import EventTools.Histogram.*;
 import gov.anl.ipns.MathTools.Geometry.Vector3D;
 
-public class MainApp
+public class MainApp implements IReceiveMessage
 {
   public  static final int NUM_BINS = 512;
 
   private Histogram3D histogram;
-
+  private JFrame frame3D;
   public Histogram3D DefaultHistogram( int num_bins )
   {
     // Just make default histogram aligned with coord axes.
@@ -69,11 +70,15 @@ public class MainApp
 
     EventViewHandler view_handler = new EventViewHandler( message_center );
 
-    JFrame frame = new JFrame( "Reciprocal Space Events" );
-    frame.setSize(750,750);
-    frame.setDefaultCloseOperation( JFrame.DO_NOTHING_ON_CLOSE );
-    frame.getContentPane().add( view_handler.getPanel() );
-    frame.setVisible( true );
+    frame3D = new JFrame( "Reciprocal Space Events" );
+    frame3D.setSize(750,750);
+    frame3D.setDefaultCloseOperation( JFrame.DO_NOTHING_ON_CLOSE );
+    frame3D.getContentPane().add( view_handler.getPanel() );
+    frame3D.setVisible( true );
+    
+    //where is frame2D made????
+    message_center.addReceiver( this , 
+             EventTools.ShowEventsApp.Command.Commands.LOAD_FILE );
   }
 
 
@@ -86,7 +91,31 @@ public class MainApp
   }
 
   
-  public static void main(String[] args)
+  /* (non-Javadoc)
+ * @see MessageTools.IReceiveMessage#receive(MessageTools.Message)
+ */
+@Override
+public boolean receive( Message message )
+{
+
+  if( message == null || !(message.getName().equals(
+            EventTools.ShowEventsApp.Command.Commands.LOAD_FILE)))
+     return true;
+  
+   if( (message.getValue()== null) || 
+            !(message.getValue() instanceof  LoadEventsCmd))
+        return false;
+   
+   LoadEventsCmd info = (LoadEventsCmd)message.getValue();
+   
+   frame3D.setTitle( "Reciprocal Space Events for "+info.getEventFile() );
+   //TODO set Title for frame2D
+   
+   return true;
+}
+
+
+public static void main(String[] args)
   {
 //    MainApp app = new MainApp();
      SwingUtilities.invokeLater( new Builder() );
