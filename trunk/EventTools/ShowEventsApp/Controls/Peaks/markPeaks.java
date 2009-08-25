d77d3ec371c660779f4ad06f6ad733c85c1c750c
fischerp <fischerp@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-08-25 19:58:46 +0000

    Added GPL and javadocs.

--- trunk/EventTools/ShowEventsApp/Controls/Peaks/markPeaks.java
+++ trunk/EventTools/ShowEventsApp/Controls/Peaks/markPeaks.java
@@ -1,20 +1,63 @@
+/* 
+ * File: markPeaks.java
+ *
+ * Copyright (C) 2009, Paul Fischer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0800276 and by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ */
+
 package EventTools.ShowEventsApp.Controls.Peaks;
 
 import javax.swing.*;
 
 import java.awt.event.*;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import EventTools.ShowEventsApp.Command.*;
 import MessageTools.*;
 
+/**
+ * Creates a panel to hold the mark peaks checkbox
+ * and sends messages of MARK_PEAKS when checked or unchecked.
+ */
 public class markPeaks extends JPanel
 {
    private static final long serialVersionUID = 1L;
    private MessageCenter     message_center;
    private JCheckBox         markPeaksCbx;
    
+   /**
+    * Creates the mark peaks checkbox and adds it to itself.
+    * 
+    * @param message_center
+    */
    public markPeaks(MessageCenter message_center)
    {
       this.message_center = message_center;
@@ -28,6 +71,10 @@ public class markPeaks extends JPanel
       this.add(markPeaksCbx);
    }
 
+   /**
+    * ActionListener that listens to markPeaksCheckbox and
+    * sends out a message of MARK_PEAKS.
+    */
    private class peaksListener implements ActionListener
    {
       public void actionPerformed(ActionEvent e)
@@ -36,6 +83,12 @@ public class markPeaks extends JPanel
       }
    }
    
+   /**
+    * Sends a message to the messagecenter
+    * 
+    * @param command
+    * @param value
+    */
    private void sendMessage(String command, Object value)
    {
       Message message = new Message( command,
--- trunk/EventTools/ShowEventsApp/Controls/Peaks/peakOptionsPanel.java
+++ trunk/EventTools/ShowEventsApp/Controls/Peaks/peakOptionsPanel.java
@@ -1,5 +1,39 @@
-package EventTools.ShowEventsApp.Controls.Peaks;
+/* 
+ * File: peakOptionsPanel.java
+ *
+ * Copyright (C) 2009, Paul Fischer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0800276 and by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ */
 
+package EventTools.ShowEventsApp.Controls.Peaks;
 
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
@@ -9,10 +43,20 @@ import java.awt.GridLayout;
 import EventTools.ShowEventsApp.Command.*;
 import MessageTools.*;
 
+/**
+ * Creates the options panel for peak options which include
+ * findPeaksPanel and writePeaks panel.
+ */
 public class peakOptionsPanel extends JPanel
 {
    private static final long serialVersionUID = 1L;
    
+   /**
+    * Constructor for peakOptionsPanel that builds
+    * the panel as well as sets the messagecenter.
+    * 
+    * @param message_center
+    */
    public peakOptionsPanel(MessageCenter message_center)
    {
       findPeaksPanel findPeaks = new findPeaksPanel(message_center);
