f22c7b4d8e4e09aaab78892e7d73d3fcbc509bf9
fischerp <fischerp@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-08-25 19:31:51 +0000

    Added GPL and javadocs.

--- trunk/EventTools/ShowEventsApp/Controls/HistogramControls/sliceControl.java
+++ trunk/EventTools/ShowEventsApp/Controls/HistogramControls/sliceControl.java
@@ -1,3 +1,37 @@
+/* 
+ * File: sliceControl.java
+ *
+ * Copyright (C) 2009, Paul Fischer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0800276 and by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author: fischerp $
+ *  $Date: 2009-08-25 11:02:13 -0500 (Tue, 25 Aug 2009) $            
+ *  $Revision: 19931 $
+ */
 package EventTools.ShowEventsApp.Controls.HistogramControls;
 
 import java.awt.event.*;
@@ -5,20 +39,30 @@ import java.awt.GridLayout;
 import javax.swing.*;
 
 import gov.anl.ipns.MathTools.Geometry.SlicePlane3D;
-import gov.anl.ipns.MathTools.Geometry.Vector3D;
 import gov.anl.ipns.ViewTools.UI.*;
 
 import EventTools.ShowEventsApp.Command.*;
 import MessageTools.*;
 
+/**
+ * Slice control panel that creates a
+ * SliceSelectorSplitPaneUI to be used to set the 
+ * orientation of the Histogram which will display the slice
+ * in the correct way.
+ */
 public class sliceControl extends JPanel
                           implements IReceiveMessage
 {
    private static final long  serialVersionUID  = 1L;
    private SliceSelectorSplitPaneUI scUI;
    private MessageCenter   message_center;
-   private JFrame          sliceDisplay;
    
+   /**
+    * Constructor that creates the SliceSelectorSplitPaneUI
+    * and adds it to itself.
+    * 
+    * @param mc MessageCenter to be used.
+    */
    public sliceControl(MessageCenter mc)
    {
       message_center = mc;
@@ -30,16 +74,24 @@ public class sliceControl extends JPanel
       this.add(scUI);
    }
    
+   /**
+    * Set the plane on the SliceSelectorSplitPaneUI
+    * 
+    * @param plane The plane to set the control to.
+    */
    public void setPlane(SlicePlane3D plane)
    {
       scUI.setPlane(plane);
    }
    
-   public void showSliceControl()
+   /*public void showSliceControl()
    {
       sliceDisplay.setVisible(true);
-   }
+   }*/
    
+   /**
+    * Send a message to the message center.
+    */
    private void sendMessage(String command, Object value)
    {
       Message message = new Message( command,
@@ -49,6 +101,10 @@ public class sliceControl extends JPanel
       message_center.receive( message );
    }
    
+   /**
+    * ActionListener that listens to SliceSelectorSplitPaneUI
+    * and sends a message of tyep SlicePlaneInformationCmd.
+    */
    private class Action implements ActionListener
    {
       public void actionPerformed(ActionEvent arg0)
@@ -68,6 +124,10 @@ public class sliceControl extends JPanel
       }
    }
    
+   /**
+    * Waiting for SET_ORIENTATION_MATRIX message so that it can
+    * update the SliceSelectorSplitPaneUI with the correct information.
+    */
    public boolean receive(Message message)
    {
       if (message.getName().equals(Commands.SET_ORIENTATION_MATRIX))
