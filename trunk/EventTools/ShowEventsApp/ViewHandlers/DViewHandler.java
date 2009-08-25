4152dfc90fb45a3dfbd41fafb4d53f2355cec698
fischerp <fischerp@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-08-25 18:57:32 +0000

    Added GPL and javadocs.

--- trunk/EventTools/ShowEventsApp/ViewHandlers/DViewHandler.java
+++ trunk/EventTools/ShowEventsApp/ViewHandlers/DViewHandler.java
@@ -1,3 +1,38 @@
+/* 
+ * File: DViewHandler.java
+ *
+ * Copyright (C) 2009, Paul Fischer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0800276 and by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author: fischerp $
+ *  $Date: 2009-08-25 11:02:13 -0500 (Tue, 25 Aug 2009) $            
+ *  $Revision: 19931 $
+ */
+
 package EventTools.ShowEventsApp.ViewHandlers;
 
 import java.awt.GridLayout;
@@ -8,6 +43,10 @@ import EventTools.ShowEventsApp.Command.Commands;
 import gov.anl.ipns.ViewTools.Components.OneD.FunctionViewComponent;
 import MessageTools.*;
 
+/**
+ * Builds and Displays a graph of d-spacing. Updates
+ * automatically when data is loaded and displayed on the screen.
+ */
 public class DViewHandler implements IReceiveMessage
 {
    private MessageCenter messageCenter;
@@ -19,6 +58,14 @@ public class DViewHandler implements IReceiveMessage
    private String        x_label = "d-spacing";
    private String        y_label = "Intensity";
    
+   /**
+    * Sets the message center for the DViewHandler but does
+    * not display or create anything else.  The class relies mainly
+    * on the message center.  It will display the jframe when it
+    * receives SHOW_D_GRAPH message.
+    * 
+    * @param messageCenter
+    */
    public DViewHandler(MessageCenter messageCenter)
    {
       this.messageCenter = messageCenter;
@@ -28,6 +75,11 @@ public class DViewHandler implements IReceiveMessage
       this.messageCenter.addReceiver(this, Commands.ADD_EVENTS_TO_VIEW);
    }
    
+   /**
+    * Creates a new JFrame to display the graph every time
+    * it is called.  Will display a graph if its been built
+    * or will display a placeholder saying no data loaded.
+    */
    private void displayDFrame()
    {
       dDisplayFrame = new JFrame("d-spacing View");
@@ -44,6 +96,11 @@ public class DViewHandler implements IReceiveMessage
       dDisplayFrame.repaint();
    }
    
+   /**
+    * Placeholder to put in the frame if no data is loaded.
+    * 
+    * @return Panel
+    */
    private JPanel placeholderPanel()
    {
       JPanel placeholderpanel = new JPanel();
@@ -57,6 +114,15 @@ public class DViewHandler implements IReceiveMessage
       return placeholderpanel;
    }
    
+   /**
+    * Takes the data and creates an instance of
+    * FunctionViewComponent and adds it to the graphPanel
+    * and then to the frame if the frame has been created.
+    * This allows for the graph to be updated while the frame 
+    * is displayed.
+    * 
+    * @param xyValues X,Y values of the data for the graph.
+    */
    private void setPanelInformation(float[][] xyValues)
    {
       float[] x_values = xyValues[0];
@@ -73,6 +139,12 @@ public class DViewHandler implements IReceiveMessage
          dDisplayFrame.add(graphPanel);
    }
    
+   /**
+    * Send a message to the messagecenter
+    * 
+    * @param command
+    * @param value
+    */
    private void sendMessage(String command, Object value)
    {
       Message message = new Message(command, value, true);
@@ -80,6 +152,10 @@ public class DViewHandler implements IReceiveMessage
       messageCenter.receive(message);
    }
 
+   /**
+    * Receive messages to display the frame, hide the frame,
+    * get the xy values, and set the values/create the graph.
+    */
    public boolean receive(Message message)
    {
       if (message.getName().equals(Commands.SHOW_D_GRAPH))
