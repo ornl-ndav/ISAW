71782a4a015db79e6e97488d1244571163462fd3
fischerp <fischerp@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-08-25 15:42:23 +0000

    Reverted the smoothData back for the time being.

--- EventTools/ShowEventsApp/Command/FindPeaksCmd.java
+++ EventTools/ShowEventsApp/Command/FindPeaksCmd.java
@@ -2,20 +2,28 @@ package EventTools.ShowEventsApp.Command;
 
 public class FindPeaksCmd
 {
+   private boolean smoothData;
    private boolean markPeaks;
    private int     maxNumberOfPeaks;
    private float   minPeakIntensity;
    private String  logFileName;
    
-   public FindPeaksCmd( boolean markPeaks,
+   public FindPeaksCmd( boolean smoothData,
+                        boolean markPeaks,
                         int     maxNumberOfPeaks,
                         float   minPeakIntensity, 
                         String  logFileName )
    {
+      this.smoothData = smoothData;
       this.markPeaks  = markPeaks;
       this.maxNumberOfPeaks = maxNumberOfPeaks;
       this.minPeakIntensity = minPeakIntensity;
       this.logFileName = logFileName;
+   }
+   
+   public boolean getSmoothData()
+   {
+      return smoothData;
    }
    
    public boolean getMarkPeaks()
