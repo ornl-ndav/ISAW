5bf66441ae180487ee33f5aa90ab7ccb09e391c2
fischerp <fischerp@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-08-26 18:07:43 +0000

    Added GPL and javadocs.  Changed default point size on drawingOptions to 3 and fix spelling error.  Renamed the buttons and some labels on filePanel as well as set it to start with 1 but reduce it by 1 so as to actually start with 0.

--- EventTools/ShowEventsApp/Controls/drawingOptions.java
+++ EventTools/ShowEventsApp/Controls/drawingOptions.java
@@ -1,3 +1,38 @@
+/* 
+ * File: drawingOptions.java
+ *
+ * Copyright (C) 2009, Paul Fischer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0800276 and by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ */
+
 package EventTools.ShowEventsApp.Controls;
 
 import javax.swing.*;
@@ -9,18 +44,32 @@ import java.awt.event.*;
 import EventTools.ShowEventsApp.Command.*;
 import MessageTools.*;
 
+/**
+ * Creates a panel giving the user options for 
+ * how things are drawn on the screen such as
+ * whether or not to draw points above the max data
+ * value and below the min data value.
+ * Also wether to show the axis on the screen,
+ * use an alpha value for the points, the point
+ * size and to view in orthographic or not.
+ */
 public class drawingOptions extends JPanel
 {
    public static final long serialVersionUID = 1L;
    private MessageCenter    messageCenter;
    private JCheckBox        filterAbove;
    private JCheckBox        filterBelow;
-   private JCheckBox        showAxes;
+   private JCheckBox        showAxis;
    private JCheckBox        useAlpha;
    private JTextField       pointSize;
    private JTextField       alphaValue;
    private JCheckBox        orthographic;
    
+   /**
+    * Builds the panel as well as stores the message center.
+    * 
+    * @param messageCenter
+    */
    public drawingOptions(MessageCenter messageCenter)
    {
       this.messageCenter = messageCenter;
@@ -40,6 +89,11 @@ public class drawingOptions extends JPanel
       this.add(apply);
    }
    
+   /**
+    * Builds the option panel for orthographic view.
+    * 
+    * @return JPanel
+    */
    private JPanel buildOrtho()
    {
       JPanel panel = new JPanel();
@@ -52,6 +106,12 @@ public class drawingOptions extends JPanel
       return panel;
    }
    
+   /**
+    * Builds the option panel for filtering
+    * above the max data value.
+    * 
+    * @return JPanel
+    */
    private JPanel buildMax()
    {
       JPanel panel = new JPanel();
@@ -64,6 +124,12 @@ public class drawingOptions extends JPanel
       return panel;
    }
    
+   /**
+    * Builds the option panel for filtering
+    * below the min data value.
+    * 
+    * @return JPanel
+    */
    private JPanel buildMin()
    {
       JPanel panel = new JPanel();
@@ -77,26 +143,37 @@ public class drawingOptions extends JPanel
       return panel;
    }
    
+   /**
+    * Builds the option panel for showing the axis.
+    * 
+    * @return JPanel
+    */
    private JPanel buildAxes()
    {
       JPanel panel = new JPanel();
       panel.setLayout(new GridLayout(1,1));
 
-      showAxes = new JCheckBox("Show Axes");
-      showAxes.setSelected(true);
+      showAxis = new JCheckBox("Show Axis");
+      showAxis.setSelected(true);
       
-      panel.add(showAxes);
+      panel.add(showAxis);
       
       return panel;
    }
    
+   /**
+    * Builds the option panel for the size of the 
+    * points to be drawn.
+    * 
+    * @return JPanel
+    */
    private JPanel buildPointSize()
    {
       JPanel panel = new JPanel();
       panel.setLayout(new GridLayout(1,1));
 
       JLabel label = new JLabel("Point Size");
-      String defaultSize = "1";
+      String defaultSize = "3";
       pointSize = new JTextField(defaultSize);
       pointSize.setHorizontalAlignment(JTextField.RIGHT);
 
@@ -106,6 +183,12 @@ public class drawingOptions extends JPanel
       return panel;
    }
    
+   /**
+    * Builds the option panel for using an alpha value
+    * by entering a number from 0.0 - 1.0.
+    * 
+    * @return JPanel
+    */
    private JPanel builduseAlpha()
    {
       JPanel panel = new JPanel();
@@ -122,6 +205,12 @@ public class drawingOptions extends JPanel
       return panel;
    }
    
+   /**
+    * Sends a message to the message center
+    * 
+    * @param command Command Name for others to listen to.
+    * @param value Object to send to the listener.
+    */
    private void sendMessage(String command, Object value)
    {
       Message message = new Message(command, value, true);
@@ -129,6 +218,12 @@ public class drawingOptions extends JPanel
       messageCenter.receive(message);
    }
    
+   /**
+    * Checks to make sure the values for point size
+    * and alpha value are entered correctly.
+    * 
+    * @return true if everything is valid otherwise false.
+    */
    private boolean valid()
    {
       if(pointSize.getText().equals(""))
@@ -179,6 +274,12 @@ public class drawingOptions extends JPanel
       return true;
    }
    
+   /**
+    * Listens to the apply button and then checks to make 
+    * sure all information entered is correct and then sends
+    * a message of SET_DRAWING_OPTIONS and of type
+    * DrawingOptionsCmd.
+    */
    private class displayListener implements ActionListener
    {
       public void actionPerformed(ActionEvent e)
@@ -189,7 +290,7 @@ public class drawingOptions extends JPanel
                new DrawingOptionsCmd(orthographic.isSelected(),
                            filterBelow.isSelected(),
                            filterAbove.isSelected(),
-                           showAxes.isSelected(),
+                           showAxis.isSelected(),
                            Float.parseFloat(pointSize.getText()),
                            useAlpha.isSelected(),
                            Float.parseFloat(alphaValue.getText()));
