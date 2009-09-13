226d6845be53e7601360463b473dc12f46703f02
eu7 <eu7@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-09-13 21:57:43 +0000

    This class handles the initialization of the histogram, q-mapper and event viewer, when a new file is to be loaded.  Until the file is done loading, no further attempts at loading another file are allowed.

---
+++ trunk/EventTools/ShowEventsApp/DataHandlers/InitializationHandler.java
@@ -0,0 +1,213 @@
+/* 
+ * File: InitializationHandler.java
+ *
+ * Copyright (C) 2009, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge, TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ */
+
+package EventTools.ShowEventsApp.DataHandlers;
+
+import MessageTools.IReceiveMessage;
+import MessageTools.Message;
+import MessageTools.MessageCenter;
+
+import EventTools.EventList.SNS_Tof_to_Q_map;
+import EventTools.ShowEventsApp.Command.Commands;
+import EventTools.ShowEventsApp.Command.LoadEventsCmd;
+import EventTools.ShowEventsApp.Command.SetNewInstrumentCmd;
+import EventTools.ShowEventsApp.Command.Util;
+
+
+/**
+ *  This class processes messages to load a file, or initialize the
+ *  histogram and instrument type.  It will send out commands to 
+ *  set up the Q-mapper and clear the histograms.  When it receives
+ *  messages indicating that these data structures are ready to receive
+ *  data, then it will send out a message to actually load the file data or
+ *  to begin listening for live events.  If a file is currently being 
+ *  loaded, this will IGNORE FURTHER INITIALIZATION ATTEMPTS until the
+ *  file has finished loading.
+ */
+public class InitializationHandler implements IReceiveMessage
+{
+  public static final String UNKNOWN_INSTRUMENT = "UNKNOWN_INSTRUMENT";
+
+  private MessageCenter    message_center;
+  private boolean          histogram_ok;
+  private boolean          dq_ok;
+  private boolean          instrument_ok;
+  private boolean          loading_file;
+  private boolean          load_failed;
+  private LoadEventsCmd    load_file_cmd;
+
+  public InitializationHandler( MessageCenter message_center )
+  {
+    this.message_center = message_center;
+
+    histogram_ok  = false;
+    dq_ok         = false;
+    instrument_ok = false;
+    loading_file  = false;
+    load_failed   = false;
+
+    message_center.addReceiver( this, Commands.LOAD_FILE );
+    message_center.addReceiver( this, Commands.LOAD_FAILED );
+    message_center.addReceiver( this, Commands.LOAD_FILE_DONE );
+    message_center.addReceiver( this, Commands.INIT_HISTOGRAM_DONE );
+    message_center.addReceiver( this, Commands.INIT_DQ_DONE );
+    message_center.addReceiver( this, Commands.INIT_NEW_INSTRUMENT_DONE);
+  }
+
+  public boolean receive( Message message )
+  {
+    if ( message.getName().equals(Commands.LOAD_FILE) )
+    {
+      if ( loading_file && !load_failed )
+      {
+        Util.sendInfo( "Still Loading File " + load_file_cmd.getEventFile() );
+        Util.sendInfo( "IGNORING EXTRA LOAD REQUEST" );
+        return false; 
+      }
+
+      loading_file  = true;
+      load_failed   = false;
+      histogram_ok  = false;
+      dq_ok         = false;
+      instrument_ok = false;
+
+      load_file_cmd = (LoadEventsCmd)message.getValue();
+
+      String inst_name = getInstrumentName( load_file_cmd.getEventFile() );
+      if ( inst_name.equals( UNKNOWN_INSTRUMENT ) )
+      {
+        loading_file = false;
+        Util.sendInfo("UNSUPPORTED INSTRUMENT " + inst_name + 
+                      " FROM FILE " + load_file_cmd.getEventFile() );
+        return false;
+      }  
+
+      Message init_view = new Message( Commands.INIT_EVENTS_VIEW, 
+                                       null, true, true );
+      message_center.send( init_view );
+
+      SetNewInstrumentCmd new_inst_cmd = 
+        new SetNewInstrumentCmd( inst_name,
+                                 load_file_cmd.getDetFile(),
+                                 load_file_cmd.getIncSpectrumFile() );
+
+      Message new_inst = new Message( Commands.INIT_NEW_INSTRUMENT,
+                                      new_inst_cmd, true, true );
+      message_center.send( new_inst );
+
+
+      Message clear_hist = new Message( Commands.INIT_HISTOGRAM,
+                                        new_inst_cmd, true, true );
+      message_center.send( clear_hist );
+
+
+      Message clear_dq = new Message( Commands.INIT_DQ, null, true, true );
+      message_center.send( clear_dq );
+
+      return false;
+    }
+    
+    else if ( message.getName().equals(Commands.INIT_HISTOGRAM_DONE) )
+    {
+      histogram_ok = true;
+      LoadIfPossible();
+    }
+
+    else if ( message.getName().equals(Commands.INIT_DQ_DONE) )
+    {
+      dq_ok = true;
+      LoadIfPossible();
+    }
+
+    else if ( message.getName().equals(Commands.INIT_NEW_INSTRUMENT_DONE) )
+    {
+      instrument_ok = true;
+      LoadIfPossible();
+    }
+
+    else if ( message.getName().equals(Commands.LOAD_FAILED ) )
+    {
+      load_failed  = true;
+      loading_file = false;
+    }
+
+    else if ( message.getName().equals(Commands.LOAD_FILE_DONE ) )
+    {
+      load_failed  = false;
+      loading_file = false;
+      histogram_ok  = false;
+      dq_ok         = false;
+      instrument_ok = false;
+    }
+
+    return false;
+  }
+
+
+  /**
+   *  Get the name of a supported instrument from the specified file name.
+   */
+  public String getInstrumentName( String file_name )
+  {
+    String instrument_name = UNKNOWN_INSTRUMENT;
+
+    if ( file_name.indexOf("SNAP") >= 0 )
+      instrument_name = SNS_Tof_to_Q_map.SNAP;
+
+    else if ( file_name.indexOf("ARCS") >= 0 )
+      instrument_name = SNS_Tof_to_Q_map.ARCS;
+
+    else if ( file_name.indexOf("SEQ") >= 0 )
+      instrument_name = SNS_Tof_to_Q_map.SEQ;
+
+    else if ( file_name.indexOf("TOPAZ") >= 0 )
+      instrument_name = SNS_Tof_to_Q_map.TOPAZ;
+ 
+    return instrument_name;
+  }
+  
+
+  private void LoadIfPossible()
+  {
+    if ( histogram_ok && 
+                dq_ok && 
+        instrument_ok &&
+        !load_failed  )
+    {
+      Message load = new Message( Commands.LOAD_FILE_DATA, 
+                                  load_file_cmd, true, true );
+      message_center.send( load );
+    }
+  }
+}
