be740fb34505ee041c1c6c53cdd2589833075966
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2009-09-17 22:40:08 +0000

    Initial Checkin for loading event data from UDP packets

---
+++ EventTools/ShowEventsApp/DataHandlers/SocketEventLoader.java
@@ -0,0 +1,303 @@
+/* 
+ * File: SocketEventLoader.java
+ *
+ * Copyright (C) 2009, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge, TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$:
+ *  $Date$:            
+ *  $Rev$:
+ */
+
+package EventTools.ShowEventsApp.DataHandlers;
+
+
+import EventTools.EventList.TofEventList;
+import EventTools.ShowEventsApp.Command.Commands;
+import EventTools.ShowEventsApp.Command.SetNewInstrumentCmd;
+import MessageTools.Message;
+import MessageTools.MessageCenter;
+import MessageTools.TimedTrigger;
+import NetComm.IUDPUser;
+import NetComm.UDPReceive;
+
+/**
+ * Class that loads event data sent via UDP sockets and sends the information
+ * using messages to listeners.
+ * 
+ * @author Ruth
+ * 
+ */
+public class SocketEventLoader extends UDPReceive
+{
+
+
+
+   public static int BUFF_SIZE     = 3000;
+
+   public static int SEND_MINIMUM  = 2000;
+
+   public static int SEND_MIN_TIME = 1500; // ms
+
+
+   /**
+    * Constructor
+    * 
+    * @param port
+    *           The port listening for these UDP packets
+    * @param message_center
+    *           The message center to send messages to
+    * @param Instrument
+    *           The Instrument producing the UDP packets, or null
+    */
+   public SocketEventLoader( int port, MessageCenter message_center,
+            String Instrument )
+   {
+
+      super( port , new thisIUDPUser( message_center , Instrument ) );
+   }
+
+
+   /**
+    * Test program using port 8002 and the SNAP instrument
+    * 
+    * @param args
+    */
+   public static void main( String[] args )
+   {
+
+      MessageCenter message_center = new MessageCenter( "TTest" );
+      new TimedTrigger( message_center , 30 );
+      SocketEventLoader ev = new SocketEventLoader( 8002 , message_center ,
+               "SNAP" );
+      ev.start();
+   }
+}
+
+
+/**
+ * Class for the user to process the UDP packets
+ * 
+ * @author Ruth
+ * 
+ */
+class thisIUDPUser implements IUDPUser
+{
+
+
+
+   MessageCenter      message_center;
+
+   int[]              tofBuff   = new int[ SocketEventLoader.BUFF_SIZE ];
+
+   int[]              idBuff    = new int[ SocketEventLoader.BUFF_SIZE ];
+
+   int                Buffstart = 0;
+
+   long               timeStamp = - 1;
+
+   private static int Nshown    = 0;
+
+   private static int NReceived = 0;
+
+
+   /**
+    * Constructor
+    * 
+    * @param message_center
+    *           The message center to receive messages
+    * @param Instrument
+    *           The intrument. If non null an INIT_NEW_INSTRUMENT message will
+    *           be sent.
+    * 
+    */
+   public thisIUDPUser( MessageCenter message_center, String Instrument )
+   {
+
+      this.message_center = message_center;
+      if( Instrument != null )
+         message_center
+                  .send( new Message( Commands.INIT_NEW_INSTRUMENT ,
+                           new SetNewInstrumentCmd( Instrument , null , null ) ,
+                           false ) );
+      ( new timerThread( this ) ).start();
+   }
+
+
+   /**
+    * Processes one data packet
+    * 
+    * @param data
+    *           the data packet
+    * @param length
+    *           The number of bytes in the data
+    */
+   public void ProcessData( byte[] data , int length )
+   {
+
+      Nshown = 0;
+      if( length < 28 || data == null || data.length < length )
+         return;
+
+      if( length > 23 )
+         if( data[ 0 ] == (byte) 0 && data[ 1 ] == (byte) ( 2 )
+                  && data[ 2 ] == (byte) 0 && data[ 3 ] == (byte) 0 )
+            if( data[ 27 ] > 0 && ( ( data[ 27 ] & 0x80 ) > 0 ) )
+               return;
+
+      NReceived++ ;
+
+      int NEvents = Cvrt2Int( data , 20 );
+
+      int[] ids = new int[ NEvents ];
+      int[] tofs = new int[ NEvents ];
+      int start = 24;
+      // start=0; NEvents = length/2 with Dennis' interpretation
+      if( Buffstart + NEvents >= SocketEventLoader.BUFF_SIZE )
+      {
+         // System.out.print( "PrcessDat1" );
+         SendMessage( NEvents );
+      }
+      for( int i = Buffstart ; i < Buffstart + NEvents ; i++ )
+      {
+         tofBuff[ i ] = Cvrt2Int( data , start );
+         idBuff[ i ] = Cvrt2Int( data , start + 4 );
+
+         if( Nshown < 0 && NReceived % 20 == 0 )
+         {
+            System.out.println( String.format( "%8x,%8x,%2x,%2x,%2x,%2x" ,
+                     tofs[ i ] , ids[ i ] , data[ start ] , data[ start + 1 ] ,
+                     data[ start + 2 ] , data[ start + 3 ] ) );
+            Nshown++ ;
+         }
+         start += 8;
+      }
+
+      Buffstart += NEvents;
+
+      if( Buffstart > SocketEventLoader.SEND_MINIMUM )
+      {
+         // System.out.print( "PrcessDat2" );
+         SendMessage( 0 );
+      }
+
+      // if( NReceived % 200 == 0 )
+      // System.out.println( "Received packets =" + NReceived );
+   }
+
+
+   // Sends a message if enough info has been buffered or enough time has
+   // passed
+   protected synchronized void SendMessage( int NEvents )
+   {
+
+      long currTime = System.currentTimeMillis();
+
+      if( ( Buffstart < SocketEventLoader.SEND_MINIMUM && Buffstart + NEvents < SocketEventLoader.BUFF_SIZE ) )
+         if( currTime - timeStamp < SocketEventLoader.SEND_MIN_TIME )
+            return;
+
+      if( Buffstart == 0 )
+         return;
+
+      int[] tofs = new int[ Buffstart ];
+      int[] ids = new int[ Buffstart ];
+
+      System.arraycopy( tofBuff , 0 , tofs , 0 , Buffstart );
+      System.arraycopy( idBuff , 0 , ids , 0 , Buffstart );
+      Buffstart = 0;
+     
+      message_center.send( new Message( Commands.MAP_EVENTS_TO_Q ,
+               new TofEventList( tofs , ids ) , false , true ) );
+
+      timeStamp = System.currentTimeMillis();
+      if( NEvents >= SocketEventLoader.BUFF_SIZE)
+      {
+         tofBuff= new int[ NEvents+10];
+         idBuff =new int[ NEvents+10];
+         SocketEventLoader.BUFF_SIZE = NEvents+10;
+      }
+   }
+
+
+   private int Cvrt2Int( byte[] B , int start )
+   {
+
+      int NEvents = 0;
+
+      for( int i = start + 3 ; i >= start ; i-- )
+      {
+         NEvents |= B[ i ] & 0xFF;
+         if( i > start )
+            NEvents <<= 8;
+
+      }
+      return NEvents;
+   }
+
+}
+
+
+/**
+ * Used to send messages after a given period of time has elapsed
+ * 
+ * @author Ruth
+ * 
+ */
+class timerThread extends Thread
+{
+
+
+
+   thisIUDPUser user;
+
+
+   public timerThread( thisIUDPUser user )
+   {
+
+      this.user = user;
+   }
+
+
+   public void run()
+   {
+
+      try
+      {
+         if( user == null )
+            return;
+         while( 3 == 3 )
+         {
+            Thread.sleep( SocketEventLoader.SEND_MIN_TIME );          
+            user.SendMessage( 0 );
+         }
+      }
+      catch( Exception s )
+      {
+
+      }
+   }
+}
---
+++ EventTools/ShowEventsApp/DataHandlers/SocketServerTest.java
@@ -0,0 +1,182 @@
+/* 
+ * File: SocketServerText.java
+ *
+ * Copyright (C) 2009, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge, TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$:
+ *  $Date$:            
+ *  $Rev$:
+ */
+package EventTools.ShowEventsApp.DataHandlers;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import EventTools.EventList.SNS_TofEventList;
+import NetComm.UDPSend;
+import Command.compile.*;
+
+
+/**
+ * Sends UDP packets similar to the packet structure at the SNS. The
+ * data comes from an Event file.
+ * 
+ * @author Ruth
+ *
+ */
+public class SocketServerTest extends UDPSend
+{
+
+
+   int NPacketsSent =0;
+   /**
+    * Constructor. The data is sent to port 8002 on the localhost
+    * @throws UnknownHostException
+    * @throws SocketException
+    */
+   public SocketServerTest( )throws UnknownHostException , SocketException
+   {
+      super( "localhost" , 8002 );
+   }
+   public void runTest( String EventFileName, long NEvents )
+           
+   {
+
+      
+      SNS_TofEventList eventList = new SNS_TofEventList( EventFileName );
+      if( NEvents < 80 )
+         NEvents = 80;
+      // if( NEvents >900)
+      NEvents = 900;
+      int[] ids , tof;
+      long totNevents = eventList.numEntries();
+
+      for( long firstEvent = 0 ; firstEvent < totNevents ; firstEvent += NEvents )
+      {
+         ids = eventList.eventPixelID( firstEvent , NEvents );
+         tof = eventList.eventTof( firstEvent , NEvents );
+
+         byte[] packet = new byte[ 24 + ids.length * 8 ];
+         java.util.Arrays.fill( packet , 0 , 24 , (byte) 0 );
+         int L = ids.length;
+         packet[ 20 ] = LoByte( L );
+         packet[ 21 ] = HiByte( L );
+
+         int start = 24;
+         for( int i = 0 ; i < ids.length ; i++ )
+         {
+            assign( tof[ i ] , packet , start );
+            assign( ids[ i ] , packet , start + 4 );
+
+            start += 8;
+         }
+
+         try
+         {
+            // System.out.write( packet );
+            send( packet , packet.length );
+            NPacketsSent++ ;
+            if( NPacketsSent % 200 == 0 )
+               System.out.println( "sent packets =" + NPacketsSent );
+            if( ids.length < NEvents )
+               System.exit( 0 );
+            Thread.sleep(30 );
+         }
+         catch( Exception s )
+         {
+            s.printStackTrace();
+            System.exit( 0 );
+         }
+      }
+   }
+   
+   /**
+    * Returns the hi(most significant) byte for the given number. Takes into 
+    * account of negative numbers
+    * @param offset  The number
+    * @return   The most significant byte 
+    * NOTE: The integer must be less than 2 bytes.
+    */
+   public static byte HiByte( int offset)
+   {
+      int N = 0;
+      if(offset < 0 )
+        N= 1;
+     return (byte)(offset/256 - N);
+   }
+   
+   /**
+    * Returns the low(least significant) byte for the given number. Takes into 
+    * account of negative numbers
+    * @param offset  The number
+    * @return   The least significant byte 
+    * 
+    */
+   public static byte LoByte( int offset)
+   {
+        return (byte)(offset%256);
+   }
+   
+   private void assign( int val, byte[] B, int start)
+   {
+      int TVal = val/(256*256);
+      int LVal = val %(256*256);
+      B[start]= LoByte(LVal);
+      B[start+1]= HiByte(LVal);
+      B[start+2]= LoByte(TVal);
+      B[start+3]= HiByte(TVal);
+   }
+   /**
+    * Test program
+    * @param args
+    *     args[0]  The name of the Event file
+    *     args[1]  The number of events in each packet.(Not implemented)
+    */
+   public static void main( String[] args )
+   {
+     if( args == null || args.length < 1)
+        System.exit(0);
+     
+     int NEvents = 0;
+     
+     if( args.length > 1)
+        NEvents = Integer.parseInt( args[1] );
+     
+     try
+      {
+         SocketServerTest ss = new SocketServerTest();
+         ss.runTest( args[ 0 ] , NEvents );
+      }
+      catch( Exception s )
+      {
+         s.printStackTrace();
+         System.exit( 0 );
+      }
+
+   }
+
+}
