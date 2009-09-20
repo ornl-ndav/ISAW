c08c3610f76fba4490b2291b84cdbc8c35373e79
Dennis Mikkelson <mikkelsond@uwstout.edu>
2009-09-20 22:46:58 +0000

    Now accepts additional command line arguments to determine the maximum events per pulse to send and what host gets the events.  If more than 2000 events are sent for each pulse, the events are split into packets with at most 2000 events per packet. The number of events sent per pulse is random between .8 and 1 time the maximum number of events per packet.

--- EventTools/ShowEventsApp/DataHandlers/SocketServerTest.java
+++ EventTools/ShowEventsApp/DataHandlers/SocketServerTest.java
@@ -33,6 +33,7 @@
  */
 package EventTools.ShowEventsApp.DataHandlers;
 
+import java.util.*;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
@@ -49,61 +50,124 @@ import NetComm.UDPSend;
  */
 public class SocketServerTest extends UDPSend
 {
-
-
-   int NPacketsSent =0;
    /**
-    * Constructor. The data is sent to port 8002 on the localhost
+    *  Limit on the maximum number of events to send for each 1/60 second
+    *  pulse.
+    */
+   public static final int MAX_PER_PULSE = 83333;   // 5 million events/sec
+
+   /**
+    * Constructor. The data is sent to port 8002 on the specified host.
+    *
+    * @param destination_node  The machine to which the UDP packets of events
+    *                          are sent.
+    *
     * @throws UnknownHostException
     * @throws SocketException
     */
-   public SocketServerTest( )throws UnknownHostException , SocketException
+   public SocketServerTest( String destination_node )
+                            throws UnknownHostException , SocketException
    {
-      super( "localhost" , 8002 );
+      super( destination_node, 8002 );
    }
-   public void runTest( String EventFileName, long NEvents )
-           
-   {
 
-      
+   public void runTest( String EventFileName, int events_per_pulse )
+   {
+      Random ran_gen = new Random();
+
       SNS_TofEventList eventList = new SNS_TofEventList( EventFileName );
-      if( NEvents < 80 )
-         NEvents = 80;
-      // if( NEvents >900)
-      NEvents = 900;
-      int[] ids , tof;
       long totNevents = eventList.numEntries();
 
-      for( long firstEvent = 0 ; firstEvent < totNevents ; firstEvent += NEvents )
-      {
-         ids = eventList.eventPixelID( firstEvent , NEvents );
-         tof = eventList.eventTof( firstEvent , NEvents );
+      if ( totNevents > 8000000 )                   // do at most 8 million 
+        totNevents = 8000000;                       // events for testing
 
-         byte[] packet = new byte[ 24 + ids.length * 8 ];
-         java.util.Arrays.fill( packet , 0 , 24 , (byte) 0 );
-         int L = ids.length;
-         packet[ 20 ] = LoByte( L );
-         packet[ 21 ] = HiByte( L );
+      int[] all_tofs = eventList.eventTof( 0L, totNevents );
+      int[] all_ids  = eventList.eventPixelID( 0L, totNevents );
+      int[] ids, tof;
 
-         int start = 24;
-         for( int i = 0 ; i < ids.length ; i++ )
-         {
-            assign( tof[ i ] , packet , start );
-            assign( ids[ i ] , packet , start + 4 );
+      int NPacketsSent = 0;
 
-            start += 8;
+      if ( events_per_pulse > MAX_PER_PULSE )       // limit rate to about 5 
+        events_per_pulse = MAX_PER_PULSE;           // million per second
+
+      int firstEvent = 0;
+      Vector<byte[]> packets  = new Vector<byte[]>(10);
+
+      long last_time = System.currentTimeMillis();
+      long curr_time;
+      int  elapsed_time;
+
+      while ( firstEvent < totNevents )  
+      {                        
+                                                    // Determine number of 
+                                                    // events for this pulse
+         int  n_to_send = (int)
+                 ( events_per_pulse * (0.2 * ran_gen.nextDouble() + 0.8) );
+
+                                                    // Don't try to send more
+                                                    // events than remain  
+         if ( firstEvent + n_to_send > totNevents )
+           n_to_send = (int)(totNevents - firstEvent);
+
+         packets.clear();
+         while ( n_to_send > 0 )
+         {                                          // determine number to
+           int packet_size = n_to_send;             // send in this packet
+
+           if ( packet_size > 2000 )                // can't do more than 2000
+             packet_size = 2000;
+                                                    // now build a packet of
+           ids = new int[ packet_size ];            // the correct size
+           tof = new int[ packet_size ];
+
+           System.arraycopy( all_tofs, firstEvent, tof, 0, packet_size );
+           System.arraycopy( all_ids,  firstEvent, ids, 0, packet_size );
+
+                                                    // adjust the number to
+                                                    // send and firstEvent
+           n_to_send   = n_to_send - packet_size;
+           firstEvent += packet_size;
+                                                    // push header and data 
+                                                    // into one byte array
+           byte[] packet = new byte[ 24 + ids.length * 8 ];
+           java.util.Arrays.fill( packet , 0 , 24 , (byte) 0 );
+           int L = ids.length;
+           packet[ 20 ] = LoByte( L );
+           packet[ 21 ] = HiByte( L );
+
+           int start = 24;
+           for( int i = 0 ; i < ids.length ; i++ )
+           {
+              assign( tof[ i ] , packet , start );
+              assign( ids[ i ] , packet , start + 4 );
+
+              start += 8;
+           }                                   
+                                                     // save the byte array
+           packets.add( packet );
          }
 
-         try
-         {
-            // System.out.write( packet );
-            send( packet , packet.length );
-            NPacketsSent++ ;
-            if( NPacketsSent % 200 == 0 )
-               System.out.println( "sent packets =" + NPacketsSent );
-            if( ids.length < NEvents )
-               System.exit( 0 );
-            Thread.sleep(30 );
+         try                                         // now try to actually 
+         {                                           // send each packet
+            for ( int i = 0; i < packets.size(); i++ )
+            {
+              byte[] packet = packets.elementAt(i);
+
+              // System.out.write( packet );
+              send( packet , packet.length );
+              NPacketsSent++ ;
+
+              if( NPacketsSent % 200 == 0 )
+                 System.out.println( "sent packets =" + NPacketsSent );
+            }
+                                                   // send about 60 times/sec
+            curr_time = System.currentTimeMillis();
+            elapsed_time = (int)(curr_time - last_time);
+            last_time = curr_time;
+            if ( elapsed_time < 16 )
+              Thread.sleep( 16 - elapsed_time );
+            else
+              Thread.sleep(1);                    // give the system a break
          }
          catch( Exception s )
          {
@@ -113,6 +177,7 @@ public class SocketServerTest extends UDPSend
       }
    }
    
+
    /**
     * Returns the hi(most significant) byte for the given number. Takes into 
     * account of negative numbers
@@ -127,6 +192,7 @@ public class SocketServerTest extends UDPSend
         N= 1;
      return (byte)(offset/256 - N);
    }
+
    
    /**
     * Returns the low(least significant) byte for the given number. Takes into 
@@ -139,6 +205,7 @@ public class SocketServerTest extends UDPSend
    {
         return (byte)(offset%256);
    }
+
    
    private void assign( int val, byte[] B, int start)
    {
@@ -149,25 +216,39 @@ public class SocketServerTest extends UDPSend
       B[start+2]= LoByte(TVal);
       B[start+3]= HiByte(TVal);
    }
+
+
    /**
     * Test program
     * @param args
     *     args[0]  The name of the Event file
-    *     args[1]  The number of events in each packet.(Not implemented)
+    *     args[1]  The maximum number of events to send for each pulse
+    *              at approximately 1/60 sec per pulse 
+    *     args[2]  The node name or IP address to which the event UDP packets
+    *              will be sent.
     */
    public static void main( String[] args )
    {
      if( args == null || args.length < 1)
+     {
+       System.out.println("You must at least specify an event file to use");
+       System.out.println("as the source of events sent to IsawEV.");
+
         System.exit(0);
+     }
      
-     int NEvents = 0;
+     int NEvents = 450;
      
-     if( args.length > 1)
+     if ( args.length > 1)
         NEvents = Integer.parseInt( args[1] );
+
+     String destination_node = "localhost";
+     if (args.length > 2 )
+       destination_node = args[2];
      
      try
       {
-         SocketServerTest ss = new SocketServerTest();
+         SocketServerTest ss = new SocketServerTest( destination_node );
          ss.runTest( args[ 0 ] , NEvents );
       }
       catch( Exception s )
@@ -175,7 +256,6 @@ public class SocketServerTest extends UDPSend
          s.printStackTrace();
          System.exit( 0 );
       }
-
    }
 
 }
