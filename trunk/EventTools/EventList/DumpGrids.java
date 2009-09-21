d17d6e4eae0160bfad62a344812e6f86f89a54cd
eu7 <eu7@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-09-21 02:40:47 +0000

    Uitlitiy to dump out grids from a NeXus file.

--- trunk/EventTools/EventList/DumpGrids.java
+++ trunk/EventTools/EventList/DumpGrids.java
@@ -1,3 +1,36 @@
+/* 
+ *  * File: DumpGrids.java
+ * 
+ * Copyright (C) 2009, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge, TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ */
 
 package EventTools.EventList;
 
@@ -8,7 +41,10 @@ import DataSetTools.retriever.*;
 import DataSetTools.dataset.*;
 import DataSetTools.operator.Generic.TOF_SCD.*;
 
-
+/**
+ * Write out the detector grid information from a NeXus file, in the
+ * for required by the QMapper (.DetCal).
+ */
 public class DumpGrids
 {
   
