ab699341fdf40fa36d16e8c6007e44b29e0606ef
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2009-11-13 00:26:08 +0000

    Extracted mouse and mouse motion listeners so alternative listeners can be substituted in.

---
+++ DataSetTools/components/ui/iMouse_MotionListener.java
@@ -0,0 +1,13 @@
+package DataSetTools.components.ui;
+
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+
+
+
+public interface iMouse_MotionListener extends MouseListener ,
+         MouseMotionListener
+{
+
+}
