b4bfcf2d8bfe824df3bd9098941ca1def97a9789
ajschultz <ajschultz@9b3b9736-f711-0410-b1b1-9612de3a1827>
2009-11-22 22:18:03 +0000

    This version is easier to input a formula. For example, instead of C 2 O 6 H 6 now the input is C2 O6 H6

--- trunk/Scripts/TOF_SCD/lin_abs_coef.py
+++ trunk/Scripts/TOF_SCD/lin_abs_coef.py
@@ -8,10 +8,10 @@
 # A. J. Schultz --  November, 2009
 # R. Mikkelson -- gui construction:  November, 2009
 
-class lin_abs_coef2(GenericTOF_SCD):
+class lin_abs_coef(GenericTOF_SCD):
     def setDefaultParameters(self):
         self.super__clearParametersVector()
-        self.addParameter(StringPG("Chemical formula (click Help for examples):", "C 2 O 6 H 6"))
+        self.addParameter(StringPG("Chemical formula (click Help for examples):", "C2 O6 H6"))
         self.addParameter(FloatPG("Number of formula units in the unit cell (Z):", 2))
         self.addParameter(FloatPG("Unit cell volume (A^3):", 253))
         
@@ -19,11 +19,11 @@ class lin_abs_coef2(GenericTOF_SCD):
     
         formulaString = self.getParameter(0).value
         formulaList = formulaString.split()
-        numberOfIsotopes = len(formulaList)/2	# the number of elements or isotopes in the formula
+        numberOfIsotopes = len(formulaList)     # the number of elements or isotopes in the formula
 
         zParameter = self.getParameter(1).value # number if formulas in the unit cell
 
-        unitCellVolume = self.getParameter(2).value
+        unitCellVolume = self.getParameter(2).value # unit cell volume in A^3
 
         sumScatXs = 0.0
         sumAbsXs = 0.0
@@ -48,7 +48,6 @@ class lin_abs_coef2(GenericTOF_SCD):
 
         # begin loop through each atom in the formula
         for i in range(numberOfIsotopes):
-            j = 2*i
             input = open(filename, 'r')			# this has the effect of rewinding the file
             lineString = input.readline()		# read the first comment line
             while lineString[0] == '#':         # search for the end of the comments block
@@ -57,15 +56,16 @@ class lin_abs_coef2(GenericTOF_SCD):
             # Begin to search the table for element/isotope match.
             
             lineList = lineString.split()       # this should be the H atom
+            lenSymbol = len(lineList[0])        # number of characters in the isotope name or symbol
 
-            while formulaList[j] != lineList[0]:
+            while formulaList[i][0:lenSymbol] != lineList[0]:
                 lineString = input.readline()
                 lineList = lineString.split()
 
             scatteringXs = float(lineList[1])	# the total scattering cross section
             absorptionXs = float(lineList[2])	# the true absorption cross section at 1.8 A
             atomicWeight = float(lineList[4])   # atomic weight
-            number = float(formulaList[j+1])	# the number of this nuclei in the formula
+            number = float(formulaList[i][lenSymbol:])	# the number of this nuclei in the formula
             
             print '%-5s %10.5f %10.5f' % (lineList[0], scatteringXs, absorptionXs)
             
@@ -95,11 +95,13 @@ class lin_abs_coef2(GenericTOF_SCD):
         S =StringBuffer()
         S.append("Sample Inputs of molecular formula\n")
         S.append(" For example, for oxalic acid dihydrate, C2O4H2.2H2O, or C2O6H6, input\n")
-        S.append("C 2 O 4 H 2 H 4 O 2    or    C 2 O 6 H 6\n")
+        S.append("C2 O4 H2 H4 O2    or    C2 O6 H6\n")
         S.append("For deuterated oxalic acid dihydrate, input\n")
-        S.append("C 2 O 6 D 6   or   C 2 O 6 2H 6\n")
+        S.append("C2 O6 D6   or   C2 O6 2H6\n")
         S.append("For La2NiO4.2, input\n")
-        S.append("La 2 Ni 1 O 4.2\n")
+        S.append("La2 Ni1 O4.2\n")
+        S.append("For boron-11 B4C, input\n")
+        S.append("11B4 C1\n")
         S.append("@param  formula  The chemical formula input as described above")
         S.append("@param   Z number of formula units")
         S.append("@param  UnitVolume  the unit cell volume")
@@ -112,6 +114,6 @@ class lin_abs_coef2(GenericTOF_SCD):
         return ["Macros","Single Crystal"]
         
     def __init__(self):
-        Operator.__init__(self,"lin_abs_coef2")
+        Operator.__init__(self,"lin_abs_coef")
 
 
