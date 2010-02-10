2ff1fbdff3f28319c2b61abd2c079740311195c4
Dennis Mikkelson <mikkelsond@uwstout.edu>
2010-02-10 13:50:38 +0000

    Minor additions to look at binary detector mapping files.

--- EventTools/EventList/BinaryDump.java
+++ EventTools/EventList/BinaryDump.java
@@ -1,37 +1,60 @@
+
 package EventTools.EventList;
 
 import java.io.*;
 
 public class BinaryDump
 {
+
   public static void main( String args[] ) throws IOException
   {
-    String filename = "/usr2/ARCS_SCD_2/EVENTS/ARCS_1250_neutron_event.dat";
+    if ( args.length < 1 )
+      System.out.println("Please specify file name as first argument");
+    String filename = args[0];
     int    first_byte = 0;
-    int    seg_size   = 80;
 
     RandomAccessFile r_file = new RandomAccessFile( filename, "r" );
+    int num_bytes = (int)r_file.length();
 
     r_file.seek( first_byte );
 
-    byte[] buffer = new byte[seg_size];
+    byte[] buffer = new byte[num_bytes];
     r_file.read( buffer );
 
-    for ( int i = 0; i < buffer.length; i++ )
+    int val    = 0;
+    int hits   = 0;
+    int misses = 0;
+    int MAX_TO_SHOW = 40;
+
+    for ( int i = 0; i < num_bytes; i++ )
     {
-      if ( i % 8 == 0 )
-        System.out.println();
-
-      if ( i % 4 == 0 )
-        System.out.print("   : ");
-
       int byte_1 = buffer[i];
       if ( byte_1 < 0 )
         byte_1 += 256;
-      System.out.printf( " %5d" , byte_1 );
+
+      if ( i < MAX_TO_SHOW )
+        System.out.printf( " %4d" , byte_1 );
+
+      if ( (i+1) % 4 == 0 )
+      {
+        val = SNS_TofEventList.getValue_32( buffer, i-3 );
+        if ( i < MAX_TO_SHOW )
+          System.out.printf("   : %9d   ", val);
+      }
+
+      if ( (i+1) % 4 == 0 && i < MAX_TO_SHOW )
+        System.out.println();
+
+      if ( (i+1) % 4 == 0 )
+        if ( val == i / 4 )
+          hits++;
+        else
+          misses++;
     }
     System.out.println();
+    System.out.println("Hits : " + hits + "   Misses :" + misses );
 
   }
+
 
 }
