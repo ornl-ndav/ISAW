e9b4580cb99ad909b37a2adfa91407ac1e4ff7d6
ehx <ehx@9b3b9736-f711-0410-b1b1-9612de3a1827>
2010-02-17 19:31:22 +0000

    Replaced int values by long for arguments and operator parameters. Replaced the order of parameters after isLog so BooleanEnablePG   will work  refs #104, refs #105

--- trunk/Operators/TOF_Diffractometer/MakeTimeFocusedDataSet.java
+++ trunk/Operators/TOF_Diffractometer/MakeTimeFocusedDataSet.java
@@ -73,20 +73,20 @@ public class MakeTimeFocusedDataSet extends GenericOperator{
     */
    public void setDefaultParameters(){
       clearParametersVector();
-      addParameter( new StringPG("The name of the instrument","SNAP"));
-      addParameter( new LoadFilePG("the file with events",System.getProperty("Data_Directory","")));
-      addParameter( new LoadFilePG("Det Cal file",System.getProperty("ISAW_HOME","")+"/InstrumentInfo/SNS"));
-      addParameter( new LoadFilePG("bank file",System.getProperty("ISAW_HOME","")+"/InstrumentInfo/SNS"));
-      addParameter( new LoadFilePG("Map file",System.getProperty("ISAW_HOME","")+"/InstrumentInfo/SNS"));
-      addParameter( new IntegerPG("first Event",1));
-      addParameter( new IntegerPG("Num Events To Load",20000));
-      addParameter( new FloatPG("Focus angle(degrees)",90));
-      addParameter( new FloatPG("final flight path(m) to focus",.5));
-      addParameter( new FloatPG("minimum time to consider",1000));
-      addParameter( new FloatPG("max time to consider",20000));
-      addParameter( new BooleanPG("Log binning",false));
-      addParameter( new IntegerPG("# uniform nins",1000));
-      addParameter( new FloatPG("First bin(LOG)",.0002));
+      addParameter( new StringPG("Instrument","SNAP"));
+      addParameter( new LoadFilePG("Event File Name",System.getProperty("Data_Directory","")));
+      addParameter( new LoadFilePG("DetCal File Name",System.getProperty("ISAW_HOME","")+"/InstrumentInfo/SNS"));
+      addParameter( new LoadFilePG("Bank FileName",System.getProperty("ISAW_HOME","")+"/InstrumentInfo/SNS"));
+      addParameter( new LoadFilePG("Mapping File Name",System.getProperty("ISAW_HOME","")+"/InstrumentInfo/SNS"));
+      addParameter( new LongPG("firs Event",1));
+      addParameter( new LongPG("Num Events To Load",800000));
+      addParameter( new FloatPG("focusing angle(degrees0",90));
+      addParameter( new FloatPG("Focusing flight path(m)",.5f));
+      addParameter( new FloatPG("min time to focus",1000));
+      addParameter( new FloatPG("max time to focus",30000));
+      addParameter( new BooleanEnablePG("Logarithmic binning?","[false,1,1]"));
+      addParameter( new FloatPG("Length first interval(log binning)",.7));
+      addParameter( new IntegerPG("# buns(uniform)",10000));
    }
 
 
@@ -99,7 +99,7 @@ public class MakeTimeFocusedDataSet extends GenericOperator{
    public String getDocumentation(){
       StringBuffer S = new StringBuffer();
       S.append("@overview    "); 
-      S.append("Makes a DataSet from Event Data wheere each detector is time focused");
+      S.append("Makes a DataSet from Event Data where each detector is time focused");
       S.append("@algorithm    "); 
       S.append("");
       S.append("@assumptions    "); 
@@ -119,19 +119,19 @@ public class MakeTimeFocusedDataSet extends GenericOperator{
       S.append("@param   ");
       S.append("The number of events to load");
       S.append("@param   ");
-      S.append("The \"virtual\" scattering angle, two theta,  (in degrees) to which the data should be focused");
+      S.append("The \"virtual\" scattering angle, two theta,     (in degrees) to which the data should be focused");
       S.append("@param   ");
-      S.append("The final flight path length (in meters) to which  the data should be focused");
+      S.append("The final flight path length (in meters) to which   the data should be focused");
       S.append("@param   ");
       S.append("The minimum time to consider");
       S.append("@param   ");
       S.append("The maximum time to consider");
       S.append("@param   ");
-      S.append("If true use log binning, otherwise use uniform   binnings");
+      S.append("If true use log binning, otherwise use uniform  binnings");
       S.append("@param   ");
-      S.append("The number of uniform bins( isLog=false )");
+      S.append("The length of first interval( isLog = true");
       S.append("@param   ");
-      S.append("The length of first interval( isLog = true )");
+      S.append(")The number of uniform bins( isLog=false )");
       S.append("@error ");
       S.append("");
       return S.toString();
@@ -165,16 +165,16 @@ public class MakeTimeFocusedDataSet extends GenericOperator{
          java.lang.String DetCalFileName = getParameter(2).getValue().toString();
          java.lang.String bankInfoFileName = getParameter(3).getValue().toString();
          java.lang.String MappingFileName = getParameter(4).getValue().toString();
-         int firstEvent = ((IntegerPG)(getParameter(5))).getintValue();
-         int NumEventsToLoad = ((IntegerPG)(getParameter(6))).getintValue();
+         long firstEvent = ((LongPG)(getParameter(5))).getlongValue();
+         long NumEventsToLoad = ((LongPG)(getParameter(6))).getlongValue();
          float angle_deg = ((FloatPG)(getParameter(7))).getfloatValue();
          float final_L_m = ((FloatPG)(getParameter(8))).getfloatValue();
          float min = ((FloatPG)(getParameter(9))).getfloatValue();
          float max = ((FloatPG)(getParameter(10))).getfloatValue();
-         boolean isLog = ((BooleanPG)(getParameter(11))).getbooleanValue();
-         int nUniformbins = ((IntegerPG)(getParameter(12))).getintValue();
-         float first_logStep = ((FloatPG)(getParameter(13))).getfloatValue();
-         DataSetTools.dataset.DataSet Xres=Operators.TOF_Diffractometer.Util.MakeTimeFocusedDataSet(Instrument,EventFileName,DetCalFileName,bankInfoFileName,MappingFileName,firstEvent,NumEventsToLoad,angle_deg,final_L_m,min,max,isLog,nUniformbins,first_logStep );
+         boolean isLog = ((BooleanEnablePG)(getParameter(11))).getbooleanValue();
+         float first_logStep = ((FloatPG)(getParameter(12))).getfloatValue();
+         int nUniformbins = ((IntegerPG)(getParameter(13))).getintValue();
+         DataSetTools.dataset.DataSet Xres=Operators.TOF_Diffractometer.Util.MakeTimeFocusedDataSet(Instrument,EventFileName,DetCalFileName,bankInfoFileName,MappingFileName,firstEvent,NumEventsToLoad,angle_deg,final_L_m,min,max,isLog,first_logStep,nUniformbins );
 
          return Xres;
        }catch( Throwable XXX){
