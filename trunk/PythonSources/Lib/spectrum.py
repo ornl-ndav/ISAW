d227efc5f008585641cfb0a97fa7c1b6a42fffce
ajschultz <ajschultz@9b3b9736-f711-0410-b1b1-9612de3a1827>
2010-03-12 21:09:10 +0000

    Functions required by anvred.py

---
+++ trunk/PythonSources/Lib/spectrum.py
@@ -0,0 +1,34 @@
+#--------------------------------------------------------
+#               function spectrum
+#--------------------------------------------------------
+
+#!  Obtain spectral correction from counts vs. time data
+#!  in a Bankxx_spectrum.asc file.
+#!  Fortran version: A. J. Schultz, July, 2009
+#!  Jython version: A. J. Schultz, March, 2010
+
+
+def spectrum( wavelength, xtof, averageRange, spect1, xtime, xcounts, numTimeChannels ):
+    "Returns the relative spectrum correction."
+	
+	
+# c++	TOF = WL * XTOF
+    TOF = wavelength * xtof
+	# T = TOF			# T is in units of microseconds
+	
+    # print numTimeChannels
+    for j in range(numTimeChannels):
+        
+     if xtime[j] > TOF:
+        sum = 0.0
+        for jj in range(-averageRange, averageRange):
+            sum = sum + xcounts[j + jj]
+            spect = sum / (2*(averageRange) + 1)
+        break
+    
+    spect = spect/spect1
+    
+    return spect
+    # return
+    
+    
