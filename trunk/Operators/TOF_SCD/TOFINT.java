38adfac4c58a31b19e47ddc24f097d2bd3e990e4
vel <vel@9b3b9736-f711-0410-b1b1-9612de3a1827>
2010-06-23 17:46:19 +0000

    ticket #222 changes to TOFINT

--- trunk/Operators/TOF_SCD/TOFINT.java
+++ trunk/Operators/TOF_SCD/TOFINT.java
@@ -23,6 +23,8 @@ public class TOFINT implements Wrappable,HiddenOperator{
 
   public float SIGITOT=0f;
 
+  public float RIS1=0f;
+
   public float REFLAG=0f;
 
   public int WLNUM=0;
@@ -40,12 +42,6 @@ public class TOFINT implements Wrappable,HiddenOperator{
   public int MINZ=0;
 
   public String LOG=null;
-
-  public String INCL1=null;
-
-  public String INCL2=null;
-
-  public String INCL3=null;
 
   public int MAXP1=0;
 
@@ -97,18 +93,6 @@ public class TOFINT implements Wrappable,HiddenOperator{
 
     float RATIO=0f;
 
-    float RIS1=0f;
-
-    float RIS2=0f;
-
-    float RIS3=0f;
-
-    float SIG1=0f;
-
-    float SIG2=0f;
-
-    float SIG3=0f;
-
     //-------------- code ------------
 // JHIST,Nxs,Nys, X,Y,Z,ITOT,SIGITOT,REFLAG,WLNUM,MAXP1,IPFLAG,IADD,ISX,ISY,ISZ
 //
@@ -133,6 +117,7 @@ public class TOFINT implements Wrappable,HiddenOperator{
     IZCALC=(int)((Z));
     if(MINZ<1||MAXZ>WLNUM||MINX<1||MAXX>NXS||MINY<1||MAXY>NYS){
       REFLAG=(float)((401));
+      LOG=String.format("Data range off edge of grid: MINZ %d MaXZ %d MINX %d MAXX %d MINY %d MAXY %d",MINZ,MAXZ,MINX,MAXX,MINY,MAXY);
       return null;
           }
 //-------------------------------------------------------------------------------
@@ -158,93 +143,29 @@ public class TOFINT implements Wrappable,HiddenOperator{
     int ONE=1;
     int TWO=2;
     int THREE=3;
-    int FOUR=4;
     int SIX=SEVEN-1;
-    int FIVE=SEVEN-2;
-    int FOURRT=SEVEN-3;
 //  Find maximum I/sig(I)
 //  Peak centered on peak time channel THREE of SEVEN.
     PKTIME=(float)JTIME[(int)(((TWO))-1)];
-    for(J=THREE;J<=FOURRT;J++)
+    for(J=THREE;J<=SIX;J++)
       PKTIME+=(float)JTIME[(int)(((J))-1)];
-    BKTIME=(float)((JTIME[(int)(((ONE))-1)]+JTIME[(int)(((FIVE))-1)]));
+    BKTIME=(float)((JTIME[(int)(((ONE))-1)]+JTIME[(int)(((SEVEN))-1)]));
     RATIO=(float)((PKTIME/BKTIME));
     PKCTS=(float)ISUM[(int)(((TWO))-1)];
-    for(J=THREE;J<=FOURRT;J++)
+    for(J=THREE;J<=SIX;J++)
       PKCTS+=(float)ISUM[(int)(((J))-1)];
-    BKCTS=(float)((ISUM[(int)(((ONE))-1)]+ISUM[(int)(((FIVE))-1)]));
-    INTI1=(float)((PKCTS-RATIO*BKCTS));
-//      TYPE *,'INTI1,PKCTS,BKCTS,RATIO',INTI1,PKCTS,BKCTS,RATIO
-    SIG1=(float)(((float)Math.sqrt((double)(((PKCTS+RATIO*RATIO*BKCTS))))));
-    RIS1=(float)((INTI1/SIG1));
-//      TYPE *,'INTI1,SIG1,RIS1',INTI1,SIG1,RIS1
-//  Peak centered on peak time channel FOUR of SEVEN (calculated position).
-    PKTIME=(float)JTIME[(int)(((THREE))-1)];
-    for(J=FOUR;J<=FIVE;J++)
-      PKTIME+=(float)JTIME[(int)(((J))-1)];
-    BKTIME=(float)((JTIME[(int)(((TWO))-1)]+JTIME[(int)(((SIX))-1)]));
-    RATIO=(float)((PKTIME/BKTIME));
-    PKCTS=(float)ISUM[(int)(((THREE))-1)];
-    for(J=FOUR;J<=FIVE;J++)
-      PKCTS+=(float)ISUM[(int)(((J))-1)];
-    BKCTS=(float)((ISUM[(int)(((TWO))-1)]+ISUM[(int)(((SIX))-1)]));
-    INTI2=(float)((PKCTS-RATIO*BKCTS));
-    SIG2=(float)(((float)Math.sqrt((double)(((PKCTS+RATIO*RATIO*BKCTS))))));
-    RIS2=(float)((INTI2/SIG2));
-//      TYPE *,'INTI2,SIG2,RIS2',INTI2,SIG2,RIS2
-//  Peak centered on peak time channel FIVE of SEVEN.
-    PKTIME=(float)JTIME[(int)(((FOUR))-1)];
-    for(J=FIVE;J<=SIX;J++)
-      PKTIME+=(float)JTIME[(int)(((J))-1)];
-    BKTIME=(float)((JTIME[(int)(((THREE))-1)]+JTIME[(int)(((SEVEN))-1)]));
-    RATIO=(float)((PKTIME/BKTIME));
-    PKCTS=(float)ISUM[(int)(((FOUR))-1)];
-    for(J=FIVE;J<=SIX;J++)
-      PKCTS+=(float)ISUM[(int)(((J))-1)];
-    BKCTS=(float)((ISUM[(int)(((THREE))-1)]+ISUM[(int)(((SEVEN))-1)]));
-    INTI3=(float)((PKCTS-RATIO*BKCTS));
-    SIG3=(float)(((float)Math.sqrt((double)(((PKCTS+RATIO*RATIO*BKCTS))))));
-    RIS3=(float)((INTI3/SIG3));
-//      TYPE *,'INTI3,SIG3,RIS3',INTI3,SIG3,RIS3
+    BKCTS=(float)((ISUM[(int)(((ONE))-1)]+ISUM[(int)(((SEVEN))-1)]));
+    ITOT=(float)((PKCTS-RATIO*BKCTS));
+    SIGITOT=(float)(((float)Math.sqrt((double)(((PKCTS+RATIO*RATIO*BKCTS))))));
+    RIS1=(float)((ITOT/SIGITOT));
     if(((IPFLAG)==(0))){
-        if((RIS1>RIS2)&&(RIS1>RIS3)&&(RIS1>5.0))INCL1="         Yes";
-        else INCL1="          No";
-        if((RIS2>RIS1&&RIS2>RIS3)||(RIS1>RIS2&&RIS1>RIS3&&RIS1<=5.0)
-          ||(RIS3>RIS2&&RIS3>RIS1&&RIS3<=5.0))INCL2="         Yes";
-        else INCL2="          No";
-        if((RIS3>RIS1)&&(RIS3>RIS2)&&(RIS3>5.0))INCL3="         Yes";
-        else INCL3="          No";
-        LOG=String.format("%3d%5d%7d%5d%5d%4d%4d%4d%4d %9.2f%9.2f%8.2f%s\n%3d%5d%7d%5d%5d%4d%4d%4d%4d %9.2f%9.2f%8.2f%s\n%3d%5d%7d%5d%5d%4d%4d%4d%4d %9.2f%9.2f%8.2f%s\n",
-          (ONE+FIVE)/2,Z,X,Y,(int)JHIST[X-1][Y-1][Z-1],MINX,MAXX,MINY,MAXY,INTI1,SIG1,RIS1,INCL1,
-          (TWO+SIX)/2,Z,X,Y,(int)JHIST[X-1][Y-1][Z-1],MINX,MAXX,MINY,MAXY,INTI2,SIG2,RIS2,INCL2,
-          (THREE+SEVEN)/2,Z,X,Y,(int)JHIST[X-1][Y-1][Z-1],MINX,MAXX,MINY,MAXY,INTI3,SIG3,RIS3,INCL3);
+        LOG=String.format("%3d%5d%7d%5d%5d%4d%4d%4d%4d %9.2f%9.2f%8.2f%s\n",
+          SEVEN,Z,X,Y,(int)JHIST[X-1][Y-1][Z-1],MINX,MAXX,MINY,MAXY,ITOT,SIGITOT,RIS1,"   Yes");
         LOG+="Counts for Offsets from peak time:\n ";
         for(J=ONE;J<=SEVEN;J++)
           LOG+="  "+ISUM[J-1];
         LOG+="  "+"\n ";
     }
-//  Find maximum I/Sig(I)
-    if((((RIS2)>(RIS1)&&(RIS2)>(RIS3)))){
-//            GO TO 500
-      } else {
-      if(((RIS1)>(RIS3))){
-        if(((RIS1)>=(5.0))){
-          ITOT=((INTI1));
-          SIGITOT=(float)((SIG1));
-          REFLAG=(float)((101));
-          return null;
-                  }
-        } else {
-        if(((RIS3)>=(5.0))){
-          ITOT=((INTI3));
-          SIGITOT=(float)((SIG3));
-          REFLAG=(float)((101));
-          return null;
-                  }
-              }
-          }
-    ITOT=((INTI2));
-    SIGITOT=(float)((SIG2));
     REFLAG=(float)((100));
     return null;
 
