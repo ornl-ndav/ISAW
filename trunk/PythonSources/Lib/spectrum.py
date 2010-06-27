e88b31f24866ec1d14ba3164bd7a0284ae53d1d1
ajschultz <ajschultz@9b3b9736-f711-0410-b1b1-9612de3a1827>
2010-06-27 20:20:23 +0000

    Checks to make sure the wavelength and TOF are in the range of the spectrum. If not, returns 0.0 for the spectrum correction.

--- trunk/PythonSources/Lib/spectrum.py
+++ trunk/PythonSources/Lib/spectrum.py
@@ -12,26 +12,36 @@ def spectrum( wavelength, xtof, averageRange, spect1, xtime, xcounts ):
     "Returns the relative spectrum and detector efficiency correction."
 	
 	
-# c++	TOF = WL * XTOF
-    TOF = wavelength * xtof
-	# T = TOF			# T is in units of microseconds
+# TOF = WL * XTOF in units of microseconds
+    TOF = wavelength * xtof    
 	
     numTimeChannels = len( xtime )
     
+    spect = 0.0
+     
+# check for out-of-range TOF value
+    if xtime[averageRange+1] > TOF:
+        print 'Too small, TOF = %f, wavelength = %f' % (TOF, wavelength)
+        return spect
+    if xtime[numTimeChannels - averageRange -1] < TOF:
+        print 'Too large, TOF = %f, wavelength = %f' % (TOF, wavelength)
+        return spect
+    
+# begin deteriming the spectrum correction
     for j in range(numTimeChannels):
         
         if xtime[j] > TOF:
-            print 'In spectrum: j xtime[j] TOF = %d %f %f' % (j, xtime[j], TOF)
+            # print 'In spectrum: j xtime[j] TOF = %d %f %f' % (j, xtime[j], TOF)
             sum = 0.0
             for jj in range(-averageRange, averageRange+1):
                 sum = sum + xcounts[j + jj]
-                print 'In spectrum: jj, xcounts[j+jj], sum = %d %f %f' % (jj, xcounts[j+jj], sum)
+                # print 'In spectrum: jj, xcounts[j+jj], sum = %d %f %f' % (jj, xcounts[j+jj], sum)
             spect = sum / (2*(averageRange) + 1)
-            print 'In spectrum: spect = %f' % spect
+            # print 'In spectrum: spect = %f' % spect
             break
     
     spect = spect/spect1
-    print 'In spectrum: spect1, spect = %f %f' % (spect1, spect)
+    # print 'In spectrum: spect1, spect = %f %f' % (spect1, spect)
     
     return spect
     # return
