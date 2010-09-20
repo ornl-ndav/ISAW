e4561e75aa316dfd58d8ad30346020c9c6b5d479
ajschultz <ajschultz2@gmail.com>
2010-09-20 18:51:49 +0000

    Plots corrected spectra. Modified the spectrum output file.

--- Scripts/TOF_SCD/NEW_SNS/TOPAZ_spectrum.py
+++ Scripts/TOF_SCD/NEW_SNS/TOPAZ_spectrum.py
@@ -10,16 +10,11 @@
 
 from DataSetTools.operator.DataSet.Math.DataSet import *
 from DataSetTools.operator.DataSet.Math.Scalar import *
-# from DataSetTools.operator.DataSet.Attribute import *
 from Command import *
 from Operators.Generic.Save.SaveASCII_calc import *
 from gov.anl.ipns.Util.SpecialStrings import AttributeNameString
-# from Operators.Special import *
-# from gov.anl.ipns.Util.Numeric.IntList import *
-# from DataSetTools.dataset import AttrUtil
-
-# import sys
-
+from math import *
+from absor_V_rod import *
 
 class TOPAZ_spectrum(GenericTOF_SCD):
     def setDefaultParameters(self):
@@ -28,10 +23,12 @@ class TOPAZ_spectrum(GenericTOF_SCD):
         self.addParameter(StringPG("Run number of data file:", "1268"))
         self.addParameter(StringPG("Run number of background file:", "1270"))
         self.addParameter(IntegerPG("Number of detectors:", 14))
+        self.addParameter(LoadFilePG("DetCal file:", "C:/ISAW/InstrumentInfo/SNS/TOPAZ/TOPAZ.DetCal"))
         self.addParameter(BooleanEnablePG("Apply Savitzky-Golay smoothing Filter?", "[1,3,0]"))
         self.addParameter(IntegerPG("Number of points to the left of center:", 20))
         self.addParameter(IntegerPG("Number of points to the right of center:", 20))
         self.addParameter(IntegerPG("Degree of smoothing polynomial:", 3))
+        self.addParameter(DataDirPG("Directory for output spectrum file:", "C:/Users/Arthur/Desktop/Topaz/spectrum/1268"))
         
     def getResult(self):
 
@@ -39,10 +36,27 @@ class TOPAZ_spectrum(GenericTOF_SCD):
         runNum_1 = self.getParameter(1).value
         runNum_2 = self.getParameter(2).value
         number_of_detectors = self.getParameter(3).value
-        doSmoothing = self.getParameter(4).value
-        pointsLeft = self.getParameter(5).value
-        pointsRight = self.getParameter(6).value
-        polyDegree = self.getParameter(7).value
+        DetCalFilename = self.getParameter(4).value
+        doSmoothing = self.getParameter(5).value
+        pointsLeft = self.getParameter(6).value
+        pointsRight = self.getParameter(7).value
+        polyDegree = self.getParameter(8).value
+        outPath = self.getParameter(9).value
+        
+        hom = 0.39559974    # Planck's constant divided by neutron mass
+        
+        # Open spectrum output file
+        filename = outPath + 'Spectrum_' + runNum_1 + '_' + runNum_2 + '.dat'
+        outFile = open( filename, 'w' )
+        outFile.write('# Column  Unit    Quantity\n')
+        outFile.write('# ------  ------  --------\n')
+        outFile.write('#      1  us      time-of-flight\n')
+        outFile.write('#      2  counts  counts per us corrected for vanadium rod absorption\n')
+        outFile.write('#      3  A       wavelength\n')
+        outFile.write('#      4  counts  counts per us uncorrected for absorption\n')
+        outFile.write('#      5          transmission\n')
+        outFile.write('#\n')
+       
         
         #  Obtain scaling factor from beam monitor data
         filename = path + 'TOPAZ_' + runNum_1 + '.nxs'
@@ -64,15 +78,36 @@ class TOPAZ_spectrum(GenericTOF_SCD):
         scalePC = protonCharge_1 / protonCharge_2
         print 'proton_charge ratio = %10.5f' % scalePC
         
+        # Read the detector calibration file
+        DetCalFile = open(DetCalFilename, 'r')
+        DetD = []
+        CenterX = []
+        CenterY = []
+        CenterZ = []
+        for line in DetCalFile:
+            # print line
+            lineList = line.split()
+            # print lineList
+            listLength = len( lineList )
+            if listLength > 0:
+                if lineList[0] == '7':
+                    L1 = float( lineList[1] )
+                    T0_shift = float( lineList[2] )
+                if lineList[0] == '5':
+                    DetD.append( float( lineList[7] ) )
+                    CenterX.append( float( lineList[8] ) )
+                    CenterY.append( float( lineList[9] ) )
+                    CenterZ.append( float( lineList[10] ) )
+        
         # Begin 'for' loop for each detector.
 
-        for bank in range(number_of_detectors):
-            DSnum = bank + 1
-            print 'Detector Bank = %d' % DSnum
+        for i in range(number_of_detectors):
+            bank = i + 1
+            print 'Detector Bank = %d' % bank
             filename = path + 'TOPAZ_' + runNum_1 + '.nxs'
-            ds_1 = ScriptUtil.ExecuteCommand("OneDS",[filename, DSnum, ""] )
+            ds_1 = ScriptUtil.ExecuteCommand("OneDS",[filename, bank, ""] )
             filename = path + 'TOPAZ_' + runNum_2 + '.nxs'
-            ds_2 = ScriptUtil.ExecuteCommand("OneDS",[filename, DSnum, ""] )
+            ds_2 = ScriptUtil.ExecuteCommand("OneDS",[filename, bank, ""] )
 
             #
             #  Select the pixels that were requested
@@ -120,31 +155,36 @@ class TOPAZ_spectrum(GenericTOF_SCD):
                     [newspec, pointsLeft, pointsRight, polyDegree, sGroupID, \
                     0., 20000., Boolean(0), Boolean(0)])
             
+
+            # Correct for vanadium absorption
+            
+            # First store the spectrum in the time and counts arrays
+            D = newspec.getData_entry(0)
+            time = D.getX_scale().getXs()
+            counts = D.getY_values()
+            lenCounts = len( counts )
+            
+            # Calculate detector horizontal and vertical angles
+            diagonal = sqrt( CenterX[bank]**2 + CenterZ[bank]**2 )
+            angleH = atan( CenterX[bank] / CenterZ[bank] )  # the scattering angle in the horizontal plane
+            angleV = atan( CenterY[bank] / diagonal )  # angle out of the horizontal plane
+            
+            # loop through the spectrum to obtain corrected counts
+            outFile.write( 'Bank %d\n' % bank )
+            for j in range(lenCounts):
+                wl = hom * time[j] / ( L1 + DetD[bank] )  # wavelength
+                transmission = absor_V_rod( angleH, angleV, wl )
+                countsCorr = counts[j] / transmission
+                outFile.write( ' %12.3f %12.3f %12.4f %12.3f %12.4f\n' \
+                    % ( time[j], countsCorr, wl, counts[j], transmission ) )
+                counts[j] = countsCorr
+                    
             ScriptUtil.send(newspec, IOBS)
             ScriptUtil.display(newspec, "Selected Graph View")
-
-            #
-            #  Save spectrum to an ASCII text file
-            #
-            # title = newspec.getTitle()
-            # print title
-            # xUnits = newspec.getX_units()
-            # print xUnits
-            # dataEntry = newspec.getData_entry(0)
-            # print dataEntry     # prints "Group ID: 65536"
-            # xValues = dataEntry.getX_scale().getXs()
-            # print xValues
-            # yValues = dataEntry.getY_values()
-            # print yValues
-            
-            filename = path + "Bank" + str(DSnum) + "_spectrum.asc"
-            SaveASCII(newspec, 0, "%12.3f %12.3f", filename)
-            
             
             
         print 'The End!'
             
-        # Return "All done!"
         
     def getCategoryList( self):      
         return ["Macros","Single Crystal"] 
