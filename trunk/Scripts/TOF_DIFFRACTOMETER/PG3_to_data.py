11cbbe97ea9b14bf689fdfe65cef1a1c6df1ef0b
eu7 <eu7@9b3b9736-f711-0410-b1b1-9612de3a1827>
2010-10-05 03:52:02 +0000

    Adding jython scripts from Peter. refs #251

---
+++ trunk/Scripts/TOF_DIFFRACTOMETER/PG3_to_data.py
@@ -0,0 +1,199 @@
+#Author: P.Peterson, R. Mikkelson
+#Script to reduce Powgen data to GSAS and Fullprof data files
+#Cycle 2010-A V run 733, Si run 732, 30Hz f-1 HR guide with ghost correction
+#Underlying script PG3_to_dspace.py changed to separate instrument configuration to make it cycle independent Sep 2010
+#!/bin/env jython
+
+from DataSetTools.operator import Operator
+from DataSetTools.operator.Generic.Load import GenericLoad
+from gov.anl.ipns.Parameters import * # parameters
+from DataSetTools.operator.DataSet.Math.DataSet import *
+from Command import *
+from gov.anl.ipns.Util.SpecialStrings import *
+class PG3_to_data(GenericLoad):
+
+    EventFile =0
+    BackFile  =1
+    VFile =2
+    ConfigFile = 3
+    firstEvent=4
+    NEvents=5
+    mind=6
+    maxd=7
+    delta=8
+    pchargeScalar=9
+    BadPeakWidth = 10
+    BadIntrvlReplace =11
+    BadNumChanAv  = 12
+    FilterCutOff = 13
+    FilterOrder = 14
+    toTree = 15
+    show = 16
+    data = 17
+    filetypes = 18
+    SeqNums = 19
+    
+    def __init__(self):
+        Operator.__init__(self, "PG3_to_data")
+        self.instr = "PG3"
+        try:
+            self.IOBS = IOBS
+        except:
+            self.IOBS = None
+
+    def getCommand(self):
+        return "PG3_to_data"
+
+    def setDefaultParameters(self):
+        self.super__clearParametersVector()
+        PG3ROOT = "/SNS/PG3/"
+        self.addParameter(StringPG("PG3 run number", 1370))
+        self.addParameter(IntegerPG("PG3 background run number",-1))
+        self.addParameter(IntegerPG("PG3 vanadium run number",1371))
+        self.addParameter(LoadFilePG("Configuration file name",
+                                     "/SNS/PG3/2010_2_11A_CAL/"))
+        self.addParameter(FloatPG("First event to load", 0.))
+        self.addParameter(FloatPG("Number of events to load", 1e12))
+        self.addParameter(FloatPG("Min d-spacing", 0.2))
+        self.addParameter(FloatPG("Max d-spacing", 5.0))
+        self.addParameter(FloatPG("deltaD/D", 2e-4))
+        self.addParameter(FloatPG("normalize to pcharge", 1e13))
+        self.addParameter(FloatPG("Estimated Peak Width( delta_d/d)",.0050))
+        self.addParameter(FloatPG("Interval to Replace(Times Peak Width)",1.9))
+        self.addParameter(IntegerPG("Number of Channels to Average", 10))
+        self.addParameter(FloatPG("Filter Cut off",.02))
+        self.addParameter(IntegerPG("Filter order", 2))
+        self.addParameter(BooleanPG("Send all data to tree", False))
+        self.addParameter(BooleanPG("Show plots", 0))       
+        self.addParameter(DataDirPG("Save directory","/SNS/users/"))
+        choices = ChoiceListPG("File types", "gsas and fullprof")
+        choices.addItem("gsas")
+        choices.addItem("fullprof")
+        self.addParameter(choices)
+        self.addParameter(BooleanPG("Sequential Bank Numbering",1))  
+ 
+
+    def getParamValue(self,index):
+        return self.getParameter(index).getValue()
+
+    def getRuns(self):
+        runsStr = self.getParamValue(0)
+        print runsStr
+        from gov.anl.ipns.Util.Numeric import IntList
+        runs = IntList.ToArray(runsStr)
+        print runs
+        return runs
+
+    def toGSASFilename(self, runnumber):
+        filename = str(runnumber) + ".gsa"
+        import os
+        filename = os.path.join(self.outputDir, filename)
+        return filename
+    def toFullprofFilename(self, runnumber):
+        filename = str(runnumber) + ".dat"
+        import os
+        filename = os.path.join(self.outputDir, filename)
+        return filename
+
+    def processRun(self, runnumber):
+        print runnumber
+        self.args[0] = runnumber
+        SampleDS = ScriptUtil.ExecuteCommand("PG3_to_dspace",self.args)
+
+        if isinstance(SampleDS, ErrorString):
+           print SampleDS
+           return SampleDS
+
+        if self.BackgroundDS is not None:
+           op = DataSetSubtract(SampleDS, self.BackgroundDS,0)
+           op.getResult()
+
+       
+        SampleDS.clampToZero()
+
+        GsasFileName = self.toGSASFilename(runnumber)
+
+        save_as = self.getParamValue(self.filetypes)
+
+        if GsasFileName is not None and "gsas" in save_as:
+            useSeqNumbering = self.getParamValue(self.SeqNums)
+            X = ScriptUtil.ExecuteCommand("Save3ColGSAS",[None,SampleDS, GsasFileName, useSeqNumbering])
+            if isinstance(X, ErrorString):
+               return X
+
+        if self.VanadiumDS is not None and "fullprof" in save_as:
+            op = SampleDS.getOperator("Divide by a DataSet")
+           
+            op.getParameter(0).setValue(self.VanadiumDS)
+            X = op.getResult()
+            if isinstance(X, ErrorString):
+               return X
+            filename = str(runnumber)+".dat"
+            import os
+            filename = os.path.join(self.outputDir, filename)
+            X=ScriptUtil.ExecuteCommand("SaveFullProf",[SampleDS,filename,useSeqNumbering])
+
+
+    def getResult(self):
+        # get the configuration file and load it
+        execfile(self.getParamValue(self.ConfigFile))
+        config = getConfig()
+
+        runs = self.getRuns()
+        self.args=[None, self.getParamValue(self.ConfigFile),
+                   self.getParamValue(self.firstEvent),
+                   self.getParamValue(self.NEvents),
+                   self.getParamValue(self.mind),
+                   self.getParamValue(self.maxd),
+                   self.getParamValue(self.delta),
+                   self.getParamValue(self.pchargeScalar),
+                   self.getParamValue(self.toTree),
+                   self.getParamValue(self.show)]
+       
+        BackgroundFile = self.getParamValue(1)
+        if BackgroundFile is None or BackgroundFile <=0:
+           BackgroundFile = None
+
+        if BackgroundFile is not None:
+           self.args[0]= BackgroundFile
+           self.BackgroundDS =  ScriptUtil.ExecuteCommand("PG3_to_dspace",
+                                                          self.args)
+        else:
+           self.BackgroundDS = None
+
+        VanadiumFile = self.getParamValue(self.VFile)
+        if VanadiumFile is None or VanadiumFile <=0:
+           VanadiumFile = None
+
+        if VanadiumFile is not None:
+
+           vanargs = [VanadiumFile,
+                      self.getParamValue(self.ConfigFile),
+                      self.getParamValue(self.firstEvent),
+                      self.getParamValue(self.NEvents),
+                      self.getParamValue(self.mind),
+                      self.getParamValue(self.maxd),
+                      self.getParamValue(self.delta),
+                      self.getParamValue(self.pchargeScalar),
+                      self.getParamValue(self.BadPeakWidth),
+                      self.getParamValue(self.BadIntrvlReplace),
+                      self.getParamValue(self.BadNumChanAv),
+                      self.getParamValue(self.FilterCutOff),
+                      self.getParamValue(self.FilterOrder),
+                      self.getParamValue(0),
+                      self.getParamValue(0)]
+           X =  ScriptUtil.ExecuteCommand("VanadiumLoad", vanargs)
+           if isinstance(X, ErrorString):
+              return X
+           self.VanadiumDS = X
+          						  
+        else:
+           self.VanadiumDS = None
+
+        self.outputDir = self.getParamValue(self.data)
+
+        for runnumber in runs:
+           self.processRun(runnumber)
+
+        return None
+
