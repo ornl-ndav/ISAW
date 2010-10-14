ad832ede2b7cca393d68a043fbd0ea8e8b7370ba
eu7 <eu7@9b3b9736-f711-0410-b1b1-9612de3a1827>
2010-10-14 02:17:22 +0000

    Set version to 1.9.1_04_rc3

--- trunk/IsawGUI/Isaw.java
+++ trunk/IsawGUI/Isaw.java
@@ -1016,7 +1016,7 @@ public class Isaw
         String val="";
 
         if(SharedData.VERSION.equals("Unknown_Version"))
-          version="1.9.1_04_rc2";
+          version="1.9.1_04_rc3";
         else
           version=SharedData.VERSION;
 
