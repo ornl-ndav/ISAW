3be03679f05263c47c0eba05c9ba550688038ad7
Dennis Mikkelson <mikkelsond@uwstout.edu>
2011-02-18 19:30:27 +0000

    Added two simple scripts to switch between the calibration files used in the fall of 2010 the files used staring with Feb. 18, 2011

---
+++ InstrumentInfo/SNS/TOPAZ/set_2010_cal.sh
@@ -0,0 +1,4 @@
+cp TOPAZ_2010_11_4.DetCal      TOPAZ.DetCal
+cp TOPAZ_bank_2010_09_22.xml   TOPAZ_bank.xml
+cp TOPAZ_TS_2010_09_22.dat     TOPAZ_TS.dat
+
---
+++ InstrumentInfo/SNS/TOPAZ/set_2011_cal.sh
@@ -0,0 +1,3 @@
+cp TOPAZ_2011_02_16.DetCal     TOPAZ.DetCal
+cp TOPAZ_bank_2011_02_16.xml   TOPAZ_bank.xml
+cp TOPAZ_TS_2011_02_16.dat     TOPAZ_TS.dat
