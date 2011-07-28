d2438e2c3f0f4bc791c3dad3941e8823e75d921d
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2011-07-28 14:44:31 +0000

    Refs #129. Update info in GPL section and indicated the precursors to this class.

--- DataSetTools/viewer/ThreeD/ThreeDRectViewer.java
+++ DataSetTools/viewer/ThreeD/ThreeDRectViewer.java
@@ -1,3 +1,34 @@
+/*
+ * File:  ThreeDRectViewer.java
+ *
+ * Copyright (C) 2011, Ruth Mikkelson, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ * $Log$
+ */
+
 package DataSetTools.viewer.ThreeD;
 
 import gov.anl.ipns.MathTools.Geometry.Vector3D;
@@ -57,7 +88,12 @@ import DataSetTools.viewer.IViewManager;
 import DataSetTools.viewer.ViewManager;
 import DataSetTools.viewer.ViewerState;
 
-
+/**
+ * The basic original 3D Viewer retooled to work only with rectangular detector banks.
+ * 
+ * @author ruth
+ *
+ */
 public class ThreeDRectViewer extends DataSetViewer implements Serializable,
       IPreserveState
 {
