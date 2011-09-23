ddcdf503b8eb4df09df33d6b698a1afaddd819e4
eu7 <eu7@9b3b9736-f711-0410-b1b1-9612de3a1827>
2011-09-23 11:42:03 +0000

    Changed version to 1.9.1_11a refs #293

--- trunk/IsawGUI/Isaw.java
+++ trunk/IsawGUI/Isaw.java
@@ -1018,7 +1018,7 @@ public class Isaw
         String val="";
 
         if(SharedData.VERSION.equals("Unknown_Version"))
-          version="1.9.1_11";
+          version="1.9.1_11a";
         else
           version=SharedData.VERSION;
 
