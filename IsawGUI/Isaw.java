ab52ac4b99d8674e2a5433770f8aedc3eea3f5ac
Dennis Mikkelson <mikkelsond@uwstout.edu>
2011-12-31 16:30:18 +0000

    Set version number to 1.9.1_12 refs #293

--- IsawGUI/Isaw.java
+++ IsawGUI/Isaw.java
@@ -1018,7 +1018,7 @@ public class Isaw
         String val="";
 
         if(SharedData.VERSION.equals("Unknown_Version"))
-          version="1.9.1_11a";
+          version="1.9.1_12";
         else
           version=SharedData.VERSION;
 
