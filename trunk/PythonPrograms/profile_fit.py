3afafcec7e4571ae48c841d350e6272841b60552
ajschultz <ajschultz@9b3b9736-f711-0410-b1b1-9612de3a1827>
2012-04-18 21:38:53 +0000

    Made some revisions including a linear background.

--- trunk/PythonPrograms/profile_fit.py
+++ trunk/PythonPrograms/profile_fit.py
@@ -4,13 +4,12 @@ Fit function to peak profile.
 A. J. Schultz, April 2012
 """
 
-import numpy as np
 from scipy.optimize import curve_fit
-from pylab import *
+import pylab
 
-def func(x, a, sig, mu, b):
+def func(x, a, sig, mu, b, c):
     sqrt2pi = 2.506628   # sqrt(2.0 * pi)
-    f = (a / (sig * sqrt2pi) * exp(-0.5 * (x - mu)**2 / sig**2)) + b
+    f = (a / (sig * sqrt2pi) * pylab.exp(-0.5 * (x - mu)**2 / sig**2)) + (b * x) + c
     return f
 
 output = open( 'gaussian_coefficients.dat', 'w' )
@@ -29,49 +28,45 @@ for i in range(19):
     print line
     
     for line in input:
-        # line = input.readline()
         lineList = line.split()
-        # if len(lineList) == 0: break
         for i in range(len(lineList)):
             yobs.append( float( lineList[i] ) )
     
     x = range(len(yobs))
-    x = np.array(x)
-    yobs = np.array(yobs)
-    print max(yobs)
-    
-    print x
-    print yobs
+    x = pylab.array(x)
+    yobs = pylab.array(yobs)
     
     # popt is an array of the optimized parameters
     # pcov is the covariance matrix
-    p0 = [1.0, 1.0, 1.0, 1.0]
+    p0 = pylab.zeros(5)    # initial values
     p0[0] = max(yobs) * 2.5 * sqrt2pi
+    p0[1] = 2.5
     p0[2] = 0.5 * len(yobs)
     popt, pcov = curve_fit(func, x, yobs, p0)
     a = popt[0]
     sig = popt[1]
     mu = popt[2]
     b = popt[3]
+    c = popt[4]
 
     # print '%7.4f  %7.4f  %7.4f' %  (a, b, c)
-    print '%7.4f  %7.4f  %7.4f  %7.4f' %  (a, sig, mu, b)
-    # print pcov
+    print '%7.4f  %7.4f  %7.4f  %7.4f  %7.4f' %  (a, sig, mu, b, c)
+    print pcov
     
-    # output.write('[ 1.000,  %7.4f,  %7.4f,  %7.4f,  %7.4f ],\n' %  (a, b, c, d))
+    # output.write('[ 1.000,  %7.4f,  %7.4f,  %7.4f,  %7.4f ],\n' %  (a, b, c))
 
     xcalc = []
+    ycalc = []
     for i in range(100 * len(yobs)):
         xcalc.append(float(i)/100.0)
+        ycalc.append(func(xcalc[i], a, sig, mu, b, c))
         
-    ycalc = func(xcalc, a, sig, mu, b)
-
-    plot(xcalc, ycalc)
-    plot(x, yobs, '+')
-    show()
+    pylab.plot(xcalc, ycalc)
+    pylab.plot(x, yobs, '+')
+    pylab.show()
     
     input.close()
     exit()
 
-# show()
+# pylab.show()
 
