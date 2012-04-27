f01782ff230efcf3eb96c75742d0cc20a8c27994
ajschultz <ajschultz2@gmail.com>
2012-04-27 15:57:10 +0000

    Writes a graphic plot file for each peak.

--- PythonPrograms/profile_fit.py
+++ PythonPrograms/profile_fit.py
@@ -1,72 +1,121 @@
 #!/usr/bin/env python
 """
-Fit function to peak profile.
+Fit gaussian to peak profile.
 A. J. Schultz, April 2012
 """
 
 from scipy.optimize import curve_fit
 import pylab
+import math
+import os
 
-def func(x, a, sig, mu, b, c):
+def gaussian(x, a, sig, mu, b, c):
+    """ Gaussian function on a linear background."""
     sqrt2pi = 2.506628   # sqrt(2.0 * pi)
     f = (a / (sig * sqrt2pi) * pylab.exp(-0.5 * (x - mu)**2 / sig**2)) + (b * x) + c
     return f
 
+# Open output file of ghe Gaussian coefficients.
 output = open( 'gaussian_coefficients.dat', 'w' )
 
-sqrt2pi = 2.506628   # sqrt(2.0 * pi)
+# Make a ./plots subdirectory for the profile plot files.
+if not os.path.exists('./plots'):
+    os.mkdir('./plots')
+
+# sqrt(2.0 * pi)
+sqrt2pi = 2.506628   
+
+# open input file
+input = open( '3681qplot_qplot.dat', 'r' )
     
-for i in range(19):
+# pylab.ion()
 
-    # open input file
-    input = open( 'input.dat', 'r' )
+while True:
 
+    line = input.readline()
+    lineList = line.split()
+    if len(lineList) == 0: break
+    
+    h = int(lineList[0])
+    k = int(lineList[1])
+    l = int(lineList[2])
+    
     x = []
     yobs = []
     
-    line = input.readline() # read first line
-    print line
-    
-    for line in input:
+    for i in range(4):       
+        line = input.readline()
         lineList = line.split()
-        for i in range(len(lineList)):
-            yobs.append( float( lineList[i] ) )
-    
+        
+        for j in range(len(lineList)):
+            yobs.append( float( lineList[j] ) )
+        
     x = range(len(yobs))
     x = pylab.array(x)
     yobs = pylab.array(yobs)
     
     # popt is an array of the optimized parameters
     # pcov is the covariance matrix
-    p0 = pylab.zeros(5)    # initial values
+    p0 = pylab.zeros(5)    # initial values of parameters
     p0[0] = max(yobs) * 2.5 * sqrt2pi
     p0[1] = 2.5
     p0[2] = 0.5 * len(yobs)
-    popt, pcov = curve_fit(func, x, yobs, p0)
+    popt, pcov = curve_fit(gaussian, x, yobs, p0)
+    
     a = popt[0]
     sig = popt[1]
     mu = popt[2]
     b = popt[3]
     c = popt[4]
 
-    # print '%7.4f  %7.4f  %7.4f' %  (a, b, c)
-    print '%7.4f  %7.4f  %7.4f  %7.4f  %7.4f' %  (a, sig, mu, b, c)
-    print pcov
+    if a == 0.0:
+        siga = 999.0
+        sigsig = 999.0
+        sigmu = 999.0
+        sigb = 999.0
+        sigc = 999.0
+    else:
+        siga = math.sqrt(pcov[0][0])
+        sigsig = math.sqrt(pcov[1][1])
+        sigmu = math.sqrt(pcov[2][2])
+        sigb = math.sqrt(pcov[3][3])
+        sigc = math.sqrt(pcov[4][4])
     
-    # output.write('[ 1.000,  %7.4f,  %7.4f,  %7.4f,  %7.4f ],\n' %  (a, b, c))
+    output.write('%4d %4d %4d %12.4f  %12.4f  %12.4f  %12.4f  %12.4f %12.4f  %12.4f  %12.4f  %12.4f  %12.4f\n' 
+        %  (h, k, l, a, sig, mu, b, c, siga, sigsig, sigmu, sigb, sigc))
+    print '%4d %4d %4d %12.4f' % (h, k, l, a)
 
     xcalc = []
     ycalc = []
     for i in range(100 * len(yobs)):
         xcalc.append(float(i)/100.0)
-        ycalc.append(func(xcalc[i], a, sig, mu, b, c))
-        
+        ycalc.append(gaussian(xcalc[i], a, sig, mu, b, c))
+    
+    # pylab.ion()    
     pylab.plot(xcalc, ycalc)
     pylab.plot(x, yobs, '+')
-    pylab.show()
-    
-    input.close()
-    exit()
+    pylab.xlabel('Q channel, 2pi/d')
+    pylab.ylabel('Counts')
 
-# pylab.show()
+    plotTitle = '%d %d %d' % (h, k, l)
+    pylab.title(plotTitle)
+    
+    textString = 'f = (a/(sig * sqrt(2*pi)) * exp(-0.5 * (x - mu)**2 / sig**2)) + (b * x) + c'
+    pylab.figtext(0.5, 0.85, textString, horizontalalignment='center', fontsize='small')
+
+    textString = 'a = %.2f(%.2f)\nsig = %.2f(%.2f)\nmu = %.2f(%.2f)\nb = %.2f(%.2f)\nc = %.2f(%.2f)\n' % (
+        a, siga, sig, sigsig, mu, sigmu, b, sigb, c, sigc)
+    pylab.figtext(0.65, 0.65, textString, family='monospace')
+    
+    filename = './plots/Profile_fit_%d_%d_%d' % (h, k, l)
+    pylab.savefig(filename)
+    pylab.close()
+    
+    # pylab.show()
+    # raw_input('Type ENTER to continue.')
+    
+    # pylab.show()
+    # pylab.ioff()
+    # pylab.ion()
+
 
