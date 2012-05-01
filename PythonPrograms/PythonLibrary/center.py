c4682384ccee803028bb81f16d278e6f3a80ff92
ajschultz <ajschultz2@gmail.com>
2012-05-01 20:16:50 +0000

    Function to test for allowed or not allowed peaks due to centering.

---
+++ PythonPrograms/PythonLibrary/center.py
@@ -0,0 +1,57 @@
+def center(h, k, l, center_type):
+    """ Function to test for allowed (True) and not allowed (False)
+    peaks due to centering."""
+    
+    if center_type == 'P':
+        return True
+    
+    if center_type == 'A':
+        sum = k + l
+        if (sum % 2) == 0: return True
+        return False
+        
+    if center_type == 'B':
+        sum = h + l
+        if (sum % 2) == 0: return True
+        return False
+        
+    if center_type == 'C':
+        sum = h + k
+        if (sum % 2) == 0: return True
+        return False
+        
+    if center_type == 'F':
+        sum = h + k
+        if (sum % 2) != 0: return False
+        sum = h + l
+        if (sum % 2) != 0: return False
+        sum = k + l
+        if (sum % 2) != 0: return False
+        return True
+        
+    if center_type == 'I':
+        sum = h + k + l
+        if (sum % 2) != 0: return False
+        return True
+        
+    if center_type == 'R':
+        sum = -h + k + l
+        if (sum % 3) != 0: return False
+        return True
+        
+    print 'Centering type not P, A, B, C, F, I or R.'
+    
+# test  
+# h = 1
+# k = 5
+# l = 3
+# center_type = 'F'
+
+# x = center(h, k, l, center_type)
+
+# if x == True:
+    # print 'x is True'
+# if x == False:
+    # print 'x is False'
+
+        
