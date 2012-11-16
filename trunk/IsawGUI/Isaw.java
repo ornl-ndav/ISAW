acf8b4d4d701ef169d785d826d056d79f7f14650
mikkelsond <mikkelsond@9b3b9736-f711-0410-b1b1-9612de3a1827>
2012-11-16 02:18:23 +0000

    Changing version number to 1.9.1_16

--- trunk/IsawGUI/Isaw.java
+++ trunk/IsawGUI/Isaw.java
@@ -1018,7 +1018,7 @@ public class Isaw
         String val="";
 
         if(SharedData.VERSION.equals("Unknown_Version"))
-          version="1.9.1_15";
+          version="1.9.1_16";
         else
           version=SharedData.VERSION;
 
