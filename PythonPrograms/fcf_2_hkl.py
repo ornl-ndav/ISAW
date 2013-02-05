2ca46b2abd780fac4cb239733689482eea17993c
ajschultz <ajschultz2@gmail.com>
2013-02-05 17:36:29 +0000

    test

--- PythonPrograms/fcf_2_hkl.py
+++ PythonPrograms/fcf_2_hkl.py
@@ -8,6 +8,7 @@
 #   Script to rewrite a fcf file (LIST 4) to an hkl file (HKLF 4).
 #   First run SHELX with MERG 0, HKLF 2 and LIST 4.
 
+
 print ''
 print 'Script to rewrite a fcf file (LIST 4) to an hkl file (HKLF 4).'
 print 'First run SHELX with MERG 0, HKLF 2 and LIST 4.'
