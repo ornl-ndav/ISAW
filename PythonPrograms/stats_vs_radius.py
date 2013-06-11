c49307e4e911c0749c9291b258b58d6407910424
ajschultz <ajschultz2@gmail.com>
2013-06-11 19:29:31 +0000

    This version plots and writes stats for individual detectors.

--- PythonPrograms/stats_vs_radius.py
+++ PythonPrograms/stats_vs_radius.py
@@ -28,6 +28,23 @@ else:
 from mantid.simpleapi import *
 from mantid.api import *
 
+# Function to print statistics for individual detectors.
+def print_detector_stats( sig_level, number_of_detectors, detnum,
+        num_radius_steps, min_peak_radius, radius_step, det_sigx, output ):
+    
+    output.write('\nDetector statistics for %ssig data\n' % str(sig_level))
+    output.write('        Detector number ------------->\n')
+    output.write('  RADIUS')
+    for i in range(number_of_detectors):
+        output.write( '%8s' % detnum[i] )
+    for j in range(num_radius_steps):
+        peak_radius = min_peak_radius + (j * radius_step)
+        output.write('\n%8.3f' % peak_radius)
+        for i in range(number_of_detectors):
+            output.write( '%8d' % det_sigx[i][j] )
+    output.write( '\n' )
+# End of function.
+
 print "API Version"
 print apiVersion()
 
@@ -57,6 +74,7 @@ radius_step               = float( params_dictionary[ "radius_step" ] )
 num_radius_steps          = int( params_dictionary[ "num_radius_steps" ] )
 bkg_width                 = float( params_dictionary[ "bkg_width" ] )
 integrate_if_edge_peak    = params_dictionary[ "integrate_if_edge_peak" ]
+per_detector_sig          = params_dictionary[ "per_detector_sig" ]
 
 
 #
@@ -80,9 +98,24 @@ print "\nProcessing File: " + full_name + " ......\n"
 # Name the files for this run
 #
 run_niggli_matrix_file = output_directory + "/" + run + "_Niggli.mat"
-run_niggli_integrate_file = output_directory + "/" + run + "_Niggli.integrate"
+run_niggli_integrate_file = output_directory + run + "_Niggli.integrate"
 temp_integrate_file = output_directory + "/temp.integrate"
 
+#
+# get number of detectors and their numbers
+#
+Niggli_integrate_file = open( run_niggli_integrate_file, 'r' )
+detnum = []
+for line in Niggli_integrate_file:
+    lineList = line.split()
+    if lineList[0] == '0': break
+    if lineList[0] == '5':
+        detnum.append( lineList[1] )
+number_of_detectors = len( detnum )
+print '\nNumber of detectors is %d\n' % number_of_detectors
+Niggli_integrate_file.close()
+
+# load integrate file and UB matrix into peaks_ws
 peaks_ws = LoadIsawPeaks( Filename = run_niggli_integrate_file )
 LoadIsawUB( InputWorkspace = peaks_ws, Filename = run_niggli_matrix_file )
 
@@ -121,15 +154,21 @@ MDEW = ConvertToMD( InputWorkspace=event_ws, QDimensions="Q3D",
 statsFilename = output_directory + 'stats.lst'
 output = open(statsFilename, 'w')
 
-output.write('\nInput integrate files are ' + run_niggli_integrate_file + '\n')
+# output.write('\nInput integrate files are ' + run_niggli_integrate_file + '\n')
 output.write('\n        ***  SUMMARY OF INTENSITY STATISTICS  ***\n\n')
 output.write('    RADIUS     TOTAL      2SIG      3SIG      5SIG     10SIG\n')
 
+# zero arrays for each detector
+det_sig2 = pylab.zeros(( number_of_detectors, num_radius_steps ))
+det_sig3 = pylab.zeros(( number_of_detectors, num_radius_steps ))
+det_sig5 = pylab.zeros(( number_of_detectors, num_radius_steps ))
+det_sig10 = pylab.zeros(( number_of_detectors, num_radius_steps ))
 
 #
 # Begin loop to integrate peaks and analyze statistics
 #                
 for i in range(num_radius_steps):
+# for i in range(1):
     peak_radius = min_peak_radius + (i * radius_step)
     bkg_inner_radius = peak_radius
     bkg_outer_radius = bkg_inner_radius + bkg_width
@@ -143,8 +182,6 @@ for i in range(num_radius_steps):
 
     #
     # Save the final integrated peaks, using the Niggli reduced cell.  
-    # This is the only file needed, for the driving script to get a combined
-    # result.
     #
     SaveIsawPeaks( InputWorkspace=peaks_ws, AppendFile=False, 
                    Filename = temp_integrate_file )
@@ -153,36 +190,57 @@ for i in range(num_radius_steps):
     # begin reading the integrate file
     sum = sig2 = sig3 = sig5 = sig10 = 0
 
+    det_index = -1
+
     input = open(temp_integrate_file, 'r')
     lineString = input.readline()           # read first header line from integrate file
-
     while True:
         lineString = input.readline()
         lineList = lineString.split()
         if (len(lineList)) == 0: break
         
         formatFlag = int(lineList[0])       # test for line type
+        
+        # set the detector index number
+        if formatFlag == 1:
+            det_index = det_index + 1
                 
         if formatFlag == 3:
             
             intI = float(lineList[14])  # intI is the integrated intensity
             sigI = float(lineList[15])  # sigI is the standard deviation
             sum = sum + 1
-            if intI > (2.0 * sigI): sig2 = sig2 + 1
-            if intI > (3.0 * sigI): sig3 = sig3 + 1
-            if intI > (5.0 * sigI): sig5 = sig5 + 1
-            if intI > (10.0 * sigI): sig10 = sig10 + 1
+            if intI > (2.0 * sigI):
+                sig2 = sig2 + 1
+                det_sig2[det_index][i] = det_sig2[det_index][i] + 1
+            if intI > (3.0 * sigI):
+                sig3 = sig3 + 1
+                det_sig3[det_index][i] = det_sig3[det_index][i] + 1
+            if intI > (5.0 * sigI):
+                sig5 = sig5 + 1
+                det_sig5[det_index][i] = det_sig5[det_index][i] + 1
+            if intI > (10.0 * sigI):
+                sig10 = sig10 + 1
+                det_sig10[det_index][i] = det_sig10[det_index][i] + 1
+            
     # end reading the integrate file
 
-
+    #
     # Print the results.
-
+    #
     outString = '%10.3f%10d%10d%10d%10d%10d\n' % (peak_radius, sum, sig2, sig3, sig5, sig10)
     print outString
     output.write(outString)
 
     input.close()
-                   
+
+# Print individual detector stats to the output file.
+sig_level = ( 2, 3, 5, 10 )
+det_sigx = det_sig2
+for i in range(4):
+    print_detector_stats( sig_level[i], number_of_detectors, detnum, num_radius_steps,
+            min_peak_radius, radius_step, det_sigx, output )
+        
 os.remove( temp_integrate_file )                 
 output.close()
 end_time = time.time()
@@ -190,9 +248,8 @@ print '\nReduced run ' + str(run) + ' in ' + str(end_time - start_time) + ' sec'
 print 'using config file ' + config_file_name 
 
 #
-# plot results
+# Plot the results
 #
-
 x = []
 ytotal = []
 y2 = []
@@ -210,6 +267,7 @@ for line in input:
 
 for line in input:
     lineList = line.split()
+    if len( lineList ) == 0: break
     x.append( float( lineList[0] ) )
     ytotal.append( int( lineList[1] ) )
     y2.append( int( lineList[2] ) )
@@ -217,12 +275,14 @@ for line in input:
     y5.append( int( lineList[4] ) )
     y10.append( int( lineList[5] ) )
 
+pylab.figure(1)
+
 pylab.plot( x, y2, marker = '^', linestyle = '-', label = '2sig' )
 pylab.plot( x, y3, marker = '^', linestyle = '-', label = '3sig' )
 pylab.plot( x, y5, marker = '^', linestyle = '-', label = '5sig' )
 pylab.plot( x, y10, marker = '^', linestyle = '-', label = '10sig' )
 
-pylab.legend( loc = 4 )     # locate the legend in the lower right
+pylab.legend( loc = 'lower right' )     # locate the legend in the lower right
 
 pylab.xlabel('Radius')
 pylab.ylabel('Number of peaks')
@@ -232,6 +292,53 @@ pylab.grid(True)
 pylab.savefig( plotTitle )   # plot saved
 
 pylab.ion()                  # turn on interactive mode
+pylab.show()
+
+raw_input('Type RETURN to continue.')
+
+#
+# Plot stats vs radius for each detector individually
+#
+fig = pylab.figure(2)
+ax = pylab.subplot(111)
+
+marker_symbol = ['o', 'D', 's', 'v', '^', '<', '>', '+',
+        'o', 'D', 's', 'v', '^', '<', '>', '+']
+
+for i in range(number_of_detectors):
+
+    y = []
+    for j in range(num_radius_steps):
+        if per_detector_sig == '2':
+            y.append( det_sig2[i][j] )
+        elif per_detector_sig == '3':
+            y.append( det_sig3[i][j] )
+        elif per_detector_sig == '5':
+            y.append( det_sig5[i][j] )
+        elif per_detector_sig == '10':
+            y.append( det_sig10[i][j] )
+            
+            
+    if i < 8:
+        pylab.plot( x, y, marker = marker_symbol[i], linestyle = '-', label = detnum[i])
+    else:
+        pylab.plot( x, y, marker = marker_symbol[i], markerfacecolor = 'None',
+        linestyle = '-', label = detnum[i])
+        
+pylab.xlabel('Radius')
+pylab.ylabel('Number of peaks')
+plotTitle = 'Detector_stats_' + per_detector_sig + 'sig'
+pylab.title( plotTitle )
+
+# Shink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.85, box.height])
+
+# pylab.legend(loc = 'lower right')
+# pylab.legend(bbox_to_anchor=(1.05, 1), loc = 'upper right', borderaxespad=0.)
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+pylab.savefig( plotTitle )   # plot saved
 pylab.show()
 
 raw_input('Type RETURN to exit.')
